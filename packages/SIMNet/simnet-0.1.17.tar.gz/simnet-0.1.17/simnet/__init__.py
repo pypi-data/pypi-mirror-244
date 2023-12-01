c00bdcf774efda0f0bbe4e0417a47e2aa23b7953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 15:03:00 +0000

    Add package SIMNet/0.1.17/simnet-0.1.17.tar.gz

---
+++ packages/SIMNet/simnet-0.1.17.tar.gz/simnet-0.1.17/simnet/__init__.py
@@ -0,0 +1,5 @@
+from simnet.client.genshin import GenshinClient
+from simnet.client.starrail import StarRailClient
+from simnet.utils.enum_ import Game, Region
+
+__all__ = ("StarRailClient", "GenshinClient", "Game", "Region")
