4e5903553a24cfe562ec580198284babef104737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 14:03:06 +0000

    Add package not1mm/23.11.30.1/not1mm-23.11.30.1-py3-none-any.whl

---
+++ packages/not1mm/not1mm-23.11.30.1-py3-none-any.whl/not1mm/plugins/winter_field_day.py
@@ -0,0 +1,136 @@
+"""Winter Field Day plugin"""
+
+# pylint: disable=invalid-name, unused-argument, unused-variable, c-extension-no-member
+
+from PyQt5 import QtWidgets
+from not1mm.lib.plugin_common import gen_adif
+
+cabrillo_name = "NOT_DONE_YET"
+name = "Winter Field Day"
+# 1 once per contest, 2 work each band, 3 each band/mode, 4 no dupe checking
+mode = "BOTH"  # CW SSB BOTH RTTY
+columns = [
+    "YYYY-MM-DD HH:MM:SS",
+    "Call",
+    "Freq",
+    "Exchange1",
+    "Sect",
+    "PTS",
+]
+
+advance_on_space = [True, True, True, True, True]
+
+dupe_type = 4
+
+
+def init_contest(self):
+    """setup plugin"""
+    set_tab_next(self)
+    set_tab_prev(self)
+    interface(self)
+
+
+def interface(self):
+    """Setup user interface"""
+    self.field1.hide()
+    self.field2.hide()
+    self.field3.show()
+    self.field4.show()
+    label = self.field3.findChild(QtWidgets.QLabel)
+    label.setText("Class")
+    self.field3.setAccessibleName("Class")
+    label = self.field4.findChild(QtWidgets.QLabel)
+    label.setText("Section")
+    self.field4.setAccessibleName("Section")
+
+
+def reset_label(self):
+    """reset label after field cleared"""
+
+
+def set_tab_next(self):
+    """Set TAB Advances"""
+    self.tab_next = {
+        self.callsign: self.field1.findChild(QtWidgets.QLineEdit),
+        self.field1.findChild(QtWidgets.QLineEdit): self.field2.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field2.findChild(QtWidgets.QLineEdit): self.field3.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field3.findChild(QtWidgets.QLineEdit): self.field4.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field4.findChild(QtWidgets.QLineEdit): self.callsign,
+    }
+
+
+def set_tab_prev(self):
+    """Set TAB Advances"""
+    self.tab_prev = {
+        self.callsign: self.field4.findChild(QtWidgets.QLineEdit),
+        self.field1.findChild(QtWidgets.QLineEdit): self.callsign,
+        self.field2.findChild(QtWidgets.QLineEdit): self.field1.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field3.findChild(QtWidgets.QLineEdit): self.field2.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field4.findChild(QtWidgets.QLineEdit): self.field3.findChild(
+            QtWidgets.QLineEdit
+        ),
+    }
+
+
+def set_contact_vars(self):
+    """Contest Specific"""
+
+
+def predupe(self):
+    """called after callsign entered"""
+
+
+def prefill(self):
+    """Fill SentNR"""
+
+
+def points(self):
+    """Calc point"""
+
+
+def show_mults(self):
+    """Return display string for mults"""
+
+
+def show_qso(self):
+    """Return qso count"""
+    result = self.database.fetch_qso_count()
+    if result:
+        return int(result.get("qsos", 0))
+    return 0
+
+
+def get_points(self):
+    """Return raw points before mults"""
+    result = self.database.fetch_points()
+    if result:
+        return int(result.get("Points", 0))
+    return 0
+
+
+def calc_score(self):
+    """Return calculated score"""
+    result = self.database.fetch_points()
+
+
+def adif(self):
+    """Call the generate ADIF function"""
+    gen_adif(self, cabrillo_name, "WFD")
+
+
+def cabrillo(self):
+    """Generates Cabrillo file. Maybe."""
+
+
+def recalculate_mults(self):
+    """Recalculates multipliers after change in logged qso."""
