1e2c7b8df10074825f7a6e45c9785f89b696a1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 16:03:25 +0000

    Add package garpix-user/3.10.0rc14/garpix_user-3.10.0rc14-py3-none-any.whl

---
+++ packages/garpix-user/garpix_user-3.10.0rc14-py3-none-any.whl/garpix_user/forms/login.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext as _
+
+User = get_user_model()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        super(LoginForm, self).clean()
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        valid = False
+        if username and password:
+            user = User.objects.filter(username=username.lower()).first()
+
+            if not user or user.keycloak_auth_only:
+                raise forms.ValidationError(_('User is not found'))
+
+            if user and not user.is_active:
+                raise forms.ValidationError(_('User is inactive. You must confirm the registration email address at registration.'))
+
+            valid = user.check_password(password)
+        if not valid:
+            raise forms.ValidationError(_('Invalid: username / password'))
+        return valid
