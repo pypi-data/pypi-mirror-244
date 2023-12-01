fa5aa2cc18b2540a877737e1af095131d148f5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 14:02:43 +0000

    Add package windmill-api/1.219.0/windmill_api-1.219.0-py3-none-any.whl

---
+++ packages/windmill-api/windmill_api-1.219.0-py3-none-any.whl/windmill_api/models/update_flow_json_body_value_modules_item.py
@@ -0,0 +1,249 @@
+from typing import TYPE_CHECKING, Any, Dict, List, Type, TypeVar, Union
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+if TYPE_CHECKING:
+    from ..models.update_flow_json_body_value_modules_item_mock import UpdateFlowJsonBodyValueModulesItemMock
+    from ..models.update_flow_json_body_value_modules_item_retry import UpdateFlowJsonBodyValueModulesItemRetry
+    from ..models.update_flow_json_body_value_modules_item_sleep_type_0 import (
+        UpdateFlowJsonBodyValueModulesItemSleepType0,
+    )
+    from ..models.update_flow_json_body_value_modules_item_sleep_type_1 import (
+        UpdateFlowJsonBodyValueModulesItemSleepType1,
+    )
+    from ..models.update_flow_json_body_value_modules_item_stop_after_if import (
+        UpdateFlowJsonBodyValueModulesItemStopAfterIf,
+    )
+    from ..models.update_flow_json_body_value_modules_item_suspend import UpdateFlowJsonBodyValueModulesItemSuspend
+
+
+T = TypeVar("T", bound="UpdateFlowJsonBodyValueModulesItem")
+
+
+@_attrs_define
+class UpdateFlowJsonBodyValueModulesItem:
+    """
+    Attributes:
+        id (str):
+        value (Any):
+        stop_after_if (Union[Unset, UpdateFlowJsonBodyValueModulesItemStopAfterIf]):
+        sleep (Union['UpdateFlowJsonBodyValueModulesItemSleepType0', 'UpdateFlowJsonBodyValueModulesItemSleepType1',
+            Unset]):
+        cache_ttl (Union[Unset, float]):
+        timeout (Union[Unset, float]):
+        summary (Union[Unset, str]):
+        mock (Union[Unset, UpdateFlowJsonBodyValueModulesItemMock]):
+        suspend (Union[Unset, UpdateFlowJsonBodyValueModulesItemSuspend]):
+        priority (Union[Unset, float]):
+        retry (Union[Unset, UpdateFlowJsonBodyValueModulesItemRetry]):
+    """
+
+    id: str
+    value: Any
+    stop_after_if: Union[Unset, "UpdateFlowJsonBodyValueModulesItemStopAfterIf"] = UNSET
+    sleep: Union[
+        "UpdateFlowJsonBodyValueModulesItemSleepType0", "UpdateFlowJsonBodyValueModulesItemSleepType1", Unset
+    ] = UNSET
+    cache_ttl: Union[Unset, float] = UNSET
+    timeout: Union[Unset, float] = UNSET
+    summary: Union[Unset, str] = UNSET
+    mock: Union[Unset, "UpdateFlowJsonBodyValueModulesItemMock"] = UNSET
+    suspend: Union[Unset, "UpdateFlowJsonBodyValueModulesItemSuspend"] = UNSET
+    priority: Union[Unset, float] = UNSET
+    retry: Union[Unset, "UpdateFlowJsonBodyValueModulesItemRetry"] = UNSET
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        from ..models.update_flow_json_body_value_modules_item_sleep_type_0 import (
+            UpdateFlowJsonBodyValueModulesItemSleepType0,
+        )
+
+        id = self.id
+        value = self.value
+        stop_after_if: Union[Unset, Dict[str, Any]] = UNSET
+        if not isinstance(self.stop_after_if, Unset):
+            stop_after_if = self.stop_after_if.to_dict()
+
+        sleep: Union[Dict[str, Any], Unset]
+        if isinstance(self.sleep, Unset):
+            sleep = UNSET
+
+        elif isinstance(self.sleep, UpdateFlowJsonBodyValueModulesItemSleepType0):
+            sleep = UNSET
+            if not isinstance(self.sleep, Unset):
+                sleep = self.sleep.to_dict()
+
+        else:
+            sleep = UNSET
+            if not isinstance(self.sleep, Unset):
+                sleep = self.sleep.to_dict()
+
+        cache_ttl = self.cache_ttl
+        timeout = self.timeout
+        summary = self.summary
+        mock: Union[Unset, Dict[str, Any]] = UNSET
+        if not isinstance(self.mock, Unset):
+            mock = self.mock.to_dict()
+
+        suspend: Union[Unset, Dict[str, Any]] = UNSET
+        if not isinstance(self.suspend, Unset):
+            suspend = self.suspend.to_dict()
+
+        priority = self.priority
+        retry: Union[Unset, Dict[str, Any]] = UNSET
+        if not isinstance(self.retry, Unset):
+            retry = self.retry.to_dict()
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "id": id,
+                "value": value,
+            }
+        )
+        if stop_after_if is not UNSET:
+            field_dict["stop_after_if"] = stop_after_if
+        if sleep is not UNSET:
+            field_dict["sleep"] = sleep
+        if cache_ttl is not UNSET:
+            field_dict["cache_ttl"] = cache_ttl
+        if timeout is not UNSET:
+            field_dict["timeout"] = timeout
+        if summary is not UNSET:
+            field_dict["summary"] = summary
+        if mock is not UNSET:
+            field_dict["mock"] = mock
+        if suspend is not UNSET:
+            field_dict["suspend"] = suspend
+        if priority is not UNSET:
+            field_dict["priority"] = priority
+        if retry is not UNSET:
+            field_dict["retry"] = retry
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        from ..models.update_flow_json_body_value_modules_item_mock import UpdateFlowJsonBodyValueModulesItemMock
+        from ..models.update_flow_json_body_value_modules_item_retry import UpdateFlowJsonBodyValueModulesItemRetry
+        from ..models.update_flow_json_body_value_modules_item_sleep_type_0 import (
+            UpdateFlowJsonBodyValueModulesItemSleepType0,
+        )
+        from ..models.update_flow_json_body_value_modules_item_sleep_type_1 import (
+            UpdateFlowJsonBodyValueModulesItemSleepType1,
+        )
+        from ..models.update_flow_json_body_value_modules_item_stop_after_if import (
+            UpdateFlowJsonBodyValueModulesItemStopAfterIf,
+        )
+        from ..models.update_flow_json_body_value_modules_item_suspend import UpdateFlowJsonBodyValueModulesItemSuspend
+
+        d = src_dict.copy()
+        id = d.pop("id")
+
+        value = d.pop("value")
+
+        _stop_after_if = d.pop("stop_after_if", UNSET)
+        stop_after_if: Union[Unset, UpdateFlowJsonBodyValueModulesItemStopAfterIf]
+        if isinstance(_stop_after_if, Unset):
+            stop_after_if = UNSET
+        else:
+            stop_after_if = UpdateFlowJsonBodyValueModulesItemStopAfterIf.from_dict(_stop_after_if)
+
+        def _parse_sleep(
+            data: object,
+        ) -> Union[
+            "UpdateFlowJsonBodyValueModulesItemSleepType0", "UpdateFlowJsonBodyValueModulesItemSleepType1", Unset
+        ]:
+            if isinstance(data, Unset):
+                return data
+            try:
+                if not isinstance(data, dict):
+                    raise TypeError()
+                _sleep_type_0 = data
+                sleep_type_0: Union[Unset, UpdateFlowJsonBodyValueModulesItemSleepType0]
+                if isinstance(_sleep_type_0, Unset):
+                    sleep_type_0 = UNSET
+                else:
+                    sleep_type_0 = UpdateFlowJsonBodyValueModulesItemSleepType0.from_dict(_sleep_type_0)
+
+                return sleep_type_0
+            except:  # noqa: E722
+                pass
+            if not isinstance(data, dict):
+                raise TypeError()
+            _sleep_type_1 = data
+            sleep_type_1: Union[Unset, UpdateFlowJsonBodyValueModulesItemSleepType1]
+            if isinstance(_sleep_type_1, Unset):
+                sleep_type_1 = UNSET
+            else:
+                sleep_type_1 = UpdateFlowJsonBodyValueModulesItemSleepType1.from_dict(_sleep_type_1)
+
+            return sleep_type_1
+
+        sleep = _parse_sleep(d.pop("sleep", UNSET))
+
+        cache_ttl = d.pop("cache_ttl", UNSET)
+
+        timeout = d.pop("timeout", UNSET)
+
+        summary = d.pop("summary", UNSET)
+
+        _mock = d.pop("mock", UNSET)
+        mock: Union[Unset, UpdateFlowJsonBodyValueModulesItemMock]
+        if isinstance(_mock, Unset):
+            mock = UNSET
+        else:
+            mock = UpdateFlowJsonBodyValueModulesItemMock.from_dict(_mock)
+
+        _suspend = d.pop("suspend", UNSET)
+        suspend: Union[Unset, UpdateFlowJsonBodyValueModulesItemSuspend]
+        if isinstance(_suspend, Unset):
+            suspend = UNSET
+        else:
+            suspend = UpdateFlowJsonBodyValueModulesItemSuspend.from_dict(_suspend)
+
+        priority = d.pop("priority", UNSET)
+
+        _retry = d.pop("retry", UNSET)
+        retry: Union[Unset, UpdateFlowJsonBodyValueModulesItemRetry]
+        if isinstance(_retry, Unset):
+            retry = UNSET
+        else:
+            retry = UpdateFlowJsonBodyValueModulesItemRetry.from_dict(_retry)
+
+        update_flow_json_body_value_modules_item = cls(
+            id=id,
+            value=value,
+            stop_after_if=stop_after_if,
+            sleep=sleep,
+            cache_ttl=cache_ttl,
+            timeout=timeout,
+            summary=summary,
+            mock=mock,
+            suspend=suspend,
+            priority=priority,
+            retry=retry,
+        )
+
+        update_flow_json_body_value_modules_item.additional_properties = d
+        return update_flow_json_body_value_modules_item
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
