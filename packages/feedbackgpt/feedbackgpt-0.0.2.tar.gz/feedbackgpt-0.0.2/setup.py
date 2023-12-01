4474fa12435e796a0b050dc20c9bc70caa7bff0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 15:03:14 +0000

    Add package feedbackgpt/0.0.2/feedbackgpt-0.0.2.tar.gz

---
+++ packages/feedbackgpt/feedbackgpt-0.0.2.tar.gz/feedbackgpt-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="feedbackgpt",
+	version="0.0.2",
+	author="Kunal Tangri, Noah Faro",
+	description="A GPT wrapper that learns from user feedback",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent"
+	],
+	python_requires='>=3.6',
+	py_modules=["feedbackgpt"],
+	package_dir={'':'feedbackgpt/src'},
+	install_requires=['openai', 'numpy', 'pinecone-client']
+)
