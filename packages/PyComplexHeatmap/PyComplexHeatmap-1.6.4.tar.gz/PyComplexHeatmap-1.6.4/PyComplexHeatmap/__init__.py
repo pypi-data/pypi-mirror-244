ed86d374352d369f11905bbe4a13b1e43e2ed310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 14:02:48 +0000

    Add package PyComplexHeatmap/1.6.4/PyComplexHeatmap-1.6.4.tar.gz

---
+++ packages/PyComplexHeatmap/PyComplexHeatmap-1.6.4.tar.gz/PyComplexHeatmap-1.6.4/PyComplexHeatmap/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from .clustermap import (heatmap, ClusterMapPlotter, composite,
+                         DendrogramPlotter)
+from .oncoPrint import oncoprint, oncoPrintPlotter
+from .annotations import *
+from .dotHeatmap import *
+from .colors import *
+from .tools import *
+from .utils import set_default_style
+
+# __all__=['*']
+__version__ = "1.6.4"
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
