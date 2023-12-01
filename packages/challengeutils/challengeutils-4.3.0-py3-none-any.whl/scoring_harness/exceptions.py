46720eb35428a5f85e323358ed3d77c2c50bd28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 14:02:50 +0000

    Add package challengeutils/4.3.0/challengeutils-4.3.0-py3-none-any.whl

---
+++ packages/challengeutils/challengeutils-4.3.0-py3-none-any.whl/scoring_harness/exceptions.py
@@ -0,0 +1,15 @@
+"""
+Define Python user-defined exceptions
+"""
+
+
+class Error(Exception):
+    """Base class for other exceptions"""
+
+    pass
+
+
+class InvalidSubmission(Error):
+    """Raised when the submission is invalid"""
+
+    pass
