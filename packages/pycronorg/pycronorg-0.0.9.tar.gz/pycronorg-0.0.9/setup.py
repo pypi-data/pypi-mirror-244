ae1c0c221b3667a60b0782f166c9fcb92326a8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 15:04:15 +0000

    Add package pycronorg/0.0.9/pycronorg-0.0.9.tar.gz

---
+++ packages/pycronorg/pycronorg-0.0.9.tar.gz/pycronorg-0.0.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+with open("VERSION", "r") as f:
+    VERSION = f.read().strip()
+
+DESCIPTION = "a simple cron-job.org sdk"
+
+with open("requirements.txt", "r") as requirements_file:
+    REQUIREMENTS = requirements_file.readlines()
+
+with open("README.md", "r") as requirements_file:
+    README = requirements_file.read()
+
+setup(
+    name="pycronorg",
+    version=VERSION,
+    author="axdjuraev",
+    author_email="<axdjuraev@gmail.com>",
+    description=DESCIPTION,
+    long_description=README,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    include_package_data=True,
+)
+
