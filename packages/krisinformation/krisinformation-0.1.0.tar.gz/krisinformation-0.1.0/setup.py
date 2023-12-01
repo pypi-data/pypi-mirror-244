72f070ca9c63f3feb042bbfe935a5bf768f10f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 17:03:07 +0000

    Add package krisinformation/0.1.0/krisinformation-0.1.0.tar.gz

---
+++ packages/krisinformation/krisinformation-0.1.0.tar.gz/krisinformation-0.1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name='krisinformation',
+version='0.1.0',
+description='API facade for Krisinformation',
+url='https://github.com/obennet/krisinformation',
+author='Oscar Bennet',
+install_requires=['json', 'requests'],
+author_email='oscar.bennet@outlook.com',
+packages=setuptools.find_packages())
