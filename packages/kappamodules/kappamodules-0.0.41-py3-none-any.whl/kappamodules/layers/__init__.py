e2c402440670cc1cc8839d66a508afedf6040e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 14:02:54 +0000

    Add package kappamodules/0.0.41/kappamodules-0.0.41-py3-none-any.whl

---
+++ packages/kappamodules/kappamodules-0.0.41-py3-none-any.whl/kappamodules/layers/__init__.py
@@ -0,0 +1,6 @@
+from .drop_path import DropPath
+from .identity import Identity
+from .paramless_batchnorm import ParamlessBatchNorm1d
+from .normalize import Normalize
+from .residual import Residual
+from .weight_norm_linear import WeightNormLinear
