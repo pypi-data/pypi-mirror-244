6a9f8e792ffb4267891fcdda8fa76d2ca39da4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 14:03:08 +0000

    Add package requesto-py/1.0.0/requesto_py-1.0.0-py3-none-any.whl

---
+++ packages/requesto-py/requesto_py-1.0.0-py3-none-any.whl/requesto/__init__.py
@@ -0,0 +1,17 @@
+from urllib import request
+
+
+def connectionTest():
+    try:
+        request.urlopen('http://google.com', timeout=1)
+    except Exception:
+        import warnings
+
+        warnings.warn(
+            "Seems like there's no internet connection."
+            " 'http://google.com' cannot be pinged."
+            " App will continue to run without connection, but some functions may be missing.",
+        )
+
+
+connectionTest()
