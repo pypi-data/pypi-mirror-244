9676e0ebee0934d79608f1006ba94427804bca2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 14:02:53 +0000

    Add package mikan-card-downloader/3.0.1/mikan_card_downloader-3.0.1-py3-none-any.whl

---
+++ packages/mikan-card-downloader/mikan_card_downloader-3.0.1-py3-none-any.whl/mikan/plugins/sifasstills.py
@@ -0,0 +1,24 @@
+import bs4
+from aiohttp import ClientResponse
+
+from mikan.html_parser import ParsingError
+from mikan.plugins.default import DefaultPlugin
+from mikan.plugins.sif2 import SIF2
+
+
+class SIFASStills(SIF2, DefaultPlugin):
+    card_dir = "SIFAS_Stills"
+    url = "https://idol.st/ajax/allstars/still/"
+    list_url = "https://idol.st/ajax/allstars/stills/?page="
+    cli_arg = "stills"
+    desc = "SIFAS stills"
+
+    class ItemParser:
+        async def create_item(self, data: ClientResponse) -> list[str]:
+            page = bs4.BeautifulSoup(await data.text(), features="lxml")
+
+            if isinstance(top_item := page.find(class_="top-item"), bs4.Tag):
+                links = top_item.find_all("a")
+                return [link.get("href") for link in links]
+
+            raise ParsingError("An error occured while getting a still.")
