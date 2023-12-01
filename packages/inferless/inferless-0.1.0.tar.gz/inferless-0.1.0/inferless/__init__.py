f493da773d0a36a6c45dee2279bae3d92e4cf867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 17:03:09 +0000

    Add package inferless/0.1.0/inferless-0.1.0.tar.gz

---
+++ packages/inferless/inferless-0.1.0.tar.gz/inferless-0.1.0/inferless/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+from inferless.api import call, async_call
+
+
+__version__ = "0.0.1"
+__all__ = ["call", "async_call"]
---
+++ packages/inferless/inferless-0.1.0.tar.gz/inferless-0.1.0/inferless/api.py
@@ -0,0 +1,35 @@
+import os
+from typing import Optional
+
+
+def call(url: str, workspace_api_key: Optional[str] = None, data: Optional[dict] = None):
+    import requests
+    if workspace_api_key is None:
+        workspace_api_key = os.environ.get("INFERLESS_API_KEY")
+    headers = {"Content-Type": "application/json",
+               "Authorization": f"Bearer {workspace_api_key}"}
+    if data is None:
+        data = {}
+    response = requests.post(url, data=data, headers=headers)
+    if response.status_code != 200:
+        raise Exception(
+            f"Failed to call {url} with {data} with status code {response.status_code} and response {response.text}")
+    return response.json()
+
+
+async def async_call(url: str, workspace_api_key: Optional[str] = None, data: Optional[dict] = None):
+    import aiohttp
+    if workspace_api_key is None:
+        workspace_api_key = os.environ.get("INFERLESS_API_KEY")
+
+    headers = {"Content-Type": "application/json",
+               "Authorization": f"Bearer {workspace_api_key}"}
+    if data is None:
+        data = {}
+
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.post(url, data=data) as response:
+            if response.status != 200:
+                raise Exception(
+                    f"Failed to call {url} with {data} with status code {response.status} and response {await response.text()}")
+            return await response.json()
