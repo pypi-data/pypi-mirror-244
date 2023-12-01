857e52b748b7b50540814fdff704595f9ae44286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 16:03:49 +0000

    Add package python-calamine/0.1.7/python_calamine-0.1.7-cp39-cp39-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/python-calamine/python_calamine-0.1.7-cp39-cp39-manylinux_2_17_s390x.manylinux2014_s390x.whl/python_calamine/__init__.py
@@ -0,0 +1,19 @@
+from ._python_calamine import (
+    CalamineError,
+    CalamineSheet,
+    CalamineWorkbook,
+    SheetMetadata,
+    SheetTypeEnum,
+    SheetVisibleEnum,
+    load_workbook,
+)
+
+__all__ = (
+    "CalamineError",
+    "CalamineSheet",
+    "CalamineWorkbook",
+    "SheetMetadata",
+    "SheetTypeEnum",
+    "SheetVisibleEnum",
+    "load_workbook",
+)
