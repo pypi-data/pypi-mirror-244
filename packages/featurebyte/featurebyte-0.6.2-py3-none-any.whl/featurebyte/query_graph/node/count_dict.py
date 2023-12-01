4474875601d345c63f4a57a179282d3a361a58db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 16:03:41 +0000

    Add package featurebyte/0.6.2/featurebyte-0.6.2-py3-none-any.whl

---
+++ packages/featurebyte/featurebyte-0.6.2-py3-none-any.whl/featurebyte/query_graph/node/count_dict.py
@@ -0,0 +1,225 @@
+"""
+This module contains datetime operation related node classes
+"""
+# DO NOT include "from __future__ import annotations" as it will trigger issue for pydantic model nested definition
+from typing import ClassVar, List, Literal, Optional, Sequence, Set, Tuple, Union
+from typing_extensions import Annotated
+
+from abc import ABC, abstractmethod  # pylint: disable=wrong-import-order
+
+from pydantic import BaseModel, Field
+
+from featurebyte.common.typing import Scalar
+from featurebyte.enum import DBVarType
+from featurebyte.query_graph.enum import NodeType
+from featurebyte.query_graph.node.agg_func import construct_agg_func
+from featurebyte.query_graph.node.base import BaseSeriesOutputNode
+from featurebyte.query_graph.node.metadata.operation import AggregationColumn, OperationStructure
+from featurebyte.query_graph.node.metadata.sdk_code import (
+    CodeGenerationConfig,
+    CodeGenerationContext,
+    ExpressionStr,
+    StatementT,
+    ValueStr,
+    VariableNameGenerator,
+    VarNameExpressionInfo,
+)
+
+
+class BaseCountDictOpNode(BaseSeriesOutputNode, ABC):
+    """BaseCountDictOpNode class"""
+
+    @property
+    def max_input_count(self) -> int:
+        return 2
+
+    def _get_required_input_columns(
+        self, input_index: int, available_column_names: List[str]
+    ) -> Sequence[str]:
+        return self._assert_empty_required_input_columns()
+
+    def _derive_sdk_code(
+        self,
+        node_inputs: List[VarNameExpressionInfo],
+        var_name_generator: VariableNameGenerator,
+        operation_structure: OperationStructure,
+        config: CodeGenerationConfig,
+        context: CodeGenerationContext,
+    ) -> Tuple[List[StatementT], VarNameExpressionInfo]:
+        var_name_expressions = self._assert_no_info_dict(node_inputs)
+        var_name_expression = var_name_expressions[0].as_input()
+        other_operands = [val.as_input() for val in var_name_expressions[1:]]
+        expression = ExpressionStr(
+            self.generate_expression(operand=var_name_expression, other_operands=other_operands)
+        )
+        return [], expression
+
+    @abstractmethod
+    def generate_expression(self, operand: str, other_operands: List[str]) -> str:
+        """
+        Generate expression for the unary operation
+
+        Parameters
+        ----------
+        operand: str
+            First operand
+        other_operands: List[str]
+            Other operands
+
+        Returns
+        -------
+        str
+        """
+
+
+class CountDictTransformNode(BaseCountDictOpNode):
+    """CountDictTransformNode class"""
+
+    class Parameters(BaseModel):
+        """Parameters"""
+
+        transform_type: Literal[
+            "entropy", "most_frequent", "key_with_highest_value", "key_with_lowest_value"
+        ]
+
+    class UniqueCountParameters(BaseModel):
+        """UniqueCountParameters"""
+
+        transform_type: Literal["unique_count"]
+        include_missing: Optional[bool]
+
+    type: Literal[NodeType.COUNT_DICT_TRANSFORM] = Field(NodeType.COUNT_DICT_TRANSFORM, const=True)
+    parameters: Annotated[
+        Union[Parameters, UniqueCountParameters], Field(discriminator="transform_type")
+    ]
+
+    transform_types_with_varchar_output: ClassVar[Set[str]] = {
+        "most_frequent",
+        "key_with_highest_value",
+        "key_with_lowest_value",
+    }
+
+    def derive_var_type(self, inputs: List[OperationStructure]) -> DBVarType:
+        if self.parameters.transform_type in self.transform_types_with_varchar_output:
+            return DBVarType.VARCHAR
+        return DBVarType.FLOAT
+
+    def generate_expression(self, operand: str, other_operands: List[str]) -> str:
+        params = ""
+        if isinstance(self.parameters, self.UniqueCountParameters):
+            params = f"include_missing={ValueStr.create(self.parameters.include_missing)}"
+        return f"{operand}.cd.{self.parameters.transform_type}({params})"
+
+
+class CosineSimilarityNode(BaseCountDictOpNode):
+    """CosineSimilarityNode class"""
+
+    type: Literal[NodeType.COSINE_SIMILARITY] = Field(NodeType.COSINE_SIMILARITY, const=True)
+
+    def derive_var_type(self, inputs: List[OperationStructure]) -> DBVarType:
+        return DBVarType.FLOAT
+
+    def generate_expression(self, operand: str, other_operands: List[str]) -> str:
+        return f"{operand}.cd.cosine_similarity(other={other_operands[0]})"
+
+
+class DictionaryKeysNode(BaseSeriesOutputNode):
+    """Dictionary keys node class"""
+
+    type: Literal[NodeType.DICTIONARY_KEYS] = Field(NodeType.DICTIONARY_KEYS, const=True)
+
+    @property
+    def max_input_count(self) -> int:
+        return 1
+
+    def _get_required_input_columns(
+        self, input_index: int, available_column_names: List[str]
+    ) -> Sequence[str]:
+        return self._assert_empty_required_input_columns()
+
+    def derive_var_type(self, inputs: List[OperationStructure]) -> DBVarType:
+        return DBVarType.ARRAY
+
+    def _derive_sdk_code(
+        self,
+        node_inputs: List[VarNameExpressionInfo],
+        var_name_generator: VariableNameGenerator,
+        operation_structure: OperationStructure,
+        config: CodeGenerationConfig,
+        context: CodeGenerationContext,
+    ) -> Tuple[List[StatementT], VarNameExpressionInfo]:
+        return [], node_inputs[0]
+
+
+class GetValueFromDictionaryNode(BaseCountDictOpNode):
+    """Get value from dictionary node class"""
+
+    class Parameters(BaseModel):
+        """Parameters"""
+
+        value: Optional[Scalar]
+
+    type: Literal[NodeType.GET_VALUE] = Field(NodeType.GET_VALUE, const=True)
+    parameters: Parameters
+
+    def derive_var_type(self, inputs: List[OperationStructure]) -> DBVarType:
+        aggregations = inputs[0].aggregations
+        agg_column = aggregations[0]
+        # This assumes that dictionary features are never post-processed.
+        assert isinstance(agg_column, AggregationColumn)
+        method = agg_column.method
+        assert method is not None
+        agg_func = construct_agg_func(method)
+        # derive the output_var_type using aggregation's parent column without passing category parameter
+        # as count method doesn't have any parent column, take the first input column as parent column
+        parent_column = agg_column.column
+        if parent_column is None:
+            parent_column = inputs[0].columns[0]
+        return agg_func.derive_output_var_type(parent_column.dtype, category=None)
+
+    def generate_expression(self, operand: str, other_operands: List[str]) -> str:
+        param = other_operands[0] if other_operands else ValueStr.create(self.parameters.value)
+        return f"{operand}.cd.get_value(key={param})"
+
+
+class GetRankFromDictionaryNode(BaseCountDictOpNode):
+    """Get rank from dictionary node class"""
+
+    class Parameters(BaseModel):
+        """Parameters"""
+
+        value: Optional[Scalar]
+        descending: bool = False
+
+    type: Literal[NodeType.GET_RANK] = Field(NodeType.GET_RANK, const=True)
+    parameters: Parameters
+
+    def derive_var_type(self, inputs: List[OperationStructure]) -> DBVarType:
+        return DBVarType.FLOAT
+
+    def generate_expression(self, operand: str, other_operands: List[str]) -> str:
+        key = other_operands[0] if other_operands else self.parameters.value
+        descending = ValueStr.create(self.parameters.descending)
+        params = f"key={key}, descending={descending}"
+        return f"{operand}.cd.get_value(key={params})"
+
+
+class GetRelativeFrequencyFromDictionaryNode(BaseCountDictOpNode):
+    """Get relative frequency from dictionary node class"""
+
+    class Parameters(BaseModel):
+        """Parameters"""
+
+        value: Optional[Scalar]
+
+    type: Literal[NodeType.GET_RELATIVE_FREQUENCY] = Field(
+        NodeType.GET_RELATIVE_FREQUENCY, const=True
+    )
+    parameters: Parameters
+
+    def derive_var_type(self, inputs: List[OperationStructure]) -> DBVarType:
+        return DBVarType.FLOAT
+
+    def generate_expression(self, operand: str, other_operands: List[str]) -> str:
+        param = other_operands[0] if other_operands else ValueStr.create(self.parameters.value)
+        return f"{operand}.cd.get_relative_frequency(key={param})"
---
+++ packages/featurebyte/featurebyte-0.6.2-py3-none-any.whl/featurebyte/query_graph/node/date.py
@@ -0,0 +1,217 @@
+"""
+This module contains datetime operation related node classes
+"""
+# DO NOT include "from __future__ import annotations" as it will trigger issue for pydantic model nested definition
+from typing import List, Literal, Optional, Sequence, Tuple
+
+from pydantic import BaseModel, Field
+
+from featurebyte.common.typing import DatetimeSupportedPropertyType, TimedeltaSupportedUnitType
+from featurebyte.enum import DBVarType
+from featurebyte.query_graph.enum import NodeType
+from featurebyte.query_graph.node.base import (
+    BaseSeriesOutputNode,
+    BaseSeriesOutputWithSingleOperandNode,
+)
+from featurebyte.query_graph.node.metadata.operation import NodeOutputCategory, OperationStructure
+from featurebyte.query_graph.node.metadata.sdk_code import (
+    ClassEnum,
+    CodeGenerationConfig,
+    CodeGenerationContext,
+    ExpressionStr,
+    StatementT,
+    ValueStr,
+    VariableNameGenerator,
+    VariableNameStr,
+    VarNameExpressionInfo,
+)
+
+
+class DatetimeExtractNode(BaseSeriesOutputNode):
+    """DatetimeExtractNode class"""
+
+    class Parameters(BaseModel):
+        """Parameters"""
+
+        property: DatetimeSupportedPropertyType
+        timezone_offset: Optional[str]
+
+    type: Literal[NodeType.DT_EXTRACT] = Field(NodeType.DT_EXTRACT, const=True)
+    parameters: Parameters
+
+    def derive_var_type(self, inputs: List[OperationStructure]) -> DBVarType:
+        return DBVarType.INT
+
+    @property
+    def max_input_count(self) -> int:
+        return 2
+
+    def _get_required_input_columns(
+        self, input_index: int, available_column_names: List[str]
+    ) -> Sequence[str]:
+        return self._assert_empty_required_input_columns()
+
+    def _derive_sdk_code(
+        self,
+        node_inputs: List[VarNameExpressionInfo],
+        var_name_generator: VariableNameGenerator,
+        operation_structure: OperationStructure,
+        config: CodeGenerationConfig,
+        context: CodeGenerationContext,
+    ) -> Tuple[List[StatementT], VarNameExpressionInfo]:
+        var_name_expressions = self._assert_no_info_dict(node_inputs)
+        ts_operand: str = var_name_expressions[0].as_input()
+
+        offset_operand: Optional[str]
+        if self.parameters.timezone_offset is not None:
+            offset_operand = ValueStr.create(self.parameters.timezone_offset).as_input()
+        elif len(var_name_expressions) == 2:
+            offset_operand = var_name_expressions[1].as_input()
+        else:
+            offset_operand = None
+
+        date_property: str = self.parameters.property
+        if date_property == "dayofweek":
+            date_property = "day_of_week"
+
+        output: VarNameExpressionInfo
+        if offset_operand is None:
+            output = VariableNameStr(f"{ts_operand}.dt.{date_property}")
+        else:
+            output = ExpressionStr(f"{ts_operand}.dt.tz_offset({offset_operand}).{date_property}")
+
+        return [], output
+
+
+class TimeDeltaExtractNode(BaseSeriesOutputWithSingleOperandNode):
+    """TimeDeltaExtractNode class"""
+
+    class Parameters(BaseModel):
+        """Parameters"""
+
+        property: TimedeltaSupportedUnitType
+
+    type: Literal[NodeType.TIMEDELTA_EXTRACT] = Field(NodeType.TIMEDELTA_EXTRACT, const=True)
+    parameters: Parameters
+
+    def derive_var_type(self, inputs: List[OperationStructure]) -> DBVarType:
+        return DBVarType.FLOAT
+
+    def generate_expression(self, operand: str) -> str:
+        return f"{operand}.dt.{self.parameters.property}"
+
+
+class DateDifference(BaseSeriesOutputNode):
+    """DateDifference class"""
+
+    type: Literal[NodeType.DATE_DIFF] = Field(NodeType.DATE_DIFF, const=True)
+
+    @property
+    def max_input_count(self) -> int:
+        return 2
+
+    def _get_required_input_columns(
+        self, input_index: int, available_column_names: List[str]
+    ) -> Sequence[str]:
+        return self._assert_empty_required_input_columns()
+
+    def derive_var_type(self, inputs: List[OperationStructure]) -> DBVarType:
+        return DBVarType.TIMEDELTA
+
+    def _derive_sdk_code(
+        self,
+        node_inputs: List[VarNameExpressionInfo],
+        var_name_generator: VariableNameGenerator,
+        operation_structure: OperationStructure,
+        config: CodeGenerationConfig,
+        context: CodeGenerationContext,
+    ) -> Tuple[List[StatementT], VarNameExpressionInfo]:
+        var_name_expressions = self._assert_no_info_dict(node_inputs)
+        left_operand: str = var_name_expressions[0].as_input()
+        right_operand = var_name_expressions[1].as_input()
+        return [], ExpressionStr(f"{left_operand} - {right_operand}")
+
+
+class TimeDelta(BaseSeriesOutputNode):
+    """TimeDelta class"""
+
+    class Parameters(BaseModel):
+        """Parameters"""
+
+        unit: TimedeltaSupportedUnitType
+
+    type: Literal[NodeType.TIMEDELTA] = Field(NodeType.TIMEDELTA, const=True)
+    parameters: Parameters
+
+    @property
+    def max_input_count(self) -> int:
+        return 2
+
+    def _get_required_input_columns(
+        self, input_index: int, available_column_names: List[str]
+    ) -> Sequence[str]:
+        return self._assert_empty_required_input_columns()
+
+    def derive_var_type(self, inputs: List[OperationStructure]) -> DBVarType:
+        return DBVarType.TIMEDELTA
+
+    def _derive_sdk_code(
+        self,
+        node_inputs: List[VarNameExpressionInfo],
+        var_name_generator: VariableNameGenerator,
+        operation_structure: OperationStructure,
+        config: CodeGenerationConfig,
+        context: CodeGenerationContext,
+    ) -> Tuple[List[StatementT], VarNameExpressionInfo]:
+        var_name_expressions = self._assert_no_info_dict(node_inputs)
+        var_name_expression = var_name_expressions[0]
+        statements: List[StatementT] = []
+        var_name = var_name_generator.generate_variable_name(
+            node_output_type=operation_structure.output_type,
+            node_output_category=operation_structure.output_category,
+            node_name=self.name,
+        )
+        obj = ClassEnum.TO_TIMEDELTA(series=var_name_expression, unit=self.parameters.unit)
+        statements.append((var_name, obj))
+        return statements, var_name
+
+
+class DateAdd(BaseSeriesOutputNode):
+    """DateAdd class"""
+
+    class Parameters(BaseModel):
+        """Parameters"""
+
+        value: Optional[int]
+
+    type: Literal[NodeType.DATE_ADD] = Field(NodeType.DATE_ADD, const=True)
+    parameters: Parameters
+
+    @property
+    def max_input_count(self) -> int:
+        return 2
+
+    def _get_required_input_columns(
+        self, input_index: int, available_column_names: List[str]
+    ) -> Sequence[str]:
+        return self._assert_empty_required_input_columns()
+
+    def derive_var_type(self, inputs: List[OperationStructure]) -> DBVarType:
+        if inputs[0].output_category == NodeOutputCategory.FEATURE:
+            # when the inputs[0] is requested column, inputs[0].columns is empty.
+            # in this case, we should derive the var type from inputs[0].aggregations
+            return inputs[0].aggregations[0].dtype
+        return inputs[0].columns[0].dtype
+
+    def _derive_sdk_code(
+        self,
+        node_inputs: List[VarNameExpressionInfo],
+        var_name_generator: VariableNameGenerator,
+        operation_structure: OperationStructure,
+        config: CodeGenerationConfig,
+        context: CodeGenerationContext,
+    ) -> Tuple[List[StatementT], VarNameExpressionInfo]:
+        var_name_expressions = self._assert_no_info_dict(node_inputs)
+        left_operand: str = var_name_expressions[0].as_input()
+        right_operand = var_name_expressions[1].as_input()
+        return [], ExpressionStr(f"{left_operand} + {right_operand}")
