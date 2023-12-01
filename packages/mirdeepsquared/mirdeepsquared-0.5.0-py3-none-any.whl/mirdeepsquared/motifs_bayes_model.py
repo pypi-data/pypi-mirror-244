d9c3c75a20879aacf16f6d8c87f69b60b579e9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 17:03:13 +0000

    Add package mirdeepsquared/0.5.0/mirdeepsquared-0.5.0-py3-none-any.whl

---
+++ packages/mirdeepsquared/mirdeepsquared-0.5.0-py3-none-any.whl/mirdeepsquared/motifs_bayes_model.py
@@ -0,0 +1,34 @@
+from sklearn.naive_bayes import CategoricalNB
+from mirdeepsquared.common import Y_values
+from mirdeepsquared.model import MirdeepSquaredModel
+import pickle
+
+import numpy as np
+
+
+# Accuracy: 0.628428927680798
+class MotifModel(MirdeepSquaredModel):
+    def __init__(self):
+        self.model = CategoricalNB(force_alpha=True)
+
+    def features_used(self):
+        return ['motifs']
+
+    def train(self, train, val):
+        self.model.fit(self.X(train), Y_values(train))
+
+    def save(self, model_path):
+        with open(model_path, 'wb') as f:
+            pickle.dump(self.model, f)
+
+    def load(self, model_path):
+        with open(model_path, 'rb') as f:
+            self.model = pickle.load(f)
+
+    def predict(self, X):
+        # Only return the false positive probability
+        return np.asarray([x[1] for x in self.model.predict_proba(X)])
+
+    # Binary crossentropy (on val): Structure: 2.5, Numericals: 3.8, density map: 0.55834, motifs: 5.98, Ensemble: 0.5587
+    def weight(self):
+        return 2
