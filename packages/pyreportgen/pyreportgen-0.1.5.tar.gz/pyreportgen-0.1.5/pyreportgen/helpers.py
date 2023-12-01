52afbd520f4a81459f7b9e49d3857dfe5dfbd335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 16:03:33 +0000

    Add package pyreportgen/0.1.5/pyreportgen-0.1.5.tar.gz

---
+++ packages/pyreportgen/pyreportgen-0.1.5.tar.gz/pyreportgen-0.1.5/pyreportgen/helpers.py
@@ -0,0 +1,22 @@
+import uuid
+from pyreportgen.base import _DATA_DIR
+import os
+
+def clamp(num, min, max):
+    if num < min:
+        return min
+    if num > max:
+        return max
+    return num
+
+def random_path(filetype):
+    return f"{_DATA_DIR}/{str(uuid.uuid4())}."+filetype
+
+def to_html_path(path):
+    return "file://"+str(os.path.abspath('index.html'))
+
+def tagwrap(content:str, tag:str, classList="", props="", close=True) -> str:
+    if close:
+        return f"<{tag} class='{classList}' {props}>{content}</{tag}>"
+    else:
+        return f"<{tag} class='{classList}' {props} />"
