b07680b9aaacbd8de0364013fc90e4ef71322840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 14:02:56 +0000

    Add package hangar-api/2023.12/hangar_api-2023.12-py3-none-any.whl

---
+++ packages/hangar-api/hangar_api-2023.12-py3-none-any.whl/hangar_api/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from hangar_api.api.api_keys_api import APIKeysApi
+from hangar_api.api.authentication_api import AuthenticationApi
+from hangar_api.api.pages_api import PagesApi
+from hangar_api.api.permissions_api import PermissionsApi
+from hangar_api.api.projects_api import ProjectsApi
+from hangar_api.api.users_api import UsersApi
+from hangar_api.api.versions_api import VersionsApi
+
