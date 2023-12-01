7821305cd8848636407b88755223d0eb22c14657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 16:03:42 +0000

    Add package dftools-core/0.0.15/dftools-core-0.0.15.tar.gz

---
+++ packages/dftools-core/dftools-core-0.0.15.tar.gz/dftools-core-0.0.15/dftools/core/structure/compare/__init__.py
@@ -0,0 +1,9 @@
+
+from dftools.core.structure.compare.structure_compare import (
+    StructureComparisonResult
+    , StructureComparator
+)
+from dftools.core.structure.compare.structure_catalog_compare import (
+    StructureCatalogComparator
+    , StructureCatalogComparisonResult
+)
---
+++ packages/dftools-core/dftools-core-0.0.15.tar.gz/dftools-core-0.0.15/dftools/core/structure/jinja/__init__.py
@@ -0,0 +1,5 @@
+
+from dftools.core.structure.jinja.statement_gen import (
+    StructureJinjaDictEncoder
+    , StructureComparedJinjaDictEncoder
+)
