0729372969a18cff3968b47d948d4226444f5215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 16:03:42 +0000

    Add package dftools-core/0.0.15/dftools_core-0.0.15-py3-none-any.whl

---
+++ packages/dftools-core/dftools_core-0.0.15-py3-none-any.whl/dftools/core/structure/__init__.py
@@ -0,0 +1,38 @@
+
+from dftools.core.structure.core import (
+    DataBank
+    , Namespace
+    , NamespaceImpl
+    , FieldCharacterisation
+    , FieldCharacterisationStd
+    , Field
+    , FieldCatalog
+    , StructureRef
+    , Structure
+    , StructureCatalog
+)
+from dftools.core.structure.compare import (
+      StructureComparisonResult
+    , StructureComparator
+    , StructureCatalogComparator
+    , StructureCatalogComparisonResult
+)
+from dftools.core.structure.api import (
+    StructureCatalogCsv
+    , FieldCatalogCsv
+    , StructureCatalogApi
+)
+
+from dftools.core.structure.template import (
+    StructureTemplate
+)
+from dftools.core.structure.decoder import (
+    BaseFieldDecoder
+    , StdFieldDecoder
+    , BaseStructureDecoder
+    , StdStructureDecoder
+)
+
+from dftools.core.structure.jinja import (
+    StructureJinjaDictEncoder
+)
