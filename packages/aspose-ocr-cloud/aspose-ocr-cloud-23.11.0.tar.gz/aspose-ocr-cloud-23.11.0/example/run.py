7203b0c3202c2a50b7b68bc292f5da175a523d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 15:03:10 +0000

    Add package aspose-ocr-cloud/23.11.0/aspose-ocr-cloud-23.11.0.tar.gz

---
+++ packages/aspose-ocr-cloud/aspose-ocr-cloud-23.11.0.tar.gz/aspose-ocr-cloud-23.11.0/example/run.py
@@ -0,0 +1,25 @@
+import recognize_image_api_examples
+import binarize_image_api_examples
+import convert_text_to_speech_examples
+import convert_text_to_speech_trial_examples
+import utils
+
+from aspose_ocr_cloud.configuration import Configuration
+
+
+def run_demo():
+    utils.create_results_dir()
+
+    config_trial = Configuration()
+    convert_text_to_speech_trial_examples.run_convert_text_to_speech_trial_demo(config_trial)
+
+    config = Configuration(client_id='Your client_id', \
+                           client_secret='Your client_secret')
+    
+    recognize_image_api_examples.run_recognize_image_demo(config)
+    binarize_image_api_examples.run_binarize_image_demo(config)
+    convert_text_to_speech_examples.run_convert_text_to_speech_demo(config)
+
+
+if __name__ == '__main__':
+    run_demo()
