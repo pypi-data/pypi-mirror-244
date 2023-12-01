6ba64e5698aee17fe25fe9d512d57fda38b164dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 14:03:06 +0000

    Add package Python-Algorithm-pyal/1.1.5/Python_Algorithm_pyal-1.1.5-py3-none-any.whl

---
+++ packages/Python-Algorithm-pyal/Python_Algorithm_pyal-1.1.5-py3-none-any.whl/pyal/list/dequeue.py
@@ -0,0 +1,35 @@
+'''
+Author: Tian Xia (TianXia0209@gmail.com)
+'''
+
+from pyal.list.linked_list import LinkedList
+
+
+class Dequeue:
+  def __init__(self):
+    self._data = LinkedList()
+
+  def push_front(self, data):
+    self._data.push_front(data)
+
+  def push_back(self, data):
+    self._data.push_back(data)
+
+  def pop_back(self):
+    assert self._data.size() > 0
+    return self._data.pop_back()
+
+  def pop_front(self):
+    assert self._data.size() > 0
+    return self._data.pop_front()
+
+  def size(self):
+    return self._data.size()
+
+  def peek_back(self):
+    assert self._data.size() > 0
+    return self._data.rbegin().get()
+
+  def peek_front(self):
+    assert self._data.size() > 0
+    return self._data.begin().get()
