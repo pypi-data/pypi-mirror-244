35bc6c7b7dcf81082ad5ba7856b8db8331d4d51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 14:03:02 +0000

    Add package fendi/0.0.0.2/fendi-0.0.0.2.tar.gz

---
+++ packages/fendi/fendi-0.0.0.2.tar.gz/fendi-0.0.0.2/src/fendi/__init__.py
@@ -0,0 +1,5 @@
+def main():
+    """Entry point for the application script"""
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("I am Fedi HAMDI currently leaving in France, i get board sometimes so I code, give me a call we can hang out")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
