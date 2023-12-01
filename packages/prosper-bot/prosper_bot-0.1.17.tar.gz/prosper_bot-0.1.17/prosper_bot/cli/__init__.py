0d462449f16f4132af243723b941634e571d2674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 15:03:57 +0000

    Add package prosper-bot/0.1.17/prosper_bot-0.1.17.tar.gz

---
+++ packages/prosper-bot/prosper_bot-0.1.17.tar.gz/prosper_bot-0.1.17/prosper_bot/cli/__init__.py
@@ -0,0 +1,23 @@
+from decimal import Decimal
+
+from prosper_api.config import Config
+from prosper_shared.omni_config import config_schema
+from schema import Optional
+
+
+@config_schema
+def _schema():
+    return {
+        Optional(
+            "bot", default={"dry-run": False, "verbose": False, "min-bid": Decimal(25)}
+        ): {
+            Optional("dry-run", default=False): bool,
+            Optional("verbose", default=False): bool,
+            Optional("min-bid", default=Decimal(25.00)): Decimal,
+        }
+    }
+
+
+def build_config() -> Config:
+    """Compiles all the config sources into a single config."""
+    return Config.autoconfig(["prosper-api", "prosper-bot"], validate=True)
