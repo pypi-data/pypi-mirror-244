2a6e5ddd928fb5b68633e142b33449fe97ec352e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 16:03:32 +0000

    Add package kairospay/2.1.0/kairospay-2.1.0.tar.gz

---
+++ packages/kairospay/kairospay-2.1.0.tar.gz/kairospay-2.1.0/src/kairospay/binancepay.py
@@ -0,0 +1,7 @@
+# This code is developed by Solomon Olatunji.
+# Software Developer and DevOps Engineer
+# Email: aotoluwalopen@gmail.com
+
+import requests
+import json
+from requests.auth import HTTPBasicAuth
