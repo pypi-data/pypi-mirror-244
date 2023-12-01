9e223215dd3fb3c9d209add720346ca5f433487f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 16:03:57 +0000

    Add package amsdal_models/0.0.13/amsdal_models-0.0.13-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/amsdal_models/amsdal_models-0.0.13-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/amsdal_models/classes/data_models/dependencies.pyi
@@ -0,0 +1,18 @@
+from amsdal_models.enums import MetaClasses as MetaClasses
+from amsdal_models.schemas.manager import BuildSchemasManager as BuildSchemasManager
+from pydantic import BaseModel
+from typing import Any
+
+class DependencyModelNames(BaseModel):
+    type_model_names: list[str]
+    core_model_names: list[str]
+    contrib_model_names: list[str]
+    user_model_names: list[str]
+    reference_model_names: list[str]
+    @classmethod
+    def build_from_schemas_manager(cls, schemas_manager: BuildSchemasManager) -> DependencyModelNames: ...
+
+class DependencyItem(BaseModel):
+    module: tuple[str | None, str]
+    def __eq__(self, other: Any) -> bool: ...
+    def __hash__(self): ...
