f23e8ab9f2e223f7f291531fcc97177c1e593aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 15:03:51 +0000

    Add package aws-iam-cli/1.2.0/aws_iam_cli-1.2.0-py3-none-any.whl

---
+++ packages/aws-iam-cli/aws_iam_cli-1.2.0-py3-none-any.whl/iam_cli/main.py
@@ -0,0 +1,27 @@
+import argparse
+import sys
+
+from iam_cli.command import Command
+from iam_cli import VERSION
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description="Process a CSV file for IAM users.")
+    parser.add_argument('-v', '--version', action='version', version=f'iam-cli v{VERSION}')
+    
+    return parser
+
+
+def main():
+    try:
+        parser = get_arguments()
+        args = parser.parse_args()  # 인자를 파싱하는 부분을 추가함
+
+        Command()
+    except KeyboardInterrupt:
+        print('Cancelled by user.')
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main()
