e78ab4b2fcfb953f054853ba018601e3b63dc332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:03:44 +0000

    Add package progressiveai/0.0.1/progressiveai-0.0.1-py3-none-any.whl

---
+++ packages/progressiveai/progressiveai-0.0.1-py3-none-any.whl/progressiveai/request.py
@@ -0,0 +1,30 @@
+import requests
+
+
+class Chat:
+    def __init__(self, api_key, text, model):
+        self.api_key = api_key
+        self.text = text
+        self.model = model
+        # self.temperature = 0.7
+        # self.max_tokens = 50
+        self.session = requests.Session()  # Create a Session object
+
+    def get_response(self):
+        params = {
+            'api_key': self.api_key,
+            'prompt': self.text,
+            'temperature': self.temperature,
+            'max_tokens': self.max_tokens
+        }
+        response = self.session.get(  # Use session.get instead of requests.get
+            f'https://api.progressiveai.org/v1/{self.model}', params=params)
+        try:
+            data = response.json()
+        except Exception as e:
+            print(f"Status code: {response.status_code}")
+            print(f"Response content: {response.content}")
+            raise
+        if 'error' in data:
+            raise Exception(data['error'])
+        return data['response']
