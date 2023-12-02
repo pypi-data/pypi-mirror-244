77f14c4bf3df0950f7e72d7571a91d3a1244d0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 14:04:18 +0000

    Add package indexhtmlappender/0.5.0/indexhtmlappender-0.5.0.tar.gz

---
+++ packages/indexhtmlappender/indexhtmlappender-0.5.0.tar.gz/indexhtmlappender-0.5.0/indexhtmlappender/__main__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+"""Package entry point."""
+from indexhtmlappender.cli import app
+
+if __name__ == '__main__':  # pragma: no cover
+    app()  # pylint: disable=no-value-for-parameter
