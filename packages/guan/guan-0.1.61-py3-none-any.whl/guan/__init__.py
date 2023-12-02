7c3c3517d84bdd1a6d4c1ced122eff4331cc7afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 04:04:33 +0000

    Add package guan/0.1.61/guan-0.1.61-py3-none-any.whl

---
+++ packages/guan/guan-0.1.61-py3-none-any.whl/guan/__init__.py
@@ -0,0 +1,22 @@
+# Guan is an open-source python package developed and maintained by https://www.guanjihuan.com/about (Ji-Huan Guan, 关济寰). The primary location of this package is on website https://py.guanjihuan.com. The GitHub location of this package is on https://github.com/guanjihuan/py.guanjihuan.com.
+
+# 函数的装饰器，用于软件包的统计
+def function_decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        import guan
+        guan.statistics_of_guan_package(func.__name__)
+        return result
+    return wrapper
+
+from .basic_functions import *
+from .Fourier_transform import *
+from .Hamiltonian_of_examples import *
+from .band_structures_and_wave_functions import *
+from .Green_functions import *
+from .density_of_states import *
+from .quantum_transport import *
+from .topological_invariant import *
+from .machine_learning import *
+from .data_processing import *
+from .others import *
