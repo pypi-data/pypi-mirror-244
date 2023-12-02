9b0232077235dfb4c53525a6baf0a393f4c5012d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 15:03:21 +0000

    Add package PyMemoryEditor/1.5.19/pymemoryeditor-1.5.19.tar.gz

---
+++ packages/PyMemoryEditor/pymemoryeditor-1.5.19.tar.gz/pymemoryeditor-1.5.19/PyMemoryEditor/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+Multi-platform library developed with ctypes for reading, writing and
+searching process memory, in a simple and friendly way with Python 3.
+
+The package supports Windows and Linux (32-bit and 64-bit).
+"""
+
+__author__ = "Jean Loui Bernard Silva de Jesus"
+__version__ = "1.5.19"
+
+
+from .enums import ScanTypesEnum
+import sys
+
+# For Windows.
+if "win" in sys.platform:
+    from .win32.process import WindowsProcess
+    from .win32.enums.process_operations import ProcessOperationsEnum
+    OpenProcess = WindowsProcess
+
+# For Linux.
+else:
+    from .linux.process import LinuxProcess
+    from .linux.ptrace import ptrace
+    from .linux.ptrace.enums import PtraceCommandsEnum
+    OpenProcess = LinuxProcess
---
+++ packages/PyMemoryEditor/pymemoryeditor-1.5.19.tar.gz/pymemoryeditor-1.5.19/tests/package.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+
+current_dir = os.getcwd()
+sys.path.append(current_dir)
+
+from PyMemoryEditor import OpenProcess, ScanTypesEnum
+from PyMemoryEditor import __version__ as version
