1f842e6da3df9948d991164bb7f6594fc6988955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 04:03:41 +0000

    Add package hspylib-kafman/0.9.142/hspylib_kafman-0.9.142-py3-none-any.whl

---
+++ packages/hspylib-kafman/hspylib_kafman-0.9.142-py3-none-any.whl/kafman/__main__.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HsPyLib-Kafman
+   @package: kafman
+      @file: __main__.py
+   @created: Fri, 1 Jul 2022
+    @author: "<B>H</B>ugo <B>S</B>aporetti <B>J</B>unior")"
+      @site: "https://github.com/yorevs/hspylib")
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2023, HsPyLib team
+"""
+
+from hqt.qt_application import QtApplication
+from hspylib.core.enums.charset import Charset
+from hspylib.core.zoned_datetime import now
+from hspylib.modules.application.version import Version
+from kafman.__classpath__ import _Classpath
+from kafman.views.main_qt_view import MainQtView
+from textwrap import dedent
+
+import logging as log
+import sys
+
+
+class Main(QtApplication):
+    """Kafman application main class"""
+
+    # The welcome message
+    DESCRIPTION = _Classpath.get_source_path("welcome.txt").read_text(encoding=Charset.UTF_8.val)
+
+    # Location of the .version file
+    VERSION_DIR = _Classpath.source_path()
+
+    # Location of the resources dir
+    RESOURCE_DIR = str(_Classpath.resource_path())
+
+    # Location of the UI font
+    FONT_PATH = _Classpath.get_resource_path("fonts/Droid-Sans-Mono-for-Powerline-Nerd-Font-Complete.otf")
+
+    # Application icon
+    APP_ICON_PATH = _Classpath.get_resource_path("app-icon.png")
+
+    def __init__(self, app_name: str):
+        version = Version.load(load_dir=self.VERSION_DIR)
+        description = self.DESCRIPTION.format(version)
+        super().__init__(MainQtView, app_name, version, description, resource_dir=self.RESOURCE_DIR)
+        self.set_application_font(self.FONT_PATH)
+        self.set_application_icon(self.APP_ICON_PATH)
+        log.info(
+            dedent(
+                f"""
+        {self._app_name} v{self._app_version}
+
+        Settings ==============================
+                STARTED: {now("%Y-%m-%d %H:%M:%S")}
+        """
+            )
+        )
+
+
+if __name__ == "__main__":
+    # Application entry point
+    Main("kafman").INSTANCE.run(sys.argv[1:])
