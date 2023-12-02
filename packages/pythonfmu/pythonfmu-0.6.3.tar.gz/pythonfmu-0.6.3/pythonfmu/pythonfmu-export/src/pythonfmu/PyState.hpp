8de7eb3e911fcbf16e5db0d9e4fede8ccf99d90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:03:23 +0000

    Add package pythonfmu/0.6.3/pythonfmu-0.6.3.tar.gz

---
+++ packages/pythonfmu/pythonfmu-0.6.3.tar.gz/pythonfmu-0.6.3/pythonfmu/pythonfmu-export/src/pythonfmu/PyState.hpp
@@ -0,0 +1,43 @@
+
+#ifndef PYTHONFMU_PYTHONSTATE_HPP
+#define PYTHONFMU_PYTHONSTATE_HPP
+
+#include <Python.h>
+#include <iostream>
+
+namespace pythonfmu
+{
+
+class PyState
+{
+public:
+    PyState()
+    {
+        was_initialized_ = Py_IsInitialized();
+
+        if (!was_initialized_) {
+            Py_SetProgramName(L"./PythonFMU");
+            Py_Initialize();
+#if PY_VERSION_HEX < 0x03070000
+            PyEval_InitThreads();
+#endif
+            _mainPyThread = PyEval_SaveThread();
+        }
+    }
+
+    ~PyState()
+    {
+        if (!was_initialized_) {
+            PyEval_RestoreThread(_mainPyThread);
+            Py_Finalize();
+        }
+    }
+
+private:
+    bool was_initialized_;
+    PyThreadState* _mainPyThread;
+};
+
+} // namespace pythonfmu
+
+#endif //PYTHONFMU_PYTHONSTATE_HPP
