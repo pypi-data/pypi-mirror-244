13bfd215c10184cf93f392f1b92f2e330c3d6af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 03:06:15 +0000

    Add package scrapgo/1.4.4/scrapgo-1.4.4.tar.gz

---
+++ packages/scrapgo/scrapgo-1.4.4.tar.gz/scrapgo-1.4.4/scrapgo/lib/logger.py
@@ -0,0 +1,23 @@
+import logging
+from logging import DEBUG, WARNING, CRITICAL, INFO, NOTSET
+
+
+
+def get_logger(logfile=None, logger_name=None,  mode='a', loglevel=DEBUG):
+
+    logger = logging.getLogger(logger_name or __name__)
+
+    formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+    logger.addHandler(streamHandler)
+
+    if logfile:
+        fileHandler = logging.FileHandler(logfile,mode=mode)
+        fileHandler.setFormatter(formatter)
+        logger.addHandler(fileHandler)
+
+    logger.setLevel(level=loglevel)
+    return logger
+
