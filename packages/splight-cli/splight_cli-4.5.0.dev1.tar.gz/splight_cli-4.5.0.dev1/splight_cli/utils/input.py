82b5918df58acb5a7670c8fcc64a2dfec8aa6b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 04:03:38 +0000

    Add package splight-cli/4.5.0.dev1/splight_cli-4.5.0.dev1.tar.gz

---
+++ packages/splight-cli/splight_cli-4.5.0.dev1.tar.gz/splight_cli-4.5.0.dev1/splight_cli/utils/input.py
@@ -0,0 +1,108 @@
+from functools import partial
+from pydoc import locate
+from typing import Dict, List, Type, Union
+
+import click
+
+Primitive = Union[int, str, float, bool]
+
+
+def prompt_data_address_value():
+    while True:
+        asset_id = click.prompt(
+            text="Asset ID",
+            type=str,
+            default=None,
+        )
+        if asset_id:
+            break
+        else:
+            click.echo("Asset ID cannot be empty. Please try again.")
+
+    while True:
+        attribute_id = click.prompt(
+            text="Attribute ID",
+            type=str,
+            default=None,
+        )
+        if attribute_id:
+            break
+        else:
+            click.echo("Attribute ID cannot be empty. Please try again.")
+
+    return {"asset": asset_id, "attribute": attribute_id}
+
+
+def prompt_param(param: Dict[str, Primitive], prefix: str = "") -> Primitive:
+    """Prompt the user for a single parameter
+
+    Parameters
+    ----------
+    param: Dict
+        The parameter to prompt
+    prefix: str
+        Prefix for the parameter name
+
+    Returns
+    -------
+    Primitive
+    """
+    param_name = param["name"]
+    new_value = input_single(
+        {
+            "name": f"{prefix} {param_name}",
+            "type": param["type"],
+            "required": param.get("required", True),
+            "multiple": param.get("multiple", False),
+            "value": param.get("value"),
+        }
+    )
+    return new_value
+
+
+def list_of(input_value: Union[List, str], param_type: Type = str) -> List:
+    if isinstance(input_value, list):
+        return input_value
+    return [param_type(v.strip()) for v in input_value.split(",")]
+
+
+def input_single(param: Dict[str, Primitive]) -> Primitive:
+    """Prompt user for input for a single parameter
+
+    Parameters
+    ----------
+    param : Dict[str, Primitive]
+
+    Returns
+    -------
+    Primitive the value given by the user
+    """
+    default = (
+        "None"
+        if param.get("value") is None and not param["required"]
+        else param["value"]
+    )
+    required = param.get("required", False)
+    param_name = param.get("name", "")
+    param_type = param.get("type")
+    name = f"{'*' if required else ' '}{param_name} ({param_type})"
+    param_type = locate(param["type"])
+    if not param_type:
+        param_type = str
+    value_proc = None
+    if param.get("multiple"):
+        value_proc = partial(list_of, param_type=param_type)
+
+    val = click.prompt(
+        text=name,
+        type=param_type,
+        default=default,
+        value_proc=value_proc,
+    )
+    if isinstance(val, str):
+        val = val.strip(" ")
+        if not val:
+            val = None
+    if val == "None":
+        val = None
+    return val
