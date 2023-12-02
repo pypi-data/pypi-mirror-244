67a5424d7450a0fa4712880f19915fd29994c678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:03:56 +0000

    Add package opteryx/0.12.0b8/opteryx-0.12.0b8-cp39-cp39-macosx_10_15_universal2.whl

---
+++ packages/opteryx/opteryx-0.12.0b8-cp39-cp39-macosx_10_15_universal2.whl/opteryx/components/heuristic_optimizer/strategies/predicate_pushdown.py
@@ -0,0 +1,134 @@
+from orso.tools import random_string
+
+from opteryx.components.binder.binder_visitor import extract_join_fields
+from opteryx.components.binder.binder_visitor import get_mismatched_condition_column_types
+from opteryx.components.logical_planner import LogicalPlan
+from opteryx.components.logical_planner import LogicalPlanNode
+from opteryx.components.logical_planner import LogicalPlanStepType
+from opteryx.managers.expression import NodeType
+from opteryx.managers.expression import get_all_nodes_of_type
+from opteryx.models import Node
+
+from .optimization_strategy import HeuristicOptimizerContext
+from .optimization_strategy import OptimizationStrategy
+
+
+def _add_condition(existing_condition, new_condition):
+    if not existing_condition:
+        return new_condition
+    _and = Node(node_type=NodeType.AND)
+    _and.left = new_condition
+    _and.right = existing_condition
+    return _and
+
+
+class PredicatePushdownStrategy(OptimizationStrategy):
+    def visit(
+        self, node: LogicalPlanNode, context: HeuristicOptimizerContext
+    ) -> HeuristicOptimizerContext:
+        if not context.optimized_plan:
+            context.optimized_plan = context.pre_optimized_tree.copy()
+
+        if node.node_type in (
+            LogicalPlanStepType.Scan,
+            LogicalPlanStepType.FunctionDataset,
+            LogicalPlanStepType.Subquery,
+        ):
+            # Handle predicates specific to node types
+            context = self._handle_predicates(node, context)
+
+            context.optimized_plan.add_node(context.node_id, LogicalPlanNode(**node.properties))
+            if context.last_nid:
+                context.optimized_plan.add_edge(context.node_id, context.last_nid)
+
+        elif node.node_type == LogicalPlanStepType.Filter:
+            # collect predicates we can probably push
+            if node.simple and len(node.relations) > 0:
+                context.collected_predicates.append(node)
+                context.optimized_plan.remove_node(context.node_id, heal=True)
+
+        elif node.node_type == LogicalPlanStepType.Join:
+            # push predicates which reference multiple relations here
+
+            if node.type == "cross join" and node.unnest_column:
+                # if it's a CROSS JOIN UNNEST - don't try to push any further
+                # IMPROVE: we should push everything that doesn't reference the unnested column
+                context = self._handle_predicates(node, context)
+            elif node.type in ("cross join", "inner"):
+                # we may be able to rewrite as an inner join
+                remaining_predicates = []
+                for predicate in context.collected_predicates:
+                    if len(predicate.relations) == 2 and set(
+                        node.right_relation_names + node.left_relation_names
+                    ) == set(predicate.relations):
+                        node.type = "inner"
+                        node.on = _add_condition(node.on, predicate.condition)
+                    else:
+                        remaining_predicates.append(predicate)
+
+                    new_left_columns, new_right_columns = extract_join_fields(
+                        node.on, node.left_relation_names, node.right_relation_names
+                    )
+
+                    if not node.right_columns:
+                        node.right_columns = []
+                    node.right_columns.extend(new_right_columns)
+                    if not node.left_columns:
+                        node.left_columns = []
+                    node.left_columns.extend(new_left_columns)
+
+                mismatches = get_mismatched_condition_column_types(node.on)
+                if mismatches:
+                    from opteryx.exceptions import IncompatibleTypesError
+
+                    raise IncompatibleTypesError(**mismatches)
+                node.columns = get_all_nodes_of_type(node.on, (NodeType.IDENTIFIER,))
+                context.collected_predicates = remaining_predicates
+
+            else:
+                # IMPROVE, allow pushing past OUTER, SEMI, ANTI joins on one leg
+                for predicate in context.collected_predicates:
+                    context.optimized_plan.insert_node_after(
+                        random_string(), predicate, context.node_id
+                    )
+                context.collected_predicates = []
+
+            for predicate in context.collected_predicates:
+                remaining_predicates = []
+                for predicate in context.collected_predicates:
+                    if len(predicate.relations) == 2 and set(
+                        node.right_relation_names + node.left_relation_names
+                    ) == set(predicate.relations):
+                        node.condition = _add_condition(node.condition, predicate)
+                    else:
+                        remaining_predicates.append(predicate)
+                context.collected_predicates = remaining_predicates
+
+        # DEBUG: log (context.optimized_plan.draw())
+        context.optimized_plan.add_node(context.node_id, node)
+        return context
+
+    def complete(self, plan: LogicalPlan, context: HeuristicOptimizerContext) -> LogicalPlan:
+        # anything we couldn't push, we need to put back
+        if context.collected_predicates:
+            context.collected_predicates.reverse()
+            exit_node_id = context.optimized_plan.get_exit_points()[0]
+            for predicate in context.collected_predicates:
+                context.optimized_plan.insert_node_before(random_string(), predicate, exit_node_id)
+        return context.optimized_plan
+
+    def _handle_predicates(
+        self, node: LogicalPlanNode, context: HeuristicOptimizerContext
+    ) -> HeuristicOptimizerContext:
+        remaining_predicates = []
+        for predicate in context.collected_predicates:
+            if len(predicate.relations) == 1 and predicate.relations.intersection(
+                (node.relation, node.alias)
+            ):
+                context.optimized_plan.insert_node_after(
+                    random_string(), predicate, context.node_id
+                )
+                continue
+            remaining_predicates.append(predicate)
+        context.collected_predicates = remaining_predicates
+        return context
---
+++ packages/opteryx/opteryx-0.12.0b8-cp39-cp39-macosx_10_15_universal2.whl/opteryx/connectors/sql_connector.py
@@ -0,0 +1,144 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+
+"""
+from decimal import Decimal
+
+from orso import DataFrame
+from orso.schema import FlatColumn
+from orso.schema import RelationSchema
+from orso.types import PYTHON_TO_ORSO_MAP
+
+from opteryx.connectors.base.base_connector import DEFAULT_MORSEL_SIZE
+from opteryx.connectors.base.base_connector import INITIAL_CHUNK_SIZE
+from opteryx.connectors.base.base_connector import BaseConnector
+from opteryx.exceptions import MissingDependencyError
+from opteryx.exceptions import UnmetRequirementError
+from opteryx.third_party.query_builder import Query
+
+
+class SqlConnector(BaseConnector):
+    __mode__ = "Sql"
+
+    def __init__(self, *args, connection: str = None, engine=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            from sqlalchemy import MetaData
+            from sqlalchemy import create_engine
+        except ImportError as err:  # pragma: nocover
+            raise MissingDependencyError(err.name) from err
+
+        if engine is None and connection is None:
+            raise UnmetRequirementError(
+                "SQL Connections require either a SQL Alchemy connection string in the 'connection' parameter, or a SQL Alchemy Engine in the 'engine' parameter."
+            )
+
+        # create the SqlAlchemy engine
+        if engine is None:
+            self._engine = create_engine(connection)
+        else:
+            self._engine = engine
+
+        self.schema = None
+        self.metadata = MetaData()
+
+    def read_dataset(
+        self, columns: list = None, chunk_size: int = INITIAL_CHUNK_SIZE
+    ) -> "DatasetReader":
+        from sqlalchemy.sql import text
+
+        self.chunk_size = chunk_size
+        result_schema = self.schema
+
+        query_builder = Query().FROM(self.dataset)
+
+        # if we're projecting, update the SQL and the target morsel schema
+        if columns:
+            column_names = [col.name for col in columns]
+            query_builder.add("SELECT", *column_names)
+            result_schema.columns = [col for col in self.schema.columns if col.name in column_names]
+        else:
+            query_builder.add("SELECT", "*")
+
+        morsel = DataFrame(schema=result_schema)
+
+        with self._engine.connect() as conn:
+            # DEBUG: log ("READ DATASET\n", str(query_builder))
+            for row in conn.execute(text(str(query_builder))):
+                morsel._rows.append(row)
+                if len(morsel) == self.chunk_size:
+                    yield morsel.arrow()
+
+                    if morsel.nbytes > 0:
+                        self.chunk_size = int(len(morsel) // (morsel.nbytes / DEFAULT_MORSEL_SIZE))
+
+                    morsel = DataFrame(schema=result_schema)
+
+        if len(morsel) > 0:
+            yield morsel.arrow()
+
+    def get_dataset_schema(self) -> RelationSchema:
+        from sqlalchemy import Table
+
+        if self.schema:
+            return self.schema
+
+        # Try to read the schema from the metastore
+        self.schema = self.read_schema_from_metastore()
+        if self.schema:
+            return self.schema
+
+        # get the schema from the dataset
+        # DEBUG: log ("GET SQL SCHEMA:", self.dataset)
+        try:
+            table = Table(self.dataset, self.metadata, autoload_with=self._engine)
+
+            self.schema = RelationSchema(
+                name=table.name,
+                columns=[
+                    FlatColumn(
+                        name=column.name,
+                        type=PYTHON_TO_ORSO_MAP[column.type.python_type],
+                        precision=None
+                        if column.type.python_type != Decimal
+                        else column.type.precision,
+                        scale=None if column.type.python_type != Decimal else column.type.scale,
+                        nullable=column.nullable,
+                    )
+                    for column in table.columns
+                ],
+            )
+        except Exception as err:
+            # Fall back to getting the schema from the first row, this is the column names, and where
+            # possible, column types.
+            # DEBUG: log (f"APPROXIMATING SCHEMA OF {self.dataset} BECAUSE OF {type(err).__name__}({err})")
+            from sqlalchemy.sql import text
+
+            with self._engine.connect() as conn:
+                query = Query().SELECT("*").FROM(self.dataset).LIMIT("1")
+                # DEBUG: log ("READ ROW\n", str(query))
+                row = conn.execute(text(str(query))).fetchone()._asdict()
+                # DEBUG: log ("ROW:", row)
+                self.schema = RelationSchema(
+                    name=self.dataset,
+                    columns=[
+                        FlatColumn(
+                            name=column,
+                            type=0 if value is None else PYTHON_TO_ORSO_MAP[type(value)],
+                        )
+                        for column, value in row.items()
+                    ],
+                )
+
+        return self.schema
