a6623d17f0e867c0a1d8dcc7802959070210a867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:03:23 +0000

    Add package execdata/5.2.0/execdata-5.2.0.tar.gz

---
+++ packages/execdata/execdata-5.2.0.tar.gz/execdata-5.2.0/execdata/format/__init__.py
@@ -0,0 +1,16 @@
+'''
+Date         : 2023-11-02 17:34:28
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2023-11-30 12:15:50
+LastEditors  : BDFD
+Description  : 
+FilePath     : \execdata\format\__init__.py
+Copyright (c) 2023 by BDFD, All Rights Reserved. 
+'''
+from ._data_conversion import convint
+from ._data_conversion import convfloat
+
+from ._format_data import fill_zeros
+from ._format_data import concat_transformed_df
+from ._format_data import concat_inversed_df
---
+++ packages/execdata/execdata-5.2.0.tar.gz/execdata-5.2.0/execdata/format/_data_conversion.py
@@ -0,0 +1,19 @@
+'''
+Date         : 2023-01-17 10:33:06
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2023-01-17 16:00:00
+LastEditors  : BDFD
+Description  : 
+FilePath     : \execdata\data_conv.py
+Copyright (c) 2023 by BDFD, All Rights Reserved. 
+'''
+def convint(str):
+    result = None
+    result = int(str)
+    return result
+
+def convfloat(str):
+    result = None
+    result = float(str)
+    return result
