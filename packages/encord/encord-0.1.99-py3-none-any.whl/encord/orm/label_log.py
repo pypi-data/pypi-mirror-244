f982657573b4049ef1dbae02f3bc087ac1a23a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 03:03:45 +0000

    Add package encord/0.1.99/encord-0.1.99-py3-none-any.whl

---
+++ packages/encord/encord-0.1.99-py3-none-any.whl/encord/orm/label_log.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import datetime
+from enum import IntEnum
+from typing import Optional
+
+from encord.orm.base_dto import BaseDTO
+
+
+@dataclass(frozen=True)
+class LabelLog:
+    log_hash: str
+    user_hash: str
+    user_email: str
+    annotation_hash: str  # Legacy value. Replaced by identifier.
+    identifier: str
+    data_hash: str
+    feature_hash: str
+    action: Action
+    label_name: str
+    time_taken: int
+    created_at: datetime
+    frame: int
+
+
+class Action(IntEnum):
+    ADD = 0
+    EDIT = 1
+    DELETE = 2
+    START = 3
+    END = 4
+    MARK_AS_NOT_LABELLED = 5
+    MARK_AS_IN_PROGRESS = 6
+    MARK_AS_LABELLED = 7
+    MARK_AS_REVIEW_REQUIRED = 8
+    MARK_AS_REVIEWED = 9
+    MARK_AS_REVIEWED_TWICE = 10
+    SUBMIT_TASK = 11
+    APPROVE_LABEL = 12
+    REJECT_LABEL = 13
+    CLICK_SAVE = 14
+    CLICK_UNDO = 15
+    CLICK_REDO = 16
+    CLICK_BULK = 17
+    CLICK_ZOOM = 19
+    CLICK_BRIGHTNESS = 20
+    CLICK_HOTKEYS = 21
+    CLICK_SETTINGS = 22
+    ADD_ATTRIBUTE = 23
+    EDIT_ATTRIBUTE = 24
+    DELETE_ATTRIBUTE = 25
+    APPROVE_NESTED_ATTRIBUTE = 26
+    REJECT_NESTED_ATTRIBUTE = 27
+    SUBMIT_LABEL = 28
+    SUBMIT_NESTED_ATTRIBUTE = 29
+    BUFFERING_OVERLAY_SHOWN = 30
+    BITRATE_WARNING_SHOWN = 31
+    SEEKING_OVERLAY_SHOWN = 32
+
+
+class LabelLogParams(BaseDTO):
+    user_hash: Optional[str]
+    data_hash: Optional[str]
+    start_timestamp: Optional[int]
+    end_timestamp: Optional[int]
+    user_email: Optional[str]
+    # Flag for backwards compatibility
+    include_user_email_and_interface_key: bool = True
