114679a54bbdf0aada69fffcaabfdcd9de58ee14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 03:06:17 +0000

    Add package mds-cashbook-bookcategory/6.8.4/mds_cashbook_bookcategory-6.8.4.tar.gz

---
+++ packages/mds-cashbook-bookcategory/mds_cashbook_bookcategory-6.8.4.tar.gz/mds_cashbook_bookcategory-6.8.4/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# This file is part of the cashbook-module from m-ds for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the
+# full copyright notices and license terms.
+
+from trytond.pool import Pool
+from .category import Category
+from .book import CategoryCashbookRel, Cashbook
+
+
+def register():
+    Pool.register(
+        Category,
+        Cashbook,
+        CategoryCashbookRel,
+        module='cashbook_bookcategory', type_='model')
