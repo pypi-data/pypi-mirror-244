c3a67eeb1bea056ed269779ce83394fd3d3b99c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 04:04:46 +0000

    Add package mds-party-notes/7.0.1/mds_party_notes-7.0.1.tar.gz

---
+++ packages/mds-party-notes/mds_party_notes-7.0.1.tar.gz/mds_party_notes-7.0.1/tests/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# This file is part of the party-notes module for Tryton from m-ds.de.
+# The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
