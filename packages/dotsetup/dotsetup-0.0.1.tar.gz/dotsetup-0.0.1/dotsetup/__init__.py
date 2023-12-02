4f61196d5ebdaaee384796f328bfb11640edf1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 03:03:52 +0000

    Add package dotsetup/0.0.1/dotsetup-0.0.1.tar.gz

---
+++ packages/dotsetup/dotsetup-0.0.1.tar.gz/dotsetup-0.0.1/dotsetup/__init__.py
@@ -0,0 +1,10 @@
+"""
+__init__.py
+
+
+This module initializes the dotsetup package
+"""
+
+from dotsetup.parse import DotSetup
+from dotsetup.exception import DotSetupException, FileNotFoundError, VariableNotFoundError, JSONDecodeError
+
