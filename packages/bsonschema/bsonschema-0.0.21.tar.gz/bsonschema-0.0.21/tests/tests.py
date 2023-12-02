cc1b2fea9af2035c228048a0a79d3531347c9d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:04:09 +0000

    Add package bsonschema/0.0.21/bsonschema-0.0.21.tar.gz

---
+++ packages/bsonschema/bsonschema-0.0.21.tar.gz/bsonschema-0.0.21/tests/tests.py
@@ -0,0 +1,35 @@
+import bson 
+import json
+from bsonschema import Draft3Validator
+
+# Parse BSON file to dict.
+# returns: a decoded 'dict' parsed from the file.
+def read_bson_from_disk(filepath: str) -> dict:
+    contents = open(filepath, "rb").read()
+
+    # Handle null case
+    if len(contents) == 0:
+        return dict()
+
+    # Determine if the document is binary encoded. This is possible because ASCII encoded
+    # docs will typically not have null chars (or any chars < 10 on the ASCII table).
+    for c in contents:
+        if c < 10:
+            return bson.BSON.decode(
+                contents, bson.CodecOptions(unicode_decode_error_handler="ignore")
+            )
+
+    # Else, this doc is ASCII-encoded (an older format), and should be
+    # parsed using 'json.loads()' and converted to BSON.
+    return bson.BSON.decode(
+        bson.BSON.encode(json.loads(contents)),
+        bson.CodecOptions(unicode_decode_error_handler="ignore"),
+    )
+
+if __name__ == "__main__":
+    try:
+        inst = read_bson_from_disk("./test_bson_for_validation.bson")
+        valid = Draft3Validator(json.load(open("./mdb_schema.json", "r")))
+        valid.validate(inst)
+    except Exception as e:
+        print(str(e))
