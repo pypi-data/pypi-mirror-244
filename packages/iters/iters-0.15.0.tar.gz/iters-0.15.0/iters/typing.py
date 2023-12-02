8ebed888bc918161b3c1bebd44b30f5d26fec732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 06:03:37 +0000

    Add package iters/0.15.0/iters-0.15.0.tar.gz

---
+++ packages/iters/iters-0.15.0.tar.gz/iters-0.15.0/iters/typing.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from abc import abstractmethod as required
+from typing import Protocol, TypeVar, runtime_checkable
+
+__all__ = ("Sum", "Product")
+
+S = TypeVar("S", bound="Sum")
+
+
+@runtime_checkable
+class Sum(Protocol):
+    """Represents types for which adding `self: S` to `other: S` returns `S`."""
+
+    @required
+    def __add__(self: S, __other: S) -> S:
+        raise NotImplementedError
+
+
+P = TypeVar("P", bound="Product")
+
+
+@runtime_checkable
+class Product(Protocol):
+    """Represents types for which multiplying `self: P` with `other: P` returns `P`."""
+
+    @required
+    def __mul__(self: P, __other: P) -> P:
+        raise NotImplementedError
