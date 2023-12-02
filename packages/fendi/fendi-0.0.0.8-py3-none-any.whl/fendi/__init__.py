c69dd874acc57df4d4708de14efcd754c2991fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 03:05:31 +0000

    Add package fendi/0.0.0.8/fendi-0.0.0.8-py3-none-any.whl

---
+++ packages/fendi/fendi-0.0.0.8-py3-none-any.whl/fendi/__init__.py
@@ -0,0 +1,5 @@
+def main():
+    """Entry point for the application script"""
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("I am Fedi HAMDI, currently living in France. I get bored sometimes, so I code. Feel free to give me a call; we can hang out.")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
