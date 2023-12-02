2822cef3f8ff2ba5ff3cd923e74d5414ee87ea46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 15:04:44 +0000

    Add package map-nl/0.0.4/map_nl-0.0.4-py3-none-any.whl

---
+++ packages/map-nl/map_nl-0.0.4-py3-none-any.whl/map_nl/foo.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+
+def foo() -> str:
+    """Summary line.
+
+    Extended description of function.
+
+    Args:
+        foo (str): Description of arg1
+
+    Returns:
+        str: Description of return value
+    """
+
+    return "foo"
+
+
+if __name__ == "__main__":  # pragma: no cover
+    pass
