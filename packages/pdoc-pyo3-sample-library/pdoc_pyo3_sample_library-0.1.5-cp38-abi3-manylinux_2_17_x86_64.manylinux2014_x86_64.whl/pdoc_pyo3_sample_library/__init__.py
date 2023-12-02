a4f0d43928f27ae6fa62920bf660f9a7ad389328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 03:06:13 +0000

    Add package pdoc-pyo3-sample-library/0.1.5/pdoc_pyo3_sample_library-0.1.5-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pdoc-pyo3-sample-library/pdoc_pyo3_sample_library-0.1.5-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pdoc_pyo3_sample_library/__init__.py
@@ -0,0 +1,5 @@
+from .pdoc_pyo3_sample_library import *
+
+__doc__ = pdoc_pyo3_sample_library.__doc__
+if hasattr(pdoc_pyo3_sample_library, "__all__"):
+    __all__ = pdoc_pyo3_sample_library.__all__
