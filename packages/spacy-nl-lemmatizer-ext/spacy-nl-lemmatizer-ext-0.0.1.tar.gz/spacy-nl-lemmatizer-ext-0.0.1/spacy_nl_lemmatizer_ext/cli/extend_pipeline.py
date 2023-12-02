e4603a96393a91643bb5892da8176629b6b465dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 03:03:46 +0000

    Add package spacy-nl-lemmatizer-ext/0.0.1/spacy-nl-lemmatizer-ext-0.0.1.tar.gz

---
+++ packages/spacy-nl-lemmatizer-ext/spacy-nl-lemmatizer-ext-0.0.1.tar.gz/spacy-nl-lemmatizer-ext-0.0.1/spacy_nl_lemmatizer_ext/cli/extend_pipeline.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from typing import Dict
+
+import srsly
+from radicli import Arg
+import spacy
+
+from . import cli
+
+
+@cli.command(
+    "extend-pipeline",
+    model=Arg(help="spaCy model to extend"),
+    lexicon=Arg(help="Lexicon file"),
+    model_output=Arg(help="Extended spaCy model"),
+)
+def extend_pipeline(model: str, lexicon: Path, model_output: Path):
+    """Add the GiGaNT lemmatizer to a pipeline"""
+    nlp = spacy.load(model)
+    lexicon: Dict[str, Dict[str, str]] = srsly.read_json(lexicon)
+    lemmatizer = nlp.add_pipe("gigant_lemmatizer")
+    lemmatizer.initialize(lexicon=lexicon)
+    nlp.to_disk(model_output)
