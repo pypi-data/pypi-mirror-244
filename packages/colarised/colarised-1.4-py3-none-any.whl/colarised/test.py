e86bd5209d5651f2c659e989849bf1ac3e17b621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:04:09 +0000

    Add package colarised/1.4/colarised-1.4-py3-none-any.whl

---
+++ packages/colarised/colarised-1.4-py3-none-any.whl/colarised/test.py
@@ -0,0 +1,88 @@
+#https://github.com/kittyplot/hexcolurs
+#from hexcolurs import *
+#u can use it like this
+
+#red,blue,orange,etc
+
+#from hexcolurs import Colors, Colorate
+
+#print(Colorate.Horizontal(Colors.yellow_to_red, "Hello", 1))
+
+#from hexcolurs import Colors, Colorate
+
+#text = "Hello world!"
+
+#print(Colors.blue + text)
+
+
+
+
+#from hexcolurs import Write, Colors
+
+#name = Write.Input("Enter your name -> ", Colors.red_to_purple, interval=0.0025)
+#Write.Print(f"Nice to meet you, {name}!", Colors.blue_to_green, interval=0.05)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
