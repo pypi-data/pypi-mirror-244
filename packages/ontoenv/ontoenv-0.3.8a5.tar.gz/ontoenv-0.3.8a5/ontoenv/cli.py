d800d6bb60dae7a8bf36721e9ee54f0efa79dadf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:04:11 +0000

    Add package ontoenv/0.3.8a5/ontoenv-0.3.8a5.tar.gz

---
+++ packages/ontoenv/ontoenv-0.3.8a5.tar.gz/ontoenv-0.3.8a5/ontoenv/cli.py
@@ -0,0 +1,67 @@
+import sys
+import click
+import logging
+from ontoenv import OntoEnv
+import networkx as nx
+
+
+@click.group(help="Manage ontology definition mappings")
+@click.option("-v", is_flag=True)
+def i(v):
+    if v:
+        logging.basicConfig(level=logging.INFO)
+
+
+@i.command(help="Initializes .ontoenv in the current directory")
+@click.option("-v", help="Verbose output", is_flag=True)
+@click.option("-s", help="Strict mode (error on missing ontologies)", is_flag=True)
+def init(v, s):
+    if v:
+        logging.basicConfig(level=logging.INFO)
+    OntoEnv(initialize=True, strict=s)
+
+
+@i.command(help="Rebuilds the .ontoenv cache and mapping in the current directory")
+@click.option("-v", help="Verbose output", is_flag=True)
+@click.option("-s", help="Strict mode (error on missing ontologies)", is_flag=True)
+def refresh(v, s):
+    if v:
+        logging.basicConfig(level=logging.INFO)
+    oe = OntoEnv(initialize=False, strict=s)
+    oe.refresh()
+
+
+@i.command(help="Print mapping of ontology URI => filename!")
+@click.option("-v", help="Verbose output", is_flag=True)
+@click.option("-s", help="Strict mode (error on missing ontologies)", is_flag=True)
+def dump(v, s):
+    if v:
+        logging.basicConfig(level=logging.INFO)
+    oe = OntoEnv(initialize=False, strict=s)
+    for ontology, filename in oe.mapping.items():
+        print(f"{ontology} => {filename}")
+
+
+@i.command(help="Output dependency graph")
+@click.argument("output_filename", default="dependencies.pdf")
+def output(output_filename):
+    try:
+        import matplotlib.pyplot as plt
+    except ImportError:
+        logging.error("Could not import matplotlib; please install and try again")
+        sys.exit(1)
+    oe = OntoEnv(initialize=False)
+    pos = nx.spring_layout(oe._dependencies, 2)
+    nx.draw_networkx(oe._dependencies, pos=pos, with_labels=True)
+    plt.savefig(output_filename)
+
+
+@i.command(help="Print dependency graph")
+@click.argument("root_uri", default="")
+def deps(root_uri):
+    oe = OntoEnv(initialize=False)
+    oe.print_dependency_graph(root_uri)
+
+
+if __name__ == "__main__":
+    i()
