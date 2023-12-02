1722190c0232123c9f032087f8f4d98c66340abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 15:03:36 +0000

    Add package openj/0.1.0/openj-0.1.0-py3-none-any.whl

---
+++ packages/openj/openj-0.1.0-py3-none-any.whl/openj/schema.sql
@@ -0,0 +1,29 @@
+-- Initialize the database.
+-- Drop any existing data and create empty tables.
+
+DROP TABLE IF EXISTS lane;
+DROP TABLE IF EXISTS card;
+
+CREATE TABLE lane (
+        id INTEGER PRIMARY KEY,
+        created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at DATETIME DEFAULT NULL,
+        title TEXT UNIQUE NOT NULL,
+        slug TEXT UNIQUE NOT NULL
+);
+
+INSERT INTO lane (slug, title) VALUES
+        ("backlog", "Backlog"),
+        ("doing", "Doing"),
+        ("review", "Review"),
+        ("done", "Done");
+
+CREATE TABLE card (
+        id INTEGER PRIMARY KEY,
+        created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at DATETIME DEFAULT NULL,
+        title TEXT UNIQUE NOT NULL,
+        description TEXT NOT NULL,
+        lane_id INTEGER NOT NULL,
+        FOREIGN KEY(lane_id) REFERENCES lane(id) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
