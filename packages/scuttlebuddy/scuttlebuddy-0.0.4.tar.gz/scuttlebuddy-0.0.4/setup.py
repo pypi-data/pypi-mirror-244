c843951c374b0cce5871b90450ed8b0914561cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:03:20 +0000

    Add package scuttlebuddy/0.0.4/scuttlebuddy-0.0.4.tar.gz

---
+++ packages/scuttlebuddy/scuttlebuddy-0.0.4.tar.gz/scuttlebuddy-0.0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import pathlib
+
+setup(
+    name='scuttlebuddy',
+    version='0.0.4',
+    requires=[],
+    description="External Scripting Framework for developers who play League of Legends.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://discord.gg/huywRMDEmE",
+    author="Business",
+    author_email="",
+    license="MIT License",
+    project_urls={
+        "Discord": "https://discord.gg/huywRMDEmE"
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11"
+    ],
+    python_requires=">=3.10,<3.12",
+    install_requires=[
+        "requests",
+        "numpy",
+        "pywin32",
+        "orjson"
+    ],
+    extras_require={},
+    packages=find_packages(),
+    include_package_data=True
+)
