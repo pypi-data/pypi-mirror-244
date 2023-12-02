b66a5c3dd2868b2e6330a8ab7c602e2583e82cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:04:02 +0000

    Add package async-pyarrow/0.2.0/async_pyarrow-0.2.0.tar.gz

---
+++ packages/async-pyarrow/async_pyarrow-0.2.0.tar.gz/async_pyarrow-0.2.0/README.md
@@ -0,0 +1,21 @@
+# Simple Arrow Flight Asyncio Client 
+
+currently, [pyarrow](https://arrow.apache.org/docs/python/index.html) package only supports sync api, at least in terms of `do_get`. In order to use pyarrow flight in an asyncio application, we will have to use the `concurrent.futures.ThreadPoolExecutor`. 
+
+`async_pyarrow` adds asyncio support for `do_get`.
+
+```python
+    from async_pyarrow.flight import connect as async_connect
+    client = await async_connect(f'localhost:50051')
+    get_call = client.do_get(flight.Ticket(ticket=b"test"))
+    table = await get_call.read_all()
+```
+
+
+caveat: the code assumes the format of the arrow flight tables, hence will break if the format ever changes. 
+
+## install 
+
+```cmd
+pip install async_pyarrow
+```
---
+++ packages/async-pyarrow/async_pyarrow-0.2.0.tar.gz/async_pyarrow-0.2.0/tests/test_flight.py
@@ -0,0 +1,29 @@
+import pytest
+from async_pyarrow.flight import connect as async_connect
+import pyarrow as pa
+from pyarrow import flight
+import pandas as pd
+
+
+class ExampleServer(flight.FlightServerBase):
+    def do_get(self, context, ticket):
+        data = [{'column1': 1, 'column2': 1.1},
+                {'column1': 2, 'column2': 2.2},
+                {'column1': 3, 'column2': 3.3}]
+        table = pa.Table.from_pandas(pd.DataFrame(data))
+        return flight.RecordBatchStream(table)
+
+
+@pytest.fixture
+def flight_server():
+   with ExampleServer() as server:
+        yield server
+
+@pytest.mark.asyncio
+async def test_async_flight_client(flight_server):
+    # Use the client and server in the same test
+    client = await async_connect(f'localhost:{flight_server.port}')
+    get_call = client.do_get(flight.Ticket(ticket=b"test"))
+    table = await get_call.read_all()
+    assert table.num_rows == 3
+    assert table.num_columns == 2
