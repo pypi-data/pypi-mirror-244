e43695f23ab2875c62f644ba875baf059c6ae389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 04:03:42 +0000

    Add package text-to-sql/0.0.5/text_to_sql-0.0.5.tar.gz

---
+++ packages/text-to-sql/text_to_sql-0.0.5.tar.gz/text_to_sql-0.0.5/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+with open('./README.md', 'r') as file:
+    long_description = file.read()
+
+setup(
+    name="text_to_sql",
+    version="0.0.5",
+    description="""
+    A Python package which converts natural language text to PostgreSQL commands
+    based on provided database schema
+    """,
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AI-Camp-Sprouts-AI/NaturalLangToSQL",
+    author="""
+    Vignesh Prakash (pranomvignesh)
+    Sullivan Dovie - sullivandovie@gmail.com
+    Elhanan Wong - wong.elhanan@gmail.com
+    Ayana Gaur -  ayanagaur2@gmail.com
+    Mikhael Gonzalez - mikhael.gonzalez@gmail.com
+    Elizabeth Petit - elizavetapetit@gmail.com
+    """,
+    author_email="pranomvignesh@gmail.com",
+    license="MIT",
+    install_requires=[
+        'langchain',
+        'langchain-experimental',
+        'openai',
+        'python-dotenv',
+        'psycopg2-binary',
+        'InquirerPy',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-check',
+            'pytest-html',
+            'tqdm',
+            'faker',
+            'twine'
+        ]
+    },
+    entry_points="""
+    [console_scripts]
+        text_to_sql-example=src.text_to_sql:run_sample_in_terminal
+        text_to_sql-test=src.text_to_sql:run_test_suites
+        text_to_sql-mock=src.text_to_sql:run_mock_data_generator
+    """
+)
