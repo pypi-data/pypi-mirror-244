a32c9e5d391d246eb97b6ae5828c38ba882afe4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 15:03:19 +0000

    Add package render-engine-microblog/2023.12.1b1/render_engine_microblog-2023.12.1b1-py3-none-any.whl

---
+++ packages/render-engine-microblog/render_engine_microblog-2023.12.1b1-py3-none-any.whl/render_engine_microblog/collection.py
@@ -0,0 +1,27 @@
+from render_engine.blog import Blog
+from render_engine.page import Page
+from render_engine.parsers.markdown import MarkdownPageParser
+
+from .themes import microblog_theme
+
+
+class MicroBlogPost(Page):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.content = self._content
+
+    @property
+    def _slug(self):
+        return self.date.strftime("%Y%m%d%H%M")
+
+    @property
+    def _title(self):
+        return ""
+
+
+class MicroBlog(Blog):
+    required_themes = [microblog_theme]
+    PageParser = MarkdownPageParser
+    content_type = MicroBlogPost
+    archive_template = "microblog.html"
+    has_archive = True
