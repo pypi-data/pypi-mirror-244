a088d0b63bbafccd37257603f371e58a83aa9ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 06:03:31 +0000

    Add package SigProfilerMatrixGenerator/1.2.20/SigProfilerMatrixGenerator-1.2.20-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.20-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.20'
+version = '1.2.20'
+Update = 'v1.2.20: Resolve path installation issue for Windows'
+
+	
