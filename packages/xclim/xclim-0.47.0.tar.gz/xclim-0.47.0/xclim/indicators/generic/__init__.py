f5afbcc1d0733da6dae76e69519ede60e5c28c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:03:44 +0000

    Add package xclim/0.47.0/xclim-0.47.0.tar.gz

---
+++ packages/xclim/xclim-0.47.0.tar.gz/xclim-0.47.0/xclim/indicators/generic/__init__.py
@@ -0,0 +1,7 @@
+"""
+Generic Indicators
+==================
+"""
+from __future__ import annotations
+
+from ._stats import *
---
+++ packages/xclim/xclim-0.47.0.tar.gz/xclim-0.47.0/xclim/indicators/land/__init__.py
@@ -0,0 +1,8 @@
+"""
+Land Indicators
+===============
+"""
+from __future__ import annotations
+
+from ._snow import *
+from ._streamflow import *
