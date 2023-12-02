e5c4e95959959a645d27e58b5ffe2917b42868b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 15:03:50 +0000

    Add package transmute-core/1.13.7/transmute_core-1.13.7-py3-none-any.whl

---
+++ packages/transmute-core/transmute_core-1.13.7-py3-none-any.whl/transmute_core/frameworks/tornado/__init__.py
@@ -0,0 +1,7 @@
+from transmute_core import *
+
+# from .handler import convert_to_handler
+# from .route import route
+from .route_set import RouteSet
+from .url import url_spec
+from .swagger import add_swagger
