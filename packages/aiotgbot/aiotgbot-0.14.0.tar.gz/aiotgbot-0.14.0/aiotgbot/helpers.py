c5a7e3712940b3dbf8f595ca8e22128c1be8bcea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:03:21 +0000

    Add package aiotgbot/0.14.0/aiotgbot-0.14.0.tar.gz

---
+++ packages/aiotgbot/aiotgbot-0.14.0.tar.gz/aiotgbot-0.14.0/aiotgbot/helpers.py
@@ -0,0 +1,51 @@
+import asyncio
+from contextlib import asynccontextmanager
+from typing import Any, AsyncIterator, Final, Hashable
+from weakref import WeakValueDictionary
+
+import msgspec.json
+from aiohttp import web
+
+__all__ = (
+    "BotKey",
+    "KeyLock",
+    "get_python_version",
+    "get_software",
+    "json_dumps",
+)
+
+
+def json_dumps(obj: Any) -> str:
+    return msgspec.json.encode(obj).decode()
+
+
+def get_python_version() -> str:
+    from sys import version_info as version
+
+    return f"{version.major}.{version.minor}.{version.micro}"
+
+
+def get_software() -> str:
+    from . import __version__
+
+    return f"Python/{get_python_version()} aiotgbot/{__version__}"
+
+
+class KeyLock:
+    def __init__(self) -> None:
+        self._locks: (
+            "Final[WeakValueDictionary[Hashable," "asyncio.Lock]]"
+        ) = WeakValueDictionary()
+
+    @asynccontextmanager
+    async def resource(self, key: Hashable) -> AsyncIterator[None]:
+        if key not in self._locks:
+            lock = asyncio.Lock()
+            self._locks[key] = lock
+        else:
+            lock = self._locks[key]
+        async with lock:
+            yield
+
+
+BotKey = web.AppKey
