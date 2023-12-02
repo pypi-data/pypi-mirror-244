f477a9930b4432840610d305820619a5415039ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 15:03:35 +0000

    Add package render-engine-theme-kjaymiller/2023.12.1a2/render-engine-theme-kjaymiller-2023.12.1a2.tar.gz

---
+++ packages/render-engine-theme-kjaymiller/render-engine-theme-kjaymiller-2023.12.1a2.tar.gz/render-engine-theme-kjaymiller-2023.12.1a2/tests/test_blog_html.py
@@ -0,0 +1,5 @@
+
+
+def test_blog_title_class_is_title(site):
+    print(list(site.output_path.joinpath("blog").iterdir()))
+    assert site.output_path.joinpath("blog", "test-blog-post.html").read_text().find('class="title"')
