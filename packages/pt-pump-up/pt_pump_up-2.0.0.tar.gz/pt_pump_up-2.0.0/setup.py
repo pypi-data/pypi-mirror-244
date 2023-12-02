f8958d2503a81dc2348f606094d35fc879e7457c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:03:15 +0000

    Add package pt-pump-up/2.0.0/pt_pump_up-2.0.0.tar.gz

---
+++ packages/pt-pump-up/pt_pump_up-2.0.0.tar.gz/pt_pump_up-2.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pt_pump_up',
+    version='2.0.0',
+    description='Hub for Portuguese NLP resources',
+    install_requires=['beanie', 'mechanize'],
+    packages=find_packages(),
+    author='Rúben Almeida'
+)
