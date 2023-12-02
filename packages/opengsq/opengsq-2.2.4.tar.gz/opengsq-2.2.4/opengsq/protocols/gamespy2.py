a994196466779d178d8ef6af41248be2303ecad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 14:04:18 +0000

    Add package opengsq/2.2.4/opengsq-2.2.4.tar.gz

---
+++ packages/opengsq/opengsq-2.2.4.tar.gz/opengsq-2.2.4/opengsq/protocols/gamespy2.py
@@ -0,0 +1,132 @@
+from enum import Flag, auto
+
+from opengsq.binary_reader import BinaryReader
+from opengsq.protocol_base import ProtocolBase
+from opengsq.socket_async import SocketAsync
+
+
+class GameSpy2(ProtocolBase):
+    """GameSpy Protocol version 2"""
+    full_name = 'GameSpy Protocol version 2'
+
+    class Request(Flag):
+        INFO = auto()
+        PLAYERS = auto()
+        TEAMS = auto()
+
+    async def get_status(self, request: Request = Request.INFO | Request.PLAYERS | Request.TEAMS) -> dict:
+        """Retrieves information about the server including, Info, Players, and Teams."""
+        # Connect to remote host
+        with SocketAsync() as sock:
+            sock.settimeout(self._timeout)
+            await sock.connect((self._host, self._port))
+
+            # Send Request
+            sock.send(b'\xFE\xFD\x00\x04\x05\x06\x07' + self.__get_request_bytes(request))
+
+            # Server response
+            response = await sock.recv()
+
+        # Remove the first 5 bytes { 0x00, 0x04, 0x05, 0x06, 0x07 }
+        br = BinaryReader(response[5:])
+
+        status = {}
+
+        if self.__has_flag(request, self.Request.INFO):
+            status['info'] = self.__get_info(br)
+
+        if self.__has_flag(request, self.Request.PLAYERS):
+            status['players'] = self.__get_players(br)
+
+        if self.__has_flag(request, self.Request.TEAMS):
+            status['teams'] = self.__get_teams(br)
+
+        return status
+
+    def __get_request_bytes(self, request: Request):
+        request_bytes = self.__has_flag(request, self.Request.INFO) and b'\xFF' or b'\x00'
+        request_bytes += self.__has_flag(request, self.Request.PLAYERS) and b'\xFF' or b'\x00'
+        request_bytes += self.__has_flag(request, self.Request.TEAMS) and b'\xFF' or b'\x00'
+
+        return request_bytes
+
+    def __has_flag(self, request, flag) -> bool:
+        return request & flag == flag
+
+    def __get_info(self, br: BinaryReader) -> dict:
+        info = {}
+
+        # Read all key values
+        while br.length() > 0:
+            key = br.read_string()
+
+            if key == '':
+                break
+
+            info[key] = br.read_string().strip()
+
+        return info
+
+    def __get_players(self, br: BinaryReader) -> list:
+        players = []
+
+        # Skip a byte
+        br.read_byte()
+
+        # Get player count
+        player_count = br.read_byte()
+
+        # Get all keys
+        keys = []
+
+        while br.length() > 0:
+            key = br.read_string()
+
+            if key == '':
+                break
+
+            keys.append(key.rstrip('_'))
+
+        # Set all keys and values
+        for _ in range(player_count):
+            players.append({key: br.read_string().strip() for key in keys})
+
+        return players
+
+    def __get_teams(self, br: BinaryReader) -> list:
+        teams = []
+
+        # Skip a byte
+        br.read_byte()
+
+        # Get team count
+        team_count = br.read_byte()
+
+        # Get all keys
+        keys = []
+
+        while br.length() > 0:
+            key = br.read_string()
+
+            if key == '':
+                break
+
+            keys.append(key.rstrip('t').rstrip('_'))
+
+        # Set all keys and values
+        for _ in range(team_count):
+            teams.append({key: br.read_string().strip() for key in keys})
+
+        return teams
+
+
+if __name__ == '__main__':
+    import asyncio
+    import json
+
+    async def main_async():
+        gs2 = GameSpy2(host='158.69.118.94', port=23000, timeout=5.0)
+        status = await gs2.get_status()
+        print(json.dumps(status, indent=None) + '\n')
+
+    asyncio.run(main_async())
---
+++ packages/opengsq/opengsq-2.2.4.tar.gz/opengsq-2.2.4/opengsq/protocols/quake1.py
@@ -0,0 +1,125 @@
+import re
+
+from opengsq.binary_reader import BinaryReader
+from opengsq.protocol_base import ProtocolBase
+from opengsq.socket_async import SocketAsync
+
+
+class Quake1(ProtocolBase):
+    """Quake1 Protocol"""
+    full_name = 'Quake1 Protocol'
+
+    def __init__(self, host: str, port: int, timeout: float = 5.0):
+        """Quake1 Query Protocol"""
+        super().__init__(host, port, timeout)
+        self._delimiter1 = b'\\'
+        self._delimiter2 = b'\n'
+        self._request_header = b'status'
+        self._response_header = 'n'
+
+    async def get_status(self) -> dict:
+        """This returns server information and players."""
+        br = await self._get_response_binary_reader()
+
+        return {
+            'info': self._parse_info(br),
+            'players': self._parse_players(br),
+        }
+
+    async def _get_response_binary_reader(self) -> BinaryReader:
+        response_data = await self._connect_and_send(self._request_header)
+
+        br = BinaryReader(response_data)
+        header = br.read_string(self._delimiter1)
+
+        if header != self._response_header:
+            raise Exception(f'Packet header mismatch. Received: {header}. Expected: {self._response_header}.')
+
+        return br
+
+    def _parse_info(self, br: BinaryReader) -> dict:
+        info = {}
+
+        # Read all key values until meet \n
+        while br.length() > 0:
+            key = br.read_string(self._delimiter1)
+
+            if key == '':
+                break
+
+            info[key] = br.read_string([self._delimiter1, self._delimiter2])
+
+            br.stream_position -= 1
+
+            if bytes([br.read_byte()]) == self._delimiter2:
+                break
+
+        return info
+
+    def _parse_players(self, br: BinaryReader) -> list:
+        players = []
+
+        for matches in self._get_player_match_collections(br):
+            matches: list[re.Match] = [match.group() for match in matches]
+
+            players.append({
+                'id': int(matches[0]),
+                'score': int(matches[1]),
+                'time': int(matches[2]),
+                'ping': int(matches[3]),
+                'name': str(matches[4]).strip('"'),
+                'skin': str(matches[5]).strip('"'),
+                'color1': int(matches[6]),
+                'color2': int(matches[7]),
+            })
+
+        return players
+
+    def _get_player_match_collections(self, br: BinaryReader):
+        match_collections = []
+
+        # Regex to split with whitespace and double quote
+        regex = re.compile(r'"(\\"|[^"])*?"|[^\s]+')
+
+        # Read all players
+        while not br.is_end():
+            match_collections.append(regex.finditer(br.read_string(self._delimiter2)))
+
+        return match_collections
+
+    async def _connect_and_send(self, data):
+        # Connect to remote host
+        with SocketAsync() as sock:
+            sock.settimeout(self._timeout)
+            await sock.connect((self._host, self._port))
+
+            header = b'\xFF\xFF\xFF\xFF'
+
+            # Send Request
+            sock.send(header + data + b'\x00')
+
+            # Server response
+            response_data = await sock.recv()
+
+        # Remove the last 0x00 if exists (Only if Quake1)
+        if response_data[-1] == 0:
+            response_data = response_data[:-1]
+
+        # Add \n at the last of responseData if not exists
+        if response_data[-1] != self._delimiter2:
+            response_data += self._delimiter2
+
+        # Remove the first four 0xFF
+        return response_data[len(header):]
+
+
+if __name__ == '__main__':
+    import asyncio
+    import json
+
+    async def main_async():
+        quake1 = Quake1(host='35.185.44.174', port=27500, timeout=5.0)
+        status = await quake1.get_status()
+        print(json.dumps(status, indent=None) + '\n')
+
+    asyncio.run(main_async())
