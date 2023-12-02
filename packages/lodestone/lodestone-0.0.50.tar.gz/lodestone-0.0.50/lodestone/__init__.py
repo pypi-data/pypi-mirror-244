96081321ea5ead91d53fc022800186d73842e9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 06:03:25 +0000

    Add package lodestone/0.0.50/lodestone-0.0.50.tar.gz

---
+++ packages/lodestone/lodestone-0.0.50.tar.gz/lodestone-0.0.50/lodestone/__init__.py
@@ -0,0 +1,39 @@
+import os
+
+if os.path.isdir('plugins'):
+    try:
+        from lodestone.plugins import Base as plugins
+    except:
+        try:
+            from .plugins import Base as plugins
+        except:
+            try:
+                import lodestone
+                lodestone.get_plugins()
+            except:
+                pass
+else:
+    import lodestone
+    lodestone.get_plugins()
+    try:
+        from lodestone.plugins import Base as plugins
+    except:
+        from .plugins import Base as plugins
+
+try:
+    from lodestone.bot import createBot, Bot
+    from lodestone.utils import llm
+    from lodestone.api import fastapi
+    from lodestone.server import createServer, Server
+    from javascript import On as Event
+    from lodestone.logger import logger
+    from lodestone.gradios import ui
+except:
+    from .bot import createBot, Bot
+    from .utils import llm
+    from .api import fastapi
+    from .server import createServer, Server
+    from javascript import On as Event
+    from .logger import logger
+    from .gradios import ui
+    
