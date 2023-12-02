380308d73907e407c4d90ca1b6d82890926ba739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:03:43 +0000

    Add package nvc-dhl-tracker/0.1.7/nvc-dhl-tracker-0.1.7.tar.gz

---
+++ packages/nvc-dhl-tracker/nvc-dhl-tracker-0.1.7.tar.gz/nvc-dhl-tracker-0.1.7/src/i129f/nvc_dhl_tracker/cli.py
@@ -0,0 +1,11 @@
+import os
+
+import django
+from django.core.management import call_command
+
+
+def run_from_cli():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "i129f.nvc_dhl_tracker.settings")
+    django.setup()
+    call_command("migrate")
+    call_command("run_nvc_tracker")
