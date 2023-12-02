08bcda1dcfa9667e3c7a8f8b94c2b4975c774d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:04:09 +0000

    Add package colarised/1.4/colarised-1.4.tar.gz

---
+++ packages/colarised/colarised-1.4.tar.gz/colarised-1.4/README.md
@@ -0,0 +1,1 @@
+Documentation: https://github.com/hertzsmith/colarised
---
+++ packages/colarised/colarised-1.4.tar.gz/colarised-1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="colarised",
+    version='1.4',
+    license='Eclipse Public License 2.0',
+    authors=["hertzsmith"],
+    author_email="media-hr@gmail.com",
+    description="Bring Colorful code in python",
+    long_description='Documentation: https://github.com/hertzsmith/colarised',
+    packages=['colarised']
+)
+
