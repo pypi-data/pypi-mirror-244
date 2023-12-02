8409d44d08caca77632f1950593f3fa304e2c387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 14:03:49 +0000

    Add package PyMovieDb/0.0.9/PyMovieDb-0.0.9.tar.gz

---
+++ packages/PyMovieDb/PyMovieDb-0.0.9.tar.gz/PyMovieDb-0.0.9/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyMovieDb",
+    version="0.0.9",
+    author="Hemant Malik",
+    author_email="itsmehemant7@gmail.com",
+    description="A Python Module that represents IMDB API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/itsmehemant7/PyMovieDb",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=["requests-html"]
+)
