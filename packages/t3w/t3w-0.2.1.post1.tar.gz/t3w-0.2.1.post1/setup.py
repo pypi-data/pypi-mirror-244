b9cc1aa8d68fe9714b5069ea93ac98ae7dd88a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:03:55 +0000

    Add package t3w/0.2.1.post1/t3w-0.2.1.post1.tar.gz

---
+++ packages/t3w/t3w-0.2.1.post1.tar.gz/t3w-0.2.1.post1/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+
+__version__ = '0.2.1.post1'
+
+with open("README.md", 'r') as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="t3w",
+    version=__version__,
+    author="Yuyao Huang",
+    author_email="huangyuyao@outlook.com",
+    url="https://github.com/tjyuyao/t3w",
+    description="Typed Thin PyTorch Wrapper",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=["t3w"],
+    install_requires=[
+        "jaxtyping",
+        "pandas",
+        "aim",
+        "tqdm"
+        # "typer[all]",
+    ],
+    extras_require={
+        # "all": [
+        #     "aim",
+        #     "tqdm",
+        # ]
+    },
+)
