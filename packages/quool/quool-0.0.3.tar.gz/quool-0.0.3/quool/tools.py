8823727d2e9007bbe4947205b515da159ba8d981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:04:16 +0000

    Add package quool/0.0.3/quool-0.0.3.tar.gz

---
+++ packages/quool/quool-0.0.3.tar.gz/quool-0.0.3/quool/tools.py
@@ -0,0 +1,93 @@
+import re
+import numpy as np
+import pandas as pd
+
+
+def parse_date(
+    date: str | list = None,
+    default: str = '20000104',
+    format_: str = None,
+    errors: str = 'ignore'
+) -> tuple:
+    if not isinstance(date, list):
+        try:
+            date = (
+                pd.to_datetime(date, errors=errors, format=format_) if date is not None 
+                else pd.to_datetime(default, errors=errors, format=format_)
+            )
+            return date
+        except Exception as _:
+            return date
+            
+    else:
+        return pd.to_datetime(date, errors=errors, format=format_)
+
+def parse_commastr(
+    commastr: 'str | list',
+) -> pd.Index:
+    if isinstance(commastr, str):
+        commastr = commastr.split(',')
+        return list(map(lambda x: x.strip(), commastr))
+    elif commastr is None:
+        return None
+    else:
+        return commastr
+
+def reduce_mem_usage(df: pd.DataFrame):
+    """iterate through all the columns of a dataframe and modify the data type
+    to reduce memory usage.
+    """
+    start_mem = df.memory_usage().sum()
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+    for col in df.columns:
+        col_type = df[col].dtype
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df[col] = df[col].astype('category')
+    end_mem = df.memory_usage().sum()
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+    return df
+
+def format_code(code, format_str = '{market}.{code}', upper: bool = True):
+    if len(c := code.split('.')) == 2:
+        dig_code = c.pop(0 if c[0].isdigit() else 1)
+        market_code = c[0]
+        if upper:
+            market_code = market_code.upper()
+        return format_str.format(market=market_code, code=dig_code)
+    elif len(code.split('.')) == 1:
+        sh_code_pat = '6\d{5}|9\d{5}'
+        sz_code_pat = '0\d{5}|2\d{5}|3\d{5}'
+        bj_code_pat = '8\d{5}|4\d{5}'
+        if re.match(sh_code_pat, code):
+            return format_str.format(code=code, market='sh' if not upper else 'SH')
+        if re.match(sz_code_pat, code):
+            return format_str.format(code=code, market='sz' if not upper else 'SZ')
+        if re.match(bj_code_pat, code):
+            return format_str.format(code=code, market='bj' if not upper else 'BJ')
+    else:
+        raise ValueError("Your input code is not unstood")
+
+def strip_stock_code(code: str):
+    code_pattern = r'\.?[Ss][Zz]\.?|\.?[Ss][Hh]\.?|\.?[Bb][Jj]\.?'\
+        '|\.?[Oo][Ff]\.?'
+    return re.sub(code_pattern, '', code)
