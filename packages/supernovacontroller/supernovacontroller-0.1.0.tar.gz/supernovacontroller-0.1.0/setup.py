f699d071e5a90fae6fad19fbb625a50ffaab78d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:04:14 +0000

    Add package supernovacontroller/0.1.0/supernovacontroller-0.1.0.tar.gz

---
+++ packages/supernovacontroller/supernovacontroller-0.1.0.tar.gz/supernovacontroller-0.1.0/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+# Read GH_TOKEN from environment variables // Remove when transfer_controller v0.3.0 is approved
+gh_token = os.environ.get('GH_TOKEN')
+
+setup(
+    name='supernovacontroller',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A blocking API for interacting with the Supernova host-adapter device',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Binho LLC',
+    author_email='support@binho.io',
+    url='https://github.com/binhollc/SupernovaController',
+    license='Private',
+    install_requires=[
+      'transfer_controller==0.3.1',
+      'BinhoSupernova==0.2.0'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+)
