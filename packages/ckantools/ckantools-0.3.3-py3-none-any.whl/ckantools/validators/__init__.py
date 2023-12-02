07ba78f602e92d939def6c8cb41f8376a334f916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 05:04:11 +0000

    Add package ckantools/0.3.3/ckantools-0.3.3-py3-none-any.whl

---
+++ packages/ckantools/ckantools-0.3.3-py3-none-any.whl/ckantools/validators/__init__.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# encoding: utf-8
+
+"""
+Items for working with validators.
+"""
+
+from .misc import validate_by_schema
+from .ivalidators import (
+    list_validator,
+    list_of_dicts_validator,
+    list_of_strings,
+    object_validator,
+)
