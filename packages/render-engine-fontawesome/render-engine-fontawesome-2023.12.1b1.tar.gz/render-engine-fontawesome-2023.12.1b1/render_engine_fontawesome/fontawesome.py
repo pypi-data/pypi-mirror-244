68df1c205ef9810fb59503e89b202e63c07a40ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 15:03:19 +0000

    Add package render-engine-fontawesome/2023.12.1b1/render-engine-fontawesome-2023.12.1b1.tar.gz

---
+++ packages/render-engine-fontawesome/render-engine-fontawesome-2023.12.1b1.tar.gz/render-engine-fontawesome-2023.12.1b1/render_engine_fontawesome/fontawesome.py
@@ -0,0 +1,17 @@
+import pathlib
+
+from jinja2 import DictLoader
+from render_engine.themes import Theme
+
+# Add plugins here
+fontawesome = Theme(
+    loader=DictLoader(
+        {
+            "fontawesome.html": '<script src="https://kit.fontawesome.com/{{theme["fontawesome"]}}.js" crossorigin="anonymous"></script>',  # noqa 501
+        }
+    ),
+    static_dir=pathlib.Path(__file__).parent / "static",
+    filters=[],
+    plugins={},
+    template_globals={"head": "fontawesome.html"},
+)
