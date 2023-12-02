aee9515c8af2c911ab58907cb095c3e3d9260008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-02 03:06:36 +0000

    Add package WTech/0.0.1/WTech-0.0.1.tar.gz

---
+++ packages/WTech/WTech-0.0.1.tar.gz/WTech-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='WTech',
+    version='0.0.1',
+    description='Hello There!',
+    author='Wangtry',
+    author_email='wangtry3417@gmail.com',
+    url='https://github.com/wangtry3417/WTech.git',
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',
+        'requests',
+        # 添加其他依赖项
+    ],
+)
