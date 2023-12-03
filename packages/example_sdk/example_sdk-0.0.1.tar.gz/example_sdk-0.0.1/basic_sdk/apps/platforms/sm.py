a263282f6b833b02f1af4e218f70158a39380b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 04:04:18 +0000

    Add package example_sdk/0.0.1/example_sdk-0.0.1.tar.gz

---
+++ packages/example_sdk/example_sdk-0.0.1.tar.gz/example_sdk-0.0.1/basic_sdk/apps/platforms/sm.py
@@ -0,0 +1,15 @@
+import datetime
+
+from gdshoplib.apps.platforms.yam import YandexMarketManager
+from gdshoplib.packages.feed import Feed
+from lxml import etree, objectify
+
+
+class SberMarketManager(YandexMarketManager, Feed):
+    KEY = "SM"
+
+    def get_root(self):
+        root = etree.Element("yml_catalog")
+        objectify.deannotate(root, cleanup_namespaces=True, xsi_nil=True)
+        root.attrib["date"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        return root
