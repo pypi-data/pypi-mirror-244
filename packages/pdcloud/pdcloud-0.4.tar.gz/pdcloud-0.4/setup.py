a4ea2290af9760b5c63736be376b521ad9acc9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 14:03:59 +0000

    Add package pdcloud/0.4/pdcloud-0.4.tar.gz

---
+++ packages/pdcloud/pdcloud-0.4.tar.gz/pdcloud-0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_desc = f.read()
+
+setup(
+    name="pdcloud",
+    version="0.4",
+    packages=find_packages(),
+    description="Python pandas dataframe cloud agnostic storage",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    author="Vitali M.",
+    author_email="pilot@aviatorv.com",
+    url="",
+    license="MIT",
+    install_requires=[
+        "azure-storage-blob",
+        "pyarrow",
+        "pandas",
+        "aiohttp",
+    ],
+    extras_require={"test": ["pytest", "pytest-asyncio", "twine"]},
+    python_requires=">=3.8",
+)
