01e6563c752d43d789ba2dcafb6a2e47c162585b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 14:03:59 +0000

    Add package habluetooth/0.3.0/habluetooth-0.3.0-pp310-pypy310_pp73-macosx_11_0_x86_64.whl

---
+++ packages/habluetooth/habluetooth-0.3.0-pp310-pypy310_pp73-macosx_11_0_x86_64.whl/habluetooth/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "0.3.0"
+
+from .base_scanner import BaseHaRemoteScanner, BaseHaScanner
+from .const import (
+    CONNECTABLE_FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS,
+    FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS,
+    SCANNER_WATCHDOG_INTERVAL,
+    SCANNER_WATCHDOG_TIMEOUT,
+)
+from .models import HaBluetoothConnector
+
+__all__ = [
+    "BaseHaScanner",
+    "BaseHaRemoteScanner",
+    "HaBluetoothConnector",
+    "FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS",
+    "CONNECTABLE_FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS",
+    "SCANNER_WATCHDOG_TIMEOUT",
+    "SCANNER_WATCHDOG_INTERVAL",
+]
