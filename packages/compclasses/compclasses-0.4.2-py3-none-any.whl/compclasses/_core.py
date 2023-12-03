a7ab5a2cabe209cd63cba95188deeb28e8a8f9ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 03:03:25 +0000

    Add package compclasses/0.4.2/compclasses-0.4.2-py3-none-any.whl

---
+++ packages/compclasses/compclasses-0.4.2-py3-none-any.whl/compclasses/_core.py
@@ -0,0 +1,101 @@
+from operator import attrgetter
+from typing import Callable, Dict, Generator, Iterable, Tuple, TypeVar, Union
+
+from compclasses._delegatee import delegatee
+from compclasses._logging import logger
+
+T = TypeVar("T")
+
+
+def property_from_delegator(
+    delegatee_cls_name: str,
+    attr_name: str,
+    new_attr_name: str,
+) -> property:
+    """Defines a property called `new_attr_name` based upon `delegate_cls_name.attr_name`.
+
+    This function is used to create a property which will be injected in the class which has a delegatee attribute
+    called `delegatee_cls_name` which has an attribute/method called `attr_name`.
+
+    Remark that we don't check here if the delegatee has attr_name implemented, as delegatee_cls_name only represent the
+    attribute name to which we delegate.
+
+    We define the property as follows:
+
+        - fget: returns the value of `delegatee_cls_name.attr_name`
+        - fset: sets the value of `delegatee_cls_name.new_attr_name`
+        - fdel: deletes the value of `delegatee_cls_name.new_attr_name`
+
+    and using `wrapped_delegatee = attrgetter(delegatee_cls_name)` which allows us to access `self.delegatee_cls_name`
+    by calling  `wrapped_delegatee(self)`.
+
+    Arguments:
+        delegatee_cls_name: Name of the attribute from which we forward the method.
+        attr_name: Attribute/method of delegatee_cls_name which we want to forward.
+        new_attr_name: Name of the new attribute to be created in the scope of the class which will be used to access
+            the attribute/method of delegatee_cls.
+
+    Returns:
+        property: Property which will be injected in the class.
+    """
+
+    wrapped_delegatee = attrgetter(delegatee_cls_name)  # => wrapped_delegatee(self) returns self.delegatee_cls_name
+
+    def fget(self):
+        """Function to be used for getting an attribute value."""
+        return getattr(wrapped_delegatee(self), attr_name)
+
+    def fset(self, value):
+        """Function to be used for setting an attribute value."""
+        setattr(wrapped_delegatee(self), new_attr_name, value)
+
+    def fdel(self):
+        """Function to be used for deleting an attribute value."""
+        delattr(wrapped_delegatee(self), new_attr_name)
+
+    return property(fget=fget, fset=fset, fdel=fdel, doc=fget.__doc__)
+
+
+def generate_properties(
+    delegates: Dict[str, Union[Iterable[str], delegatee]],
+    verbose: bool = True,
+    log_func: Callable[[str], None] = logger.info,
+) -> Generator[Tuple[str, property], None, None]:
+    """Creates a generator of (`new_attr_name`, `property_to_inject`), which is used to inject the property into the
+    class of interest, by iterating over the delegates argument.
+
+    Arguments:
+        delegates: Key-value pair of delegates.
+        verbose: Whether to log the injection of the properties.
+        log_func: Function used to log, unused if verbose is set to False.
+
+    Returns:
+        Generator[Tuple[str, property], None, None]: generator of (`new_attr_name`, `property_to_inject`) which is used
+            to inject the property into the class of interest.
+    """
+
+    for delegatee_name, delegatee_instance in delegates.items():
+        is_delegatee = isinstance(delegatee_instance, delegatee)
+
+        for attr_name in delegatee_instance:
+            if is_delegatee and not delegatee._is_dunder_method(attr_name):
+                pfx, sfx = (
+                    delegatee_instance._prefix,  # type: ignore
+                    delegatee_instance._suffix,  # type: ignore
+                )
+
+            else:
+                pfx, sfx = "", ""
+
+            new_attr_name = f"{pfx}{attr_name}{sfx}"
+
+            property_to_inject = property_from_delegator(
+                delegatee_cls_name=delegatee_name,
+                attr_name=attr_name,
+                new_attr_name=new_attr_name,
+            )
+
+            if verbose:
+                log_func(f"Setting {new_attr_name} from {delegatee_name}.{attr_name}")
+
+            yield new_attr_name, property_to_inject
---
+++ packages/compclasses/compclasses-0.4.2-py3-none-any.whl/compclasses/_decorator.py
@@ -0,0 +1,75 @@
+from typing import Callable, Dict, Iterable, Type, TypeVar, Union
+
+from compclasses._core import generate_properties, logger
+from compclasses._delegatee import delegatee
+
+T = TypeVar("T")
+
+
+def compclass(
+    _cls: Union[Type[T], None] = None,
+    delegates: Union[Dict[str, Union[Iterable[str], delegatee]], None] = None,
+    verbose: bool = True,
+    log_func: Callable[[str], None] = logger.info,
+) -> Union[Type[T], Callable[[Type[T], Dict[str, Union[Iterable[str], delegatee]]], Type[T]]]:
+    """Decorator that adds class attributes/methods from `delegates` to `_cls` object as class properties.
+
+    Arguments:
+        _cls: Class to which attributes/methods should be forwarded to.
+        delegates: Key-value pair of delegates.
+
+            - key: Name of the class/instance attribute to which the delegate instance is assigned to.
+            - value: Must be either a sequence/iterable of method names or a `delegatee` instance.
+                They represent the attributes/methods to forward.
+
+        verbose: Defines the level of verbosity when setting those forwarded methods.
+        log_func: Function to use for logging, if verbose is set to True.
+
+    Raises:
+        ValueError: `delegates` param cannot be `None`.
+
+    Returns:
+        Class with added methods from delegates.
+
+    Usage:
+
+    The function can be used as a class decorator:
+
+    ```python
+    class Foo:
+        a = 1
+
+        def __len__(self) -> int:
+            # custom len method
+            return 42
+
+    @compclass(delegates={"_foo": ("a", "__len__")})
+    class Bar:
+        def __init__(self, foo: Foo):
+            self._foo = foo
+
+    foo = Foo()
+    bar = Bar(foo)
+
+    bar.a  # -> 1 (instead of bar._foo.a)
+    len(bar)  # -> 42 (instead of len(bar._foo))
+    ```
+    """
+    if delegates is None:
+        raise ValueError("`delegates` param cannot be `None`")
+
+    def wrap(
+        _cls: Type[T],
+        delegates: Dict[str, Union[Iterable[str], delegatee]] = delegates,  # type: ignore
+    ) -> Type[T]:
+        for _name, _to_inject in generate_properties(delegates, verbose, log_func):
+            setattr(_cls, _name, _to_inject)
+
+        return _cls
+
+    if _cls is None:
+        # Called with parens: @compclass(delegates=...)
+        return wrap
+
+    # Called directly on class C = compclass(C, delegates)
+    return wrap(_cls)
