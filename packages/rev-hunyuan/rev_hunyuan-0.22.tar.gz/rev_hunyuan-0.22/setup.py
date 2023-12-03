f228c17f6cd19af0c7b5a7409d5b968cf75fdcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 14:03:34 +0000

    Add package rev-hunyuan/0.22/rev_hunyuan-0.22.tar.gz

---
+++ packages/rev-hunyuan/rev_hunyuan-0.22.tar.gz/rev_hunyuan-0.22/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="rev_hunyuan",
+    version="0.22",
+    description="通过cookie信息快速访问腾讯混元",
+    author="Harcic",
+    author_email="harcic@outlook.com",
+    url="https://github.com/HarcicYang/rev_HunYuan",
+    packages=find_packages(),
+    include_package_data=True,
+)
