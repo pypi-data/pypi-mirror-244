9b255b640421cf1d1d046e1a48f643d9b34d6100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 04:04:15 +0000

    Add package newshomepages/0.0.82/newshomepages-0.0.82.tar.gz

---
+++ packages/newshomepages/newshomepages-0.0.82.tar.gz/newshomepages-0.0.82/newshomepages/sources/javascript/nytimes.js
@@ -0,0 +1,7 @@
+document.querySelector('[data-testid="StandardAd"]').remove()
+
+document.querySelectorAll(
+  '.ad,.e1xxpj0j0'
+).forEach(el => el.style.display = "none")
+
+document.querySelector('#complianceOverlay').remove()
