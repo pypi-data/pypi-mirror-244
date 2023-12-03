b02c9e3117d56219af9b70b3e2ab447b9b1803cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 18:04:18 +0000

    Add package arclet-letoderea/0.9.0/arclet-letoderea-0.9.0.tar.gz

---
+++ packages/arclet-letoderea/arclet-letoderea-0.9.0.tar.gz/arclet-letoderea-0.9.0/arclet/letoderea/event.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import inspect
+from functools import lru_cache
+from typing import Protocol, runtime_checkable
+
+from .auxiliary import BaseAuxiliary
+from .provider import Provider, ProviderFactory
+from .typing import Contexts
+
+
+@runtime_checkable
+class BaseEvent(Protocol):
+    async def gather(self, context: Contexts):
+        ...
+
+
+@lru_cache(4096)
+def get_providers(
+    event: type[BaseEvent] | BaseEvent,
+) -> list[Provider | ProviderFactory]:
+    res = getattr(event, "providers", [])
+    res.extend(
+        p
+        for _, p in inspect.getmembers(
+            event,
+            lambda x: inspect.isclass(x) and issubclass(x, (Provider, ProviderFactory)),
+        )
+    )
+    return [p() if isinstance(p, type) else p for p in res]
+
+
+@lru_cache(4096)
+def get_auxiliaries(event: type[BaseEvent] | BaseEvent) -> list[BaseAuxiliary]:
+    res = getattr(event, "auxiliaries", [])
+    res.extend(p for _, p in inspect.getmembers(event, lambda x: inspect.isclass(x) and issubclass(x, BaseAuxiliary)))
+    return [a() if isinstance(a, type) else a for a in res]
+
+
+def make_event(cls: type) -> type:
+    async def _gather(self, context: Contexts):
+        for key in cls.__annotations__:
+            context[key] = getattr(self, key)
+
+    cls.gather = _gather  # type: ignore
+    return cls  # type: ignore
