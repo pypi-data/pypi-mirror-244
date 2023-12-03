9d5b07435477af53f4e9fc678e75fd64aafc6539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 04:04:58 +0000

    Add package kalm/3.6.4/kalm-3.6.4.tar.gz

---
+++ packages/kalm/kalm-3.6.4.tar.gz/kalm-3.6.4/src/kalm/netbox/redis.py
@@ -0,0 +1,21 @@
+import redis
+from ..common import prettyllog
+
+def refresh_netbox_from_redis(myenv):
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    data = {}
+    details = {}
+    mykey = "netbox"
+    knownservers = r.keys("kalm:vmware:*:known")
+    detailedservers = r.keys("kalm:vmware:*:details")
+    for server in knownservers:
+        key = server.decode("utf-8")
+        value = r.get(key)
+        data[key] = value.decode("utf-8")
+        prettyllog("netbox", "get", "server", key, value.decode("utf-8") , "getting server", severity="INFO")
+    for server in detailedservers:
+        key = server.decode("utf-8")
+        value = r.get(key)
+        details[key] = value.decode("utf-8")
+    print(data)
+    return data
