d40576f2cc303c567ac60dfa1115203e18b92701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 03:04:25 +0000

    Add package pi-heif/0.14.0/pi_heif-0.14.0.tar.gz

---
+++ packages/pi-heif/pi_heif-0.14.0.tar.gz/pi_heif-0.14.0/libheif/macos/libheif.rb
@@ -0,0 +1,35 @@
+# https://github.com/Homebrew/homebrew-core/blob/master/Formula/lib/libheif.rb
+
+class Libheif < Formula
+  desc "ISO/IEC 23008-12:2017 HEIF file format decoder and encoder"
+  homepage "https://www.libde265.org/"
+  url "https://github.com/strukturag/libheif/releases/download/v1.17.5/libheif-1.17.5.tar.gz"
+  sha256 "38ab01938ef419dbebb98346dc0b1c8bb503a0449ea61a0e409a988786c2af5b"
+  license "LGPL-3.0-only"
+  # Set current revision from what it was taken plus 10
+  revision 10
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libde265"
+
+  def install
+    args = %W[
+      -DWITH_RAV1E=OFF
+      -DWITH_DAV1D=OFF
+      -DWITH_SvtEnc=OFF
+      -DWITH_AOM=OFF
+      -DWITH_X265=OFF
+      -DWITH_LIBSHARPYUV=OFF
+      -DENABLE_PLUGIN_LOADING=OFF
+      -DWITH_JPEG_DECODER=OFF
+      -DWITH_JPEG_ENCODER=OFF
+      -DWITH_OpenJPEG_DECODER=OFF
+      -DWITH_OpenJPEG_ENCODER=OFF
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+end
