613ca7303cb5407f5bad09b9e1130f3ab54dd1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 18:04:02 +0000

    Add package energypylinear/1.2.0/energypylinear-1.2.0-py3-none-any.whl

---
+++ packages/energypylinear/energypylinear-1.2.0-py3-none-any.whl/energypylinear/accounting/accounting.py
@@ -0,0 +1,166 @@
+"""Account for the use, cost and carbon emissions of electricity and gas."""
+
+import numpy as np
+import pandas as pd
+import pydantic
+
+import energypylinear as epl
+from energypylinear.defaults import defaults
+
+
+class Account(pydantic.BaseModel):
+    """An account containing economic cost and carbon emissions."""
+
+    cost: float
+    emissions: float
+
+    def __str__(self) -> str:
+        """A string representation of self."""
+        return f"<Account profit={-1 * self.cost:.2f} emissions={self.emissions:.4f}>"
+
+    def __repr__(self) -> str:
+        """A string representation of self."""
+        return f"<Account profit={-1 * self.cost:.2f} emissions={self.emissions:.4f}>"
+
+    def __sub__(self, other: object) -> "Account":
+        """Subtracts two Account objects and returns a new Account.
+
+        Args:
+            other (object): The Account object to subtract from self.
+
+        Returns:
+            Account: New Account object with updated cost and emissions.
+
+        Raises:
+            NotImplementedError: If 'other' is not an instance of Account.
+        """
+        if not isinstance(other, Account):
+            raise NotImplementedError("Cannot compare {other} to Account")
+
+        return Account(
+            cost=self.cost - other.cost,
+            emissions=self.emissions - other.emissions,
+        )
+
+
+class ElectricityAccount(Account):
+    """An electricity account, containing import and export costs and emissions."""
+
+    import_cost: float
+    export_cost: float
+    cost: float
+
+    import_emissions: float
+    export_emissions: float
+    emissions: float
+
+    @pydantic.field_validator("import_emissions", "export_emissions")
+    @classmethod
+    def no_infinite(cls, v: float) -> float:
+        """Check that our value is finite."""
+        assert np.isfinite(v)
+        return v
+
+
+class GasAccount(Account):
+    """A natural gas account, containing cost and emissions."""
+
+    pass
+
+
+class Accounts(Account):
+    """A set of accounts for electricity and gas."""
+
+    electricity: ElectricityAccount = pydantic.Field(..., repr=False)
+    gas: GasAccount = pydantic.Field(..., repr=False)
+
+    cost: float
+    profit: float
+    emissions: float
+
+    def __str__(self) -> str:
+        """A string representation of self."""
+        return f"<Accounts profit={self.profit:.2f} emissions={self.emissions:.4f}>"
+
+    def __repr__(self) -> str:
+        """A string representation of self."""
+        return f"<Accounts profit={self.profit:.2f} emissions={self.emissions:.4f}>"
+
+
+def get_one_gas_account(
+    results: pd.DataFrame,
+    price_results: pd.DataFrame,
+) -> GasAccount:
+    """Calculate a single gas account."""
+    return GasAccount(
+        cost=(
+            price_results["site-gas_prices"] * results["total-gas_consumption_mwh"]
+        ).sum(),
+        emissions=(
+            defaults.gas_carbon_intensity * results["total-gas_consumption_mwh"]
+        ).sum(),
+    )
+
+
+def get_one_electricity_account(
+    results: pd.DataFrame,
+    price_results: pd.DataFrame,
+) -> ElectricityAccount:
+    """Calculate a single electricity account."""
+    import_cost = (
+        price_results["site-electricity_prices"] * results["site-import_power_mwh"]
+    ).sum()
+    export_cost = -(
+        price_results["site-electricity_prices"] * results["site-export_power_mwh"]
+    ).sum()
+
+    import_emissions = (
+        price_results["site-electricity_carbon_intensities"]
+        * results["site-import_power_mwh"]
+    ).sum()
+    export_emissions = -(
+        price_results["site-electricity_carbon_intensities"]
+        * results["site-export_power_mwh"]
+    ).sum()
+
+    return ElectricityAccount(
+        import_cost=import_cost,
+        export_cost=export_cost,
+        cost=import_cost + export_cost,
+        import_emissions=import_emissions,
+        export_emissions=export_emissions,
+        emissions=import_emissions + export_emissions,
+    )
+
+
+def get_accounts(
+    results: pd.DataFrame,
+    price_results: pd.DataFrame | None = None,
+    validate: bool = True,
+    verbose: bool = True,
+) -> Accounts:
+    """Create a pair of gas and electricity accounts.
+
+    Args:
+        results: (pd.DataFrame)
+            Simulation results used for accounts.
+        price_results: (pd.DataFrame | None)
+            Optional source of electricity & gas prices or carbon intensities.
+    """
+    if price_results is None:
+        price_results = results
+
+    if validate:
+        epl.check_results(results, verbose=verbose)
+        epl.check_results(price_results, verbose=verbose)
+
+    electricity = get_one_electricity_account(results, price_results)
+    gas = get_one_gas_account(results, price_results)
+
+    return Accounts(
+        electricity=electricity,
+        gas=gas,
+        cost=electricity.cost + gas.cost,
+        profit=-(electricity.cost + gas.cost),
+        emissions=electricity.emissions + gas.emissions,
+    )
---
+++ packages/energypylinear/energypylinear-1.2.0-py3-none-any.whl/energypylinear/objectives.py
@@ -0,0 +1,249 @@
+"""Linear programming objective cost functions for price and carbon."""
+import dataclasses
+import typing
+
+import numpy as np
+import pulp
+
+import energypylinear as epl
+from energypylinear.defaults import defaults
+
+
+@dataclasses.dataclass
+class Term:
+    """A single term in the objective function.
+
+    objective = Term + Term + Term"""
+
+    variable: str
+    asset_type: str | None = None
+    interval_data: str | None = None
+    asset_name: str | None = None
+    coefficient: float = 1.0
+
+
+@dataclasses.dataclass
+class CustomObjectiveFunction:
+    """A custom objective function - a sum of terms.
+
+    CustomObjectiveFunction = Term + Term + Term"""
+
+    terms: list[Term]
+
+
+def price_objective(
+    optimizer: "epl.Optimizer",
+    ivars: "epl.IntervalVars",
+    interval_data: "epl.assets.site.SiteIntervalData",
+) -> pulp.LpAffineExpression:
+    """Linear programming objective for cost minimization.  Equivalent to profit maximization.
+
+    The objective is expressed as a linear combination of the costs for site import/export of power,
+    spillage, charge for spillage EVs, gas consumption by generators and boilers.
+
+    Inputs:
+        optimizer: an instance of `epl.Optimizer` class.
+        vars: a dictionary of linear programming variables in the optimization problem.
+        interval_data: interaval data used in the simulation.
+
+    Returns:
+        A linear programming objective as an instance of `pulp.LpAffineExpression` class.
+    """
+    sites = typing.cast(
+        list[list["epl.assets.site.SiteOneInterval"]],
+        ivars.filter_objective_variables_all_intervals(
+            epl.assets.site.SiteOneInterval, asset_name="site"
+        ),
+    )
+    spills = typing.cast(
+        list[list["epl.assets.spill.SpillOneInterval"]],
+        ivars.filter_objective_variables_all_intervals(
+            epl.assets.spill.SpillOneInterval
+        ),
+    )
+    spill_evs = typing.cast(
+        list[list["epl.assets.evs.EVSpillOneInterval"]],
+        ivars.filter_objective_variables_all_intervals(
+            epl.assets.evs.EVSpillOneInterval
+        ),
+    )
+    generators = typing.cast(
+        list[list["epl.assets.chp.CHPOneInterval"]],
+        ivars.filter_objective_variables_all_intervals(epl.assets.chp.CHPOneInterval),
+    )
+    boilers = typing.cast(
+        list[list["epl.assets.boiler.BoilerOneInterval"]],
+        ivars.filter_objective_variables_all_intervals(
+            epl.assets.boiler.BoilerOneInterval
+        ),
+    )
+
+    assert isinstance(interval_data.gas_prices, np.ndarray)
+    assert isinstance(interval_data.electricity_prices, np.ndarray)
+    assert isinstance(interval_data.export_electricity_prices, np.ndarray)
+
+    obj: list[typing.Any | float] = [
+        [
+            site.import_power_mwh * interval_data.electricity_prices[i]
+            - site.export_power_mwh * interval_data.export_electricity_prices[i]
+            for site in sites[i]
+        ]
+        + [
+            spill.electric_generation_mwh * defaults.spill_objective_penalty
+            + spill.high_temperature_generation_mwh * defaults.spill_objective_penalty
+            + spill.electric_load_mwh * defaults.spill_objective_penalty
+            + spill.high_temperature_load_mwh * defaults.spill_objective_penalty
+            for spill in spills[i]
+        ]
+        + [
+            spill_ev.electric_charge_mwh * defaults.spill_objective_penalty
+            + spill_ev.electric_discharge_mwh * defaults.spill_objective_penalty
+            for spill_ev in spill_evs[i]
+        ]
+        + [
+            generator.gas_consumption_mwh * interval_data.gas_prices[i]
+            for generator in generators[i]
+        ]
+        + [
+            boiler.gas_consumption_mwh * interval_data.gas_prices[i]
+            for boiler in boilers[i]
+        ]
+        for i in interval_data.idx
+    ]
+    return optimizer.sum(obj)
+
+
+def carbon_objective(
+    optimizer: "epl.Optimizer",
+    ivars: "epl.interval_data.IntervalVars",
+    interval_data: "epl.assets.site.SiteIntervalData",
+) -> pulp.LpAffineExpression:
+    """Linear programming objective for carbon emission minimization.
+
+    The objective is expressed as a linear combination of the costs for site import/export of power,
+    spillage, charge for spillage EVs, gas consumption by generators and boilers.
+
+    Inputs:
+        optimizer: an instance of `epl.Optimizer` class.
+        vars: a dictionary of linear programming variables in the optimization problem.
+        interval_data: interaval data used in the simulation.
+
+    Returns:
+        A linear programming objective as an instance of `pulp.LpAffineExpression` class.
+    """
+    sites = typing.cast(
+        list[list["epl.assets.site.SiteOneInterval"]],
+        ivars.filter_objective_variables_all_intervals(
+            epl.assets.site.SiteOneInterval, asset_name="site"
+        ),
+    )
+    spills = typing.cast(
+        list[list["epl.assets.spill.SpillOneInterval"]],
+        ivars.filter_objective_variables_all_intervals(
+            epl.assets.spill.SpillOneInterval
+        ),
+    )
+    spill_evs = typing.cast(
+        list[list["epl.assets.evs.EVSpillOneInterval"]],
+        ivars.filter_objective_variables_all_intervals(
+            epl.assets.evs.EVSpillOneInterval
+        ),
+    )
+    generators = typing.cast(
+        list[list["epl.assets.chp.CHPOneInterval"]],
+        ivars.filter_objective_variables_all_intervals(epl.assets.chp.CHPOneInterval),
+    )
+    boilers = typing.cast(
+        list[list["epl.assets.boiler.BoilerOneInterval"]],
+        ivars.filter_objective_variables_all_intervals(
+            epl.assets.boiler.BoilerOneInterval
+        ),
+    )
+
+    assert isinstance(interval_data.electricity_carbon_intensities, np.ndarray)
+    obj: list[typing.Any | float] = [
+        [
+            site.import_power_mwh * interval_data.electricity_carbon_intensities[i]
+            - site.export_power_mwh * interval_data.electricity_carbon_intensities[i]
+            for site in sites[i]
+        ]
+        + [
+            spill.electric_generation_mwh * defaults.spill_objective_penalty
+            + spill.high_temperature_generation_mwh * defaults.spill_objective_penalty
+            + spill.electric_load_mwh * defaults.spill_objective_penalty
+            + spill.electric_charge_mwh * defaults.spill_objective_penalty
+            + spill.electric_discharge_mwh * defaults.spill_objective_penalty
+            #  dumping heat has no penalty
+            #  so high_temperature_load_mwh and low_temperature_load_mwh
+            #  are not included here
+            for spill in spills[i]
+        ]
+        + [
+            spill.electric_generation_mwh * defaults.spill_objective_penalty
+            + spill.high_temperature_generation_mwh * defaults.spill_objective_penalty
+            + spill.electric_load_mwh * defaults.spill_objective_penalty
+            + spill.electric_charge_mwh * defaults.spill_objective_penalty
+            + spill.electric_discharge_mwh * defaults.spill_objective_penalty
+            for spill in spill_evs[i]
+        ]
+        + [
+            generator.gas_consumption_mwh * defaults.gas_carbon_intensity
+            for generator in generators[i]
+        ]
+        + [
+            boiler.gas_consumption_mwh * defaults.gas_carbon_intensity
+            for boiler in boilers[i]
+        ]
+        for i in interval_data.idx
+    ]
+    return optimizer.sum(obj)
+
+
+def get_objective(
+    objective: str | dict | CustomObjectiveFunction,
+    optimizer: "epl.Optimizer",
+    ivars: "epl.interval_data.IntervalVars",
+    interval_data: "epl.assets.site.SiteIntervalData",
+) -> pulp.LpAffineExpression:
+    """Creates the objective function - either from a hardcoded function or from a custom objective function."""
+    hardcoded_objectives = {"price": price_objective, "carbon": carbon_objective}
+
+    if isinstance(objective, str):
+        if objective in hardcoded_objectives:
+            return hardcoded_objectives[objective](optimizer, ivars, interval_data)
+        else:
+            raise ValueError(
+                f"objective {objective} not in objectives, available objectives: {hardcoded_objectives.keys()}"
+            )
+
+    elif isinstance(objective, dict):
+        objective = CustomObjectiveFunction(
+            terms=[Term(**t) for t in objective["terms"]]
+        )
+
+    else:
+        assert isinstance(objective, CustomObjectiveFunction)
+
+    obj: list[typing.Any | float] = []
+    for i in interval_data.idx:
+        for term in objective.terms:
+            if term.asset_type == "*":
+                assets = ivars[i]
+            else:
+                assets = ivars.filter_objective_variables(
+                    instance_type=term.asset_type, i=i, asset_name=term.asset_name
+                )
+            for asset in assets:
+                obj.extend(
+                    [
+                        getattr(asset, term.variable)
+                        * (
+                            getattr(interval_data, term.interval_data)[i]
+                            if term.interval_data is not None
+                            else 1
+                        )
+                        * term.coefficient,
+                    ]
+                )
+
+    return optimizer.sum(obj)
