a14d28a5648ab30953963f1dc30f70cea18cd5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 14:04:02 +0000

    Add package collie-watch/0.0.12/collie_watch-0.0.12-py3-none-any.whl

---
+++ packages/collie-watch/collie_watch-0.0.12-py3-none-any.whl/collie_watch/widgets/raw_html.py
@@ -0,0 +1,12 @@
+
+from src.collie_watch.widgets.widget import Widget
+
+class RawHtml(Widget):
+    def __init__(self,html="",id=""):
+        super().__init__(id=id)
+        self.html = html
+
+    def render(self):
+        return self.html
+    
+    
---
+++ packages/collie-watch/collie_watch-0.0.12-py3-none-any.whl/collie_watch/widgets/text.py
@@ -0,0 +1,12 @@
+
+from src.collie_watch.widgets.widget import Widget
+
+class Text(Widget):
+    def __init__(self,text="",id=""):
+        super().__init__(id=id)
+        self.text = text
+
+    def render(self):
+        return f'<span id="{self.id}">{self.text}</span>'
+    
+    
