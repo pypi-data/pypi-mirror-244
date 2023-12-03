46d99a0b2ae88e80b769d42ee2a2c2b18d8843d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 03:05:15 +0000

    Add package lifelib/0.9.3/lifelib-0.9.3-py3-none-any.whl

---
+++ packages/lifelib/lifelib-0.9.3-py3-none-any.whl/lifelib/projects/ifrs17sim/plot_csm_waterfall.py
@@ -0,0 +1,37 @@
+"""
+ifrs17sim: CSM waterfall chart
+====================================
+
+The script draws a CSM waterfall chart.
+The script is broken down into pieces of code, and explained in a Jupyter notebook,
+:doc:`/projects/ifrs17sim_csm_waterfall`.
+
+The live version of the notebook is available online.
+
+*Launch this notebook online!* |binder ifrs17sim_csm_waterfall|
+
+.. include:: /banners.rst
+   :start-after: Begin binder ifrs17sim_csm_waterfall
+   :end-before: End binder ifrs17sim_csm_waterfall
+
+.. seealso::
+    * The :mod:`~ifrs17sim` library
+"""
+import modelx as mx
+from draw_charts import draw_waterfall, get_waterfalldata
+import seaborn as sns
+sns.set_theme(style="darkgrid")
+
+model = mx.read_model("model")
+proj = model.OuterProj[1]
+
+csmrf = get_waterfalldata(
+        proj,
+        items=['CSM',
+               'IntAccrCSM',
+               'AdjCSM_FlufCF',
+               'TransServices'],
+        length=15,
+        reverseitems=['TransServices'])
+
+draw_waterfall(csmrf)
