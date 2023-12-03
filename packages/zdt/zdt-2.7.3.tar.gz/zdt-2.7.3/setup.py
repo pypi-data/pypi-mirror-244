5584ea49b922888b00f891dd40ffe8bdfb4fb043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 14:03:30 +0000

    Add package zdt/2.7.3/zdt-2.7.3.tar.gz

---
+++ packages/zdt/zdt-2.7.3.tar.gz/zdt-2.7.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '2.7.3'
+DESCRIPTION = 'zdt'
+
+# Setting up
+setup(
+    name="zdt",
+    version=VERSION,
+    author="Dickson",
+    author_email="<suwon2912@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['sqlalchemy', 'typing', 'hdbcli','redmail','pandas>=1.1','datetime','python-dateutil','numpy','apache-airflow','azure-storage-file-datalake'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
+
+
+
+
+
