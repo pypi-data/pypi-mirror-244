6bc1b6bb3a62226b74968b710d02c137fb5fb901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 14:03:58 +0000

    Add package lfpreviewer/1.0.0/lfpreviewer-1.0.0.tar.gz

---
+++ packages/lfpreviewer/lfpreviewer-1.0.0.tar.gz/lfpreviewer-1.0.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import setuptools
+import glob
+
+setuptools.setup(
+    name='lfpreviewer',
+    version='1.0.0',
+    description='Image Previewer for LFP',
+    license='GPLv3',
+    python_requires='>=3.6',
+    install_requires=['docopt', 'attrs>=18.2.0', 'pillow'],
+    include_package_data=True,
+    package_data={ '': ['*.sh'] },
+    packages=setuptools.find_packages(),
+    entry_points={ 'console_scripts': [
+        'lfpreviewer=lfpreviewer.__main__:main'
+    ]},
+    ext_modules=[
+        setuptools.Extension("lfpreviewer.X", glob.glob("lfpreviewer/X/*.c"),
+        libraries=["X11", "Xext", "XRes"], include_dirs=["lfpreviewer/X"]),
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: X11 Applications',
+        'Operating System :: POSIX :: Linux'
+    ]
+)
