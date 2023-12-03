a79bb9320263ab8f225d9e3812711d3ed0aff6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 03:05:18 +0000

    Add package CurvPy/1.1.2/CurvPy-1.1.2-py3-none-any.whl

---
+++ packages/CurvPy/CurvPy-1.1.2-py3-none-any.whl/curvpy/__init__.py
@@ -0,0 +1,7 @@
+from .curvpy import optimize
+from .curvpy import savitzky_golay
+from .curvpy import missing_data_imputation
+from .curvpy import summary_statistics
+from .curvpy import plots
+from .curvpy import regression
+from .curvpy import model_analysis
