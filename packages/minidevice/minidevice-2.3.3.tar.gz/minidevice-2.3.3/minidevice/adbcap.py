f50338ae731a9eae6a9923344750c4a26fc99768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 18:04:07 +0000

    Add package minidevice/2.3.3/minidevice-2.3.3.tar.gz

---
+++ packages/minidevice/minidevice-2.3.3.tar.gz/minidevice-2.3.3/minidevice/adbcap.py
@@ -0,0 +1,24 @@
+from adbutils import adb, adb_path
+from minidevice.screencap import ScreenCap
+import subprocess
+
+
+class ADBcap(ScreenCap):
+    def __init__(self, serial) -> None:
+        """
+        __init__ ADB 截图方式
+
+        Args:
+            serial (str): 设备id
+        """
+        self.__adb = adb.device(serial)
+
+    def screencap_raw(self) -> bytes:
+        adb_command = [adb_path(), "-s", self.__adb.serial, "exec-out", "screencap", "-p"]
+        process = subprocess.Popen(
+            adb_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        data, err = process.communicate(timeout=10)
+        
+        if process.returncode == 0:
+            return data
