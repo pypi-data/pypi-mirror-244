11934a23a65625cd2a0f02da4e59ddcb2a5b06ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 14:04:03 +0000

    Add package tieui/0.2.8/tieui-0.2.8.tar.gz

---
+++ packages/tieui/tieui-0.2.8.tar.gz/tieui-0.2.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tieui',
+    version='0.2.8',
+    packages=find_packages(),
+    install_requires=[
+        'flask_cors',
+        'flask_socketio',
+        'flask',
+        'subprocess',
+        'json',
+        'os',
+        'signal',
+        'openai',
+        'gunicorn',
+        'eventlet',
+        'stripe',
+        'sendgrid',
+        'websocket-client',
+        're',
+        'tempfile',
+    ],
+    author='TieUi',
+    author_email='info@tieUi.com',
+    description='Tie Ui package for local development',
+    url='https://tieui.app',
+)
