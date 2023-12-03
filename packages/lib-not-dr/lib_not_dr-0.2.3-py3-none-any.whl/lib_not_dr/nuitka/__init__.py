e71cdb3b81d4615df5cd1addb18644641df9e0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 18:04:04 +0000

    Add package lib-not-dr/0.2.3/lib_not_dr-0.2.3-py3-none-any.whl

---
+++ packages/lib-not-dr/lib_not_dr-0.2.3-py3-none-any.whl/lib_not_dr/nuitka/__init__.py
@@ -0,0 +1,18 @@
+#  -------------------------------
+#  Difficult Rocket
+#  Copyright © 2020-2023 by shenjackyuanjie 3695888@qq.com
+#  All rights reserved
+#  -------------------------------
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from lib_not_dr.nuitka import (
+        reader,
+        compile
+    )
+
+__all__ = [
+    "reader",
+    "compile"
+]
