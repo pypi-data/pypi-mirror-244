876a7000dab620ca370fa24373a1d791d88c488c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 03:03:33 +0000

    Add package test-technologii-grudzien/0.0.1/test_technologii_grudzien-0.0.1.tar.gz

---
+++ packages/test-technologii-grudzien/test_technologii_grudzien-0.0.1.tar.gz/test_technologii_grudzien-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test_technologii_grudzien",
+    version="0.0.1",
+    packages=find_packages(),
+    description="super opis z grudnia"
+)
