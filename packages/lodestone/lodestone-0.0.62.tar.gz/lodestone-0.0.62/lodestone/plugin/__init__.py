9fede6bede69c16cfc5bbc2a3e0a1ac9b5ca9e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 04:04:41 +0000

    Add package lodestone/0.0.62/lodestone-0.0.62.tar.gz

---
+++ packages/lodestone/lodestone-0.0.62.tar.gz/lodestone-0.0.62/lodestone/plugin/__init__.py
@@ -0,0 +1,11 @@
+from .cactus_farm_builder import cactus
+from .discord_webhook import discord
+from .schematic_builder import schematic
+from .discordrp import discordrp
+
+class plugins:
+    cactus = cactus
+    discordrp = discordrp
+    discord = discord
+    schematic = schematic
+
