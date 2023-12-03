e159e6888f823c8d903be75bb030a928b5022e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 14:04:00 +0000

    Add package solteron/0.1.4/solteron-0.1.4.tar.gz

---
+++ packages/solteron/solteron-0.1.4.tar.gz/solteron-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Solteron',
+    description="setuptools.build_meta",
+    version='0.1.4',
+    author           = 'Hernán A. Teszkiewicz Novick',
+    author_email     = 'herni@cajadeideas.ar',
+    url= 'https://github.com/Hernanatn/solteron.py',
+    download_url     =  'https://pypi.org/project/solteron/0.1.4/#files',
+    packages=['solteron'],
+)
