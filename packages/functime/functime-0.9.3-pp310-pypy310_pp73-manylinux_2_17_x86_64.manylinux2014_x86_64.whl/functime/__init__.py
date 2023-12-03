c8705b37e79611933963b1cb758271fac55e1c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 04:04:54 +0000

    Add package functime/0.9.3/functime-0.9.3-pp310-pypy310_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/functime/functime-0.9.3-pp310-pypy310_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/functime/__init__.py
@@ -0,0 +1,8 @@
+"functime: Time-series machine learning at scale."
+__version__ = "0.9.3"
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+from functime.feature_extractors import FeatureExtractor
