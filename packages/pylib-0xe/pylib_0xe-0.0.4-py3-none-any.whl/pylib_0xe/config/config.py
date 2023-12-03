5b12d8974fb2adb596e55fe9be1f7fd5bdc28392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 03:05:14 +0000

    Add package pylib-0xe/0.0.4/pylib_0xe-0.0.4-py3-none-any.whl

---
+++ packages/pylib-0xe/pylib_0xe-0.0.4-py3-none-any.whl/pylib_0xe/config/config.py
@@ -0,0 +1,33 @@
+from typing import Any
+from ..file.file import File
+from ..path.path_helper import PathHelper
+from ..json.json_helper import JsonHelper
+
+
+class Config:
+    DEFAULT_CONFIG_FOLDER_NAME = "configs"
+
+    def __init__(
+        self,
+        filename: str,
+        config_folder_name: str = DEFAULT_CONFIG_FOLDER_NAME,
+    ) -> None:
+        filename += ".json"
+        self.json = File.read_json(
+            PathHelper.from_root(__file__, config_folder_name, filename)
+        )
+
+    def get(self, selector: str = "", default: Any = None) -> Any:
+        value = JsonHelper.selector_get_value(self.json, selector)
+        if value != {}:
+            return value
+        return default
+
+    @staticmethod
+    def read(selector: str, **kwargs) -> Any:
+        index = selector.find(".")
+        if index < 0:
+            return Config(selector).get(**kwargs)
+        filename = selector[:index]
+        selector = selector[index + 1 :]
+        return Config(filename).get(selector, **kwargs)
