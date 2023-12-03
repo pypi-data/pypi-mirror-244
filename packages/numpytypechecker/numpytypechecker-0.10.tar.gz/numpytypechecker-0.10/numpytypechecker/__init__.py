ecc77415b451f91a0374c887bc833be9f835dc4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 18:04:20 +0000

    Add package numpytypechecker/0.10/numpytypechecker-0.10.tar.gz

---
+++ packages/numpytypechecker/numpytypechecker-0.10.tar.gz/numpytypechecker-0.10/numpytypechecker/__init__.py
@@ -0,0 +1,109 @@
+import sys
+import numpy as np
+
+
+def dtypecheck(array, filterna=True, float2int=True, dtypes=(np.uint8,
+                                                             np.int8,
+                                                             np.uint16,
+                                                             np.int16,
+                                                             np.uint32,
+                                                             np.int32,
+                                                             np.uint64,
+                                                             np.int64,
+                                                             np.uintp,
+                                                             np.intp,
+                                                             np.float16,
+                                                             np.float32,
+                                                             np.float64,
+                                                             'M',
+                                                             'm',
+                                                             'O',
+                                                             'P',
+                                                             'S',
+                                                             'U',
+                                                             'V',
+                                                             'p',
+                                                             's',
+                                                             np.complex64,
+                                                             np.complex128,
+                                                             np.datetime64,
+
+                                                             np.timedelta64,
+                                                             np.void, bool, np.bool_,
+                                                             object
+                                                             )):
+    r"""
+    Check and convert the data type of a NumPy array based on a predefined set of data types.
+
+    Parameters:
+    - array (numpy.ndarray): Input NumPy array.
+    - filterna (bool, optional): If True, remove NaN values from the array before type checking.
+                                  Default is True.
+    - float2int (bool, optional): If True, convert float arrays to integer if they contain only integers.
+                                  Default is True.
+    - dtypes (tuple, optional): Tuple of NumPy data types to check against. Default includes various numeric,
+                               datetime, timedelta, complex, boolean, and object types.
+
+    Returns:
+    - numpy.ndarray: NumPy array with the converted data type.
+
+    Examples:
+    ```python
+
+        from numpytypechecker import dtypecheck
+        import numpy as np
+        # Example
+        a1D = np.array([1, 2, 3, 4])
+        a2D = np.array([[1, 2], [3, 4]])
+        a3D = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+        b1 = np.array([2., 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9])
+        b2 = np.array([[1., 1., 1., 0.],
+                       [1., 1., 0., 1.],
+                       [0., 0., -3., 0.],
+                       [0., 0., np.nan, -4.]])
+
+        print(dtypecheck(a1D, filterna=True, float2int=True, ).dtype)
+        print(dtypecheck(a2D, filterna=True, float2int=True, ).dtype)
+        print(dtypecheck(a3D, filterna=True, float2int=True, ).dtype)
+        print(dtypecheck(b1, filterna=True, float2int=True, ).dtype)
+        print(dtypecheck(b2, filterna=False, float2int=True, ).dtype)
+
+        # uint8
+        # uint8
+        # uint8
+        # float64
+        # float64
+
+    """
+    try:
+        arr = array.copy()
+        if filterna:
+            try:
+                arr = arr[~np.isnan(arr)]
+            except Exception as ca:
+                sys.stderr.write(f'{ca}\n')
+                sys.stderr.flush()
+
+        for dty in dtypes:
+            try:
+                if arr.ndim > 1:
+                    littletest = arr[0].astype(dty)
+                    _ = np.all(arr == littletest)
+                nd = arr.astype(dty)
+                if np.all(arr == nd):
+                    if not float2int:
+                        if '.' in arr.ravel()[0] and '.' not in arr.ravel()[0]:
+                            continue
+
+                    if filterna:
+                        return array.astype(dty)
+                    return array.astype(dty)
+            except Exception as fe:
+                sys.stderr.write(f'{fe}\n')
+                sys.stderr.flush()
+        return array
+    except Exception as fe:
+        sys.stderr.write(f'{fe}\n')
+        sys.stderr.flush()
+    return array
+
