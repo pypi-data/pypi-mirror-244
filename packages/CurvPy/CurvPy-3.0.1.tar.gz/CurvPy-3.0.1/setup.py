ce6ed80f3eaec0507050ca55fef437dbfd1b9941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 18:04:16 +0000

    Add package CurvPy/3.0.1/CurvPy-3.0.1.tar.gz

---
+++ packages/CurvPy/CurvPy-3.0.1.tar.gz/CurvPy-3.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='CurvPy',
+    version='3.0.1',
+    author='sidharth',
+    author_email='sidharthss2690@gmail.com',
+    description='A regression analysis library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'seaborn',
+        'pandas',
+        'sklearn',
+    ],
+)
