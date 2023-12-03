ab05135f617d6740f72473c5b0f96d328134b2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 03:05:16 +0000

    Add package lmcloud/0.4.12/lmcloud-0.4.12.tar.gz

---
+++ packages/lmcloud/lmcloud-0.4.12.tar.gz/lmcloud-0.4.12/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="lmcloud",
+    version="0.4.12",
+    description="A Python implementation of the new La Marzocco API",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zweckj/lmcloud",
+    author="Josef Zweck",
+    author_email="24647999+zweckj@users.noreply.github.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "httpx>=0.16.1",
+        "authlib>=0.15.5",
+        "websockets>=11.0.2",
+        "bleak>=0.20.2",
+        "aiohttp>=3.8.0",
+    ],
+    package_data={
+        "lmcloud": ["py.typed"],
+    },
+)
