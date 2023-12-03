31fcc1d39693aaacff5fbd70435fc3be29e9bc20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 15:04:03 +0000

    Add package rev-baichuan/0.1/rev_baichuan-0.1.tar.gz

---
+++ packages/rev-baichuan/rev_baichuan-0.1.tar.gz/rev_baichuan-0.1/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="rev_baichuan",
+    version="0.1",
+    description="通过cookie信息快速访问百川智能大语言模型",
+    author="Harcic",
+    author_email="harcic@outlook.com",
+    url="https://github.com/HarcicYang/rev_Baichuan",
+    packages=find_packages(),
+    include_package_data=True,
+)
