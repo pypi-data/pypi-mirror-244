a85e5618843a3f63d43c642f29969716d3c0ddc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-03 03:04:58 +0000

    Add package known/0.0.13/known-0.0.13.tar.gz

---
+++ packages/known/known-0.0.13.tar.gz/known-0.0.13/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name =                      'known',
+    version =                   '0.0.13',
+    url =                       'https://github.com/Nelson-iitp/known',
+    author =                    'Nelson.S',
+    author_email =              'mail.nelsonsharma@gmail.com',
+    description =               'Module :: known',
+    packages =                  ['known'],
+    classifiers=                ['License :: OSI Approved :: MIT License'],
+    package_dir =               { '' : 'module'},
+    install_requires =          [],
+    include_package_data =      True
+)   
