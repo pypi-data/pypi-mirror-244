64ddc57a5d6226907dc6561cb324530bc99dd872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:05:19 +0000

    Add package Hydrogram/0.1.2/hydrogram-0.1.2-py3-none-any.whl

---
+++ packages/Hydrogram/hydrogram-0.1.2-py3-none-any.whl/hydrogram/raw/functions/messages/fave_sticker.py
@@ -0,0 +1,80 @@
+#  Hydrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2023-present Amano LLC <https://amanoteam.com>
+#
+#  This file is part of Hydrogram.
+#
+#  Hydrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Hydrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Hydrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+from io import BytesIO
+
+from hydrogram.raw.core.primitives import Int, Long, Int128, Int256, Bool, Bytes, String, Double, Vector
+from hydrogram.raw.core import TLObject
+from hydrogram import raw
+from typing import List, Optional, Any
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#               !!! WARNING !!!               #
+#          This is a generated file!          #
+# All changes made in this file will be lost! #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+
+class FaveSticker(TLObject):  # type: ignore
+    """Telegram API function.
+
+    Details:
+        - Layer: ``166``
+        - ID: ``B9FFC55B``
+
+    Parameters:
+        id (:obj:`InputDocument <hydrogram.raw.base.InputDocument>`):
+            N/A
+
+        unfave (``bool``):
+            N/A
+
+    Returns:
+        ``bool``
+    """
+
+    __slots__: List[str] = ["id", "unfave"]
+
+    ID = 0xb9ffc55b
+    QUALNAME = "functions.messages.FaveSticker"
+
+    def __init__(self, *, id: "raw.base.InputDocument", unfave: bool) -> None:
+        self.id = id  # InputDocument
+        self.unfave = unfave  # Bool
+
+    @staticmethod
+    def read(b: BytesIO, *args: Any) -> "FaveSticker":
+        # No flags
+        
+        id = TLObject.read(b)
+        
+        unfave = Bool.read(b)
+        
+        return FaveSticker(id=id, unfave=unfave)
+
+    def write(self, *args) -> bytes:
+        b = BytesIO()
+        b.write(Int(self.ID, False))
+
+        # No flags
+        
+        b.write(self.id.write())
+        
+        b.write(Bool(self.unfave))
+        
+        return b.getvalue()
