3df4c4bc95fd243fbcdaf693dbd5bfe8bebf37bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 14:05:19 +0000

    Add package Geode-Implicit/2.5.1/Geode_Implicit-2.5.1-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-Implicit/Geode_Implicit-2.5.1-cp310-cp310-win_amd64.whl/geode_implicit/implicitation.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import geode_numerics
+
+from .bin.geode_implicit_py_implicitation import *
+ImplicitImplicitationLibrary.initialize()
---
+++ packages/Geode-Implicit/Geode_Implicit-2.5.1-cp310-cp310-win_amd64.whl/geode_implicit/model_io.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode_geosciences
+import opengeode_geosciencesio
+
+from .bin.geode_implicit_py_io import *
+ImplicitIOLibrary.initialize()
