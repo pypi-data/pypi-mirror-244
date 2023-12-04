70e5010e33cb2630838e00e18a2d1b1e0c58bb27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:04:54 +0000

    Add package sphinxcontrib-relativeinclude/0.0.5/sphinxcontrib_relativeinclude-0.0.5.tar.gz

---
+++ packages/sphinxcontrib-relativeinclude/sphinxcontrib_relativeinclude-0.0.5.tar.gz/sphinxcontrib_relativeinclude-0.0.5/README.md
@@ -0,0 +1,88 @@
+# sphinxcontrib-relativeinclude
+
+[![Build](https://img.shields.io/github/actions/workflow/status/FynnFreyer/sphinxcontrib-relativeinclude/publish.yml)](https://github.com/FynnFreyer/sphinxcontrib-relativeinclude/actions/workflows/publish.yml)
+[![Docs](https://img.shields.io/github/actions/workflow/status/FynnFreyer/sphinxcontrib-relativeinclude/docs.yml?label=docs)](https://github.com/FynnFreyer/sphinxcontrib-relativeinclude/actions/workflows/docs.yml)
+[![PyPI - Version](https://img.shields.io/pypi/v/sphinxcontrib-relativeinclude.svg)](https://pypi.org/project/sphinxcontrib-relativeinclude)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/sphinxcontrib-relativeinclude.svg)](https://pypi.org/project/sphinxcontrib-relativeinclude)
+[![License: MIT](https://img.shields.io/badge/license-MIT-purple)](https://github.com/FynnFreyer/sphinxcontrib-relativeinclude/blob/main/LICENSE.txt)
+
+## About
+
+This package implements a new reST directive to include files and translate paths included in those files.
+
+## Installation
+
+The project is hosted on [PyPI](https://pypi.org/project/sphinxcontrib-relativeinclude), and can be installed via `pip`.
+
+```console
+pip install sphinxcontrib-relativeinclude
+```
+
+You can find the contents of the README and the module documentation for the latest release [online](https://fynnfreyer.github.io/sphinxcontrib-relativeinclude).
+It's also available [PDF](https://fynnfreyer.github.io/sphinxcontrib-relativeinclude/files/sphinxcontrib-relativeinclude.pdf) or [EPUB](https://fynnfreyer.github.io/sphinxcontrib-relativeinclude/files/sphinxcontrib-relativeinclude.epub).
+
+## Motivation
+
+Let's assume you want to transclude the `README.rst` sitting in your repository root, in your `docs/index.rst`, so it automatically shows up in your generated documentation.
+You can just use reST's standard [`include`](https://docutils.sourceforge.io/docs/ref/rst/directives.html#include) directive.
+
+```rst
+.. include: ../README.rst
+```
+
+**Sidenote:** If you're using a parser, like [MyST](https://myst-parser.readthedocs.io/en/stable/), you could also easily include markdown files, of course.
+
+This will insert the contents of your README in the appropriate place, and even take care of heading levels for you.
+
+The problem arises when you have images or other files included in there. Sphinx won't resolve those links properly, i.e., relative to the README, but instead relative to your Sphinx index document.
+That means those pictures won't show up, which, needless to say, is not optimal.
+If you use your valuable time to create visual resources for your documentation these should also be included in the documentation output.
+
+|                           ![A honey badger](docs/assets/honey_badger-wikimedia_commons_CCBYSA4_Sumeetmoghe.jpg)                          |
+|:----------------------------------------------------------------------------------------------------------------------------------------:|
+| Picture of a honey badger by [Sumeetmoghe on Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Honey_Badger.jpg) (CC-BY-SA-4.0) |
+
+This picture uses a relative path to a file in the [`docs/assets`](docs/assets) directory, and would happily show up in your Git repo, but not in your documentation.
+
+This is what this extension is supposed to solve.
+It defines a new `relativeinclude` directive, that takes relative paths in included files, and resolves them into absolute ones.
+This way your images show up in your documentation output, but you don't have to hardcode absolute paths in your documentation.
+(Cf. this awesome honey badger [here](https://fynnfreyer.github.io/sphinxcontrib-relativeinclude/#motivation))
+
+**Caveat emptor:** At this point in time, nested includes are unfortunately not supported.
+(See [TODOs](#todos))
+
+## Usage
+
+You have to enable the extension in your `conf.py` file.
+
+```python
+# in conf.py
+extensions = [
+    # ...
+    "sphinxcontrib.relativeinclude",
+    # ...
+]
+```
+
+After that, the directive will be available for you to use in your documentation.
+
+```rst
+.. relativeinclude: ../README.md
+    :parser: myst_parser.docutils_
+```
+
+It supports the same options as the standard [`include`](https://docutils.sourceforge.io/docs/ref/rst/directives.html#include) directive.
+If not, you've found a bug, and I'd be happy if you reported it on the [issue tracker](https://github.com/FynnFreyer/sphinxcontrib-relativeinclude/issues).
+Please provide thorough description, and a minimal reproducible example, i.e., (abbreviated) reST files you used, potentially your `conf.py` contents and maybe other relevant info.
+
+If you want to see some real code, check out this repository's documentation at [`docs/index.rst`](https://github.com/FynnFreyer/sphinxcontrib-relativeinclude/blob/main/docs/index.rst).
+
+## TODOs
+
+- support multiple levels of indirection
+- properly register directive on install
+
+## License
+
+`sphinxcontrib-relativeinclude` is distributed under the terms of the [MIT](https://github.com/FynnFreyer/sphinxcontrib-relativeinclude/blob/main/LICENSE.txt) license.
---
+++ packages/sphinxcontrib-relativeinclude/sphinxcontrib_relativeinclude-0.0.5.tar.gz/sphinxcontrib_relativeinclude-0.0.5/sphinxcontrib/relativeinclude.py
@@ -0,0 +1,217 @@
+# SPDX-FileCopyrightText: 2023-present Fynn Freyer <fynn.freyer@googlemail.com>
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Module Documentation
+====================
+
+The :mod:`sphinxcontrib-relativeinclude.relativeinclude` module implements the :class:`RelativeInclude` class, which
+provides a new ``relativeinclude`` directive for reST. This directive works just like a normal include, but it will
+translate relative paths in the included document, so that they're relative to the document we're including from.
+
+.. warning:: This does not support nested includes yet!
+"""
+
+from os.path import relpath
+from pathlib import Path
+from typing import ClassVar, Collection, List
+
+from docutils.nodes import Element, GenericNodeVisitor, Node
+from docutils.nodes import document as Document  # noqa: N812
+from docutils.parsers.rst.directives.misc import Include
+from sphinx import __display_version__
+from sphinx.util import logging
+
+logger = logging.getLogger(__name__)
+
+__app_name__ = "sphinxcontrib-relativeinclude"
+__description__ = "Implements a new reST include directive to translate relative paths."
+
+__author_name__ = "Fynn Freyer"
+__author_email__ = "fynn.freyer@googlemail.com"
+
+__authors__ = [
+    {"name": __author_name__, "email": __author_email__},
+]
+
+__version__ = "0.0.5"
+
+__all__ = ["LinkTranslator", "RelativeInclude", "setup", "__app_name__", "__author_name__", "__version__"]
+
+
+def _identify(obj: object) -> str:
+    """Provide a human-readable identifier for arbitrary objects."""
+    from hashlib import md5
+
+    # this depends on a CPython implementation detail,
+    # and should not be used in arbitrary contexts
+    raw_hash = str(hash(obj) if hasattr(obj, "__hash__") else id(obj)).encode()
+    # it doesn't matter here, because this is protected and only for logging
+    pretty_hash = md5(raw_hash).hexdigest()[:6].upper()  # noqa: S324
+    return f"{obj.__class__.__name__}_{pretty_hash}"
+
+
+class LinkTranslator(GenericNodeVisitor):
+    """
+    Translates relative links in included documents with respect to the source document.
+    """
+
+    resolve_attrs: ClassVar[Collection[str]] = (
+        "reftarget",
+        "uri",
+    )
+    """ Attributes, that need changing. """
+
+    ignore_schemes: ClassVar[Collection[str]] = (
+        "http",
+        "https",
+        "data",
+    )
+    """ If we encounter these prefixes in a link, we leave it alone. """
+
+    def __init__(self, document: Document, absolute_base: Path, relative_base: Path):
+        """
+        Initialize a ``LinkTranslator``.
+
+        :param document: The document Node we're working on.
+        :param absolute_base: The path of the file from which we are including.
+        :param relative_base: The path of the file we are including.
+        """
+        super().__init__(document)
+
+        self.abs_base = absolute_base.resolve().parent
+        self.rel_base = relative_base.resolve().parent
+
+    def default_visit(self, node: Node):
+        """When visiting a node, we change all attrs with relative paths, defined in :attr:`change_attrs`."""
+        # if we're resolved already, or there aren't any attributes to resolve, we move on
+        if not isinstance(node, Element) or (hasattr(node, "resolved") and node.resolved):
+            return
+
+        attrs_to_resolve = [attr for attr in self.resolve_attrs if attr in node.attributes]
+
+        for attr in attrs_to_resolve:
+            old_target = node[attr]
+
+            # if this is a link, or a data URL, we skip it
+            if any(old_target.startswith(scheme) for scheme in self.ignore_schemes):
+                logger.info(f"{_identify(self)}: skipping data URL: {old_target}")
+                continue
+
+            new_absolute_target = (self.rel_base / old_target).resolve()
+            new_relative_target = relpath(new_absolute_target, self.abs_base)
+
+            if new_absolute_target.exists():
+                node[attr] = new_relative_target
+                logger.info(
+                    f"{_identify(self)}: changed {attr} in {_identify(node)} "
+                    f"from {old_target} to {new_relative_target}."
+                )
+            else:
+                logger.warn(
+                    f"{_identify(self)}: couldn't resolve {_identify(node)} "
+                    f"target path {new_absolute_target} derived from {old_target}. Skipping!"
+                )
+
+            if hasattr(node, "resolved"):
+                node.resolved = True  # type: ignore [attr-defined]
+
+    def unknown_visit(self, node: Node):
+        """If we don't know a Node, we ignore it."""
+        logger.warn(f"{_identify(self)}: visited unknown node {_identify(node)} of type {type(node)}")
+
+    def default_departure(self, node: Node):
+        """Override abstract method for completeness."""
+        logger.info(f"{_identify(self)}: departing from {_identify(node)}.")
+
+
+class RelativeInclude(Include):
+    """
+    This class is supposed to implement a ``relativeinclude`` directive in Sphinx. It should automatically translate
+    relative links in included documents and apart from that works just like ``.. include::``.
+    """
+
+    resolve_attrs: ClassVar[Collection[str]] = (
+        "reftarget",
+        "uri",
+    )
+    """ Attributes, that need changing. """
+
+    @property
+    def include_source(self) -> Path:
+        """The absolute path of the document we're including from."""
+        source = None
+
+        if source is None:
+            source = self.state.document.current_source
+
+        return Path(source).resolve()
+
+    @property
+    def include_target(self) -> Path:
+        """The absolute path of the document we're including."""
+        target = Path(self.arguments[0])
+        if target.is_absolute():
+            return target.resolve()  # force error if not existent
+        return Path(self.include_source.parent / target).resolve()
+
+    def run(self) -> List[Node]:
+        """
+        Include a file as part of the content of this reST file, and translate any relative paths in the included file.
+        """
+
+        included_nodes = super().run()
+
+        translator = LinkTranslator(self.state.document, self.include_source, self.include_target)
+        for node in included_nodes:
+            node.walk(translator)
+
+        return included_nodes
+
+    def translate(self, node: Node, absolute_base: Path, relative_base: Path):
+        """
+        Recursively translate relative nodes.
+
+        :param node: The node we're currently working on.
+        :param absolute_base: The absolute path of the file from which we are including.
+        :param relative_base: The absolute path of the file we are including.
+        """
+        # if we're resolved already, or there aren't any attributes to resolve, we move on
+        if not isinstance(node, Element) or (
+            hasattr(node, "resolved") and node.resolved  # type: ignore [attr-defined]
+        ):
+            return
+
+        attrs_to_resolve = [attr for attr in self.resolve_attrs if attr in node.attributes]
+
+        for attr in attrs_to_resolve:
+            old_target = node[attr]
+            new_absolute_target = (relative_base / old_target).resolve()
+            new_relative_target = relpath(new_absolute_target, absolute_base)
+
+            # the previously included path is the one we're including from now
+            for child in node.children:
+                self.translate(child, relative_base, new_absolute_target)
+
+            node[attr] = new_relative_target
+
+            logger.info(
+                f"{_identify(self)}: "
+                f"changed {attr} in {_identify(node)} "
+                f"from {old_target} to {new_absolute_target}."
+            )
+
+            if hasattr(node, "resolved"):
+                node.resolved = True  # type: ignore [attr-defined]
+
+
+def setup(app):
+    """Register the directive."""
+    app.add_directive("relativeinclude", RelativeInclude)
+
+    return {
+        "version": __display_version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
