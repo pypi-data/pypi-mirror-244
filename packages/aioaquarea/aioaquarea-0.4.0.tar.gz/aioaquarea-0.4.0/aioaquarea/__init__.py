10c26c00abac3057843a7a837aaa8367eb819fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 04:03:51 +0000

    Add package aioaquarea/0.4.0/aioaquarea-0.4.0.tar.gz

---
+++ packages/aioaquarea/aioaquarea-0.4.0.tar.gz/aioaquarea-0.4.0/aioaquarea/__init__.py
@@ -0,0 +1,48 @@
+"""Asynchronous library to control Panasonic Aquarea devices"""
+from __future__ import annotations
+
+from typing import Tuple
+
+from .core import Client
+from .data import (
+    Device,
+    DeviceAction,
+    DeviceInfo,
+    DeviceStatus,
+    ExtendedOperationMode,
+    OperationStatus,
+    QuietMode,
+    Tank,
+    UpdateOperationMode,
+)
+from .errors import (
+    ApiError,
+    AuthenticationError,
+    AuthenticationErrorCodes,
+    ClientError,
+    DataNotAvailableError,
+    RequestFailedError,
+)
+from .statistics import Consumption, ConsumptionType, DateType
+
+__all__: Tuple[str, ...] = (
+    "Client",
+    "Device",
+    "ClientError",
+    "RequestFailedError",
+    "AuthenticationError",
+    "AuthenticationErrorCodes",
+    "ApiError",
+    "DeviceInfo",
+    "DeviceStatus",
+    "DeviceAction",
+    "Tank",
+    "OperationStatus",
+    "ExtendedOperationMode",
+    "UpdateOperationMode",
+    "QuietMode",
+    "DateType",
+    "Consumption",
+    "ConsumptionType",
+    "DataNotAvailableError",
+)
