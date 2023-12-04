c83f6cbff1936e078daa24da2fdc17dc735e4968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 14:05:31 +0000

    Add package payos-lib-python/0.2.0/payos-lib-python-0.2.0.tar.gz

---
+++ packages/payos-lib-python/payos-lib-python-0.2.0.tar.gz/payos-lib-python-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+LONG_DESCRIPTION = 'A package that allows to create payment link, cancel order, confirm webhook and get payment link information'
+
+setup(
+    name='payos-lib-python',
+    version='0.2.0',
+    author='Casso',
+    author_email='tranglq@casso.vn',
+    description='A library for create Payment Link of PayOS and more',
+    long_description_content_type="text/markdown",
+
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+    install_requires=['requests'],
+        keywords=['payos', 'vietqrpro', 'payos-python'],
+
+)
