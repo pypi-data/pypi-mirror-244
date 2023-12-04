121c1420ca00afbedc7b9a9f753b78574da2f369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 16:04:39 +0000

    Add package opendr-toolkit-compressive-learning/3.0.0/opendr-toolkit-compressive-learning-3.0.0.tar.gz

---
+++ packages/opendr-toolkit-compressive-learning/opendr-toolkit-compressive-learning-3.0.0.tar.gz/opendr-toolkit-compressive-learning-3.0.0/src/opendr/perception/compressive_learning/multilinear_compressive_learning/algorithm/data.py
@@ -0,0 +1,29 @@
+# Copyright 2020-2023 OpenDR European Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+
+
+class DataWrapper:
+    def __init__(self, opendr_dataset):
+        self.dataset = opendr_dataset
+
+    def __len__(self,):
+        return len(self.dataset)
+
+    def __getitem__(self, i):
+        x, y = self.dataset.__getitem__(i)
+        # change from rows x cols x channels to channels x rows x cols
+        x = x.convert("channels_first", "rgb")
+        return torch.from_numpy(x).float(), torch.tensor([y.data, ]).long()
