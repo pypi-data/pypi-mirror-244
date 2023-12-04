3279a77b6381c60a91e18bcef87ef2e6029dce0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 16:04:37 +0000

    Add package flask-build/1.0.20/flask-build-1.0.20.tar.gz

---
+++ packages/flask-build/flask-build-1.0.20.tar.gz/flask-build-1.0.20/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='flask-build',
+    version='1.0.20',
+    packages=['flask_build'],  # 包含的包列表，包括子包，可用find_pakages()
+    entry_points={
+        'console_scripts': [
+            'build = flask_build:main',
+        ],
+    }
+)
+
+
+
