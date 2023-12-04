dfae1f466fb098f603bc96e7e9fbdd14c7448959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:04:25 +0000

    Add package pyrecipes/0.1.10/pyrecipes-0.1.10-py3-none-any.whl

---
+++ packages/pyrecipes/pyrecipes-0.1.10-py3-none-any.whl/pyrecipes/recipes/06_data_encoding_and_processing/08_interacting_with_a_relational_database/example.py
@@ -0,0 +1,44 @@
+"""
+You need to select, insert or delete rows in a relational database.
+"""
+import sqlite3
+from tempfile import NamedTemporaryFile
+
+
+def main():
+    stocks = [
+        ("GOOG", 100, 490.1),
+        ("AAPL", 50, 545.75),
+        ("FB", 150, 7.45),
+        ("HPQ", 75, 33.2),
+    ]
+    print("stocks:", stocks)
+
+    with NamedTemporaryFile() as f:
+        db = sqlite3.connect(f.name)
+        print(db)
+
+        c = db.cursor()
+        c.execute(
+            """
+            CREATE TABLE portfolio
+            (symbol text,
+            shares integer,
+            price real)
+            """
+        )
+        db.commit()
+
+        c.executemany(
+            """
+            INSERT INTO portfolio VALUES (?, ?, ?)
+            """,
+            stocks,
+        )
+
+        for row in db.execute("SELECT * FROM portfolio"):
+            print(row)
+
+
+if __name__ == "__main__":
+    main()
