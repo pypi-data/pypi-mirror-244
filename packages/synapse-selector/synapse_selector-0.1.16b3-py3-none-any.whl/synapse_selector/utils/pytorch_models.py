8d1b9a19f31c7428a3d6bdbf35a318950906db50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 16:04:20 +0000

    Add package synapse-selector/0.1.16b3/synapse_selector-0.1.16b3-py3-none-any.whl

---
+++ packages/synapse-selector/synapse_selector-0.1.16b3-py3-none-any.whl/synapse_selector/utils/pytorch_models.py
@@ -0,0 +1,24 @@
+from .iglu_cnn import PeakDetectionModel_convout
+import torch
+import numpy as np
+
+
+class torch_cnn_model:
+    def __init__(
+        self,
+        model_path: str = "/Users/stephanweissbach/Desktop/synapse_selector_detect/models/anomaly_50_conv-out.pth",
+        window_len: int = 50,
+    ) -> None:
+        # define structure
+        self.model = PeakDetectionModel_convout()
+        # load model
+        self.model = torch.load(model_path)
+        self.model.eval()
+        self.window_len = window_len
+        self.to_pad = 0
+
+    def predict(self, arr: np.ndarray, threshold: float = 0.5) -> np.ndarray:
+        input_tensor = torch.tensor(arr, dtype=torch.float)
+        preds = self.model(input_tensor).detach().flatten()
+        infered_peaks = np.argwhere(preds > threshold)[0]
+        return infered_peaks
