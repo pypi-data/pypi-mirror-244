74b281bdb079809686263ee8c71d231894220404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 16:04:21 +0000

    Add package ktoolbox-pure-py/0.2.3/ktoolbox_pure_py-0.2.3.tar.gz

---
+++ packages/ktoolbox-pure-py/ktoolbox_pure_py-0.2.3.tar.gz/ktoolbox_pure_py-0.2.3/ktoolbox/__main__.py
@@ -0,0 +1,17 @@
+import fire
+from loguru import logger
+
+from ktoolbox.cli import KToolBoxCli
+from ktoolbox.configuration import config
+from ktoolbox.utils import logger_init, uvloop_init, generate_msg
+
+
+def main():
+    logger_init(cli_use=True)
+    logger.debug(generate_msg(config=config))
+    uvloop_init()
+    fire.Fire(KToolBoxCli)
+
+
+if __name__ == "__main__":
+    main()
