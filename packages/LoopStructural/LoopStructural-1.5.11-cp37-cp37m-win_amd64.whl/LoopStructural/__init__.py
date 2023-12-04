617f3cab0adff34bea56971a243e77b7f61186a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 14:05:07 +0000

    Add package LoopStructural/1.5.11/LoopStructural-1.5.11-cp37-cp37m-win_amd64.whl

---
+++ packages/LoopStructural/LoopStructural-1.5.11-cp37-cp37m-win_amd64.whl/LoopStructural/__init__.py
@@ -0,0 +1,27 @@
+"""
+LoopStructural API
+=======================
+
+"""
+
+import logging
+from logging.config import dictConfig
+
+__all__ = ["GeologicalModel"]
+import tempfile
+from pathlib import Path
+from .version import __version__
+
+experimental = False
+ch = logging.StreamHandler()
+formatter = logging.Formatter(
+    "%(levelname)s: %(asctime)s: %(filename)s:%(lineno)d -- %(message)s"
+)
+ch.setFormatter(formatter)
+ch.setLevel(logging.WARNING)
+loggers = {}
+from .modelling.core.geological_model import GeologicalModel
+from .utils import log_to_console, log_to_file, getLogger
+
+logger = getLogger(__name__)
+logger.info("Imported LoopStructural")
