311c03c85385eaef5dcafa0c08336eee452c2bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 15:03:57 +0000

    Add package feffery-utils-components/0.2.0b9/feffery_utils_components-0.2.0b9.tar.gz

---
+++ packages/feffery-utils-components/feffery_utils_components-0.2.0b9.tar.gz/feffery_utils_components-0.2.0b9/feffery_utils_components/FefferyHexColorPicker.py
@@ -0,0 +1,49 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class FefferyHexColorPicker(Component):
+    """A FefferyHexColorPicker component.
+
+
+Keyword arguments:
+
+- id (string; optional)
+
+- className (string; optional)
+
+- color (string; default '#44cef6')
+
+- loading_state (dict; optional)
+
+    `loading_state` is a dict with keys:
+
+    - component_name (string; optional):
+        Holds the name of the component that is loading.
+
+    - is_loading (boolean; optional):
+        Determines if the component is loading or not.
+
+    - prop_name (string; optional):
+        Holds which property is loading.
+
+- showAlpha (boolean; default False)
+
+- style (dict; optional)"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'feffery_utils_components'
+    _type = 'FefferyHexColorPicker'
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, className=Component.UNDEFINED, style=Component.UNDEFINED, color=Component.UNDEFINED, showAlpha=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'className', 'color', 'loading_state', 'showAlpha', 'style']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'className', 'color', 'loading_state', 'showAlpha', 'style']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        super(FefferyHexColorPicker, self).__init__(**args)
