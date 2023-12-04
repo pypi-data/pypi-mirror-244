1a94fe3916af227cd14a6902e015888e91213ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:03:40 +0000

    Add package nextpy-ai/0.1.24/nextpy_ai-0.1.24.tar.gz

---
+++ packages/nextpy-ai/nextpy_ai-0.1.24.tar.gz/nextpy_ai-0.1.24/openams/__init__.py
@@ -0,0 +1,25 @@
+from importlib import metadata
+from typing import Optional
+from . import agent
+from . import audio
+from . import engine
+from . import helpers
+from . import image_gen
+from . import rag
+from . import memory
+from . import tools
+from . import utils
+from .rag import vectordb
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    # Case where package metadata is not available.
+    __version__ = ""
+
+# Clear up the namespace
+del metadata
+
+# Debug options
+verbose: bool = False
+debug: bool = False
