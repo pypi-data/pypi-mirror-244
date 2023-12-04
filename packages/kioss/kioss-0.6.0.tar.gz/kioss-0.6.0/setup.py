c6a68db060818406cf6b98aafb1f0615619623a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 14:05:31 +0000

    Add package kioss/0.6.0/kioss-0.6.0.tar.gz

---
+++ packages/kioss/kioss-0.6.0.tar.gz/kioss-0.6.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup # type: ignore
+
+setup(
+    name='kioss',
+    version='0.6.0',
+    packages=['kioss'],
+    url='http://github.com/bonnal-enzo/kioss',
+    license='Apache 2.',
+    author='bonnal-enzo',
+    author_email='bonnal.enzo.dev@gmail.com',
+    description='Keep I/O Simple and Stupid: Library providing a expressive Iterator-based interface to write ETL pipelines.'
+)
