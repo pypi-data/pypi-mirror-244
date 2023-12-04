0ae68281a531f6b28a9e3f1dc4893cb1c97525f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 14:05:12 +0000

    Add package lightning-rod/1.0.0/lightning_rod-1.0.0.tar.gz

---
+++ packages/lightning-rod/lightning_rod-1.0.0.tar.gz/lightning_rod-1.0.0/lightning_rod/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "1.0.0"
+
+
+from beet import Context
+from beet.contrib.load import load
+
+
+def beet_default(ctx: Context):
+    ctx.require(
+        load(
+            data_pack={
+                "data/lightning_rod/modules": "@lightning_rod/modules",
+            },
+        ),
+        "wicked_expressions",
+    )
