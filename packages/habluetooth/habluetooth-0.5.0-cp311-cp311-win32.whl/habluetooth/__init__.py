e8fb38fbd3800f3b354a2b449a1b5c2c8dc82911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 14:05:11 +0000

    Add package habluetooth/0.5.0/habluetooth-0.5.0-cp311-cp311-win32.whl

---
+++ packages/habluetooth/habluetooth-0.5.0-cp311-cp311-win32.whl/habluetooth/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "0.5.0"
+
+from .base_scanner import BaseHaRemoteScanner, BaseHaScanner
+from .const import (
+    CONNECTABLE_FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS,
+    FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS,
+    SCANNER_WATCHDOG_INTERVAL,
+    SCANNER_WATCHDOG_TIMEOUT,
+)
+from .models import HaBluetoothConnector
+from .scanner import BluetoothScanningMode, HaScanner, ScannerStartError
+
+__all__ = [
+    "BluetoothScanningMode",
+    "ScannerStartError",
+    "HaScanner",
+    "BaseHaScanner",
+    "BaseHaRemoteScanner",
+    "HaBluetoothConnector",
+    "FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS",
+    "CONNECTABLE_FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS",
+    "SCANNER_WATCHDOG_TIMEOUT",
+    "SCANNER_WATCHDOG_INTERVAL",
+]
