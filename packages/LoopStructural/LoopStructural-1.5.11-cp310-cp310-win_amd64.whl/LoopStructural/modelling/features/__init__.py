92824eb7103b1d2b6b522d16f10f7166fe274454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 14:05:05 +0000

    Add package LoopStructural/1.5.11/LoopStructural-1.5.11-cp310-cp310-win_amd64.whl

---
+++ packages/LoopStructural/LoopStructural-1.5.11-cp310-cp310-win_amd64.whl/LoopStructural/modelling/features/__init__.py
@@ -0,0 +1,36 @@
+from enum import IntEnum
+from ...utils import getLogger
+
+
+class FeatureType(IntEnum):
+    """
+    Enum for the different interpolator types
+
+    1-9 should cover interpolators with supports
+    9+ are data supported
+    """
+
+    BASE = 0
+    INTERPOLATED = 1
+    STRUCTURALFRAME = 2
+    REGION = 3
+    FOLDED = 4
+    ANALYTICAL = 5
+    LAMBDA = 6
+    UNCONFORMITY = 7
+    INTRUSION = 8
+    FAULT = 9
+    DOMAINFAULT = 10
+
+
+from ._base_geological_feature import BaseFeature
+from ._geological_feature import GeologicalFeature
+from ._lambda_geological_feature import LambdaGeologicalFeature
+
+# from .builders._geological_feature_builder import GeologicalFeatureBuilder
+from ._structural_frame import StructuralFrame
+from ._cross_product_geological_feature import CrossProductGeologicalFeature
+
+from ._unconformity_feature import UnconformityFeature
+from ._analytical_feature import AnalyticalGeologicalFeature
+from ._structural_frame import StructuralFrame
