79338118afea354df304b085e9dfe9897551f6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 04:03:56 +0000

    Add package Geode-Implicit/2.5.1rc1/Geode_Implicit-2.5.1rc1-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/Geode-Implicit/Geode_Implicit-2.5.1rc1-cp311-cp311-manylinux2014_x86_64.whl/geode_implicit/model_io.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode_geosciences
+import opengeode_geosciencesio
+
+from .lib64.geode_implicit_py_io import *
+ImplicitIOLibrary.initialize()
