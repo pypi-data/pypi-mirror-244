d5dad654c95db176ff78328c07d070f9aa3b814a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:05:27 +0000

    Add package iterative/0.1.0/iterative-0.1.0.tar.gz

---
+++ packages/iterative/iterative-0.1.0.tar.gz/iterative-0.1.0/src/iterative/models/config.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from iterative.models.iterative import IterativeModel
+from typing import Optional
+import logging
+
+# logging level enum
+class LoggingLevel(str, Enum):
+    DEBUG = logging.DEBUG
+    INFO = logging.INFO
+    WARNING = logging.WARNING
+    ERROR = logging.ERROR
+    CRITICAL = logging.CRITICAL
+
+
+class IterativeAppConfig(IterativeModel):
+    actions_search_path: Optional[str] = "actions"
+    model_generation_path: Optional[str] = "models"
+    services_generation_path: Optional[str] = "services"
+    api_generation_path: Optional[str] = "api"
+    data_path: Optional[str] = "data"
+    logs_path: Optional[str] = "logs"
+    tests_path: Optional[str] = "tests"
+    reload_dirs: Optional[list[str]] = ["."]
+    reload: Optional[bool] = True
+    persist_cache_as_db: Optional[bool]
+    default_ai_model: Optional[str]
+    fastapi_port: Optional[int]
+    fastapi_host: Optional[str]
+    do_not_discover: Optional[bool]
+    assistant_id: Optional[str]
+    assistant_conversation_thread_id: Optional[str]
+    logging_level: Optional[LoggingLevel] = LoggingLevel.INFO
