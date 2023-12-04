45a9d6f12e7a4958a7a86ff4a2c9556b8c92bfa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:05:03 +0000

    Add package amazon-denseclus/0.2.0/amazon_denseclus-0.2.0.tar.gz

---
+++ packages/amazon-denseclus/amazon_denseclus-0.2.0.tar.gz/amazon_denseclus-0.2.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env/python3
+import setuptools
+
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="amazon_denseclus",
+    version="0.2.0",
+    author="Charles Frenzel & Baichuan Sun",
+    description="Dense Clustering for Mixed Data Types",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/awslabs/amazon-denseclus",
+    packages=setuptools.find_packages(include=["denseclus", "denseclus.*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires=">=3.10, <3.12",
+    license_files=("LICENSE",),
+    install_requires=required,
+    keywords="amazon dense clustering",
+    project_urls={
+        "Bug Tracker": "https://github.com/awslabs/amazon-denseclus/issues",
+        "Documentation": "https://github.com/awslabs/amazon-denseclus/notebooks",
+        "Source Code": "https://github.com/awslabs/amazon-denseclus",
+    },
+    platforms=["any"],
+)
