5dd414772fc053d30f0ef9d3df30c994ddcf66dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 04:04:13 +0000

    Add package evercycle-models/0.0.33/evercycle_models-0.0.33-py3-none-any.whl

---
+++ packages/evercycle-models/evercycle_models-0.0.33-py3-none-any.whl/evercycle_models/models/integrations.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Integrations(models.Model):
+    id = models.IntegerField(primary_key=True)
+    created_at = models.DateTimeField()
+    name = models.CharField(max_length=50)
+    oauth_redirect_url = models.CharField(max_length=50)
+    metadata = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    logo = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'integrations'
+
