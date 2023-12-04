fcdad3854fd4f9952ec326e95e98039bf1cb4ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 04:04:05 +0000

    Add package ex-cd/1.8.1/ex_cd-1.8.1.tar.gz

---
+++ packages/ex-cd/ex_cd-1.8.1.tar.gz/ex_cd-1.8.1/ex_cd/gallery_dl_exec.py
@@ -0,0 +1,22 @@
+import subprocess
+
+
+def main(*args):
+    return subprocess.Popen(args=args).wait()
+
+
+if __name__ == "__main__":
+    import sys
+    import os
+    # main(sys.executable, "-m", "gallery_dl", "-h")
+    os.chdir(os.path.dirname(os.path.dirname(sys.argv[0])))
+    main(
+        "./gallery-dl/gallery-dl.exe",
+        "--no-download",
+        "--write-metadata",
+        "--postprocessor-option", 'directory=metadata',
+        '-v',
+        '--sleep-request', '1',
+        '-c', '.vscode/gallery-dl.config.json',
+        'https://exhentai.org/g/2752577/8ffd3778cb/'
+    )
