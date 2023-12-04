db3cdc84bc3abf4d60065388f5f0c84dca0f3a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:05:03 +0000

    Add package amazon-denseclus/0.2.0/amazon_denseclus-0.2.0-py3-none-any.whl

---
+++ packages/amazon-denseclus/amazon_denseclus-0.2.0-py3-none-any.whl/denseclus/__init__.py
@@ -0,0 +1,14 @@
+"""
+Dense Clus Library
+
+Authors: Charles Frenzel, Baichaun Sun
+Date: November 2023
+"""
+
+from .DenseClus import DenseClus
+from .utils import extract_categorical, extract_numerical
+
+__version__ = "0.2.0"
+
+if __name__ == "__main__":  # pragma: no cover
+    print(type(DenseClus), type(extract_categorical), type(extract_numerical))
