3f177c64b1f7208b6c7844c1a95d3848817736e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:04:24 +0000

    Add package sangmyung-univ-auth/1.0.3/sangmyung-univ-auth-1.0.3.tar.gz

---
+++ packages/sangmyung-univ-auth/sangmyung-univ-auth-1.0.3.tar.gz/sangmyung-univ-auth-1.0.3/sangmyung_univ_auth/auth.py
@@ -0,0 +1,13 @@
+from sangmyung_univ_auth.authenticator import authenticate, get_userinfo
+from sangmyung_univ_auth.response import AuthResponse, _auth_failed, _success, _unknown_issue
+
+
+def auth(username: str, password: str) -> AuthResponse:
+    try:
+        session = authenticate(username, password)
+        if not session:
+            return _auth_failed()
+        userinfo: dict = get_userinfo(session)
+        return _success(body=userinfo)
+    except Exception:
+        return _unknown_issue()
