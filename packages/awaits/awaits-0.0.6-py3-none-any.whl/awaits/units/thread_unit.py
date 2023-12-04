6bbf9e8997c23e6af8a37ca4c480cc9d1fc23ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 15:04:00 +0000

    Add package awaits/0.0.6/awaits-0.0.6-py3-none-any.whl

---
+++ packages/awaits/awaits-0.0.6-py3-none-any.whl/awaits/units/thread_unit.py
@@ -0,0 +1,23 @@
+from awaits.units.abstract_unit import AbstractUnit
+
+
+class ThreadUnit(AbstractUnit):
+    """
+    Экземпляр класса соответствует одному потоку. Здесь выполняются таски.
+    """
+    def __init__(self, queue, pool, index):
+        self.index = index
+        self.queue = queue
+        self.pool = pool
+
+    def run(self):
+        """
+        Принимаем из очереди таски и выполняем их.
+        """
+        while True:
+            try:
+                task = self.queue.get()
+                task.do()
+                self.queue.task_done()
+            except Exception:
+                pass
