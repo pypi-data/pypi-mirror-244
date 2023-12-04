43f2038730af23f1bda6713ab1f9075cbf62ad13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:05:02 +0000

    Add package auto-ts/0.0.82/auto_ts-0.0.82.tar.gz

---
+++ packages/auto-ts/auto_ts-0.0.82.tar.gz/auto_ts-0.0.82/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="auto_ts",
+    version="0.0.82",
+    author="Ram Seshadri",
+    # author_email="author@example.com",
+    description="Automatically Build Multiple Time Series models fast - now with Facebook Prophet!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache License 2.0',
+    url="https://github.com/AutoViML/Auto_TS",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=[
+        "ipython",
+        "jupyter",
+        "pmdarima",
+        "numpy",
+        "xlrd",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "prophet",
+        "scikit-learn>=0.24.0",
+        "statsmodels",
+        "xgboost>=1.5.1",
+        "prettytable",
+        "dask>=2022.1.0",
+        "pyyaml>=5.4.1",
+        "distributed>=2022.2.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
