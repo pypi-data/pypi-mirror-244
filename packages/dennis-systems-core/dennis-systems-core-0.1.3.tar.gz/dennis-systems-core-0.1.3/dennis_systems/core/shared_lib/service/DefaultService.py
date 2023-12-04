9f8b84a06611088c8d5551f0df58cc6a13a4ac4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 16:04:50 +0000

    Add package dennis-systems-core/0.1.3/dennis-systems-core-0.1.3.tar.gz

---
+++ packages/dennis-systems-core/dennis-systems-core-0.1.3.tar.gz/dennis-systems-core-0.1.3/dennis_systems/core/shared_lib/service/DefaultService.py
@@ -0,0 +1,21 @@
+from typing import List, Optional
+
+from django.db.models import Model
+
+
+class DefaultService:
+    def list(self, limit: int, page: int) -> List[Model]:
+        pass
+
+    def save(self, item: Model) -> Model:
+        pass
+
+    def delete(self, item: Model) -> Model:
+        pass
+
+    def get(self, id: int) -> Optional[Model]:
+        pass
+
+    def save_item(self, item: Model) -> Model:
+        item.save()
+        return item
