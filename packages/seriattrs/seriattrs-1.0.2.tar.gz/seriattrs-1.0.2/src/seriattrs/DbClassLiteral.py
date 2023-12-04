e30b30ebdde68ea71e7ae197df2f2e0d9dce6811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:03:58 +0000

    Add package seriattrs/1.0.2/seriattrs-1.0.2.tar.gz

---
+++ packages/seriattrs/seriattrs-1.0.2.tar.gz/seriattrs-1.0.2/src/seriattrs/DbClassLiteral.py
@@ -0,0 +1,32 @@
+from attr import fields
+from attrs import define
+
+from .DbClass import DbClass
+from .db_attrs_converter import db_attrs_converter
+
+
+@define
+class DbClassLiteral(DbClass):
+    pass
+
+
+def _handle_new_db(value, db_class_type):
+    if isinstance(value, int):
+        return value
+    id_value = value.pop("_id")
+    new_instance = db_class_type(**value)
+    new_instance._id = id_value
+    value["_id"] = id_value
+    for f in fields(db_class_type):
+        if issubclass(f.type, DbClassLiteral):
+            setattr(
+                new_instance,
+                f.name,
+                db_attrs_converter.structure(value[f.name], f.type),
+            )
+    return new_instance
+
+
+db_attrs_converter.register_structure_hook_func(
+    lambda t: issubclass(t, DbClass), _handle_new_db
+)
