8c52af32b0b94f1df3958e1a7936235ec5885783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 16:04:38 +0000

    Add package cg/53.4.1/cg-53.4.1.tar.gz

---
+++ packages/cg/cg-53.4.1.tar.gz/cg-53.4.1/cg/constants/orderforms.py
@@ -0,0 +1,39 @@
+from enum import StrEnum
+
+from cg.constants import ANALYSIS_SOURCES, METAGENOME_SOURCES
+from cg.models.orders.order import OrderType
+
+SEX_MAP = {"male": "M", "female": "F", "unknown": "unknown"}
+REV_SEX_MAP = {value: key for key, value in SEX_MAP.items()}
+CONTAINER_TYPES = ["Tube", "96 well plate"]
+SOURCE_TYPES = set().union(METAGENOME_SOURCES, ANALYSIS_SOURCES)
+
+CASE_PROJECT_TYPES = [
+    str(OrderType.MIP_DNA),
+    str(OrderType.BALSAMIC),
+    str(OrderType.MIP_RNA),
+]
+
+
+class Orderform(StrEnum):
+    BALSAMIC: str = "1508"
+    BALSAMIC_QC: str = "1508"
+    BALSAMIC_UMI: str = "1508"
+    FASTQ: str = "1508"
+    FLUFFY: str = "1604"
+    METAGENOME: str = "1605"
+    MICROSALT: str = "1603"
+    MIP_DNA: str = "1508"
+    MIP_RNA: str = "1508"
+    RNAFUSION: str = "1508"
+    RML: str = "1604"
+    SARS_COV_2: str = "2184"
+
+
+ORDERFORM_VERSIONS = {
+    Orderform.MIP_DNA: "29",
+    Orderform.RML: "17",
+    Orderform.METAGENOME: "11",
+    Orderform.MICROSALT: "11",
+    Orderform.SARS_COV_2: "7",
+}
