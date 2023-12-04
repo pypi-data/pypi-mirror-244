d6f2a8819ada590c33f4bc279306109283134abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 15:03:47 +0000

    Add package reytool/0.304/reytool-0.304-py3-none-any.whl

---
+++ packages/reytool/reytool-0.304-py3-none-any.whl/reytool/__init__.py
@@ -0,0 +1,37 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2022-12-05 14:09:21
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Rey's personal tool set.
+
+Modules
+-------
+rall : All methods.
+rcompress : Compress methods.
+rdata : Data methods.
+rdatabase : Database methods.
+remail : E-mail methods.
+ros : Operation system methods.
+rimage : Image methods.
+rlog : Log methods.
+rmonkey : Monkey patch methods.
+rmultitask : Multi task methods.
+rnumber : Number methods.
+rstdout : Standard output methods.
+rregular : Regular methods.
+rrequest : Request methods.
+rsystem : Interpreter system methods.
+rtable : Table methods.
+rtext : Text methods.
+rtime : Time methods.
+rwrap : Decorators.
+"""
+
+
+from typing import Final
+
+
+__version__: Final[str] = "0.304"
