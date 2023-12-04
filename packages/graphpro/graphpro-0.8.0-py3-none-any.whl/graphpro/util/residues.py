58b12d4eda84d0432052ab81a1bdec81bde6c4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:05:27 +0000

    Add package graphpro/0.8.0/graphpro-0.8.0-py3-none-any.whl

---
+++ packages/graphpro/graphpro-0.8.0-py3-none-any.whl/graphpro/util/residues.py
@@ -0,0 +1,27 @@
+_RES_NAME = {
+    'GLY': 'G',
+    'ALA': 'A',
+    'LEU': 'L',
+    'MET': 'M',
+    'PHE': 'F',
+    'TRP': 'W',
+    'LYS': 'K',
+    'GLN': 'Q',
+    'GLU': 'E',
+    'SER': 'S',
+    'PRO': 'P',
+    'VAL': 'V',
+    'ILE': 'I',
+    'CYS': 'C',
+    'TYR': 'Y',
+    'HSD': 'H',
+    'HIS': 'H',
+    'ARG': 'R',
+    'ASN': 'N',
+    'ASP': 'D',
+    'THR': 'T',
+}
+
+
+def one_letter_res(res: str) -> str:
+    return _RES_NAME.get(res, 'X')
