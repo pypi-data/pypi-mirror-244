5185f5152a5629daeed479245d219ee1abe9bc5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 03:05:02 +0000

    Add package pyAutoSummarizer/1.1.8/pyAutoSummarizer-1.1.8.tar.gz

---
+++ packages/pyAutoSummarizer/pyAutoSummarizer-1.1.8.tar.gz/pyAutoSummarizer-1.1.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name='pyAutoSummarizer',
+    version='1.1.8',
+    license='GNU',
+    author='Valdecy Pereira',
+    author_email='valdecy.pereira@gmail.com',
+    url='https://github.com/Valdecy/pyAutoSummarizer',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'chardet',
+        'numpy',
+        'llmx',
+        'regex',
+        'scikit-learn',
+        'sentence_transformers',
+        'openai',
+        'transformers'
+    ],
+    zip_safe=True,
+    description='An Extractive and Abstractive Summarization Library Powered with Artificial Intelligence',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
