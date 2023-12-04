623fb936828fcec83b043c568653fe2e041f8fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-04 14:05:08 +0000

    Add package LoopStructural/1.5.11/LoopStructural-1.5.11-cp39-cp39-win_amd64.whl

---
+++ packages/LoopStructural/LoopStructural-1.5.11-cp39-cp39-win_amd64.whl/LoopStructural/datasets/_example_models.py
@@ -0,0 +1,13 @@
+from _base import load_claudius
+from .. import GeologicalModel
+
+vis = True
+try:
+    from LoopStructural.visualisation import LavaVuModelViewer
+except:
+    print("No visualisation")
+    vis = False
+
+
+def _build_claudius():
+    pass
