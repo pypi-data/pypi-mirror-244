fdd21b8e61c321ec99ddf7bfd2d8c83df67af05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:05:21 +0000

    Add package pipeyaml/0.1.0/pipeyaml-0.1.0.tar.gz

---
+++ packages/pipeyaml/pipeyaml-0.1.0.tar.gz/pipeyaml-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='pipeyaml',
+    version='0.1.0',
+    description='Access YAML data in the CLI.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/skovranek/yaml_pipe',
+    author='Matt Skovranek',
+    author_email='mattjskov@gmail.com',
+    license='GPL-3.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+    ],
+    keywords='yaml json utility extract autocomplete',
+    packages=find_packages(['yaml_pipe', 'yaml_pipe.*']),
+    install_requires=[
+        'argcomplete==3.1.6',
+        'pyparsing==3.1.1',
+        'PyYAML==6.0.1',
+        'ruamel.yaml==0.17.35',
+        'ruamel.yaml.clib==0.2.8'
+    ],
+    python_requires='>=3.12.0',
+    entry_points={
+        'console_scripts': ['yamlpipe=yaml_pipe.main:main']
+    }
+)
