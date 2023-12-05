e326ea7fc42c50f6f00c1019dada3b3d53adee9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 06:04:16 +0000

    Add package JeriPy/0.1.1/JeriPy-0.1.1.tar.gz

---
+++ packages/JeriPy/JeriPy-0.1.1.tar.gz/JeriPy-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JeriPy',
+    packages=find_packages(),
+    version='0.1.1',
+    author='Felipe Gomez',
+    author_email='felipenicolasgomezmolina@gmail.com',
+    description='JeriPy es una libreria que permite la traducción de espanol a Jerigonzo y de jerigonzo a espanol.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Felipe1401/JeriPy.git',
+    install_requires=[
+        "pyphen",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    license='MIT',
+)
