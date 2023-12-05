3173553d6c3e1341d3a1764a45f066f9e3abbc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 03:05:46 +0000

    Add package pulser-core/0.16.0/pulser_core-0.16.0-py3-none-any.whl

---
+++ packages/pulser-core/pulser_core-0.16.0-py3-none-any.whl/pulser/json/abstract_repr/validation.py
@@ -0,0 +1,41 @@
+# Copyright 2022 Pulser Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Function for validation of JSON serialization to abstract representation."""
+import json
+from typing import Literal
+
+import jsonschema
+from referencing import Registry, Resource
+
+from pulser.json.abstract_repr import SCHEMAS
+
+REGISTRY: Registry = Registry().with_resources(
+    [("device-schema.json", Resource.from_contents(SCHEMAS["device"]))]
+)
+
+
+def validate_abstract_repr(
+    obj_str: str, name: Literal["sequence", "device"]
+) -> None:
+    """Validate the abstract representation of an object.
+
+    Args:
+        obj_str: A JSON-formatted string encoding the object.
+        name: The type of object to validate (can be "sequence" or "device").
+    """
+    obj = json.loads(obj_str)
+    validate_args = dict(instance=obj, schema=SCHEMAS[name])
+    if name == "sequence":
+        validate_args["registry"] = REGISTRY
+    jsonschema.validate(**validate_args)
