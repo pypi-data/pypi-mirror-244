d504461ded5d0e883350b6ca7116abc2ba939307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 15:04:00 +0000

    Add package edc-auth/0.3.70/edc-auth-0.3.70.tar.gz

---
+++ packages/edc-auth/edc-auth-0.3.70.tar.gz/edc-auth-0.3.70/runtests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import logging
+from os.path import abspath, dirname
+
+from edc_constants.constants import IGNORE
+from edc_test_utils import DefaultTestSettings, func_main
+
+app_name = "edc_auth"
+base_dir = dirname(abspath(__file__))
+
+project_settings = DefaultTestSettings(
+    calling_file=__file__,
+    EDC_NAVBAR_VERIFY_ON_LOAD=IGNORE,
+    EDC_AUTH_CODENAMES_WARN_ONLY=True,
+    EDC_AUTH_SKIP_SITE_AUTHS=True,
+    EDC_AUTH_SKIP_AUTH_UPDATER=True,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
+    add_dashboard_middleware=True,
+    add_lab_dashboard_middleware=True,
+    use_test_urls=True,
+).settings
+
+
+def main():
+    func_main(project_settings, f"{app_name}.tests")
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
