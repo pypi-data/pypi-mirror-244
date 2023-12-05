474b2fcbadfe68bfe9d952fcadd0e14541131f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 06:04:14 +0000

    Add package px-services/0.0.3/px_services-0.0.3.tar.gz

---
+++ packages/px-services/px_services-0.0.3.tar.gz/px_services-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+'Development Status :: 2 - Pre-Alpha',
+'Intended Audience :: Developers',
+'Environment :: Console',
+'License :: OSI Approved :: MIT License',
+'Programming Language :: Python :: 3',
+'Topic :: Software Development',
+'Topic :: Utilities',
+]
+
+setup(
+name='px_services',
+version='0.0.3',
+description='A blockchain Wallet Service APIs',
+long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+url='',
+author= 'Prince Foli',
+author_email='princefoli@qodehub.com',
+license= 'MIT',
+classifiers=classifiers,
+keywords='Blockchain, Wallet, HDWallet, BlockCypher',
+packages=find_packages(),
+install_requires=['blockcyher','bitcoinlib']
+)
