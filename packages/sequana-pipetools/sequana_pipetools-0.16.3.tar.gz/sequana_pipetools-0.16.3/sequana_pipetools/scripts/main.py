aafbe716f8ed08077d7c24d56535b5e3d032beac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 14:04:30 +0000

    Add package sequana-pipetools/0.16.3/sequana_pipetools-0.16.3.tar.gz

---
+++ packages/sequana-pipetools/sequana_pipetools-0.16.3.tar.gz/sequana_pipetools-0.16.3/sequana_pipetools/scripts/main.py
@@ -0,0 +1,34 @@
+#
+#  This file is part of Sequana software
+#
+#  Copyright (c) 2016-2021 - Sequana Dev Team (https://sequana.readthedocs.io)
+#
+#  Distributed under the terms of the 3-clause BSD license.
+#  The full license is in the LICENSE file, distributed with this software.
+#
+#  Website:       https://github.com/sequana/sequana
+#  Documentation: http://sequana.readthedocs.io
+#  Contributors:  https://github.com/sequana/sequana/graphs/contributors
+##############################################################################
+import rich_click as click
+from sequana_pipetools import version
+
+click.rich_click.USE_MARKDOWN = True
+click.rich_click.SHOW_METAVARS_COLUMN = False
+click.rich_click.APPEND_METAVARS_HELP = True
+click.rich_click.STYLE_ERRORS_SUGGESTION = "magenta italic"
+click.rich_click.SHOW_ARGUMENTS = True
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option("--version", is_flag=True)
+def main(**kwargs):
+    """ """
+    if kwargs["version"]:
+        print(f"sequana_pipetools v{version}")
+
+
+if __name__ == "__main__":
+    main()
