45b3dbcb0c97ac561468fa330136479c8f0d589f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 14:04:39 +0000

    Add package mathkit/4.1.1/mathkit-4.1.1-py3-none-any.whl

---
+++ packages/mathkit/mathkit-4.1.1-py3-none-any.whl/mathkit/__init__.py
@@ -0,0 +1,24 @@
+from .main import add
+from .main import cube
+from .main import cbrt
+from .main import div
+from .main import fact
+from .main import hcf
+from .main import lcm
+from .main import log
+from .main import mod
+from .main import mul
+from .main import percentage
+from .main import power
+from .main import sqrt
+from .main import sqr
+from .main import sub
+from .main import trig
+from .main import simple_interest
+from .main import compound_interest
+from .main import main
+
+__all__ = ['add', 'cube', 'cbrt', 'div', 'fact', 'hcf', 'lcm', 'log', 'mod', 'mul', 'percentage', 'power', 'sqrt', 'sqr', 'sub','trig','simple_interest','compound_interest','main']
+
+
+
