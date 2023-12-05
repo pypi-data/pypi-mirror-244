2afdf8180888ab1e2a3e86e6574b28c4b7ba2d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 15:04:41 +0000

    Add package syndb-cassandra/5.1.0/syndb_cassandra-5.1.0.tar.gz

---
+++ packages/syndb-cassandra/syndb_cassandra-5.1.0.tar.gz/syndb_cassandra-5.1.0/syndb_cassandra/models/__init__.py
@@ -0,0 +1,23 @@
+from cassandra.cqlengine.models import Model
+
+from syndb_cassandra.models.cell_structure import cell_structure_models
+from syndb_cassandra.models.organelle import organelle_models
+from syndb_cassandra.utils.misc import get_class_names, get_column_types
+from syndb_constants.table import SyndbTable, table_name_to_syndb_table
+
+structure_models: list[Model] = [Neuron, *cell_structure_models]
+structure_model_names: list[str] = get_class_names(structure_models)
+
+daughter_models: list[Model] = [*cell_structure_models, *organelle_models]
+
+brain_unit_models: list[Model] = [Neuron, *daughter_models]
+brain_unit_model_names: list[str] = get_class_names(brain_unit_models)
+
+model_name_to_model: dict[str, Model] = dict(zip(brain_unit_model_names, brain_unit_models))
+
+model_name_to_schema_dict: dict[str, dict[str, str]] = {
+    n: get_column_types(model) for n, model in model_name_to_model.items()
+}
+syndb_table_to_schema_dict: dict[SyndbTable, dict[str, str]] = {
+    table_name_to_syndb_table[n]: s for n, s in model_name_to_schema_dict.items()
+}
