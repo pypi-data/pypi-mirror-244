70613d862704cb819341610c1450586f17065c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 16:04:43 +0000

    Add package proof-report-parser/0.1.0/proof_report_parser-0.1.0.tar.gz

---
+++ packages/proof-report-parser/proof_report_parser-0.1.0.tar.gz/proof_report_parser-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from time import time
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="proof_report_parser",
+    version="0.1.0",
+    author="happy2wh",
+    author_email="youremail@gmail.com",
+    description="A test package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    install_requires=["python-docx"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
