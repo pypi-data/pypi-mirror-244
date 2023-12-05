d54de1b8640a88971149ecaf616d62967e2df002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:05:09 +0000

    Add package infi-const/0.0.1/infi_const-0.0.1-py3-none-any.whl

---
+++ packages/infi-const/infi_const-0.0.1-py3-none-any.whl/infi_conts/url_const.py
@@ -0,0 +1,13 @@
+PREPROCESS_SERVICE_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8081/activate'
+
+TRIGGER_SERVICE_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8082/docs#/'
+
+EMAIL_SERVICE_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8083/emails/send_email'
+
+TGV_VALIDATIONS_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8084/docs#/'
+
+FLIGHTS_RECORD_SERVICE_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8085/docs#/'
+
+AZCOPY_SERVICE_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8087/'
+
+SKYLINE_SERVICE_URL: str = 'https://pmapi.skylineglobe.com/startproject/Mipui/poda/a247acd3-977c-4a77-b93f-e713a8632d57'
