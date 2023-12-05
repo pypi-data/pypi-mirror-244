5bb3dbc689cd87f896045d1cc3d8dcd58a08bc67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 15:04:30 +0000

    Add package py4j-example/0.3.1/py4j-example-0.3.1.tar.gz

---
+++ packages/py4j-example/py4j-example-0.3.1.tar.gz/py4j-example-0.3.1/setup.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+version_file = os.path.join(this_directory, 'py4j_example/version.py')
+
+try:
+    exec(open(version_file).read())
+except IOError:
+    print(f"Failed to load version file for packaging. {version_file} not found!", file=sys.stderr)
+    sys.exit(-1)
+VERSION = __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="py4j-example",
+    version=VERSION,
+    packages=find_packages(include=["py4j_example*", "py4j_example.*"]),
+    package_data={"py4j_example": ["jars/*.jar"]},  # Include JAR files in the package
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
