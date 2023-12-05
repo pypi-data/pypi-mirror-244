4fe7297405e929e4640a9b32e2fd957fd883d99c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:06:33 +0000

    Add package word-alignment-visualization/0.1.3/word-alignment-visualization-0.1.3.tar.gz

---
+++ packages/word-alignment-visualization/word-alignment-visualization-0.1.3.tar.gz/word-alignment-visualization-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='word-alignment-visualization',
+    version='0.1.3',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'ipython',
+    ],
+)
