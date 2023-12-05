7dbe3d1cced0717e244e0ca90e7fece8637ee5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 03:04:30 +0000

    Add package xtgeo/3.5.2/xtgeo-3.5.2-cp311-cp311-win_amd64.whl

---
+++ packages/xtgeo/xtgeo-3.5.2-cp311-cp311-win_amd64.whl/xtgeo/grid3d/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+"""The XTGeo grid3d package"""
+
+
+from xtgeo.common.exceptions import (
+    DateNotFoundError,
+    KeywordFoundNoDateError,
+    KeywordNotFoundError,
+)
+
+from ._ecl_grid import GridRelative, Units
+from .grid import Grid
+from .grid_properties import GridProperties
+from .grid_property import GridProperty
