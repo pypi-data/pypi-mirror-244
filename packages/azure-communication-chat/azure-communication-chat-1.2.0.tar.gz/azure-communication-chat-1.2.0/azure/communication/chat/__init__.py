84ebb8262e2599d14b7519c375805e917f5ea270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 14:04:47 +0000

    Add package azure-communication-chat/1.2.0/azure-communication-chat-1.2.0.tar.gz

---
+++ packages/azure-communication-chat/azure-communication-chat-1.2.0.tar.gz/azure-communication-chat-1.2.0/azure/communication/chat/__init__.py
@@ -0,0 +1,63 @@
+from ._version import VERSION
+from ._chat_client import ChatClient
+from ._chat_thread_client import ChatThreadClient
+from ._generated.models import (
+    ChatAttachmentType,
+    SendChatMessageResult,
+    ChatThreadItem,
+    ChatMessageType,
+    ChatError
+)
+
+from ._models import (
+    ChatAttachment,
+    ChatParticipant,
+    ChatMessage,
+    ChatThreadProperties,
+    ChatMessageReadReceipt,
+    ChatMessageContent,
+    CreateChatThreadResult
+)
+
+from ._shared.user_credential import CommunicationTokenCredential
+from ._shared.models import (
+    CommunicationIdentifier,
+    CommunicationIdentifierKind,
+    CommunicationUserIdentifier,
+    CommunicationUserProperties,
+    identifier_from_raw_id,
+    PhoneNumberIdentifier,
+    PhoneNumberProperties,
+    MicrosoftTeamsUserIdentifier,
+    MicrosoftTeamsUserProperties,
+    UnknownIdentifier
+)
+
+__all__ = [
+    'ChatClient',
+    'ChatThreadClient',
+    'ChatMessage',
+    'ChatMessageContent',
+    'ChatMessageReadReceipt',
+    'SendChatMessageResult',
+    'ChatThreadProperties',
+    'ChatThreadItem',
+    'ChatParticipant',
+    'ChatMessageType',
+    'ChatAttachment',
+    'ChatAttachmentType',
+    'CreateChatThreadResult',
+    'ChatError',
+    'CommunicationTokenCredential',
+    'CommunicationIdentifier',
+    'CommunicationIdentifierKind',
+    'CommunicationUserIdentifier',
+    'CommunicationUserProperties',
+    'MicrosoftTeamsUserIdentifier',
+    'MicrosoftTeamsUserProperties',
+    'identifier_from_raw_id',
+    'PhoneNumberIdentifier',
+    'PhoneNumberProperties',
+    'UnknownIdentifier'
+]
+__version__ = VERSION
