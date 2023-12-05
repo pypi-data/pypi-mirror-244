bb794eccc0a824bd0bb81bc140048dceafd25917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:05:59 +0000

    Add package tttest-cli/1.0.6/tttest_cli-1.0.6-py3-none-any.whl

---
+++ packages/tttest-cli/tttest_cli-1.0.6-py3-none-any.whl/tttest/version.py
@@ -0,0 +1,9 @@
+__version__ = "1.0.6"
+
+
+def get_version_banner(module_name='cli'):
+    return f'''           
+    *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *     
+    *  tttest {module_name} {__version__}   https://doc.tttest.run  *
+    *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+    '''
