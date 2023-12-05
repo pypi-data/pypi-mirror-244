655fd649ad8da00d8eec59e027140008950c04c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 03:05:00 +0000

    Add package glumpy/1.2.1/glumpy-1.2.1.tar.gz

---
+++ packages/glumpy/glumpy-1.2.1.tar.gz/glumpy-1.2.1/glumpy/app/window/backends/__init__.py
@@ -0,0 +1,15 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2009-2016 Nicolas P. Rougier. All rights reserved.
+# Distributed under the (new) BSD License.
+# -----------------------------------------------------------------------------
+__backends__ = ('glfw_deprecated',
+                'glfw',
+                'glfw_imgui',
+                'pyglet',
+                'sdl',
+                'sdl2',
+                'osxglut',
+                'freeglut',
+                'qt5',
+                'pyside',
+                'pyside2')
