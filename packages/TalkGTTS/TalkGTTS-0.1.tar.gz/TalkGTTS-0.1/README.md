460cc37ef8d242ccd513a560a1dbd9f01e3901ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:05:46 +0000

    Add package TalkGTTS/0.1/TalkGTTS-0.1.tar.gz

---
+++ packages/TalkGTTS/TalkGTTS-0.1.tar.gz/TalkGTTS-0.1/README.md
@@ -0,0 +1,2 @@
+This is a module that formats gTTS into talkGTTS.say("YOUR TEXT HERE"). I will 
+say those words and then leave no trace.
---
+++ packages/TalkGTTS/TalkGTTS-0.1.tar.gz/TalkGTTS-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='TalkGTTS',
+    version='0.1',
+    packages=['TalkGTTS'],
+    url='https://github.com/Talklet123/Talk-Module',
+    license='MIT',
+    author='Talklet123',
+    author_email='developer1234567890234@yahoo.com',
+    description='Uses GTTS to make it talk.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'gTTS>=2.4.0',
+    ],
+)
+
