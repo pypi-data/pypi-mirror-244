724f05df506d9c36255787e0c1f942995f21025a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 16:05:05 +0000

    Add package pepe-granular/1.0.1/pepe-granular-1.0.1.tar.gz

---
+++ packages/pepe-granular/pepe-granular-1.0.1.tar.gz/pepe-granular-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='pepe-granular',  
+     version='1.0.1',
+     author="Jack Featherstone",
+     author_email="jdfeathe@ncsu.edu",
+     license='MIT',
+     url='https://jfeatherstone.github.io/pepe/pepe',
+     description="Toolbox for granular analysis of photoelastic images",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3.7",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
