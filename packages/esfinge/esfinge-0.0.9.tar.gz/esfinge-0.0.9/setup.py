5f36d34d62137a826360de8fc414d16ab682be72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 14:04:40 +0000

    Add package esfinge/0.0.9/esfinge-0.0.9.tar.gz

---
+++ packages/esfinge/esfinge-0.0.9.tar.gz/esfinge-0.0.9/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='esfinge',
+    version='0.0.9',
+    packages=['esfinge'],
+    install_requires=[],
+    package_data={
+        'esfinge': [
+            'layout.html',
+            'theme.conf',
+            'esfinge.css'
+        ]
+    }
+)
