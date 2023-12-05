26fc087e983efe83041459dd027116d9cb2d7e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 14:04:48 +0000

    Add package esfinge/0.0.11/esfinge-0.0.11.tar.gz

---
+++ packages/esfinge/esfinge-0.0.11.tar.gz/esfinge-0.0.11/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name='esfinge',
+    version='0.0.11',
+    packages=['esfinge'],
+    install_requires=[],
+    package_data={
+        'esfinge': [
+            'defindex.html',
+            'esfinge.css',
+            'domainindex.html',
+            'genindex-single.html',
+            'genindex-split.html',
+            'genindex.html',
+            'globaltoc.html',
+            'layout.html',
+            'localtoc.html',
+            'page.html',
+            'relations.html',
+            'search.html',
+            'searchbox.html',
+            'searchfield.html',
+            'sourcelink.html',
+            'theme.conf'
+        ]
+    }
+)
