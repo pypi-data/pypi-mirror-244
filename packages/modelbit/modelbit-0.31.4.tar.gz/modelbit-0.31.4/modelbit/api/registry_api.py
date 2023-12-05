8fab4dd18b1932d03b386d088975032f9464135e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 06:05:04 +0000

    Add package modelbit/0.31.4/modelbit-0.31.4.tar.gz

---
+++ packages/modelbit/modelbit-0.31.4.tar.gz/modelbit-0.31.4/modelbit/api/registry_api.py
@@ -0,0 +1,35 @@
+from typing import Any, Dict, List
+from modelbit.api import MbApi, writeLimiter, readLimiter
+from modelbit.helpers import getCurrentBranch
+from modelbit.internal.secure_storage import DownloadableObjectInfo
+
+
+class RegistryApi:
+  api: MbApi
+
+  def __init__(self, api: MbApi):
+    self.api = api
+
+  def delete(self, names: List[str]):
+    writeLimiter.maybeDelay()
+    self.api.getJsonOrThrow("api/cli/v1/registry/delete", {"branch": getCurrentBranch(), "names": names})
+
+  def storeContentHashAndMetadata(self, objects: Dict[str, Dict[str, Any]]):
+    writeLimiter.maybeDelay()
+    self.api.getJsonOrThrow("api/cli/v1/registry/set", {"branch": getCurrentBranch(), "objects": objects})
+
+  def getRegistryDownloadInfo(self):
+    readLimiter.maybeDelay()
+    resp = self.api.getJsonOrThrow("api/cli/v1/registry/get_signed_url", {"branch": getCurrentBranch()})
+    if "downloadInfo" in resp:
+      return RegistryDownloadInfo(resp["downloadInfo"])
+
+
+class RegistryDownloadInfo(DownloadableObjectInfo):
+
+  def __init__(self, data: Dict[str, Any]):
+    super().__init__(data)
+    self.id: str = data["id"]
+
+  def cachekey(self) -> str:
+    return self.id
