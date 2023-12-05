180f15c0ceb6d0208e80eac8b52484d6fcd17645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:06:33 +0000

    Add package ximage/0.0.3/ximage-0.0.3-py3-none-any.whl

---
+++ packages/ximage/ximage-0.0.3-py3-none-any.whl/ximage/labels/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+Created on Mon Jul 10 13:33:22 2023
+
+@author: ghiggi
+"""
---
+++ packages/ximage/ximage-0.0.3-py3-none-any.whl/ximage/patch/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+Created on Mon Jul 10 13:32:49 2023
+
+@author: ghiggi
+"""
