faa6c741ad43cb6b040e6ce5484db488d03a6751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 05:05:18 +0000

    Add package logze/0.1.8/logze-0.1.8-py3-none-any.whl

---
+++ packages/logze/logze-0.1.8-py3-none-any.whl/logze/models/log.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+class Log:
+    """Attributes:
+    source (str): Source project/file of the log.
+    level (str): 'info', 'warn' or 'error'.
+    message (str): Log detailed message.
+    event (str): Event where the log was triggered.
+    """
+
+    def __init__(self, source: str, level: str, message: str, event: str):
+        self.source = source
+        self.timestamp = str(datetime.now())
+        self.level = level.lower()
+        self.message = message
+        self.event = event
