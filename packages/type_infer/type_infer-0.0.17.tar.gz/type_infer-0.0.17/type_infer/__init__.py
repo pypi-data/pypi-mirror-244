92ff30f0e90337fe79bb738da9463634fae5ffc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 15:04:44 +0000

    Add package type_infer/0.0.17/type_infer-0.0.17.tar.gz

---
+++ packages/type_infer/type_infer-0.0.17.tar.gz/type_infer-0.0.17/type_infer/__init__.py
@@ -0,0 +1,10 @@
+from type_infer import base
+from type_infer import dtype
+from type_infer import infer
+from type_infer import helpers
+
+
+__version__ = '0.0.17'
+
+
+__all__ = ['base', 'dtype', 'infer', 'helpers', '__version__']
