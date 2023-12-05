66213c916bfe80cd198a8c0c18216c50d7646eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 07:03:52 +0000

    Add package blib-py/1.3.6/blib-py-1.3.6.tar.gz

---
+++ packages/blib-py/blib-py-1.3.6.tar.gz/blib-py-1.3.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+from src.blib import __version__
+
+with open("requirements.txt", "r") as fid:
+    install_requires = [line.strip() for line in fid.readlines() if line.strip()]
+
+print(install_requires)
+
+with open("README.md", "r") as fid:
+    long_description = fid.read()
+
+# Setup
+setup(
+    name="blib-py",
+    version=__version__,
+    description="Boonleng's Python Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Boonleng Cheong",
+    author_email="boonleng@ou.edu",
+    url="https://github.com/boonleng/blib-py",
+    install_requires=install_requires,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    package_data={
+        "blib.fonts": ["*.ttf"],
+        "blib.blob": ["line-*.png"],
+    },
+    include_package_data=True,
+    license="MIT",
+    zip_safe=False,
+)
