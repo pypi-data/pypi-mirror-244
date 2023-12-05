571bdb25c94195dea1459a8a8b7fc31504a2b0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 14:04:13 +0000

    Add package connector-vk-private-lib/0.1.6/connector_vk_private_lib-0.1.6.tar.gz

---
+++ packages/connector-vk-private-lib/connector_vk_private_lib-0.1.6.tar.gz/connector_vk_private_lib-0.1.6/connector_vk_private_lib/src/interfaces/attachments.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+from connector_vk_private_lib.src.interfaces.photo import Photo
+from connector_vk_private_lib.src.interfaces.video import Video
+
+
+class Attachment(BaseModel):
+    type: str
+    photo: Optional[Photo] = None
+    video: Optional[Video] = None
+    audio: Optional[Video] = None
+    text: Optional[str] = None
+    user_id: Optional[int] = None
+    owner_id: Optional[int] = None
+    web_view_token: Optional[str] = None
+    description: Optional[str] = None
+    duration: Optional[int] = None
+
+
+class RequestAttachments(BaseModel):
+    data: list[Attachment]
