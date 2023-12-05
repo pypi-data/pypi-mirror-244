28ec9e35cbbf2307ec29f7468e037d280bc622ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 03:04:36 +0000

    Add package st-kaggle-connector/0.1.2/st_kaggle_connector-0.1.2.tar.gz

---
+++ packages/st-kaggle-connector/st_kaggle_connector-0.1.2.tar.gz/st_kaggle_connector-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.2'
+DESCRIPTION = "Kaggle dataset connector for Streamlit"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+setup(
+    name="st_kaggle_connector",
+    version=VERSION,
+    author="Oleksandr Arsentiev",
+    author_email="<arsentiev9393@gmail.com>",
+    description=DESCRIPTION,
+    long_description=f"{readme}",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['streamlit', 'pandas', 'kaggle'],
+    keywords=['streamlit', 'custom', 'component', 'kaggle', 'dataset', 'connector'],
+    license="MIT",
+    url="https://github.com/arsentievalex/kaggle-streamlit-data-connector/tree/main",
+)
