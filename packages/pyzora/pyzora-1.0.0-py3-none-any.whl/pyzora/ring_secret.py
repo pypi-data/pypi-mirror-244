da2159e4abf9043774f66c93fc88162950fc660f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:06:44 +0000

    Add package pyzora/1.0.0/pyzora-1.0.0-py3-none-any.whl

---
+++ packages/pyzora/pyzora-1.0.0-py3-none-any.whl/pyzora/ring_secret.py
@@ -0,0 +1,170 @@
+"""pyzora - Python library to help parsing secrets from Zelda OoS/OoA
+
+Ring secret class. See RingSecret for more details.
+
+(c) 2023 fortwoone.
+All rights reserved.
+
+This file is part of pyzora.
+
+pyzora is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+pyzora is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with pyzora. If not, see <https://www.gnu.org/licenses/>.
+"""
+from pyzora.secret import *
+from pyzora.ring_types import *
+
+
+class RingSecret(BaseSecret):
+    """A ring secret. Ring secrets can be used to transfer a player's ring
+    collection from one game to another.
+
+    You can check if a ring is contained in a secret with the 'in' keyword.
+
+    For example, if you want to check if the player already has the Friendship
+    Ring, you can do this :
+
+    if FRIENDSHIP in secret:
+        do_whatever_you_want()"""
+    __args = ("game_id", "rings", "ring_str", "region")
+    __rings = 0
+    __ring_str = ""
+
+    def __set_rings(self, value: int):
+        if value > int(AllRings):
+            raise ValueError("cannot set a value higher than AllRings")
+        if value < 0:
+            raise ValueError("value must be an unsigned integer")
+        # Converting the parameter into an integer in case we've been given a RingType instance
+        self.__rings = int(value)
+        self.__ring_str = bin(int(value))[2:].rjust(64, "0")
+
+    rings = property(lambda self: self.__rings, __set_rings,
+                     doc="""Get the rings stored in the secret as an integer.
+                     
+                     :type: int, AllRings or NoRings""")
+
+    @property
+    def ring_count(self):
+        """Count how many rings the player has."""
+        return self.__ring_str.count("1")
+
+    def __init__(self, *args, **kwargs):
+        for pos, arg in enumerate(self.__args):
+            kwargs.setdefault(arg, None)
+            if kwargs.get(arg) is None:
+                try:
+                    setattr(self, arg, args[pos])
+                except LookupError:
+                    pass
+            else:
+                try:
+                    setattr(self, arg, kwargs.get(arg))
+                except LookupError:
+                    pass
+
+    def __contains__(self, item):
+        if item is NoRings:
+            return not self.rings
+        if item is AllRings:
+            return self.rings == int(AllRings)
+        return (item & self.rings) == item
+
+    @classmethod
+    def load(cls, secret: bytearray | bytes | str, region: GameRegion) -> "RingSecret":
+        """Load a secret encoded with a certain region.
+
+        :param secret: The secret string/byte array to decode (if secret is an instance of str, then it will be parsed prior to loading).
+        :type secret: str or bytearray
+        :param region: The region to use when loading the secret.
+        :type region: GameRegion
+        :raise SecretError: if the given data is not 15 bytes long (only if used with bytes or bytearray, since strings will be parsed and reduced to 15 bytes).
+        :raise NotARingCodeError: if the given data is not for a ring secret.
+        :raise ChecksumError: if the given data's checksum doesn't match the expected one.
+        :return: A secret based on the data contained in secret.
+        :rtype: RingSecret"""
+        if isinstance(secret, str):
+            # Secret string. Parse it before doing anything else.
+            # Allows the user to give the region only once
+            return cls.load(parse_secret(secret, region), region)
+        if len(secret) != 15:
+            raise SecretError("secret is expected to have exactly 15 bytes")
+        decoded_bytes = cls.decode_bytes(secret, region)
+        decoded_secret = byte_array_to_string(decoded_bytes)
+        cloned_bytes = decoded_bytes.copy()
+        cloned_bytes[14] = 0
+        checksum = calculate_checksum(cloned_bytes)
+        if (decoded_bytes[14] & 0xF) != (checksum & 0xF):
+            raise ChecksumError(f"checksum {checksum} does not match expected value : {decoded_bytes[14]}")
+        if decoded_secret[3] != "0" or decoded_secret[4] != "1":
+            raise NotARingCodeError("given secret is not a ring code")
+        game_id = int(reverse_substring(decoded_secret, 5, 15))
+        ring_str = "".join((reverse_substring(decoded_secret, 36, 8),
+                            reverse_substring(decoded_secret, 76, 8),
+                            reverse_substring(decoded_secret, 28, 8),
+                            reverse_substring(decoded_secret, 60, 8),
+                            reverse_substring(decoded_secret, 44, 8),
+                            reverse_substring(decoded_secret, 68, 8),
+                            reverse_substring(decoded_secret, 20, 8),
+                            reverse_substring(decoded_secret, 52, 8)))
+        rings = int(ring_str, base=2)
+        return RingSecret(game_id=game_id, rings=rings, ring_str=ring_str,
+                          region=region)
+
+    def __bytes__(self):
+        ring_row1 = self.rings
+        ring_row2 = ring_row1 >> 8
+        ring_row3 = ring_row1 >> 16
+        ring_row4 = ring_row1 >> 24
+        ring_row5 = ring_row1 >> 32
+        ring_row6 = ring_row1 >> 40
+        ring_row7 = ring_row1 >> 48
+        ring_row8 = ring_row1 >> 56
+        cipher_key = ((self.game_id >> 8) + (self.game_id & 255)) & 7
+        unencoded_secret = (
+                reverse_string(integer_string(cipher_key).rjust(3, "0")) + "01"
+        )
+        secret_strings = map(reverse_string, (
+            integer_string(self.game_id).rjust(15, "0"),
+            integer_string(ring_row2).rjust(8, "0"),
+            integer_string(ring_row6).rjust(8, "0"),
+            integer_string(ring_row8).rjust(8, "0"),
+            integer_string(ring_row4).rjust(8, "0"),
+            integer_string(ring_row1).rjust(8, "0"),
+            integer_string(ring_row5).rjust(8, "0"),
+            integer_string(ring_row3).rjust(8, "0"),
+            integer_string(ring_row7).rjust(8, "0"),
+        ))
+        unencoded_secret += "".join(secret_strings)
+        unencoded_bytes = string_to_byte_array(unencoded_secret)
+        del (
+            ring_row1,
+            ring_row2,
+            ring_row3,
+            ring_row4,
+            ring_row5,
+            ring_row6,
+            ring_row7,
+            ring_row8,
+            secret_strings,
+            unencoded_secret,
+        )
+        unencoded_bytes[14] = calculate_checksum(unencoded_bytes)
+        secret = self._encode_bytes(unencoded_bytes, self.region)
+        return bytes(secret)
+
+    def to_list(self):
+        """Return self as a list of ring types."""
+        return list(filter(lambda tp: tp in self, RING_TYPES))
+
+    def __hash__(self):
+        return hash((self.__game_id, self.__rings))
---
+++ packages/pyzora/pyzora-1.0.0-py3-none-any.whl/pyzora/secret.py
@@ -0,0 +1,376 @@
+"""pyzora - Python library to help parsing secrets from Zelda OoS/OoA
+
+Base secret class.
+
+(c) 2023 fortwoone.
+All rights reserved.
+
+This file is part of pyzora.
+
+pyzora is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+pyzora is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with pyzora. If not, see <https://www.gnu.org/licenses/>.
+"""
+import unicodedata
+import re
+import sys
+from .enums import *
+from .exceptions import *
+
+_udata_lookup = unicodedata.lookup
+
+U_HEART = U_GURMUKHI_THREE = _udata_lookup("GURMUKHI DIGIT THREE")
+U_ASTERISK = chr(42)
+U_DOLLAR_SIGN = chr(36)
+U_DIAMOND = chr(9830)
+U_SPADE = chr(0x2660)
+U_CLUB = chr(0x2663)
+U_COMMERCIAL_AT = chr(0x0040)
+U_AMPERSAND = chr(0x0026)
+U_PERCENT = chr(0x0025)
+U_SQUARE = chr(0x25A0)
+U_CIRCLE = chr(0x25CF)
+U_TRIANGLE = chr(0x25B2)
+U_UPWARDS_ARROW = _udata_lookup("UPWARDS ARROW")
+U_DOWNWARDS_ARROW = _udata_lookup("DOWNWARDS ARROW")
+U_RIGHTWARDS_ARROW = _udata_lookup("RIGHTWARDS ARROW")
+U_LEFTWARDS_ARROW = _udata_lookup("LEFTWARDS ARROW")
+
+_SYMBOL_REGEXES = (
+    # Japan
+    {
+        r"a": "あ", r"i": "い", r"u": "う", r"e": "え", r"o": "お",
+        r"ka": "か", r"ki": "き", r"ku": "く", r"ke": "け", r"ko": "こ",
+        r"sa": "さ", r"si": "し", r"su": "す", r"se": "せ", r"so": "そ",
+        r"ta": "た", r"ti": "ち", r"tu": "つ", r"te": "て", r"to": "と",
+        r"na": "な", r"ni": "に", r"ne": "ね", r"no": "の",
+        r"ha": "は", r"hi": "ひ", r"hu": "ふ", r"he": "へ", r"ho": "ほ",
+        r"ma": "ま", r"mi": "み", r"mu": "む", r"me": "め", r"mo": "も",
+        r"ya": "や", r"yu": "ゆ", r"yo": "よ",
+        r"ra": "ら", r"ri": "り", r"ru": "る", r"re": "れ",
+        r"za": "ざ",
+        r"wa": "わ", r"wo": "を",
+        r"ga": "が", r"go": "ご",
+        r"zo": "ぞ",
+        r"da": "だ", r"do": "ど",
+        r"pi": "ぴ", r"pu": "ぷ", r"pe": "ぺ",
+        r"bo": "ぼ",
+        r"n": "ん",
+        r"shi": "し",
+        r"chi": "ち",
+        r"tsu": "つ",
+        r"fu": "ふ",
+        r"\s+": ""
+    },
+    # US/PAL
+    {
+        r"\{?spade\}?": U_SPADE,
+        r"\{?heart\}?": U_HEART,
+        r"\{?diamond\}?": U_DIAMOND,
+        r"\{?club\}?": U_CLUB,
+        r"\{?circle\}?": U_CIRCLE,
+        r"\{?square\}?": U_SQUARE,
+        r"\{?triangle\}?": U_TRIANGLE,
+        r"\{?up\}?": U_UPWARDS_ARROW,
+        r"\{?down\}?": U_DOWNWARDS_ARROW,
+        r"\{?left\}?": U_LEFTWARDS_ARROW,
+        r"\{?right\}?": U_RIGHTWARDS_ARROW,
+        r"<": "(",
+        r">": ")",
+        r" ": "",
+    }
+)
+
+_VALID_CHARS_SELECT = (
+    ('え', 'か', 'く', '0', 'け', 'つ', '1', 'し',
+     'に', 'ね', 'そ', 'ぺ', '2', 'た', 'せ', 'い',
+     'て', 'み', 'ほ', 'す', 'う', 'お', 'ら', 'の',
+     '3', 'ふ', 'さ', 'ざ', 'き', 'ひ', 'わ', 'や',
+     'こ', 'は', 'ゆ', 'よ', 'へ', 'る', 'な', 'と',
+     '5', '6', '7', 'を', 'ぷ', 'も', 'め', 'り',
+     'ち', 'ま', 'あ', 'ん', 'ぞ', 'れ', '8', 'ご',
+     'ど', 'む', 'ぴ', '9', '4', 'ぼ', 'が', 'だ'),
+    ("B", "D", "F", "G", "H", "J", "L", "M",
+     "♠", "♥", "♦", "♣", "#", "N", "Q",
+     "R", "S", "T", "W", "Y", "!", "●", "▲",
+     "■", "+", "-", "b", "d", "f", "g", "h",
+     "j", "m", "$", "*", "/", ":", "~", "n",
+     "q", "r", "s", "t", "w", "y", "?", "%", "&",
+     "(", "=", ")", "2", "3", "4", "5", "6", "7",
+     "8", "9", "↑", "↓", "←", "→", "@",)
+)
+
+
+def parse_secret(secret_string: str, region: GameRegion) -> bytearray:
+    """Convert a secret string into a byte array.
+
+    :param secret_string: The secret string to convert.
+    :type secret_string: str
+    :param region: The game region to use. Be aware that not setting the correct region before parsing might net you some unexpected errors.
+    :type region: GameRegion
+    :raise SecretError: if the secret string contains invalid symbols.
+    :return: The converted array.
+    :rtype: bytearray
+    """
+    for key, value in _SYMBOL_REGEXES[region].items():
+        secret_string = re.sub(key, value, secret_string, 0, re.IGNORECASE)
+    secret_length = len(secret_string)  # storing that to avoid iterating over the string twice
+    data = bytearray(secret_length)
+    for pos, value in enumerate(secret_string):
+        try:
+            symbol = _VALID_CHARS_SELECT[region.value].index(value)
+            if symbol < 0 or symbol > 63:
+                raise SecretError(f"Secret contains invalid value : {value}")
+        except ValueError as exc:
+            # There's a chance the user has used the wrong region
+            raise SecretError(f"Secret contains invalid value : {value}. Perhaps you used the wrong region?") from exc
+        data[pos] = symbol
+    return data
+
+
+def create_string(data: bytearray, region: GameRegion) -> str:
+    """Produces a secret string from a byte array with a given region.
+
+    :param data: The array to convert.
+    :type data: bytearray
+    :param region: The game region to use during conversion.
+    :type region: GameRegion
+    :raise SecretError: if the byte array contains invalid values.
+    :return: The data converted to a string using the given encoding.
+    :rtype: str"""
+    secret_chars = []
+    for pos, value in enumerate(data):
+        if value > 63:
+            raise SecretError("given data contains invalid values")
+        secret_chars.append(_VALID_CHARS_SELECT[region][value])
+        if pos % 5 == 4:
+            secret_chars.append(" ")
+    return "".join(secret_chars)
+
+
+def calculate_checksum(secret: bytearray) -> int:
+    """Calculate the checksum for a given secret.
+
+    :param secret: The secret a checksum needs to be calculated for.
+    :type secret: bytearray
+    :return: The needed checksum integer.
+    :rtype: int
+    """
+    return sum(secret) & 0xF
+
+
+def transform_byte_to_bitstring(byte: int) -> str:
+    """Return a bitstring from a byte integer.
+
+    :param byte: The integer to convert.
+    :type byte: int
+    :return: The integer converted to a bitstring.
+    :rtype: str
+    :meta private:"""
+    return bin(byte)[2:].rjust(6, "0")
+
+
+def byte_array_to_string(array: bytearray) -> str:
+    """Convert a decoded array to a bitstring.
+
+    :param array: The decoded array.
+    :type array: bytearray
+    :return: The bitstring for this array.
+    :rtype: str
+    :meta private:"""
+    return "".join(map(transform_byte_to_bitstring, array))
+
+
+def Byte(integer: str) -> int:
+    """Return a byte integer from a bitstring.
+
+    :param integer: The bitstring to convert.
+    :type integer: str
+    :return: The corresponding byte integer.
+    :rtype: int"""
+    byteorder = sys.byteorder
+    return int.from_bytes(bytearray(int(integer, 2).to_bytes(1, byteorder)), byteorder)
+
+
+def string_to_byte_array(string: str) -> bytearray:
+    """Convert a bitstring to a byte array.
+
+    :param string: The bitstring to convert.
+    :type string: str
+    :return: The non-encoded byte array for this string.
+    :rtype: bytearray
+    :meta private:"""
+    secret_length = len(string) // 6 + 1
+    secret = bytearray(secret_length)
+    for x in range(secret_length):
+        multi = x * 6
+        substr = string[multi: multi + 6]
+        if not substr:
+            break
+        secret[x] = Byte(substr)
+    return secret
+
+
+def reverse_substring(string: str, start: int, length: int) -> str:
+    """Return a reversed slice of the given string.
+
+    :param string: The string to extract the slice from.
+    :type string: str
+    :param start: The start position for the slice to extract.
+    :type start: int
+    :param length: The slice's length.
+    :type length: int
+    :raise ValueError: if either the start or length values are not positive.
+    :return: The reversed substring from the source string.
+    :rtype: str
+    :meta private:"""
+    if start < 0:
+        raise ValueError(f"Expected a positive integer for the slice start, got {start}")
+    if length < 0:
+        raise ValueError(f"length integer must be positive (got {start})")
+    return "".join(reversed(string[start: start + length]))
+
+
+def integer_string(number: int) -> str:
+    """Return the binary string for an integer.
+
+    :param number: The integer to convert.
+    :type number: int
+    :return: The integer's binary string.
+    :rtype: str
+    :meta private:"""
+    return bin(number)[2:]  # starting at the third character to skip the prefix
+
+
+def reverse_string(string: str) -> str:
+    """Return the reversed version of a string.
+
+    :param string: The string to reverse.
+    :type string: str
+    :return: A reversed copy of the source string.
+    :rtype: str
+    :meta private:"""
+    return "".join(reversed(string))
+
+
+class BaseSecret:
+    """Base secret class for all secret objects.
+
+    This class contains all base methods other secret classes can rely on.
+    It should not be instantiated directly.
+
+    This class defines a basic `__hash__` method, but said hash will be different depending on
+    the secret class."""
+    _CIPHERS = (
+        # Japan
+        bytes((0x31, 0x09, 0x29, 0x3b, 0x18, 0x3c, 0x17, 0x33,
+               0x35, 0x01, 0x0b, 0x0a, 0x30, 0x21, 0x2d, 0x25,
+               0x20, 0x3a, 0x2f, 0x1e, 0x39, 0x19, 0x2a, 0x06,
+               0x04, 0x15, 0x23, 0x2e, 0x32, 0x28, 0x13, 0x34,
+               0x10, 0x0d, 0x3f, 0x1a, 0x37, 0x0f, 0x3e, 0x36,
+               0x38, 0x02, 0x16, 0x3d, 0x2c, 0x0e, 0x1b, 0x12)),
+        # US/PAL
+        bytes(
+            (21, 35, 46, 4, 13, 63, 26, 16,
+             58, 47, 30, 32, 15, 62, 54, 55,
+             9, 41, 59, 49, 2, 22, 61, 56,
+             40, 19, 52, 50, 1, 11, 10, 53,
+             14, 27, 18, 44, 33, 45, 37, 48,
+             25, 42, 6, 57, 60, 23, 51, 24)
+        )
+    )
+    __game_id = 0  # Can be any possible value between 0 and 32766.
+    __region = GameRegion.US_PAL
+    __required_length__ = 0
+
+    def __set_game_id(self, value: int):
+        if value > 32766 or value < 0:
+            raise SecretError(f"invalid game ID : {value}")
+        self.__game_id = value
+
+    game_id = property(lambda self: self.__game_id, __set_game_id,
+                       doc="""The secret's game ID. A game ID is generated upon creating
+                       a normal save file in either Ages or Seasons, and is kept when using a secret
+                       to start a linked save in the other game. After being first input
+                       through the Labrynna/Holodrum secret, this ID will be compared
+                       against further secret inputs' game IDs, which means that all further
+                       secrets will have to use the same ID.
+                       
+                       :type: int""")
+
+    def __set_region(self, value: GameRegion | int):
+        self.__region = GameRegion(value)
+
+    region = property(lambda self: self.__region, __set_region,
+                      doc="""The secret's region. It's needed to parse correctly
+                      data for a given secret string.
+                      
+                      :type: GameRegion""")
+
+    def __bytes__(self):
+        """Convert self to a byte array."""
+        return bytes(20)
+
+    def __hash__(self):
+        return hash((self.__game_id, self.__region))
+
+    def __str__(self):
+        """Return a password string from self.
+
+        WARNING : PLEASE NOTE THAT OUTPUT SECRET STRINGS MIGHT
+        BE DIFFERENT FROM THE INPUT SECRET. IN MOST CASES,
+        YOU DON'T NEED TO BOTHER ABOUT IT, SINCE THE INFORMATION STORED
+        WILL BE THE SAME. YOU CAN CHECK WITH AN EQUALITY TEST IF THE
+        OUTPUT IS DIFFERENT."""
+        return create_string(bytearray(bytes(self)), self.__region)
+
+    def __eq__(self, other: "BaseSecret"):
+        if type(self) is not type(other):
+            return False
+        return self.game_id == other.game_id and other.region == self.region
+
+    @classmethod
+    def _encode_bytes(cls, data: bytearray, region: GameRegion):
+        """Encode the given data."""
+        cipher_key = data[0] >> 3
+        cipher_pos = cipher_key * 4
+        secret = bytearray(len(data))
+        cipher = cls._CIPHERS[region]
+        for key, value in enumerate(data):
+            secret[key] = value ^ cipher[cipher_pos + key]
+        secret[0] = (secret[0] & 7) | (cipher_key << 3)
+        return secret
+
+    __repr__ = __str__
+
+    @classmethod
+    def decode_bytes(cls, secret: bytearray | str, region: GameRegion) -> bytearray:
+        """Decode a parsed secret string or byte array using a certain region.
+
+        :param secret: The secret to decode, either as a string or a byte array.
+        :type secret: bytearray or str
+        :param region: The region to use.
+        :type region: GameRegion
+        :return: The decoded byte array.
+        :rtype: bytearray"""
+        if isinstance(secret, str):  # depends on the context
+            bsecret = bytes(secret, "utf-8")
+        else:
+            bsecret = bytes(secret)
+        cipher = cls._CIPHERS[region]
+        cipher_key = bsecret[0] >> 3
+        cipher_pos = cipher_key * 4
+        decoded_bytes = bytearray(len(bsecret))
+        for key, value in enumerate(bsecret):
+            decoded_bytes[key] = value ^ cipher[cipher_pos + key]
+        decoded_bytes[0] &= 7 | (cipher_key << 3)
+        return decoded_bytes
