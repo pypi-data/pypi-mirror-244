468ab068b3eab4517cae6705f7a8226ab4699158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:06:23 +0000

    Add package waxfetcher/0.1.2/waxfetcher-0.1.2.tar.gz

---
+++ packages/waxfetcher/waxfetcher-0.1.2.tar.gz/waxfetcher-0.1.2/waxfetcher/__init__.py
@@ -0,0 +1,8 @@
+from waxfetcher import (
+    fetch_container,
+    save_and_supply,
+    read_blockchain_and_save,
+    get_transfers_deposit,
+    parse_collections,
+    get_template_name_and_count
+)
