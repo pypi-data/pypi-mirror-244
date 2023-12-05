b27514e201b0e9bb9cd21172b5081085ab6939fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 05:04:58 +0000

    Add package syndb-api-client/0.1.1/syndb_api_client-0.1.1-py3-none-any.whl

---
+++ packages/syndb-api-client/syndb_api_client-0.1.1-py3-none-any.whl/syndb-api-client/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from syndb-api-client.api.auth_api import AuthApi
+from syndb-api-client.api.dataset_api import DatasetApi
+from syndb-api-client.api.default_api import DefaultApi
+from syndb-api-client.api.metadata_api import MetadataApi
+from syndb-api-client.api.neurodata_api import NeurodataApi
+from syndb-api-client.api.s3_api import S3Api
+from syndb-api-client.api.tag_api import TagApi
+from syndb-api-client.api.user_api import UserApi
+
