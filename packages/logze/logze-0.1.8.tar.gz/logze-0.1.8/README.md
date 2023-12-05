207d1968b1bfd64ed592c14047f9f383bdc208e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 05:05:18 +0000

    Add package logze/0.1.8/logze-0.1.8.tar.gz

---
+++ packages/logze/logze-0.1.8.tar.gz/logze-0.1.8/README.md
@@ -0,0 +1,50 @@
+
+# LOGZE
+
+[![PyPI version fury.io](https://badge.fury.io/py/logze.svg)](https://pypi.python.org/pypi/logze/)
+[![PyPI download month](https://img.shields.io/pypi/dm/logze.svg)](https://pypi.python.org/pypi/logze/)
+[![Bandit](https://github.com/josehenriqueroveda/logze/actions/workflows/bandit.yml/badge.svg)](https://github.com/josehenriqueroveda/logze/actions/workflows/bandit.yml)
+[![Percentage of issues still open](http://isitmaintained.com/badge/open/Naereen/badges.svg)](http://isitmaintained.com/project/logze/badges "Percentage of issues still open")
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+
+
+Library in Python to store logs in MongoDB and send notifications of errors to MS Teams.
+
+
+## Install
+
+Install **logze** using [PyPI](https://pypi.org/project/logze/) or directly via *pip*:
+
+```bash
+  pip install logze
+```
+    
+## Usage
+
+```python
+from logze.recorder.logger import LogRecorder
+from logze.models.log import Log
+
+
+recorder = LogRecorder("{YOUR_CONN_STRING}", "{DB_NAME}", "{COLLECTION_NAME}", "{MS_TEAMS_WEBHOOK}")
+
+log = Log("{SOURCE}", "{LOG_TYPE}", "{MESSAGE}", "{EVENT}")
+recorder.record_log(log)
+```
+
+## Running unit tests
+
+```shell
+$ export MONGO_URI="mongodb://user:password@host:port/?authMechanism=DEFAULT"
+$ export DB_NAME="YOUR_DB_NAME"
+$ python -m unittest discover tests
+...
+------------------------------------------------------------------------------
+Ran 3 tests in 0.144s
+
+OK
+```
+## Author
+
+- [@josehenriqueroveda](https://www.github.com/josehenriqueroveda)
+
---
+++ packages/logze/logze-0.1.8.tar.gz/logze-0.1.8/logze/recorder/logger.py
@@ -0,0 +1,48 @@
+import requests
+from pymongo import MongoClient
+from logze.models.log import Log
+
+
+class LogRecorder:
+    """Attributes:
+    mongo_uri (str): MongoDB connection string.
+    db_name (str): MongoDB database name.
+    logs_collection_name (str): Name of collection to record data.
+    teams_webhook (str): Microsoft Teams webhook url to send error logs in a channel.
+    """
+
+    def __init__(
+        self,
+        mongo_uri: str,
+        db_name: str,
+        logs_collection_name: str,
+        teams_webhook=None,
+    ):
+        self.mongo_uri = mongo_uri
+        self.db_name = db_name
+        self.logs_collection_name = logs_collection_name
+        self.teams_webhook = teams_webhook
+
+    def record_log(self, log: Log):
+        """Record the log into MongoDB
+        Attributes:
+            log (Log): Log object.
+        """
+        client = MongoClient(self.mongo_uri)
+        db = client[self.db_name]
+        logs_collection = db[self.logs_collection_name]
+        logs_collection.insert_one(log.__dict__)
+        client.close()
+
+        if log.level == "error" and self.teams_webhook is not None:
+            url = self.teams_webhook
+            headers = {"Content-Type": "application/json"}
+            payload = {
+                "@type": "MessageCard",
+                "@context": "http://schema.org/extensions",
+                "summary": "Log message",
+                "themeColor": "ff0000",
+                "title": f"Log: {log.source}",
+                "text": str(log.timestamp) + " " + log.message,
+            }
+            requests.post(url, headers=headers, json=payload, timeout=5)
