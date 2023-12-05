7d9f405a2ccd476233c36d9b4b7eecce35887728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 16:05:29 +0000

    Add package common-chewtils/1.0.0.2/common-chewtils-1.0.0.2.tar.gz

---
+++ packages/common-chewtils/common-chewtils-1.0.0.2.tar.gz/common-chewtils-1.0.0.2/src/common-chewtils/testmodule.py
@@ -0,0 +1,9 @@
+class TestModule:
+    def sayHello():
+        print("This is a hello!")
+
+    def add(number, number2):
+        return number + number2
+
+    def subtract(number, number2):
+        return number - number2
