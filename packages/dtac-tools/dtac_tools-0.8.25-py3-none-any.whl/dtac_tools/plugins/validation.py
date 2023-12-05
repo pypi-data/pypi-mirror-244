f79173c7ebc3d8f30cc41d181d030ccb25b2518c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 05:05:41 +0000

    Add package dtac-tools/0.8.25/dtac_tools-0.8.25-py3-none-any.whl

---
+++ packages/dtac-tools/dtac_tools-0.8.25-py3-none-any.whl/dtac_tools/plugins/validation.py
@@ -0,0 +1,17 @@
+import json
+
+from pydantic import BaseModel, create_model
+from typing import Type, get_type_hints, Optional, Union
+
+class Validator(object):
+    @classmethod
+    def generate_schema(cls, model_class: Type[BaseModel], allow_additional: bool = False) -> str:
+        # # Modify the Config of the model class to handle 'extra' behavior
+        # class DynamicConfig:
+        #     extra = 'allow' if allow_additional else 'ignore'
+        #
+        # # Create a subclass of the model with the dynamic configuration
+        # dynamic_model = type('DynamicModel', (model_class,), {'Config': DynamicConfig})
+
+        # Generate the JSON schema and serialize it to a JSON string
+        return json.dumps(model_class.schema_json(), indent=2)
