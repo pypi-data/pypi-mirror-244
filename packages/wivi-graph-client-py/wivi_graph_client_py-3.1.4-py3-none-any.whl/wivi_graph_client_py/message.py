be467dfdf55f9bc5b31f7e0093e404b78238dca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 16:05:05 +0000

    Add package wivi-graph-client-py/3.1.4/wivi_graph_client_py-3.1.4-py3-none-any.whl

---
+++ packages/wivi-graph-client-py/wivi_graph_client_py-3.1.4-py3-none-any.whl/wivi_graph_client_py/message.py
@@ -0,0 +1,29 @@
+class Message_Query:
+    get_message_query = """
+        query GetMessages($input: MessageFilterArgs) {
+            message(input: $input) {
+                id
+                arbId
+                name
+                networkId
+                ecuId
+                uploadId
+            }
+        }
+    """
+
+class Message_Mutation:
+    create_message_mutation = """
+        mutation CreateNewMessage($input: CreateMessageInput) {
+            createMessage(input: $input) {
+                message {
+                    id
+                    arbId
+                    name
+                    networkId
+                    ecuId
+                    uploadId
+                }
+            }
+        }
+    """
