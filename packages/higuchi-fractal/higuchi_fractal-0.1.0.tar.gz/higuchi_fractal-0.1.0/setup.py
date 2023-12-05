1cd39714c08742d05c3ddb8d94ad08d2471ff1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:05:46 +0000

    Add package higuchi-fractal/0.1.0/higuchi_fractal-0.1.0.tar.gz

---
+++ packages/higuchi-fractal/higuchi_fractal-0.1.0.tar.gz/higuchi_fractal-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='higuchi_fractal',
+    version='0.1.0',  # Start with a small version number like 0.1.0
+    description='A Python library for calculating Higuchi Fractal Dimension',
+    long_description='Longer description of your package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/higuchi_fractal',  # Optional
+    packages=['higuchi_fractal'],
+    install_requires=[
+        'numpy',
+        # Add other dependencies here
+    ],
+)
