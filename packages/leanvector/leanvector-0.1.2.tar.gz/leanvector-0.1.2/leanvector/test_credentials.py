64d02da1ff77641146c766400c883a538e925339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 16:05:36 +0000

    Add package leanvector/0.1.2/leanvector-0.1.2.tar.gz

---
+++ packages/leanvector/leanvector-0.1.2.tar.gz/leanvector-0.1.2/leanvector/test_credentials.py
@@ -0,0 +1,4 @@
+endpoint="https://s3.us-south.cloud-object-storage.appdomain.cloud"
+accesskey="60a41ea4a36448dfa44166b03938b596"
+secretkey="bed6beaae24ee6e42ce008abdd5a2e8f24c5d50e213a9bea"
+bucket="my-data-lake-cos-bucket"
