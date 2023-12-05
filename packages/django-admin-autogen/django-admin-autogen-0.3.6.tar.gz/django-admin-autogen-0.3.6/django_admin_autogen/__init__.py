4637723484883e50ec76aa2a13f8df4cbf1aea4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 16:04:42 +0000

    Add package django-admin-autogen/0.3.6/django-admin-autogen-0.3.6.tar.gz

---
+++ packages/django-admin-autogen/django-admin-autogen-0.3.6.tar.gz/django-admin-autogen-0.3.6/django_admin_autogen/__init__.py
@@ -0,0 +1,3 @@
+from .admin import register_all_models_of_app, register_all_fields_of_model
+
+__all__ = ["register_all_models_of_app", "register_all_fields_of_model"]
---
+++ packages/django-admin-autogen/django-admin-autogen-0.3.6.tar.gz/django-admin-autogen-0.3.6/setup.py
@@ -0,0 +1,28 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='django-admin-autogen',
+    version='0.3.6',
+    url="https://github.com/appardev/django_admin_autogen",
+    project_urls={
+        'Source': 'https://github.com/appardev/django_admin_autogen',
+        'Tracker': 'https://github.com/appardev/django_admin_autogen/issues',
+    },
+    description='A Django package to dynamically create admin classes. Including all fields in a model, or all models in an app.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Sean Chen',
+    author_email='sean@appar.com.tw',
+    packages=['django_admin_autogen'],
+    include_package_data=True,
+    install_requires=[
+        'Django>=3.0',  # Make sure to specify appropriate versions
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Update with your chosen license
+    ],
+    license='MIT',
+)
