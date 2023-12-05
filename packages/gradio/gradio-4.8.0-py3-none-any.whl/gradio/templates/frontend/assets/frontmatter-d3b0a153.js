0ded1ea4da9a0a6446399543ad5ab581b4f1bdc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 14:04:33 +0000

    Add package gradio/4.8.0/gradio-4.8.0-py3-none-any.whl

---
+++ packages/gradio/gradio-4.8.0-py3-none-any.whl/gradio/templates/frontend/assets/frontmatter-d3b0a153.js
@@ -0,0 +1,65 @@
+import { s as styleTags, f as foldNodeProp, c as foldInside, p as parseMixed, t as tags, S as StreamLanguage } from './Index-623b3100.js';
+import { yaml } from './yaml-c63fc23d.js';
+import './index-a4a7fd54.js';
+import './svelte/svelte.js';
+import './Button-4603580c.js';
+import './Index-1fd72dbe.js';
+import './Check-1d114215.js';
+import './Copy-ea46e0bf.js';
+import './Download-ea83bae3.js';
+import './BlockLabel-29e86818.js';
+import './Empty-e2e0272e.js';
+import './Example-ef3222e9.js';
+
+const frontMatterFence = /^---\s*$/m;
+const frontmatter = {
+  defineNodes: [{ name: "Frontmatter", block: true }, "FrontmatterMark"],
+  props: [
+    styleTags({
+      Frontmatter: [tags.documentMeta, tags.monospace],
+      FrontmatterMark: tags.processingInstruction
+    }),
+    foldNodeProp.add({
+      Frontmatter: foldInside,
+      FrontmatterMark: () => null
+    })
+  ],
+  wrap: parseMixed((node) => {
+    const { parser } = StreamLanguage.define(yaml);
+    if (node.type.name === "Frontmatter") {
+      return {
+        parser,
+        overlay: [{ from: node.from + 4, to: node.to - 4 }]
+      };
+    }
+    return null;
+  }),
+  parseBlock: [
+    {
+      name: "Frontmatter",
+      before: "HorizontalRule",
+      parse: (cx, line) => {
+        let end = void 0;
+        const children = new Array();
+        if (cx.lineStart === 0 && frontMatterFence.test(line.text)) {
+          children.push(cx.elt("FrontmatterMark", 0, 4));
+          while (cx.nextLine()) {
+            if (frontMatterFence.test(line.text)) {
+              end = cx.lineStart + 4;
+              break;
+            }
+          }
+          if (end !== void 0) {
+            children.push(cx.elt("FrontmatterMark", end - 4, end));
+            cx.addElement(cx.elt("Frontmatter", 0, end, children));
+          }
+          return true;
+        }
+        return false;
+      }
+    }
+  ]
+};
+
+export { frontmatter };
+//# sourceMappingURL=frontmatter-d3b0a153.js.map
---
+++ packages/gradio/gradio-4.8.0-py3-none-any.whl/gradio/templates/frontend/assets/index-42bc8bef.js
@@ -0,0 +1,6 @@
+export { L as Loader, S as StatusTracker, S as default } from './Index-1fd72dbe.js';
+export { T as Toast } from './Blocks-d34cda94.js';
+import './index-a4a7fd54.js';
+import './svelte/svelte.js';
+import './Button-4603580c.js';
+//# sourceMappingURL=index-42bc8bef.js.map
