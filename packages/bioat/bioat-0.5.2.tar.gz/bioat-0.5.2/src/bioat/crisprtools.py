02521917715ea438bd405702c86ba152c16bbf5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 03:04:52 +0000

    Add package bioat/0.5.2/bioat-0.5.2.tar.gz

---
+++ packages/bioat/bioat-0.5.2.tar.gz/bioat-0.5.2/src/bioat/crisprtools.py
@@ -0,0 +1,17 @@
+class CrisprTools:
+    """CRISPR mining toolbox."""
+    def crispr_repeats_finder(self):
+        pass
+    def cas12_finder(self):
+        pass
+    def cas13_finder(self):
+        pass
+
+
+# /Users/zhaohuanan/micromamba/envs/snakepipes_Cas-mining/bin/pilercr
+# /Users/zhaohuanan/micromamba/envs/snakepipes_Cas-mining/bin/prodigal
+# /Users/zhaohuanan/micromamba/envs/snakepipes_Cas-mining/bin/bedtools
+
+
+
+
