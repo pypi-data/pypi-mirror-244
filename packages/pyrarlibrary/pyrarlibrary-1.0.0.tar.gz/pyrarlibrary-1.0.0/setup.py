2de97a94ead1c0dd15580765fc6cf9ea2679ae03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 03:04:56 +0000

    Add package pyrarlibrary/1.0.0/pyrarlibrary-1.0.0.tar.gz

---
+++ packages/pyrarlibrary/pyrarlibrary-1.0.0.tar.gz/pyrarlibrary-1.0.0/setup.py
@@ -0,0 +1,10 @@
+#from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
+
+setup(
+  name = "pyrarlibrary",
+  version = "1.0.0",
+  packages=find_packages(),
+  author = "RayServer",
+  author_email = "susej.mabel2009@gmail.com",
+) 
