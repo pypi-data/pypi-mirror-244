96b35c98a5f014575968705a7f26e305b7fdac87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 06:04:28 +0000

    Add package ffxivcalc/0.8.60/ffxivcalc-0.8.60.tar.gz

---
+++ packages/ffxivcalc/ffxivcalc-0.8.60.tar.gz/ffxivcalc-0.8.60/src/ffxivcalc/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.8.60'
+
+"""
+Update history :
+
+0.8.60 :
+    - Added more accurate AA, weapon delay, haste affecting delay.
+    - Fixed DOT/AA issues
+    - Added AA delay, GCD timer and more DOT tests.
+    - Added cli "tests" command to run test suites.
+
+"""
