bfa77a0c214d8c1b54f42656ea1c797a8078b60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:05:58 +0000

    Add package e2b/0.13.9/e2b-0.13.9.tar.gz

---
+++ packages/e2b/e2b-0.13.9.tar.gz/e2b-0.13.9/e2b/constants.py
@@ -0,0 +1,25 @@
+import os
+
+SANDBOX_REFRESH_PERIOD = 5  # seconds
+
+TIMEOUT = 60
+
+DOMAIN = os.getenv("E2B_DOMAIN") or "e2b.dev"
+SECURE = os.getenv("E2B_SECURE", "TRUE").upper() == "TRUE"
+DEBUG = os.getenv("E2B_DEBUG") or False
+API_DOMAIN = f"api.{DOMAIN}"
+
+if DEBUG:
+    DOMAIN = "localhost:3000"
+    API_DOMAIN = "localhost:3000"
+    SECURE = False
+
+PROTOCOL = "https" if SECURE else "http"
+API_HOST = f"{PROTOCOL}://{API_DOMAIN}"
+
+
+SANDBOX_DOMAIN = DOMAIN
+
+ENVD_PORT = 49982
+WS_ROUTE = "/ws"
+FILE_ROUTE = "/file"
