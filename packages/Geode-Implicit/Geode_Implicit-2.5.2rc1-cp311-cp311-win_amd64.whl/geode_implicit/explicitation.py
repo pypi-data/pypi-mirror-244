89507096a85130da5474ffdf96c39ef367ef5cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 03:05:06 +0000

    Add package Geode-Implicit/2.5.2rc1/Geode_Implicit-2.5.2rc1-cp311-cp311-win_amd64.whl

---
+++ packages/Geode-Implicit/Geode_Implicit-2.5.2rc1-cp311-cp311-win_amd64.whl/geode_implicit/explicitation.py
@@ -0,0 +1,12 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode_geosciences
+import geode_conversion
+import geode_background
+import geode_explicit
+
+from .bin.geode_implicit_py_explicitation import *
+
+ImplicitExplicitationLibrary.initialize()
