1ba897efd20c6710340459597b5445e0e4944b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 15:04:20 +0000

    Add package seavoice-sdk-test/2.0.2/seavoice_sdk_test-2.0.2-py3-none-any.whl

---
+++ packages/seavoice-sdk-test/seavoice_sdk_test-2.0.2-py3-none-any.whl/seavoice_sdk/exceptions.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+
+class SpeechRecognitionException(Exception):
+    def __init__(self, exception: Optional[Exception] = None, message: Optional[str] = None) -> None:
+        self._exception = exception
+        self._message = message
+
+    def __str__(self) -> str:
+        return f"{type(self)}: {self._message if self._message else self._exception}"
+
+
+class ClosedException(SpeechRecognitionException):
+    pass
+
+
+class UnExpectedClosedException(SpeechRecognitionException):
+    pass
+
+
+class InternalError(SpeechRecognitionException):
+    pass
+
+
+class InvalidURI(SpeechRecognitionException):
+    pass
+
+
+class AuthenticationFail(SpeechRecognitionException):
+    pass
+
+
+class InvalidEvent(SpeechRecognitionException):
+    pass
