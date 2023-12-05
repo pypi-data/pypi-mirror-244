a05bbe844ef030f100ee3141f922c373a91b436a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:05:22 +0000

    Add package ixoncdkingress/0.0.16/ixoncdkingress-0.0.16.tar.gz

---
+++ packages/ixoncdkingress/ixoncdkingress-0.0.16.tar.gz/ixoncdkingress-0.0.16/ixoncdkingress/function/caller.py
@@ -0,0 +1,45 @@
+"""
+Functions for finding and calling Cloud Functions.
+"""
+import importlib
+import sys
+from typing import Any
+
+from ixoncdkingress.function.context import FunctionContext
+from ixoncdkingress.types import FunctionArguments, FunctionLocation
+from ixoncdkingress.utils import handle_exception
+from ixoncdkingress.webserver.config import Config
+from ixoncdkingress.webserver.response import Response
+
+
+def call_function(
+        config: Config, context: FunctionContext,
+        function_location: FunctionLocation,
+        function_kwargs: FunctionArguments,
+        response: Response
+    ) -> Any:
+    """
+    Finds, loads and calls the function specified in the body. The content_type specifies the
+    format of the body.
+    """
+
+    # Get the specified module
+    sys.path.insert(0, config.cbc_path)
+    module = importlib.import_module(function_location[0])
+    if config.production_mode is False:
+        try:
+            module = importlib.reload(module)
+        except ImportError as error:
+            handle_exception(error, response)
+            return None
+    del sys.path[0]
+
+    # Get the specified function
+    if hasattr(module, function_location[1]):
+        function = getattr(module, function_location[1])
+
+        # Check if it's exposed
+        if getattr(function, 'exposed', False):
+            return function(context, **function_kwargs)
+
+    return f"Not found, function '{'.'.join(function_location)}' does not exist or is not exposed"
