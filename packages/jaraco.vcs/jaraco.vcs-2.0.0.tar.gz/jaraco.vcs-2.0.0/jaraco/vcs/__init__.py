4b6b9ebde47a0d2481de75113a001f0a73bd6869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 16:05:13 +0000

    Add package jaraco.vcs/2.0.0/jaraco.vcs-2.0.0.tar.gz

---
+++ packages/jaraco.vcs/jaraco.vcs-2.0.0.tar.gz/jaraco.vcs-2.0.0/jaraco/vcs/__init__.py
@@ -0,0 +1,14 @@
+"""
+>>> repo().get_current_version()
+'...'
+>>> isinstance(repo(), Repo)
+True
+"""
+
+from .base import Repo
+from .subprocess import Mercurial, Git
+
+
+__all__ = ['Repo', 'repo', 'Mercurial', 'Git']
+
+repo = Repo.detect
