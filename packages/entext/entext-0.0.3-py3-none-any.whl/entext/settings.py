fecf7fd3f566f8277e0d8cd36a9104584a80ce39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:05:20 +0000

    Add package entext/0.0.3/entext-0.0.3-py3-none-any.whl

---
+++ packages/entext/entext-0.0.3-py3-none-any.whl/entext/settings.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+SYSTEM_PROMPT: str = (
+    "In the provided string, please tell me which part is the "
+    "department/school and which part the university or institution name. Reply"
+    " in JSON with no explanation. The JSON should have two fields, one called"
+    "department and one called university. For example, if the string was "
+    "'Department of Computer Science, University of Oxford', the JSON "
+    'would be {"department": "Department of Computer Science", '
+    '"university": "University of Oxford"}. Make sure that all your '
+    'responses use double quotes (") and not single quotes. Not all '
+    "institutions are universities. They might, for example, be hospitals. "
+    'Nonetheless, please always use the word "university" in your response. '
+)
+
+OPENAI_KEY: str = (Path.home() / ".openai").read_text().strip()
