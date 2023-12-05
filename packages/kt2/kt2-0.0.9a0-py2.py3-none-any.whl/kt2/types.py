4ebdb80e020aaf986152c080a0faeb8bcecfde22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 16:05:13 +0000

    Add package kt2/0.0.9a0/kt2-0.0.9a0-py2.py3-none-any.whl

---
+++ packages/kt2/kt2-0.0.9a0-py2.py3-none-any.whl/kt2/types.py
@@ -0,0 +1,21 @@
+from fhir.resources.R4B.fhirtypes import Id
+
+from kt2.models import (
+    KT2ActivityDefinition,
+    KT2CareTeam,
+    KT2Endpoint,
+    KT2Organization,
+    KT2Patient,
+    KT2Practitioner,
+    KT2Task,
+)
+
+KT2ActivityDefinitionList = list[KT2ActivityDefinition]
+KT2CareTeamList = list[KT2CareTeam]
+KT2CareTeamList = list[KT2CareTeam]
+KT2EndpointList = list[KT2Endpoint]
+KT2OrganizationList = list[KT2Organization]
+KT2PatientList = list[KT2Patient]
+KT2PracticionerList = list[KT2Practitioner]
+KT2TaskList = list[KT2Task]
+KT2Id = Id
