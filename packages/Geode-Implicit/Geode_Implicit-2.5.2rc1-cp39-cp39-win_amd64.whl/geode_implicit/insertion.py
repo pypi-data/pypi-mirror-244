9370bdaaf74c58338863cfd3e61080bec2792253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 03:05:06 +0000

    Add package Geode-Implicit/2.5.2rc1/Geode_Implicit-2.5.2rc1-cp39-cp39-win_amd64.whl

---
+++ packages/Geode-Implicit/Geode_Implicit-2.5.2rc1-cp39-cp39-win_amd64.whl/geode_implicit/insertion.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode_geosciences
+import geode_conversion
+import geode_explicit
+
+from .bin.geode_implicit_py_insertion import *
+ImplicitInsertionLibrary.initialize()
