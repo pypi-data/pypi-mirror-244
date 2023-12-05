5cabdb5b830597b3b1d03e3f1cd833ae2a75310a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 05:04:50 +0000

    Add package article-rec-db/0.0.6/article_rec_db-0.0.6.tar.gz

---
+++ packages/article-rec-db/article_rec_db-0.0.6.tar.gz/article_rec_db-0.0.6/article_rec_db/sites/base.py
@@ -0,0 +1,20 @@
+import re
+
+from pydantic import BaseModel, validator
+
+from .helpers import SiteName
+
+PATTERN_SITE_NAME_KEBAB = re.compile(r"^[a-z]+(-[a-z]+)*$")
+
+
+class Site(BaseModel):
+    name: SiteName
+
+    @validator("name")
+    def name_must_be_kebabcase(cls, value: SiteName) -> SiteName:
+        assert PATTERN_SITE_NAME_KEBAB.fullmatch(value) is not None, "Site name must be kebab-case"
+        return value
+
+    @property
+    def name_snakecase(self) -> str:
+        return self.name.replace("-", "_")
