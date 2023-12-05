7db90db677d72a08aeda8594ead1019b49ad02bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 03:04:42 +0000

    Add package oarepo-ui/5.0.88/oarepo-ui-5.0.88.tar.gz

---
+++ packages/oarepo-ui/oarepo-ui-5.0.88.tar.gz/oarepo-ui-5.0.88/run-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+OAREPO_VERSION="${OAREPO_VERSION:-11}"
+OAREPO_VERSION_MAX=$((OAREPO_VERSION+1))
+
+
+VENV=".venv"
+
+if test -d $VENV ; then
+  rm -rf $VENV
+fi
+
+python3 -m venv $VENV
+. $VENV/bin/activate
+pip install -U setuptools pip wheel
+
+pip install "oarepo>=${OAREPO_VERSION},<${OAREPO_VERSION_MAX}"
+pip install -e ".[tests]"
+
+pip uninstall -y uritemplate
+pip install uritemplate
+
+invenio index destroy --force --yes-i-know || true
+
+pytest tests
