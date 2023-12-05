24b42a3cb4472cb2ba8e02741d5904b56470eb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:06:31 +0000

    Add package AkimFindLibrary/0.0.1/AkimFindLibrary-0.0.1.tar.gz

---
+++ packages/AkimFindLibrary/AkimFindLibrary-0.0.1.tar.gz/AkimFindLibrary-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='AkimFindLibrary',
+  version='0.0.1',
+  author='Akim_b',
+  author_email='akim.petbu@gmail.com',
+  description='This is the simplest module for EGE Number 24.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/Akello1',
+  packages=find_packages(),
+  install_requires=['requests>=2.25.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='file',
+  project_urls={
+    'GitHub': 'https://github.com/Akello1'
+  },
+  python_requires='>=3.6'
+)
