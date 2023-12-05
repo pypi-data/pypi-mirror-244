2d1e012be3dc4cb6cf1c287769588ce3f4b645cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:05:46 +0000

    Add package TalkGTTS/0.1/TalkGTTS-0.1-py3-none-any.whl

---
+++ packages/TalkGTTS/TalkGTTS-0.1-py3-none-any.whl/TalkGTTS/other_module.py
@@ -0,0 +1,9 @@
+import subprocess
+from gtts import gTTS
+import os
+def say(speak):
+        tts = gTTS(speak)
+        tts.save(".outputQwerthjgfr5ty4ui3j4b5fr4t6e7wuisdjhfgrty4u.mp3")
+        os.system("afplay 
+.outputQwerthjgfr5ty4ui3j4b5fr4t6e7wuisdjhfgrty4u.mp3$
+        os.remove(".outputQwerthjgfr5ty4ui3j4b5fr4t6e7wuisdjhfgrty4u.mp3")
