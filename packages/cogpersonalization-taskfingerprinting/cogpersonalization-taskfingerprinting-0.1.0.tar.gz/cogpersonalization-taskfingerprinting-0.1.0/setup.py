24f4cced4fd1d126c480ddec8b0312f9f07887f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 04:05:18 +0000

    Add package cogpersonalization-taskfingerprinting/0.1.0/cogpersonalization-taskfingerprinting-0.1.0.tar.gz

---
+++ packages/cogpersonalization-taskfingerprinting/cogpersonalization-taskfingerprinting-0.1.0.tar.gz/cogpersonalization-taskfingerprinting-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cogpersonalization-taskfingerprinting',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+
+        ],
+    },
+    # Add additional metadata as needed
+    author='Dennis Paulino',
+    description='Your package description',
+)
+
