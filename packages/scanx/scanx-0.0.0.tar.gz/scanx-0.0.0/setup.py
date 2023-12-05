4bcb62c9b486925a23e3bf09680fe1930376daab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-05 03:04:27 +0000

    Add package scanx/0.0.0/scanx-0.0.0.tar.gz

---
+++ packages/scanx/scanx-0.0.0.tar.gz/scanx-0.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+# Third party modules
+from setuptools import setup
+
+setup(name='scanx',
+      version='0.0.0',
+      description='placeholder',
+      url='http://github.com/SK/scanx',
+      author='Stephen King',
+      author_email='stephen.ra.king@gmail.com',
+      license='MIT',
+      packages=['scanx'],
+      zip_safe=False)
