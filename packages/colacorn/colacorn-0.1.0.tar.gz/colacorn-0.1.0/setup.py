c63bf85210cb9750ad3fd54593039c3ea8dbfb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 15:04:42 +0000

    Add package colacorn/0.1.0/colacorn-0.1.0.tar.gz

---
+++ packages/colacorn/colacorn-0.1.0.tar.gz/colacorn-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="colacorn",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Wang Shengyang",
+    author_email="1095046934@qq.com"
+)
