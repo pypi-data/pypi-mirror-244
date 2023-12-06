ce520bd5239ce3846f073efa8d2956044161f333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:05:27 +0000

    Add package CowObfuscation/1.0.5/CowObfuscation-1.0.5-py3-none-any.whl

---
+++ packages/CowObfuscation/CowObfuscation-1.0.5-py3-none-any.whl/CowObfuscation/__init__.py
@@ -0,0 +1,23 @@
+
+
+from .__version__ import (
+    __title__,
+    __description__,
+    __version__,
+    __author__,
+    __author_email__,
+    __license__,
+    __copyright__,
+)
+from .CowObfuscation import CowObfuscation
+
+__all__ = [
+    '__title__',
+    '__description__',
+    '__version__',
+    '__author__',
+    '__author_email__',
+    '__license__',
+    '__copyright__',
+    'Crypto',
+]
