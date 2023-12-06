f67aecbeab963e0a38d812bfbf1ad09545ba387d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 04:04:47 +0000

    Add package libdetectability/0.2.6/libdetectability-0.2.6.tar.gz

---
+++ packages/libdetectability/libdetectability-0.2.6.tar.gz/libdetectability-0.2.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libdetectability',
+    version='0.2.6',
+    packages=find_packages(),
+    install_requires=[
+        "pytest", "numpy", "scipy", "torch"
+    ],
+    test_suite='test',
+)
