ec83cbd9acea12fec6f8ef13a1fa86c338adae30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 17:04:43 +0000

    Add package amplabs/0.0.3/amplabs-0.0.3.tar.gz

---
+++ packages/amplabs/amplabs-0.0.3.tar.gz/amplabs-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+
+''' For dev creation - '''
+# setup(
+#     name='amplabs-dev',
+#     packages=find_packages(),
+#     version='0.0.1',
+#     description='Amplabs Library',
+#     author='Amplabs',
+#     install_requires=['dash'],
+# )
+
+
+''' For prod creation '''
+setup(
+    name='amplabs',
+    packages=find_packages(),
+    version='0.0.3',
+    description='One of the AmpLabs production to create high end plots for your data',
+    author='Amplabs',
+    install_requires=['dash'],
+    readme = "README.md",
+    long_description="hello"
+)
