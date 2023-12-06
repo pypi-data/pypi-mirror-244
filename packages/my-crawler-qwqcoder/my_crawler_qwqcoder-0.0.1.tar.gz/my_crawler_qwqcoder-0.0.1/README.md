cef3a57a6d07acce8975c99f31daa3d9c22d80fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 03:06:11 +0000

    Add package my-crawler-qwqcoder/0.0.1/my_crawler_qwqcoder-0.0.1.tar.gz

---
+++ packages/my-crawler-qwqcoder/my_crawler_qwqcoder-0.0.1.tar.gz/my_crawler_qwqcoder-0.0.1/README.md
@@ -0,0 +1,4 @@
+# MyCrawler
+
++ 通过request, PyQuery 实现对豆瓣的短评爬取
++ 使用jieba, WordCloud 实现对短评的词频统计和词云图生成
---
+++ packages/my-crawler-qwqcoder/my_crawler_qwqcoder-0.0.1.tar.gz/my_crawler_qwqcoder-0.0.1/setup.py
@@ -0,0 +1,35 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi (if you dont need delete it)
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+# you need to change all these
+VERSION = '0.0.1'
+DESCRIPTION = 'a ligh weight menu , support both win and mac '
+LONG_DESCRIPTION = 'dumb_menu is a ligh weight menu ,support hot key, support both win and mac'
+
+setup(
+    name="my_crawler_qwqcoder",
+    version=VERSION,
+    author="qwqcoder",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'menu', 'crawler','windows','mac','linux'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
