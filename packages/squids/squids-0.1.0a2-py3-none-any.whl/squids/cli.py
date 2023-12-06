53894572fefadd50d91ffa08c5f777d96e8239ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 05:04:52 +0000

    Add package squids/0.1.0a2/squids-0.1.0a2-py3-none-any.whl

---
+++ packages/squids/squids-0.1.0a2-py3-none-any.whl/squids/cli.py
@@ -0,0 +1,149 @@
+import argparse
+import importlib
+import logging
+import os
+import sys
+from functools import partial
+from textwrap import dedent
+
+from squids.consumer import run_loop
+
+BANNER = dedent(
+    """\
+
+                /######   /######            /##       /##  /######
+               /##__  ## /##__  ##          |__/      | ## /##__  ##
+              | ##  \__/| ##  \ ## /##   /## /##  /#######| ##  \__/
+              |  ###### | ##  | ##| ##  | ##| ## /##__  ##|  ######
+               \____  ##| ##  | ##| ##  | ##| ##| ##  | ## \____  ##
+               /##  \ ##| ##/## ##| ##  | ##| ##| ##  | ## /##  \ ##
+              |  ######/|  ######/|  ######/| ##|  #######|  ######/
+               \______/  \____ ### \______/ |__/ \_______/ \______/
+                              \__/
+    """
+)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    # I'm not sure if I like processing many queues as long polling can kind
+    # of mess it up in terms of consumption throughput
+    parser.add_argument(
+        "-q",
+        "--queue",
+        action="store",
+        type=str,
+        required=True,
+        help="The name of the SQS queue to process.",
+    )
+    parser.add_argument(
+        "-w",
+        "--workers",
+        action="store",
+        type=int,
+        required=False,
+        default=os.cpu_count() or 2,
+        help="The number of workers to run. Defaults to the number of CPUs in the system",
+    )
+    parser.add_argument(
+        "-a",
+        "--app",
+        action="store",
+        type=str,
+        required=True,
+        help="Path to the application class something like package.module:app where app is an instance of squids.App",
+    )
+    parser.add_argument(
+        "--polling-wait-time",
+        action="store",
+        type=int,
+        required=False,
+        choices=range(0, 21),
+        default=5,
+        help=(
+            "The WaitTimeSeconds for polling for messages from the queue. Consult the AWS SQS docs on long polling "
+            "for more information about this setting. "
+            "https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-short-and-long-polling.html#sqs-long-polling"
+        ),
+    )
+    parser.add_argument(
+        "--visibility-timeout",
+        action="store",
+        type=int,
+        required=False,
+        default=30,
+        help=(
+            "The VisibilityTimeout duration (in seconds) that the received messages are hidden from subsequent "
+            "retrieve requests after being retrieved by a ReceiveMessage request."
+        ),
+    )
+    parser.add_argument(
+        "--log-level",
+        action="store",
+        type=str,
+        required=False,
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        default="INFO",
+        help="Set the logging level for the consumer. Logs will be handled using the logging.SteamHandler with the stream set to stdout",
+    )
+    return parser.parse_args()
+
+
+def import_app(import_path):
+    cwd = os.getcwd()
+    if cwd not in sys.path:
+        sys.path.insert(0, cwd)
+
+    module, path = import_path.split(":", 1)
+    mod = importlib.import_module(module)
+
+    app = mod
+    for part in path.split("."):
+        app = getattr(app, part)
+
+    return app
+
+
+def configure_logger(level):
+    logger = logging.getLogger("squidslog")
+    logger.setLevel(getattr(logging, level))
+    logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+
+
+def run(args):
+    configure_logger(args.log_level)
+    app = import_app(args.app)
+    task_names = [n for n in app._tasks]
+
+    print(BANNER)
+    print(
+        "[config]\n"
+        f"  app = {app.name}\n"
+        f"  queue = {args.queue}\n"
+        f"  workers = {args.workers}\n"
+        f"  polling-wait-time = {args.polling_wait_time}\n"
+        f"  visibility-timeout = {args.visibility_timeout}\n"
+        f"  log-level = {args.log_level}\n"
+    )
+
+    if not task_names:
+        print(f"No tasks registered", file=sys.stderr)
+        return
+
+    print("[tasks]")
+    for name in task_names:
+        print(f"  - {name}")
+    print()
+
+    run_loop(
+        app,
+        args.queue,
+        args.workers,
+        args.polling_wait_time,
+        args.visibility_timeout,
+        partial(configure_logger, args.log_level),
+    )
+
+
+def main():
+    run(parse_args())
---
+++ packages/squids/squids-0.1.0a2-py3-none-any.whl/squids/consumer.py
@@ -0,0 +1,221 @@
+from __future__ import annotations
+
+import logging
+import sys
+import time
+from concurrent.futures import Future, ProcessPoolExecutor
+from functools import partial
+from signal import SIG_IGN, SIGINT, SIGTERM, signal
+from typing import TYPE_CHECKING, Any, Callable, Dict, Hashable, Iterator, Optional, Set
+
+if TYPE_CHECKING:
+    from mypy_boto3_sqs.type_defs import MessageTypeDef
+
+    from squids import App
+
+logger = logging.getLogger("squidslog")
+
+
+class Consumer:
+    """
+    Object which consumes messages from the provided SQS queue and executes the appropriate
+    :class:`.Task` for a message. Usually you'll create a consumer instance through the
+    :meth:`.App.create_consumer` method instead of directly instantiating one yourself.
+
+    :param app: An instance of :class:`.App`.
+    :param queue_url: The queue URL.
+    """
+
+    def __init__(self, app: App, queue_url: str):
+        self.app = app
+        self.queue_url = queue_url
+
+    def _prepare_task(self, message: MessageTypeDef):
+        body = self.app._serde.deserialize(message["Body"])
+        task = self.app._tasks[body["task"]]
+        message_id = message["MessageId"]
+
+        return task, message_id, body["args"], body["kwargs"]
+
+    def _delete(self, message: MessageTypeDef) -> None:
+        self.app.sqs.delete_message(
+            QueueUrl=self.queue_url, ReceiptHandle=message["ReceiptHandle"]
+        )
+
+    def consume_messages(
+        self, options: Optional[Dict] = None
+    ) -> Iterator[MessageTypeDef]:
+        """
+        .. _SQS.Client.receive_message: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs/client/receive_message.html
+
+        Consume messages from the associated queue. Unlike :meth:`.Consumer.consume` this method
+        returns a generator over all the messages returned by calling `SQS.Client.receive_message`_.
+
+        :param options: A dict of optional values to pass to `SQS.Client.receive_message`_.
+        :return: A generator that yields message dicts.
+        """
+        if options is None:
+            options = {}
+
+        response = self.app.sqs.receive_message(QueueUrl=self.queue_url, **options)
+        for message in response.get("Messages", []):
+            yield message
+
+    def consume(self, options: Optional[Dict] = None) -> None:
+        """
+        .. _SQS.Client.receive_message: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs/client/receive_message.html
+
+        Consumes messages from the associated queue and runs the appropriate :class:`.Task` for
+        each message. Calling this only consumes as many messages as `SQS.Client.receive_message`_
+        returns. If you want to consume continuously then you'll need to put calls to in a loop.
+
+        :param options: A dict of optional values to pass to `SQS.Client.receive_message`_.
+        :return:
+        """
+        if options is None:
+            options = {}
+
+        for message in self.consume_messages(options):
+            self.run_task(message)
+
+    def run_task(self, message: MessageTypeDef) -> Any:
+        task, message_id, args, kwargs = self._prepare_task(message)
+        logger.info(
+            f"Received task: {task.name}[{message_id}]",
+            extra={
+                "message_id": message_id,
+                "task": task.name,
+                "queue": self.queue_url,
+            },
+        )
+
+        if not self.app._delete_late:
+            self._delete(message)
+
+        try:
+            result = task(message_id, *args, **kwargs)
+        except Exception as e:
+            logger.exception(
+                "Task failed",
+                extra={
+                    "message_id": message_id,
+                    "task": task.name,
+                    "queue": self.queue_url,
+                },
+            )
+            raise e
+
+        if self.app._delete_late:
+            self._delete(message)
+
+        logger.info(
+            f"Completed task: {task.name}[{message_id}]",
+            extra={
+                "message_id": message_id,
+                "task": task.name,
+                "queue": self.queue_url,
+            },
+        )
+        return result
+
+
+class ResourceLimitExceeded(Exception):
+    pass
+
+
+class ResourceTracker:
+    def __init__(self, limit: int):
+        self.limit = limit
+        self._resources: Set[Hashable] = set()
+
+    def add(self, resource: Hashable):
+        if not self.has_available_space and resource not in self._resources:
+            raise ResourceLimitExceeded()
+        self._resources.add(resource)
+
+    def remove(self, resource: Hashable):
+        self._resources.remove(resource)
+
+    def available_space(self) -> int:
+        return self.limit - len(self._resources)
+
+    @property
+    def has_available_space(self) -> int:
+        return self.available_space() > 0
+
+
+class ExitHandler:
+    def __init__(self):
+        self.should_exit = False
+        self._install_soft_shutdown()
+
+    def _install_soft_shutdown(self):
+        signal(SIGINT, self._soft_signal_handler)
+        signal(SIGTERM, self._soft_signal_handler)
+
+    def _soft_signal_handler(self, signal, frame):
+        logger.info(f"Received signal {signal}. Performing soft shutdown...")
+        logger.info(
+            f"Hit Ctrl+C again to perform a hard shutdown (will terminate all running tasks)."
+        )
+        self.should_exit = True
+        self._install_hard_shutdown()
+
+    def _install_hard_shutdown(self):
+        signal(SIGINT, self._hard_signal_handler)
+        signal(SIGTERM, self._hard_signal_handler)
+
+    def _hard_signal_handler(self, signal, frame):
+        sys.exit(signal)
+
+
+def done_callback(
+    future_tracker: ResourceTracker,
+    future: Future,
+):
+    future_tracker.remove(future)
+
+
+def initializer(configure_logger):
+    # Handles issue where KeyboardInterrupt isn't handled properly in child processes.
+    signal(SIGINT, SIG_IGN)
+    configure_logger()
+
+
+def run_loop(
+    app: App,
+    queue_name: str,
+    n_workers: int,
+    polling_wait_time: int,
+    visibility_timeout: int,
+    configure_logger: Callable,
+):
+    exit_handler = ExitHandler()
+    future_tracker = ResourceTracker(limit=n_workers * 2)
+    queue_url = app.get_queue_by_name(queue_name)
+    consumer = Consumer(app, queue_url)
+
+    with ProcessPoolExecutor(
+        max_workers=n_workers, initializer=initializer, initargs=(configure_logger,)
+    ) as executor:
+        while not exit_handler.should_exit:
+            if future_tracker.has_available_space:
+                for message in consumer.consume_messages(
+                    options={
+                        "MaxNumberOfMessages": min(
+                            future_tracker.available_space(), 10
+                        ),
+                        "WaitTimeSeconds": polling_wait_time,
+                        "VisibilityTimeout": visibility_timeout,
+                    }
+                ):
+                    future = executor.submit(consumer.run_task, message)
+                    future_tracker.add(future)
+                    future.add_done_callback(
+                        partial(
+                            done_callback,
+                            future_tracker,
+                        )
+                    )
+            else:
+                time.sleep(0.1)
