5468776dfda4f4717c94bd0ac8c71bdad297d557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 03:06:16 +0000

    Add package robocraft/1.1.4/robocraft-1.1.4.tar.gz

---
+++ packages/robocraft/robocraft-1.1.4.tar.gz/robocraft-1.1.4/setup.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from setuptools import setup
+
+
+
+
+
+setup(
+    name='robocraft',
+    version='1.1.4',
+    description='Pythonization of ComputerCraft Minecraft mod. Write Python instead Lua!',
+    author='Artem Robocodovich',
+    author_email='garo109696@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Games/Entertainment',
+    ],
+    keywords='computercraft minecraft',
+    package_data={'robocraft': ['back.lua']},
+    packages=['robocraft', 'robocraft.subapis'],
+    install_requires=['aiohttp == 3.8.5', 'greenlet'],
+    entry_points={
+        'console_scripts': ['robocraft = robocraft.server:main'],
+    },
+)
