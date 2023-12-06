e388c2025b9a436356c9777cb40818834192aa97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:05:29 +0000

    Add package functionsproject/0.1/functionsproject-0.1.tar.gz

---
+++ packages/functionsproject/functionsproject-0.1.tar.gz/functionsproject-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='functionsproject',
+    version='0.1',
+    description='Testing installation of Package',
+    url='',
+    author='Vismaya',
+    author_email='muruganvichunni@gmail.com',
+    license='MIT',
+    packages=['src'],
+    zip_safe=False
+)
---
+++ packages/functionsproject/functionsproject-0.1.tar.gz/functionsproject-0.1/src/test.py
@@ -0,0 +1,9 @@
+from src import power, average, SayHello
+
+SayHello("World")
+
+test1 = power(3,2)
+print("power(3,2) : ", test1)
+
+test2 = average(3,4)
+print("Average(3,4): ", test2)
