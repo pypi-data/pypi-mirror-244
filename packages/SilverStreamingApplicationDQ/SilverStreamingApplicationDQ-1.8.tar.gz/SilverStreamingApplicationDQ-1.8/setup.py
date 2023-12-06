389fb6cfa072661dc2f0ec2b15a1314af8f4d039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 03:05:43 +0000

    Add package SilverStreamingApplicationDQ/1.8/SilverStreamingApplicationDQ-1.8.tar.gz

---
+++ packages/SilverStreamingApplicationDQ/SilverStreamingApplicationDQ-1.8.tar.gz/SilverStreamingApplicationDQ-1.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SilverStreamingApplicationDQ',
+    version='1.8',
+    packages=['com.phida.main', 'com.phida.main.bronze', 'com.phida.tests'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='Improved appends logic to Bronze delta target'
+)
