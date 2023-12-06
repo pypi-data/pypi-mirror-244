09330e5358d2f7dc7725cac133903eca401eb39a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 05:04:49 +0000

    Add package prob-GenGauBin-distributions/0.1/prob_GenGauBin_distributions-0.1.tar.gz

---
+++ packages/prob-GenGauBin-distributions/prob_GenGauBin_distributions-0.1.tar.gz/prob_GenGauBin_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='prob_GenGauBin_distributions',
+      version='0.1',
+      description='General, Gaussian, and Binomial distributions',
+      packages=['prob_GenGauBin_distributions'],
+      author = 'Rajesh Atluri',
+      zip_safe=False)
