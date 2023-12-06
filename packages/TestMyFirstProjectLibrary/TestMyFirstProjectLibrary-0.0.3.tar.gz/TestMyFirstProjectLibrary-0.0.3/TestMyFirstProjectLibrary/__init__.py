74ee77c254e5078a9921b7c641337eea144e6d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 04:05:11 +0000

    Add package TestMyFirstProjectLibrary/0.0.3/TestMyFirstProjectLibrary-0.0.3.tar.gz

---
+++ packages/TestMyFirstProjectLibrary/TestMyFirstProjectLibrary-0.0.3.tar.gz/TestMyFirstProjectLibrary-0.0.3/TestMyFirstProjectLibrary/__init__.py
@@ -0,0 +1,3 @@
+from .Hello import HelloAkim
+import TestMyFirstProjectLibrary
+import Hello
---
+++ packages/TestMyFirstProjectLibrary/TestMyFirstProjectLibrary-0.0.3.tar.gz/TestMyFirstProjectLibrary-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+def readme():
+  with open('readme.md', 'r') as f:
+    return f.read()
+
+setup(name='TestMyFirstProjectLibrary',
+      version='0.0.3',
+      description='None',
+      packages=['TestMyFirstProjectLibrary'],
+      author_email='akim.petbu@gmail.com')
