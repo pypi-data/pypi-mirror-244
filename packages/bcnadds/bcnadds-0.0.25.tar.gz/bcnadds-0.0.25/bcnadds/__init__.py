76659a8d7538daa76c644143b132a8e4afec3d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:06:08 +0000

    Add package bcnadds/0.0.25/bcnadds-0.0.25.tar.gz

---
+++ packages/bcnadds/bcnadds-0.0.25.tar.gz/bcnadds-0.0.25/bcnadds/__init__.py
@@ -0,0 +1,14 @@
+#blackcatuserbot addons
+
+__version__ = "0.0.25"
+__license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
+__copyright__ = "Copyright (C) 2023-present bcncalling <https://github.com/bcncalling>"
+
+from .funcs import generate_cover
+from .TgGraph import TgGraph
+
+def bcnadds():
+  print(
+    "Black cat installed successfully" + "\n"
+    f"Version : {__version__}"
+  )
