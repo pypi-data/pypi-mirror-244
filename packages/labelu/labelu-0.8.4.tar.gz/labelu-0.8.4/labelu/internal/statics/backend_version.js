424f3d141eef5ba85ae6128a1f388609b246116f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:05:26 +0000

    Add package labelu/0.8.4/labelu-0.8.4.tar.gz

---
+++ packages/labelu/labelu-0.8.4.tar.gz/labelu-0.8.4/labelu/internal/statics/backend_version.js
@@ -0,0 +1,10 @@
+
+                  (function () {
+                    window.__backend = {
+  "version": "0.8.4",
+  "name": "labelu",
+  "build_time": "2023-12-06T06:18:01.641Z",
+  "commit": "87352034089dc9eddc750eca5701bb4885491bd5"
+};
+                  })();
+                
