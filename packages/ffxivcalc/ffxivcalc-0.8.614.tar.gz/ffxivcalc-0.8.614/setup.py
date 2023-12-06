d38284677f7aa7eb687186e49010190fe2a4fb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 05:05:10 +0000

    Add package ffxivcalc/0.8.614/ffxivcalc-0.8.614.tar.gz

---
+++ packages/ffxivcalc/ffxivcalc-0.8.614.tar.gz/ffxivcalc-0.8.614/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+
+setup(
+    name="ffxivcalc",  # Required
+    version="0.8.614",  # Required
+    package_dir={"": "src"},  # Optional,
+    packages=find_packages(where="src"),  # Required
+    python_requires=">=3.10",
+    install_requires=[  "contourpy",
+                        "cycler",
+                        "fonttools",
+                        "kiwisolver",
+                        "matplotlib",
+                        "numpy",
+                        "packaging",
+                        "Pillow",
+                        "pyparsing",
+                        "python-dateutil",
+                        "six",
+                        "coreapi"],  # Optional
+)
