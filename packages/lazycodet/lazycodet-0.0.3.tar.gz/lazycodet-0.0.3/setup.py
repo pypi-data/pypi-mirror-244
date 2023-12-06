f880952dfeae3e15e10ec180bf674b677f2716f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:05:26 +0000

    Add package lazycodet/0.0.3/lazycodet-0.0.3.tar.gz

---
+++ packages/lazycodet/lazycodet-0.0.3.tar.gz/lazycodet-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lazycodet',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[
+        # Các dependencies nếu có
+    ],
+)
