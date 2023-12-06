e15fe4570df12766c439b68019360786a2f16aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 03:06:13 +0000

    Add package paramount/0.0.1/paramount-0.0.1.tar.gz

---
+++ packages/paramount/paramount-0.0.1.tar.gz/paramount-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='paramount',
+   version='0.0.1',
+   description='Automated accuracy measurements for LLMs',
+   author='Hakim K',
+   author_email='hakimkse+pypi@gmail.com',
+   packages=find_packages(),
+   install_requires=[]
+)
