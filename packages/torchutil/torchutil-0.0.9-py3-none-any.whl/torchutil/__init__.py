30f3746088e75ae4ca1d2be1930dd3549b33ac24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 14:05:11 +0000

    Add package torchutil/0.0.9/torchutil-0.0.9-py3-none-any.whl

---
+++ packages/torchutil/torchutil-0.0.9-py3-none-any.whl/torchutil/__init__.py
@@ -0,0 +1,8 @@
+from . import checkpoint
+from . import download
+from . import metrics
+from . import paths
+from . import tensorboard
+from . import time
+from .iterator import iterator, multiprocess_iterator
+from .notify import notify
---
+++ packages/torchutil/torchutil-0.0.9-py3-none-any.whl/torchutil/time.py
@@ -0,0 +1,80 @@
+import contextlib
+import time
+from typing import Optional
+
+
+###############################################################################
+# Timer context
+###############################################################################
+
+
+@contextlib.contextmanager
+def context(name: Optional[str]):
+    """Wrapper to handle context changes of global timer
+
+    Arguments
+        name - Name of the timer to add time to
+    """
+    if not hasattr(context, 'timer'):
+        context.timer = Timer()
+
+    # TODO - support nested contexts
+    context.timer.name = name
+    with context.timer:
+        yield
+    context.timer.name = 'total'
+
+
+def reset():
+    """Clear timer state"""
+    try:
+        del context.timer
+    except AttributeError:
+        pass
+
+
+def results() -> dict:
+    """Get timing results
+
+    Returns
+        Timing results: {name: elapsed_time} for all names
+    """
+    if not hasattr(context, 'timer'):
+        return {}
+    return context.timer()
+
+
+###############################################################################
+# Utilities
+###############################################################################
+
+
+class Timer:
+    """Context manager timer"""
+
+    def __init__(self):
+        self.reset()
+
+    def __call__(self):
+        """Retrieve timer results"""
+        return {name: sum(times) for name, times in self.history.items()}
+
+    def __enter__(self):
+        """Start the timer"""
+        self.start = time.time()
+
+    def __exit__(self, *_):
+        """Stop the timer"""
+        elapsed = time.time() - self.start
+
+        # Add to timer history
+        if self.name not in self.history:
+            self.history[self.name] = [elapsed]
+        else:
+            self.history[self.name].append(elapsed)
+
+    def reset(self):
+        """Reset the timer"""
+        self.history = {}
+        self.start = 0.
+        self.name = 'total'
