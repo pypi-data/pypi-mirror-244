9bb760a31e0ec938b99ea4d7bb3a5fb26ab8fa81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 03:06:45 +0000

    Add package testsimpleproject/0.0.4/testsimpleproject-0.0.4.tar.gz

---
+++ packages/testsimpleproject/testsimpleproject-0.0.4.tar.gz/testsimpleproject-0.0.4/pypibasic/arithmetic.py
@@ -0,0 +1,58 @@
+"""
+Arithmetic Operations Module.
+
+Module provides functions for Arithmetic Operations like addition, subtraction, multiplication
+and division.
+"""
+
+def add_numbers(num1, num2):
+    """
+    To add the given two numbers.
+
+    Args:
+        num1 (int): The integer number is to be processed for addition.
+        num2 (int): The integer number is to be processed for addition.
+
+    Returns:
+        int: Add the num1 and num2 returns the result of num1 + num2.
+    """
+    return num1 + num2
+
+def subtract_numbers(num1, num2):
+    """
+    To subtract the given two numbers.
+
+    Args:
+        num1 (int): The integer number is to be processed for subtraction.
+        num2 (int): The integer number is to be processed for subtraction.
+
+    Returns:
+        int: Add the num1 and num2 returns the result of num1 - num2.
+    """
+    return num1 - num2
+
+def multiply_numbers(num1, num2):
+    """
+    To multiply the given two numbers.
+
+    Args:
+        num1 (int): The integer number is to be processed for multiplication.
+        num2 (int): The integer number is to be processed for multiplication.
+
+    Returns:
+        int: Add the num1 and num2 returns the result of num1 * num2.
+    """
+    return num1 * num2
+
+def divide_numbers(num1, num2):
+    """
+    To divide the given two numbers.
+
+    Args:
+        num1 (int): The integer number is to be processed for division.
+        num2 (int): The integer number is to be processed for division.
+
+    Returns:
+        int: Add the num1 and num2 returns the result of num1 / num2.
+    """
+    return num1 / num2
