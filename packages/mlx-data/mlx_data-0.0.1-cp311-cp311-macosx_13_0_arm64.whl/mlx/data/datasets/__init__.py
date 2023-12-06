eaef5998771acda0f6a6e70dd276481d8425b054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 06:04:29 +0000

    Add package mlx-data/0.0.1/mlx_data-0.0.1-cp311-cp311-macosx_13_0_arm64.whl

---
+++ packages/mlx-data/mlx_data-0.0.1-cp311-cp311-macosx_13_0_arm64.whl/mlx/data/datasets/__init__.py
@@ -0,0 +1,7 @@
+# Copyright © 2023 Apple Inc.
+
+from .mnist import load_mnist
+from .cifar import load_cifar10, load_cifar100
+from .librispeech import load_librispeech, load_librispeech_tarfile
+from .imagenet import load_imagenet, load_imagenet_metadata
+from .wikitext import load_wikitext_lines
