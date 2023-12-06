493041ad683abed5061fc3521e07bc079b73605b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 05:04:47 +0000

    Add package dsw-tdk/4.1.0/dsw_tdk-4.1.0-py2.py3-none-any.whl

---
+++ packages/dsw-tdk/dsw_tdk-4.1.0-py2.py3-none-any.whl/dsw/tdk/build_info.py
@@ -0,0 +1,17 @@
+# Generated file
+# - do not overwrite
+# - do not include in git
+from collections import namedtuple
+
+BuildInfo = namedtuple(
+    'BuildInfo',
+    ['version', 'built_at', 'sha', 'branch', 'tag'],
+)
+
+BUILD_INFO = BuildInfo(
+    version='v4.1.0~6647324',
+    built_at='2023-12-05 17:41:11Z',
+    sha='6647324f52d8be83435ceba1cd98111ae5e814bf',
+    branch='HEAD',
+    tag='v4.1.0',
+)
