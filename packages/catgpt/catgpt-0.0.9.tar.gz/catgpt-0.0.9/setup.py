be585fd541543d260be08a1d991fbc18051e08b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 14:05:08 +0000

    Add package catgpt/0.0.9/catgpt-0.0.9.tar.gz

---
+++ packages/catgpt/catgpt-0.0.9.tar.gz/catgpt-0.0.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="catgpt",
+    version="0.0.9",
+    description="A simple cli tool to generate text using GPT-turbo",
+    author="tom <tom@gmail.com>",
+    install_requires=[
+        "codefast>=23.4.18", "python-dotenv>=1.0.0", "sseclient-py>=1.7.2"
+    ],
+    tests_require=["pytest>=5.2"],
+)
