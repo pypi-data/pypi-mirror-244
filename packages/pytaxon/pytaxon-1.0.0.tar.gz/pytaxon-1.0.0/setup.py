e3931727b73a2c48c630afb4803c39436ec1a4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:05:11 +0000

    Add package pytaxon/1.0.0/pytaxon-1.0.0.tar.gz

---
+++ packages/pytaxon/pytaxon-1.0.0.tar.gz/pytaxon-1.0.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as arq:
+    readme = arq.read()
+
+setup(
+    name='pytaxon',
+    version='v1.0.0',
+    license='MIT License',
+    author='Marco Aurélio Proença Neto, Marcos Paulo Alves de Sousa',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='desousa.mpa@gmail.com',
+    keywords='pytaxon',
+    description=u'Pytaxon é uma aplicação open source de auxílio à pesquisa para identificação de erros e correção de nomenclatura taxonômica das espécies da biodiversidade',
+    packages=['pytaxon'],
+    entry_points={
+        'console_scripts': [
+            'pytaxon = pytaxon.pytaxon:main',
+        ],
+    },
+    install_requires=['pandas', 'openpyxl', 'tqdm', 'xlrd', 'thefuzz'],
+    url='https://github.com/pytaxon'
+)
