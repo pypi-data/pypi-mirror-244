3053743757f6dd927f90851bf344390c6bd64db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 14:05:02 +0000

    Add package getdaft/0.2.6/getdaft-0.2.6.tar.gz

---
+++ packages/getdaft/getdaft-0.2.6.tar.gz/getdaft-0.2.6/docs/source/api_docs/context.rst
@@ -0,0 +1,14 @@
+Configuration
+=============
+
+Setting the Runner
+******************
+
+Control the execution backend that Daft will run on by calling these functions once at the start of your application.
+
+.. autosummary::
+    :nosignatures:
+    :toctree: doc_gen/configuration_functions
+
+    daft.context.set_runner_py
+    daft.context.set_runner_ray
---
+++ packages/getdaft/getdaft-0.2.6.tar.gz/getdaft-0.2.6/local_dependencies/daft-plan/src/physical_ops/mod.rs
@@ -0,0 +1,39 @@
+mod agg;
+mod coalesce;
+mod concat;
+mod csv;
+mod explode;
+mod fanout;
+mod filter;
+mod flatten;
+#[cfg(feature = "python")]
+mod in_memory;
+mod join;
+mod json;
+mod limit;
+mod parquet;
+mod project;
+mod reduce;
+mod scan;
+mod sort;
+mod split;
+
+pub use agg::Aggregate;
+pub use coalesce::Coalesce;
+pub use concat::Concat;
+pub use csv::{TabularScanCsv, TabularWriteCsv};
+pub use explode::Explode;
+pub use fanout::{FanoutByHash, FanoutByRange, FanoutRandom};
+pub use filter::Filter;
+pub use flatten::Flatten;
+#[cfg(feature = "python")]
+pub use in_memory::InMemoryScan;
+pub use join::Join;
+pub use json::{TabularScanJson, TabularWriteJson};
+pub use limit::Limit;
+pub use parquet::{TabularScanParquet, TabularWriteParquet};
+pub use project::Project;
+pub use reduce::ReduceMerge;
+pub use scan::TabularScan;
+pub use sort::Sort;
+pub use split::Split;
