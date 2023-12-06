5ad96410eee13f9ca936ca1e159143108869be39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 15:04:32 +0000

    Add package libretro-scummvm-playlist/2.5.2/libretro_scummvm_playlist-2.5.2.tar.gz

---
+++ packages/libretro-scummvm-playlist/libretro_scummvm_playlist-2.5.2.tar.gz/libretro_scummvm_playlist-2.5.2/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['libretro_scummvm_playlist']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer[all]>=0.9.0,<0.10.0']
+
+entry_points = \
+{'console_scripts': ['libretro-scummvm-playlist = '
+                     'libretro_scummvm_playlist.__main__:main']}
+
+setup_kwargs = {
+    'name': 'libretro-scummvm-playlist',
+    'version': '2.5.2',
+    'description': 'libretro scummvm playlist builder',
+    'long_description': None,
+    'author': 'i30817',
+    'author_email': 'i30817@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
