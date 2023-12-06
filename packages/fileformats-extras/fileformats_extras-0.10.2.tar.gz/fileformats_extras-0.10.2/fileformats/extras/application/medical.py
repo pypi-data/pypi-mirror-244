e2096d7c2295505528f692cdb20baeb41cb25941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 15:04:43 +0000

    Add package fileformats-extras/0.10.2/fileformats_extras-0.10.2.tar.gz

---
+++ packages/fileformats-extras/fileformats_extras-0.10.2.tar.gz/fileformats_extras-0.10.2/fileformats/extras/application/medical.py
@@ -0,0 +1,29 @@
+import typing as ty
+from pathlib import Path
+import pydicom
+from fileformats.core import FileSet
+from fileformats.application import Dicom
+import medimages4tests.dummy.dicom.mri.t1w.siemens.skyra.syngo_d13c
+
+
+@FileSet.read_metadata.register
+def dicom_read_metadata(
+    dicom: Dicom, selected_keys: ty.Optional[ty.Sequence[str]] = None
+) -> ty.Mapping[str, ty.Any]:
+    dcm = pydicom.dcmread(dicom.fspath, specific_tags=selected_keys)
+    [getattr(dcm, a, None) for a in dir(dcm)]  # Ensure all keywords are set
+    metadata = {
+        e.keyword: e.value  # type: ignore[union-attr]
+        for e in dcm.elements()
+        if getattr(e, "keyword", False) and e.keyword != "PixelData"  # type: ignore[union-attr]
+    }
+    return metadata
+
+
+@FileSet.generate_sample_data.register
+def dicom_generate_sample_data(
+    dicom: Dicom, dest_dir: Path, seed: int, stem: ty.Optional[str]
+) -> ty.Iterable[Path]:
+    return next(
+        medimages4tests.dummy.dicom.mri.t1w.siemens.skyra.syngo_d13c.get_image().iterdir()
+    )
