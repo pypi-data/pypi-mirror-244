2283c38c50c0d487aff458d7473692e8b9a6568b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 04:05:14 +0000

    Add package st-dynamodb-connection/0.1.0/st_dynamodb_connection-0.1.0.tar.gz

---
+++ packages/st-dynamodb-connection/st_dynamodb_connection-0.1.0.tar.gz/st_dynamodb_connection-0.1.0/dynamodb_connection/__init__.py
@@ -0,0 +1,9 @@
+from .connection import DynamoDBConnection, DynamoDBConnectionApiType, DynamoDBItemType
+from .table_editor import DynamoDBTableEditor
+
+__all__ = [
+    "DynamoDBConnection",
+    "DynamoDBConnectionApiType",
+    "DynamoDBItemType",
+    "DynamoDBTableEditor"
+]
