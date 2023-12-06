10907c986b1b87bedacceeded52a710b899b076a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 14:04:25 +0000

    Add package orb-billing/1.33.3/orb_billing-1.33.3.tar.gz

---
+++ packages/orb-billing/orb_billing-1.33.3.tar.gz/orb_billing-1.33.3/src/orb/pagination.py
@@ -0,0 +1,50 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import List, Generic, Optional
+from typing_extensions import override
+
+from ._types import ModelT
+from ._models import BaseModel
+from ._base_client import BasePage, PageInfo, BaseSyncPage, BaseAsyncPage
+
+__all__ = ["SyncPage", "AsyncPage"]
+
+
+class PagePaginationMetadata(BaseModel):
+    has_more: bool
+
+    next_cursor: Optional[str]
+
+
+class SyncPage(BaseSyncPage[ModelT], BasePage[ModelT], Generic[ModelT]):
+    data: List[ModelT]
+    pagination_metadata: PagePaginationMetadata
+
+    @override
+    def _get_page_items(self) -> List[ModelT]:
+        return self.data
+
+    @override
+    def next_page_info(self) -> Optional[PageInfo]:
+        cursor = self.pagination_metadata.next_cursor
+        if not cursor:
+            return None
+
+        return PageInfo(params={"cursor": cursor})
+
+
+class AsyncPage(BaseAsyncPage[ModelT], BasePage[ModelT], Generic[ModelT]):
+    data: List[ModelT]
+    pagination_metadata: PagePaginationMetadata
+
+    @override
+    def _get_page_items(self) -> List[ModelT]:
+        return self.data
+
+    @override
+    def next_page_info(self) -> Optional[PageInfo]:
+        cursor = self.pagination_metadata.next_cursor
+        if not cursor:
+            return None
+
+        return PageInfo(params={"cursor": cursor})
