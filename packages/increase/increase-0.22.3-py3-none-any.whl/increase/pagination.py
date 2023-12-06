f8a252bcdb60966daa96070dca31db86d9583581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 14:04:52 +0000

    Add package increase/0.22.3/increase-0.22.3-py3-none-any.whl

---
+++ packages/increase/increase-0.22.3-py3-none-any.whl/increase/pagination.py
@@ -0,0 +1,43 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import List, Generic, Optional
+from typing_extensions import override
+
+from ._types import ModelT
+from ._base_client import BasePage, PageInfo, BaseSyncPage, BaseAsyncPage
+
+__all__ = ["SyncPage", "AsyncPage"]
+
+
+class SyncPage(BaseSyncPage[ModelT], BasePage[ModelT], Generic[ModelT]):
+    data: List[ModelT]
+    next_cursor: Optional[str]
+
+    @override
+    def _get_page_items(self) -> List[ModelT]:
+        return self.data
+
+    @override
+    def next_page_info(self) -> Optional[PageInfo]:
+        cursor = self.next_cursor
+        if not cursor:
+            return None
+
+        return PageInfo(params={"cursor": cursor})
+
+
+class AsyncPage(BaseAsyncPage[ModelT], BasePage[ModelT], Generic[ModelT]):
+    data: List[ModelT]
+    next_cursor: Optional[str]
+
+    @override
+    def _get_page_items(self) -> List[ModelT]:
+        return self.data
+
+    @override
+    def next_page_info(self) -> Optional[PageInfo]:
+        cursor = self.next_cursor
+        if not cursor:
+            return None
+
+        return PageInfo(params={"cursor": cursor})
