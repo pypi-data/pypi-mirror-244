890366f30a57e590374d0793deee99d24ae51a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 15:04:42 +0000

    Add package rlvortex/0.0.31/rlvortex-0.0.31.tar.gz

---
+++ packages/rlvortex/rlvortex-0.0.31.tar.gz/rlvortex-0.0.31/src/rlvortex/policy/quick_build.py
@@ -0,0 +1,22 @@
+from typing import Callable
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+def mlp(sizes, activation, output_activation: Callable = nn.Identity, last_scale=np.sqrt(2)):
+    layers = []
+    for j in range(len(sizes) - 1):
+        act = activation if j < len(sizes) - 2 else output_activation
+        layers += [nn.Linear(sizes[j], sizes[j + 1]), act()]
+    module = nn.Sequential(*layers)
+    weights = [np.sqrt(2)] * len(sizes) + [last_scale]
+    init_weights(module, weights)
+    return module
+
+
+def init_weights(sequential, weights):
+    [
+        torch.nn.init.orthogonal_(module.weight, gain=weights[idx])
+        for idx, module in enumerate(mod for mod in sequential if isinstance(mod, nn.Linear))
+    ]
