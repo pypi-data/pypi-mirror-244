d89f1856d322f8c9a2ce0acf207888b52b8b4873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 15:04:43 +0000

    Add package lazycodet-nlp/0.1/lazycodet-nlp-0.1.tar.gz

---
+++ packages/lazycodet-nlp/lazycodet-nlp-0.1.tar.gz/lazycodet-nlp-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lazycodet-nlp',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Các dependencies nếu có
+    ],
+)
