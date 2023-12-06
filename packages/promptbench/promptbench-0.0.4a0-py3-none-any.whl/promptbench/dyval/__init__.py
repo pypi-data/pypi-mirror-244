4a01af226eab4c7d0059b8d4dd52cfb08ac7aff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 03:05:46 +0000

    Add package promptbench/0.0.4a0/promptbench-0.0.4a0-py3-none-any.whl

---
+++ packages/promptbench/promptbench-0.0.4a0-py3-none-any.whl/promptbench/dyval/__init__.py
@@ -0,0 +1,13 @@
+from .dyval_dataset import DyValDataset
+from .dyval_utils import *
+
+
+DYVAL_DATASETS = [
+    "arithmetic",
+    "linear_equation",
+    "bool_logic",
+    "deductive_logic",
+    "abductive_logic",
+    "reachability",
+    "max_sum_path"
+]
