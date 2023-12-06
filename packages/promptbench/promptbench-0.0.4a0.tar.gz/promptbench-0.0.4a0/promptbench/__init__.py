3bd3d7e720698a615133a9aecd3aded0b4667c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 03:05:46 +0000

    Add package promptbench/0.0.4a0/promptbench-0.0.4a0.tar.gz

---
+++ packages/promptbench/promptbench-0.0.4a0.tar.gz/promptbench-0.0.4a0/promptbench/__init__.py
@@ -0,0 +1,7 @@
+from .models import LLMModel, SUPPORTED_MODELS
+from .prompt_engineering import PEMethod
+from .dataload import DatasetLoader, SUPPORTED_DATASETS
+from .prompts import Prompt
+from .utils import InputProcess, OutputProcess
+from .metrics import Eval
+from .dyval import DyValDataset
