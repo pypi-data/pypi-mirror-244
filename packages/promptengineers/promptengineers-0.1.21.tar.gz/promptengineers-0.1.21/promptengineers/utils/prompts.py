d6496730e19a7b1a926c96b492c1fea232a5ff1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 15:04:41 +0000

    Add package promptengineers/0.1.21/promptengineers-0.1.21.tar.gz

---
+++ packages/promptengineers/promptengineers-0.1.21.tar.gz/promptengineers-0.1.21/promptengineers/utils/prompts.py
@@ -0,0 +1,30 @@
+"""Chat GPT Clone Prompt Template"""
+from langchain.prompts import PromptTemplate
+
+def get_system_template(system_message: str):
+    prompt_template = f"""{system_message}
+---
+{{context}}
+---
+{{chat_history}}
+User: {{question}}
+Assistant: """
+    template = PromptTemplate(
+        template=prompt_template,
+        input_variables=["context", "question", "chat_history"]
+    )
+    return template
+
+def get_retrieval_template(system_message: str):
+    prompt_template = f"""{system_message}
+---
+{{context}}
+---
+{{chat_history}}
+User: {{question}}
+Assistant: """
+    template = PromptTemplate(
+        template=prompt_template,
+        input_variables=["context", "question"]
+    )
+    return template
