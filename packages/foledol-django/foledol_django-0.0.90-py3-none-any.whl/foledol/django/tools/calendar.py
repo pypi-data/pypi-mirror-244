ef2e7a37117f545b395dabf83661a77e7e6b9665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 17:04:51 +0000

    Add package foledol-django/0.0.90/foledol_django-0.0.90-py3-none-any.whl

---
+++ packages/foledol-django/foledol_django-0.0.90-py3-none-any.whl/foledol/django/tools/calendar.py
@@ -0,0 +1,44 @@
+class CalendarDay:
+    def __init__(self, label):
+        self.label = label
+
+
+class CalendarHour:
+    def __init__(self, label):
+        self.label = label
+
+
+class CalendarEvent:
+    def __init__(self, day, hour, minute, duration, label, onclick=None, css=None):
+        self.day = day
+        self.hour = hour
+        self.minute = minute
+        self.duration = duration
+        self.label = label
+        self.onclick = onclick
+        self.css = css
+
+
+class Calendar:
+    def __init__(self):
+        self.days = [
+            CalendarDay("Monday"),
+            CalendarDay("Tuesday"),
+            CalendarDay("Wednesday"),
+            CalendarDay("Thursday"),
+            CalendarDay("Friday"),
+            CalendarDay("Saterday"),
+            CalendarDay("Sunday")
+        ]
+        self.hours = []
+        for i in range(8, 20):
+            self.hours.append(CalendarHour(str(i) + ":00"))
+
+        self.events = [
+            CalendarEvent(0, 1, 30, 30, "9:30 - Cours1"),
+            CalendarEvent(1, 1, 40, 30, "9:40 - Cours2",
+              onclick="alert(\"hello\");",
+              css="calendar_event_red"
+            )
+        ]
+
