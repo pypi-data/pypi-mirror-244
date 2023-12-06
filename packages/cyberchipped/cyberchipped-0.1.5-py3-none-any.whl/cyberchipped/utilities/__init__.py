14208502a8d90f6033f2d7b67782976bd30570b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 03:05:43 +0000

    Add package cyberchipped/0.1.5/cyberchipped-0.1.5-py3-none-any.whl

---
+++ packages/cyberchipped/cyberchipped-0.1.5-py3-none-any.whl/cyberchipped/utilities/__init__.py
@@ -0,0 +1,10 @@
+from cyberchipped.settings import settings
+
+from cyberchipped.components import ai_fn, ai_model, ai_classifier
+
+__all__ = [
+    "ai_fn",
+    "ai_model",
+    "ai_classifier",
+    "settings",
+]
