f0430f524aae2956fd56b646e20858d199d5e842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:06:09 +0000

    Add package cro3n/0.0.4/cro3n-0.0.4.tar.gz

---
+++ packages/cro3n/cro3n-0.0.4.tar.gz/cro3n-0.0.4/README.md
@@ -0,0 +1,43 @@
+```
+ ██████╗ ██████╗   ██████╗3 ███╗   ██╗
+██╔════╝ ██╔══██╗ ██╔═══██╗ ████╗  ██║
+██║      ██████╔╝ ██║   ██║ ██╔██╗ ██║
+██║      ██╔══██╗ ██║   ██║ ██║╚██╗██║
+╚██████╗ ██║  ██║ ╚██████╔╝ ██║ ╚████║
+ ╚═════╝ ╚═╝  ╚═╝  ╚═════╝  ╚═╝  ╚═══╝
+```
+
+[![Release](https://github.com/StrayDragon/cro3n/actions/workflows/release.yml/badge.svg)](https://github.com/StrayDragon/cro3n/actions/workflows/release.yml)
+[![GitHub last commit](https://img.shields.io/github/last-commit/straydragon/cro3n)](https://github.com/StrayDragon/cro3n/commits)
+[![PyPI](https://img.shields.io/pypi/v/cro3n)](https://pypi.org/project/cro3n)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/cro3n)](https://pypi.org/project/cro3n)
+[![PyPI - Implementation](https://img.shields.io/pypi/implementation/cro3n)](https://pypi.org/project/cro3n)
+[![GitHub](https://img.shields.io/github/license/straydragon/cro3n)](https://github.com/StrayDragon/cro3n/blob/main/LICENSE)
+
+---
+
+Another one **cron expression** utils package for python, wrapped on rust crate [zslayton/cron](https://github.com/zslayton/cron).
+
+## Installation
+```bash
+pip install -U cro3n
+```
+
+## Example
+see [example/python](./example/python)
+
+## Dev
+you need config:
+- rust
+- python 3.7+
+- pyo3 tools
+more details see https://pyo3.rs/v0.20.0/
+
+## Related Projects
+
+- [zslayton/cron](https://github.com/zslayton/cron)
+- [PyO3/maturin](https://github.com/PyO3/maturin)
+- [PyO3/pyo3](https://github.com/PyO3/pyo3)
+
+## LICENSE
+[MIT](./LICENSE)
---
+++ packages/cro3n/cro3n-0.0.4.tar.gz/cro3n-0.0.4/src/lib.rs
@@ -0,0 +1,63 @@
+use chrono::Utc;
+use cron::Schedule;
+use pyo3::{exceptions::PyValueError, prelude::*};
+use std::str::FromStr;
+
+/// validate cron expression
+#[pyfunction]
+#[pyo3(signature = (expression))]
+fn check_cron_expression(expression: String) -> PyResult<String> {
+    let result = match Schedule::from_str(expression.as_str()) {
+        Ok(_) => String::new(),
+        Err(error) => format!("Check `{}` failed, due to {:?}", &expression, error),
+    };
+    Ok(result)
+}
+
+// class
+#[pyclass]
+struct CronExpr {
+    #[pyo3(get)]
+    source: String,
+    schedule: Schedule,
+}
+
+#[pymethods]
+impl CronExpr {
+    /// crate::cron::Schedule wrapper class
+    /// NOTE: will raise ValueError when wrong input
+    #[new]
+    #[pyo3(signature = (expression))]
+    fn py_new(expression: String) -> PyResult<Self> {
+        let err_msg = match Schedule::from_str(expression.as_str()) {
+            Ok(_) => String::new(),
+            Err(error) => format!("Check `{}` failed, due to {:?}", &expression, error),
+        };
+        if !err_msg.is_empty() {
+            Err(PyValueError::new_err(err_msg))
+        } else {
+            Ok(Self {
+                source: expression.clone(),
+                schedule: Schedule::from_str(expression.as_str()).unwrap(),
+            })
+        }
+    }
+    /// Get next n upcomming time
+    /// Example: ['2022-09-02 21:01:00 UTC', '2022-09-03 21:01:00 UTC', '2022-09-04 21:01:00 UTC', '2022-09-05 21:01:00 UTC', '2022-09-06 21:01:00 UTC']
+    #[pyo3(signature = (n))]
+    fn next_n_upcoming_time_literals(&self, n: usize) -> PyResult<Vec<String>> {
+        Ok(self
+            .schedule
+            .upcoming(Utc)
+            .take(n)
+            .map(|dt| format!("{}", dt))
+            .collect())
+    }
+}
+
+#[pymodule]
+fn cro3n(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(check_cron_expression, m)?)?;
+    m.add_class::<CronExpr>()?;
+    Ok(())
+}
