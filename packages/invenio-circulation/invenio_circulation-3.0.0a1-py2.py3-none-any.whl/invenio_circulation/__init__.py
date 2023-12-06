f364c91edea88c6e7aedca21004caa0162f05d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 04:05:33 +0000

    Add package invenio-circulation/3.0.0a1/invenio_circulation-3.0.0a1-py2.py3-none-any.whl

---
+++ packages/invenio-circulation/invenio_circulation-3.0.0a1-py2.py3-none-any.whl/invenio_circulation/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 CERN.
+# Copyright (C) 2018 RERO.
+#
+# Invenio-Circulation is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio module for the circulation of bibliographic items."""
+
+from .ext import InvenioCirculation
+
+__version__ = "3.0.0a1"
+
+__all__ = ('__version__', 'InvenioCirculation')
