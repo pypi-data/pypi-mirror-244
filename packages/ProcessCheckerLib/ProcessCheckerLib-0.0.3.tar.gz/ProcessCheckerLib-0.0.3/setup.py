f0be679de8e957e03f94954e0209dd1c66b3bbae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 05:04:39 +0000

    Add package ProcessCheckerLib/0.0.3/ProcessCheckerLib-0.0.3.tar.gz

---
+++ packages/ProcessCheckerLib/ProcessCheckerLib-0.0.3.tar.gz/ProcessCheckerLib-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='ProcessCheckerLib',
+  version='0.0.3',
+  author='brambleaka',
+  author_email='sambuka11jail@outlook.com',
+  description='This is lib for ProcessesCheckerBot.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='',
+  packages=find_packages(),
+  install_requires=['requests','flask','schedule','psutil','python-dotenv'],
+  classifiers=[],
+  keywords='',
+  python_requires='>=3.8'
+)
