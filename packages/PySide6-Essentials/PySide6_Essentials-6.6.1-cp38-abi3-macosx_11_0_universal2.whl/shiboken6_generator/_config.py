06f8baf54005cbb70756cf0bdf58f1fda0117056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 03:04:56 +0000

    Add package PySide6-Essentials/6.6.1/PySide6_Essentials-6.6.1-cp38-abi3-macosx_11_0_universal2.whl

---
+++ packages/PySide6-Essentials/PySide6_Essentials-6.6.1-cp38-abi3-macosx_11_0_universal2.whl/shiboken6_generator/_config.py
@@ -0,0 +1,10 @@
+version = "6.6.1"
+version_info = (6, 6, 1, "", "")
+
+__build_date__ = '2023-11-27T15:15:29+00:00'
+
+
+
+
+__setup_py_package_version__ = '6.6.1'
+__qt_macos_min_deployment_target__ = '11.0'
