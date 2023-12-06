6562eef8434a1f11bc27e5f34d1743db864c980e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 06:04:49 +0000

    Add package pueblo/0.0.4/pueblo-0.0.4.tar.gz

---
+++ packages/pueblo/pueblo-0.0.4.tar.gz/pueblo-0.0.4/pueblo/ngr/model.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class ItemType(Enum):
+    DOTNET = "dotnet"
+    ELIXIR = "elixir"
+    GOLANG = "golang"
+    HASKELL = "haskell"
+    JAVA = "java"
+    JAVASCRIPT = "javascript"
+    JULIA = "julia"
+    MAKE = "make"
+    PHP = "php"
+    PYTHON = "python"
+    RUBY = "ruby"
+    RUST = "rust"
