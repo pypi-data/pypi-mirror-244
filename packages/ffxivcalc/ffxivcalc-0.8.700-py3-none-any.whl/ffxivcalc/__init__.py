d14da3cce4228fe89025befd442720a001aa8114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 05:04:35 +0000

    Add package ffxivcalc/0.8.700/ffxivcalc-0.8.700-py3-none-any.whl

---
+++ packages/ffxivcalc/ffxivcalc-0.8.700-py3-none-any.whl/ffxivcalc/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.8.700'
+
+"""
+Update history :
+
+0.8.700 :
+    - Made PyPi package.
+
+0.8.60 :
+    - Added more accurate AA, weapon delay, haste affecting delay.
+    - Fixed DOT/AA issues
+    - Added AA delay, GCD timer and more DOT tests.
+    - Added cli "tests" command to run test suites.
+    
+
+"""
