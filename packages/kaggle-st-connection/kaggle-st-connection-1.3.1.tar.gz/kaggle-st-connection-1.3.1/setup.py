700944301c9cffe40308fbd54f769f1d257da9bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:05:23 +0000

    Add package kaggle-st-connection/1.3.1/kaggle-st-connection-1.3.1.tar.gz

---
+++ packages/kaggle-st-connection/kaggle-st-connection-1.3.1.tar.gz/kaggle-st-connection-1.3.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='kaggle-st-connection',
+    version='1.3.1',
+    py_modules=['KaggleAPIConnection'],
+    install_requires=['kaggle', 'streamlit', 'pandas'],
+    author='Cheah Zixu',
+    description='st.experimental_connection implementation for Kaggle Public API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/genesis331/KaggleStConnection',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
