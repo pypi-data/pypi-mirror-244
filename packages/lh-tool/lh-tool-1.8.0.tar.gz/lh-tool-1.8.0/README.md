cce7a0e45d9f5df0a13948c52453b7aed63751b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:05:47 +0000

    Add package lh-tool/1.8.0/lh-tool-1.8.0.tar.gz

---
+++ packages/lh-tool/lh-tool-1.8.0.tar.gz/lh-tool-1.8.0/README.md
@@ -0,0 +1,44 @@
+# lh-tool Package
+
+This is a tool package.
+
+# Updates
+
+ - [x] 2023.12.05: Add CI/CD
+ - [x] 2023.06.30: Add image channel conversion between imageio and cv2
+ - [x] 2023.06.12: Fix the bug that the ParallelProcess iterator returns results in the wrong order
+ - [x] 2023.06.11: Add ParallelProcess and AsyncMultiProcess iterator
+
+# Tools
+
+* image2image
+* image2video
+* image2gif
+* image2pdf
+* video2image
+* pdf2image
+* play_image
+* concat_image
+* excel2latex
+* excel2markdown
+* compress_pdf
+* startup
+* rename_postfix
+
+# Install
+
+* Installing from source
+```shell
+git clone https://github.com/lh9171338/lh-tool.git
+
+cd lh-tool
+
+py -m build
+
+pip install dist/lh_tool-1.8.0-py3-none-any.whl
+```
+
+* Install from the Python Package Index (PyPI)
+```shell
+pip install lh_tool
+```
---
+++ packages/lh-tool/lh-tool-1.8.0.tar.gz/lh-tool-1.8.0/tests/test_iterator.py
@@ -0,0 +1,65 @@
+import asyncio
+from src.lh_tool.Iterator import (
+    SingleProcess,
+    MultiProcess,
+    AsyncProcess,
+    AsyncMultiProcess,
+    ParallelProcess,
+)
+
+
+def process(a, b, opt="+"):
+    if opt == "+":
+        return a + b
+    else:
+        return a - b
+
+
+def process_batch(a_list, b_list, opt="+"):
+    res_list = []
+    for a, b in zip(a_list, b_list):
+        if opt == "+":
+            res = a + b
+        else:
+            res = a - b
+        res_list.append(res)
+    return res_list
+
+
+async def async_process(a, b, opt="+"):
+    await asyncio.sleep(1)
+    if opt == "+":
+        return a + b
+    else:
+        return a - b
+
+
+def test_iterator():
+    print("Test lh_tool.MultiProcess")
+    a = [i for i in range(10)]
+    b = [i for i in range(10)]
+    result_list = SingleProcess(process).run(a, b, opt="+")
+    print(result_list)
+
+    result_list = MultiProcess(process, nprocs=10).run(a=a, b=b, opt="+")
+    print(result_list)
+
+    result_list = AsyncProcess(async_process).run(a=a, b=b, opt="+")
+    print(result_list)
+
+    result_list = AsyncMultiProcess(async_process, nprocs=10).run(
+        a=a, b=b, opt="+"
+    )
+    print(result_list)
+
+    ret_list = ParallelProcess(process_batch, nprocs=5).run(
+        a_list=a, b_list=b, opt="+"
+    )
+    result_list = []
+    for ret in ret_list:
+        result_list.extend(ret)
+    print(result_list)
+
+
+if __name__ == "__main__":
+    test_iterator()
