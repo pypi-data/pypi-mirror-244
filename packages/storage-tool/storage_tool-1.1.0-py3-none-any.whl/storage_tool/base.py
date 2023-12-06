78ce09f2dd901d31ac5700b7090d49f3e0cb6a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 03:04:10 +0000

    Add package storage-tool/1.1.0/storage_tool-1.1.0-py3-none-any.whl

---
+++ packages/storage-tool/storage_tool-1.1.0-py3-none-any.whl/storage_tool/base.py
@@ -0,0 +1,61 @@
+from abc import ABC, abstractmethod
+
+class BaseStorage(ABC):
+    def __init__(self, config):
+        self.config = config
+
+    @abstractmethod
+    def list_repositories(self):
+        pass
+
+    @abstractmethod
+    def list(self, repository, path):
+        pass
+    
+    @abstractmethod
+    def read(self, file_path, return_type=None):
+        pass
+
+    @abstractmethod
+    def put(self, repository, file_path, content):
+        pass
+
+    @abstractmethod
+    def delete(self, repository, file_path):
+        pass
+
+    @abstractmethod
+    def move(self, repository, src_path, dest_path):
+        pass
+
+    @abstractmethod
+    def move_between_repositories(self, src_repository, src_path, dest_repository, dest_path):
+        pass
+
+    @abstractmethod
+    def copy(self, repository, src_path, dest_path):
+        pass
+
+    @abstractmethod
+    def copy(self, src_repository, src_path, dest_repository, dest_path):
+        pass
+
+    @abstractmethod        
+    def sync(self, repository, src_path, dest_path):
+        pass
+
+    @abstractmethod        
+    def sync_between_repositories(self, src_repository, src_path, dest_repository, dest_path):
+        pass
+
+    @abstractmethod
+    def exists(self, repository, file_path):
+        pass
+
+    @abstractmethod
+    def get_metadata(self, repository, file_path):
+        pass
+
+    @abstractmethod
+    def get_file_url(self, repository, file_path):
+        pass
