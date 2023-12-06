e9ceee9af78537cf70a8498025e0b3f7481342a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 05:04:38 +0000

    Add package ffxivcalc/0.8.617/ffxivcalc-0.8.617.tar.gz

---
+++ packages/ffxivcalc/ffxivcalc-0.8.617.tar.gz/ffxivcalc-0.8.617/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="ffxivcalc",  # Required
+    version="0.8.617",  # Required
+    description="DPS simulator for Final Fantasy XIV.",
+    long_description=long_description,  
+    long_description_content_type="text/markdown",  # Optional (see note above)
+    url="https://github.com/IAmPythagoras/FFXIV-Combat-Simulator",  # Optional
+    author="Anthony Desrochers",
+    author_email="anthony.desrochers17@gmail.com",
+        classifiers=[ 
+        "Development Status :: 5 - Production/Stable",
+        # Pick your license as you wish
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10"
+    ],
+    keywords="ffxiv, simulator, dps, ffxiv-combat-simulator",
+    package_dir={"": "src"},  # Optional,
+    packages=find_packages(where="src"),  # Required
+    python_requires=">=3.10",
+    project_urls={  # Optional
+        "Source": "https://github.com/IAmPythagoras/FFXIV-Combat-Simulator",
+        "Site": "https://ffxivdpscalc.azurewebsites.net/simulate/",
+        "Documentation": "https://iampythagoras.github.io/index.html",
+    },
+)
