cb174a944adc08dfdeaa564fae7cd454fe8371af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 05:04:24 +0000

    Add package rtpTTML/0.0.2/rtpTTML-0.0.2-py3-none-any.whl

---
+++ packages/rtpTTML/rtpTTML-0.0.2-py3-none-any.whl/rtpTTML/ttmlReceiver.py
@@ -0,0 +1,230 @@
+# James Sandford, copyright BBC 2020
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+from typing import List, Callable, Dict, Optional, Tuple, cast
+import socket
+import asyncio
+from collections import OrderedDict
+from rtp import RTP
+from rtpPayload_ttml import RTPPayload_TTML
+
+MAX_SEQ_NUM = (2**16) - 1
+
+
+class OrderedBuffer:
+    def __init__(self, maxSize: int = 5, maxKey: int = MAX_SEQ_NUM) -> None:
+        self._initialised = False
+        self._maxSize = maxSize
+        self._maxKey = maxKey
+        self._mostRecentKey = 0
+        self._buffer: Dict[int, RTP] = {}
+
+    def _nextKey(self) -> int:
+        nextKey = self._mostRecentKey + 1
+
+        return nextKey % (self._maxKey + 1)
+
+    def _ffwMostRecent(self) -> None:
+        if len(self._buffer) == 0:
+            return
+
+        while self._nextKey() not in self._buffer:
+            self._mostRecentKey = self._nextKey()
+
+    def pop(self) -> Optional[RTP]:
+        ret = self._buffer.pop(self._nextKey(), None)
+        self._mostRecentKey = self._nextKey()
+
+        return ret
+
+    def push(self, key: int, value: RTP) -> None:
+        self._buffer[key] = value
+
+        if not self._initialised:
+            self._mostRecentKey = key - 1
+            self._initialised = True
+
+        if len(self._buffer) >= self._maxSize:
+            self._ffwMostRecent()
+
+        if len(self._buffer) > self._maxSize:
+            self.pop()
+
+    def available(self) -> bool:
+        available = False
+
+        if self._nextKey() in self._buffer:
+            available = True
+
+        return available
+
+    def get(self) -> List[RTP]:
+        ret = []
+
+        while self.available():
+            item = self.pop()
+            if item is not None:
+                ret.append(item)
+
+        return ret
+
+    def pushGet(self, key: int, value: RTP) -> List[RTP]:
+        self.push(key, value)
+        return self.get()
+
+
+class TTMLDatagramProtocol(asyncio.DatagramProtocol):
+    def __init__(self, parent: TTMLReceiver) -> None:
+        self._parent = parent
+        super().__init__()
+
+    def datagram_received(self, data, addr) -> None:
+        self._parent._processData(data)
+
+
+class TTMLReceiver:
+    def __init__(
+       self,
+       port: int,
+       callback: Callable[[str, int], None],
+       recvBufSize: Optional[int] = None,
+       timeout: Optional[float] = None,
+       encoding: str = "UTF-8",
+       bom: bool = False) -> None:
+        self._fragments: Dict[int, str] = OrderedDict()
+        self._curTimestamp = 0
+        self._port = port
+        self._callback = callback
+        self._encoding = encoding
+        self._bom = bom
+        self._socket: Optional[socket.socket]
+        self._transport: Optional[asyncio.DatagramTransport]
+        self._protocol: Optional[TTMLDatagramProtocol]
+
+        if recvBufSize is None:
+            self._recvBufSize = 2**16
+        else:
+            self._recvBufSize = recvBufSize
+
+        self._packetBuff = OrderedBuffer()
+
+        if timeout is None:
+            self._timeout = 30.0
+        else:
+            self._timeout = timeout
+
+    def _unloopSeqNum(self, prevNum: int, thisNum: int) -> int:
+        loopOffset = MAX_SEQ_NUM + 1
+
+        prevNumMod = prevNum % loopOffset
+
+        prevNumOffset = prevNum - prevNumMod  # prevNum may already have looped
+
+        # Margins for detecting when number has looped
+        perC10 = MAX_SEQ_NUM * 0.1
+        perC90 = MAX_SEQ_NUM * 0.9
+
+        # Account for missing packets around edges
+        if (thisNum > perC10) or (prevNumMod < perC90):
+            return thisNum + prevNumOffset
+
+        # Num has looped
+        return thisNum + loopOffset + prevNumOffset
+
+    def _keysComplete(self) -> bool:
+        if len(self._fragments) == 0:
+            return False
+
+        minKey = min(self._fragments)
+        maxKey = max(self._fragments)
+        expectedLen = maxKey - minKey + 1
+
+        return len(self._fragments) == expectedLen
+
+    def _processFragments(self) -> None:
+        if not self._keysComplete():
+            # Discard
+            self._fragments.clear()
+            return
+
+        # Reconstruct the document
+        doc = ""
+
+        for k, v in self._fragments.items():
+            doc += v
+
+        # We've finished re-constructing this document.
+        # Discard the fragments.
+        self._fragments.clear()
+
+        self._callback(doc, self._curTimestamp)
+
+    def _processPacket(self, packet: RTP) -> None:
+        # New TS means a new document
+        if self._curTimestamp != packet.timestamp:
+            # If we haven't processed by now, document is incomplete
+            # so we discard it
+            self._fragments.clear()
+
+            # Assume this packet is the first in doc. If we're wrong, the doc
+            # won't be valid TTML when decoded anyway
+            self._curTimestamp = packet.timestamp
+
+        seqNumber = packet.sequenceNumber
+        if len(self._fragments) > 0:
+            seqNumber = self._unloopSeqNum(
+                max(self._fragments), packet.sequenceNumber)
+
+        payload = RTPPayload_TTML(
+            encoding=self._encoding, bom=self._bom)
+        payload.fromBytearray(packet.payload)
+        self._fragments[seqNumber] = payload.userDataWords
+
+    def _processData(self, data: bytes) -> None:
+        newPacket = RTP().fromBytes(data)
+
+        packets = self._packetBuff.pushGet(
+            newPacket.sequenceNumber, newPacket)
+
+        for packet in packets:
+            self._processPacket(packet)
+
+            if packet.marker:
+                self._processFragments()
+
+    def run(self) -> None:
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket.settimeout(self._timeout)
+        self._socket.bind(('', self._port))
+
+        while True:
+            data = self._socket.recv(self._recvBufSize)
+            self._processData(data)
+
+    def async_close(self) -> None:
+        if self._transport is not None:
+            self._transport.close()
+
+    async def async_run(self) -> None:
+        loop = asyncio.get_event_loop()
+
+        # Typeshed incorrectly assumes Base Transport and Protocol types
+        # Typeshed also incorrectly says local_addr's address can't be None
+        self._transport, self._protocol = cast(
+            Tuple[asyncio.DatagramTransport, TTMLDatagramProtocol],
+            await loop.create_datagram_endpoint(
+                lambda: TTMLDatagramProtocol(self),
+                local_addr=(None, self._port),  # type: ignore
+                family=socket.AF_INET))
---
+++ packages/rtpTTML/rtpTTML-0.0.2-py3-none-any.whl/rtpTTML/ttmlTransmitter.py
@@ -0,0 +1,204 @@
+# James Sandford, copyright BBC 2020
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+from typing import List, Optional, Tuple, cast
+from datetime import datetime
+import socket
+import asyncio
+from random import randrange
+from rtp import RTP, PayloadType
+from rtpPayload_ttml import RTPPayload_TTML, utfEncode
+
+EPOCH = datetime.utcfromtimestamp(0)
+
+
+class AsyncTTMLTransmitterConnection (object):
+    def __init__(self, parent: TTMLTransmitter) -> None:
+        self._parent = parent
+        self._transport: Optional[asyncio.DatagramTransport]
+        self._protocol: Optional[asyncio.DatagramProtocol]
+
+    @property
+    def nextSeqNum(self):
+        return self._parent._nextSeqNum
+
+    async def _open(self) -> None:
+        loop = asyncio.get_event_loop()
+
+        # Typeshed incorrectly assumes Base Transport and Protocol types
+        self._transport, self._protocol = cast(
+            Tuple[asyncio.DatagramTransport, asyncio.DatagramProtocol],
+            await loop.create_datagram_endpoint(
+                asyncio.DatagramProtocol,
+                remote_addr=(self._parent._address, self._parent._port),
+                family=socket.AF_INET))
+
+    async def _close(self) -> None:
+        if self._transport is not None:
+            self._transport.close()
+            self._transport = None
+
+    async def sendDoc(self, doc: str, time: datetime) -> None:
+        if self._transport is None:
+            return
+
+        for packet in self._parent._packetiseDoc(doc, time):
+            self._transport.sendto(packet.toBytes())
+
+
+class SyncTTMLTransmitterConnection (object):
+    def __init__(self, parent: TTMLTransmitter) -> None:
+        self._parent = parent
+        self._socket: Optional[socket.socket]
+
+    @property
+    def nextSeqNum(self):
+        return self._parent._nextSeqNum
+
+    def _open(self) -> None:
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def _close(self) -> None:
+        if self._socket is not None:
+            self._socket.close()
+
+    def sendDoc(self, doc: str, time: datetime) -> None:
+        if self._socket is None:
+            return
+
+        for packet in self._parent._packetiseDoc(doc, time):
+            self._socket.sendto(
+                packet.toBytes(),
+                (self._parent._address, self._parent._port))
+
+
+class TTMLTransmitter:
+    def __init__(
+       self,
+       address: str,
+       port: int,
+       maxFragmentSize: int = 1200,
+       payloadType: PayloadType = PayloadType.DYNAMIC_96,
+       initialSeqNum: Optional[int] = None,
+       tsOffset: Optional[int] = None,
+       encoding: str = "UTF-8",
+       bom: bool = False) -> None:
+        self._address = address
+        self._port = port
+        self._maxFragmentSize = maxFragmentSize
+        self._payloadType = payloadType
+        self._encoding = encoding
+        self._bom = bom
+
+        if initialSeqNum is not None:
+            self._nextSeqNum = initialSeqNum
+        else:
+            self._nextSeqNum = randrange(2**16)
+
+        if tsOffset is not None:
+            self._tsOffset = tsOffset
+        else:
+            self._tsOffset = randrange(2**32)
+
+        self._async_connection: Optional[AsyncTTMLTransmitterConnection] = None
+        self._sync_connection: Optional[SyncTTMLTransmitterConnection] = None
+
+    async def __aenter__(self) -> AsyncTTMLTransmitterConnection:
+        self._async_connection = AsyncTTMLTransmitterConnection(self)
+        await self._async_connection._open()
+        return self._async_connection
+
+    async def __aexit__(self, *args) -> None:
+        if self._async_connection is not None:
+            await self._async_connection._close()
+            self._async_connection = None
+
+    def __enter__(self) -> SyncTTMLTransmitterConnection:
+        self._sync_connection = SyncTTMLTransmitterConnection(self)
+        self._sync_connection._open()
+        return self._sync_connection
+
+    def __exit__(self, *args) -> None:
+        if self._sync_connection is not None:
+            self._sync_connection._close()
+            self._sync_connection = None
+
+    @property
+    def nextSeqNum(self) -> int:
+        return self._nextSeqNum
+
+    def _fragmentDoc(self, doc: str, maxLen: int) -> List[str]:
+        fragments = []
+        thisStart = 0
+
+        if doc == "":
+            return []
+
+        while True:
+            thisEnd = thisStart + maxLen
+            while len(utfEncode(
+                    doc[thisStart:thisEnd], self._encoding, self._bom
+               )) > maxLen:
+                thisEnd -= 1
+
+            fragments.append(doc[thisStart:thisEnd])
+
+            if thisEnd >= len(doc):
+                break
+
+            thisStart = thisEnd
+
+        return fragments
+
+    def _datetimeToRTPTs(self, time: datetime) -> int:
+        now_ms = int((time - EPOCH).total_seconds() * 1000)
+        timestamp = now_ms + self._tsOffset
+        truncatedTS = timestamp % 2**32
+
+        return truncatedTS
+
+    def _generateRTPPacket(
+       self, doc: str, time: int, isFirst: bool, marker: bool) -> RTP:
+        # Only include bom in first packet for doc
+        thisBOM = (isFirst and self._bom)
+
+        packet = RTP(
+            timestamp=time,
+            sequenceNumber=self._nextSeqNum,
+            payload=RTPPayload_TTML(
+                    userDataWords=doc, encoding=self._encoding, bom=thisBOM
+                ).toBytearray(),
+            marker=marker,
+            payloadType=self._payloadType
+        )
+        self._nextSeqNum += 1
+
+        return packet
+
+    def _packetiseDoc(self, doc, time):
+        packets = []
+
+        rtpTs = self._datetimeToRTPTs(time)
+        docFragments = self._fragmentDoc(doc, self._maxFragmentSize)
+
+        lastIndex = len(docFragments) - 1
+        for x in range(len(docFragments)):
+            isFirst = (x == 0)
+            isLast = (x == lastIndex)
+            packets.append(
+                self._generateRTPPacket(
+                    docFragments[x], rtpTs, isFirst, isLast))
+
+        return packets
