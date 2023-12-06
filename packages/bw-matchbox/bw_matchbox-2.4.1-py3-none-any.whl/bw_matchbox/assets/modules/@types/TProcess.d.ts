53e7a912ba6541f2c0affc554006256c2af6a162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 05:04:35 +0000

    Add package bw-matchbox/2.4.1/bw_matchbox-2.4.1-py3-none-any.whl

---
+++ packages/bw-matchbox/bw_matchbox-2.4.1-py3-none-any.whl/bw_matchbox/assets/modules/@types/TProcess.d.ts
@@ -0,0 +1,14 @@
+type TProcessId = number;
+interface TProcess {
+  id: TProcessId; // 5300
+  details_url: string; // '/process/149'
+  location: string; // 'United States'
+  match_type: string; // 'Unknown'
+  match_url: string; // '/match/149'
+  matched: boolean; // false
+  name: string; // 'Printed circuit and electronic assembly; at manufacturer'
+  product?: string; // null
+  proxy_url?: string; // null
+  unit: string; // 'USD'
+  waitlist: boolean; // false
+}
