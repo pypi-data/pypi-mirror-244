b12399daad9e33acdc4cb81fcf033aaee84f4479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 05:05:03 +0000

    Add package murenn/0.1.1/murenn-0.1.1.tar.gz

---
+++ packages/murenn/murenn-0.1.1.tar.gz/murenn-0.1.1/murenn/__init__.py
@@ -0,0 +1,19 @@
+"""
+MuReNN: MultiResolution Neural Networks
+=======================================
+
+murenn is a Python module which integrates efficient operators
+for multiresolution analysis into a differentiable computing
+framework.
+"""
+
+# List of top-level public names.
+__all__ = ["DTCWT", "DTCWTForward"]
+
+
+# Submodule imports
+from .dtcwt.transform1d import DTCWTForward
+from .version import version as __version__
+
+# PytW-like aliases
+DTCWT = DTCWTForward
