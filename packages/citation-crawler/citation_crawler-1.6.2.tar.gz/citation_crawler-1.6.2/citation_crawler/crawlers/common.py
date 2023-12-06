c0beee2f276a2a517b7be0cf8f337b4c7cf1538c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:05:46 +0000

    Add package citation-crawler/1.6.2/citation_crawler-1.6.2.tar.gz

---
+++ packages/citation-crawler/citation_crawler-1.6.2.tar.gz/citation_crawler-1.6.2/citation_crawler/crawlers/common.py
@@ -0,0 +1,77 @@
+from typing import Optional, Dict
+import os
+from datetime import datetime, timedelta
+import json
+
+import aiohttp
+import logging
+from aiofile import async_open
+from asyncio import Semaphore
+
+logger = logging.getLogger("common")
+
+
+def getenv_int(key) -> int:
+    cache_days = os.getenv(key)
+    if cache_days is not None:
+        try:
+            return int(cache_days)
+        except:
+            pass
+    return None
+
+
+http_concorent = getenv_int('HTTP_CONCORRENT')
+http_sem = Semaphore(http_concorent if http_concorent is not None else 8)
+file_sem = Semaphore(512)
+
+
+async def fetch_item(url: str) -> Optional[Dict]:
+    async with http_sem:
+        try:
+            async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+                async with session.get(url) as response:
+                    logger.info("fetch url: %s" % url)
+                    text = await response.text()
+                    try:
+                        return json.loads(text)
+                    except Exception as e:
+                        logger.error(" json err: %s: %s" % (e, text))
+        except Exception as e:
+            logger.error("fetch err: %s" % e)
+    return None
+
+
+def get_cache_datetime(path) -> datetime:
+    return datetime.fromtimestamp(os.path.getmtime(path))
+
+
+async def download_item(url: str, path: str, cache_days: int) -> Optional[Dict]:
+    save_path = os.path.join("save", path)
+    if os.path.isfile(save_path):
+        if cache_days < 0 or datetime.now() < get_cache_datetime(save_path) + timedelta(days=cache_days):
+            async with file_sem:
+                try:
+                    async with async_open(save_path, 'r') as f:
+                        logger.debug("use cache: %s -> %s" % (path, url))
+                        text = await f.read()
+                        return json.loads(text)
+                except:
+                    logger.debug(" no cache: %s" % save_path)
+        else:
+            logger.debug("old cache: %s" % save_path)
+
+    async with http_sem:
+        try:
+            async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+                async with session.get(url, proxy=os.getenv("HTTP_PROXY")) as response:
+                    logger.debug(" download: %s <- %s" % (path, url))
+                    text = await response.text()
+                    data = json.loads(text)
+                    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+                    async with async_open(save_path, 'w') as f:
+                        await f.write(text)
+                    return data
+        except Exception as e:
+            logger.error(" down err: %s" % e)
+    return None
