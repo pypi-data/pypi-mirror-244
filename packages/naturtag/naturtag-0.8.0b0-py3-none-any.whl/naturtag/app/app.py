94833e6ccece3fcf9256c9fb696a5a19f2f77029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:06:03 +0000

    Add package naturtag/0.8.0b0/naturtag-0.8.0b0-py3-none-any.whl

---
+++ packages/naturtag/naturtag-0.8.0b0-py3-none-any.whl/naturtag/app/app.py
@@ -0,0 +1,289 @@
+"""Main Qt app window and entry point"""
+import sys
+import webbrowser
+from datetime import datetime
+from importlib.metadata import version as pkg_version
+from logging import getLogger
+
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import QIcon, QKeySequence, QPixmap, QShortcut
+from PySide6.QtWidgets import (
+    QApplication,
+    QInputDialog,
+    QLineEdit,
+    QMainWindow,
+    QMessageBox,
+    QSplashScreen,
+    QStatusBar,
+    QTabWidget,
+    QWidget,
+)
+
+from naturtag.app.controls import Toolbar, UserDirs
+from naturtag.app.settings_menu import SettingsMenu
+from naturtag.app.style import fa_icon, set_theme
+from naturtag.app.threadpool import ThreadPool
+from naturtag.client import ImageSession, iNatDbClient
+from naturtag.constants import APP_ICON, APP_LOGO, ASSETS_DIR, DOCS_URL, REPO_URL
+from naturtag.controllers import ImageController, ObservationController, TaxonController
+from naturtag.settings import Settings, setup
+from naturtag.widgets import VerticalLayout, init_handler
+
+# Provide an application group so Windows doesn't use the default 'python' icon
+try:
+    from ctypes import windll  # type: ignore
+
+    windll.shell32.SetCurrentProcessExplicitAppUserModelID('pyinat.naturtag.app.1.0')
+except ImportError:
+    pass
+
+logger = getLogger(__name__)
+
+
+class NaturtagApp(QApplication):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setApplicationName('Naturtag')
+        self.setApplicationVersion(pkg_version('naturtag'))
+        self.setOrganizationName('pyinat')
+        self.setWindowIcon(QIcon(QPixmap(str(APP_ICON))))
+        self.settings = Settings.read()
+
+    def post_init(self):
+        # Run any first-time setup steps, if needed
+        setup(self.settings)
+
+        # Globally available application objects
+        self.client = iNatDbClient(self.settings.db_path)
+        self.img_session = ImageSession(self.settings.image_cache_path)
+        self.log_handler = init_handler(
+            self.settings.log_level,
+            root_level=self.settings.log_level_external,
+            logfile=self.settings.logfile,
+        )
+        self.threadpool = ThreadPool()
+        self.user_dirs = UserDirs(self.settings)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, app: NaturtagApp):
+        super().__init__()
+        self.setWindowTitle('Naturtag')
+        self.resize(*app.settings.window_size)
+        self.app = app
+
+        # Controllers
+        self.settings_menu = SettingsMenu()
+        self.image_controller = ImageController()
+        self.taxon_controller = TaxonController()
+        self.observation_controller = ObservationController()
+
+        # Connect controllers and their widgets to statusbar info
+        self.settings_menu.on_message.connect(self.info)
+        self.image_controller.on_message.connect(self.info)
+        self.image_controller.gallery.on_message.connect(self.info)
+        self.taxon_controller.on_message.connect(self.info)
+        self.observation_controller.on_message.connect(self.info)
+
+        # Select observation/taxon from image context menu, ID input fields, and iconic taxa filters
+        self.image_controller.gallery.on_select_taxon.connect(self.taxon_controller.select_taxon)
+        self.image_controller.gallery.on_select_observation.connect(
+            self.observation_controller.select_observation
+        )
+        self.image_controller.on_select_observation_id.connect(
+            self.observation_controller.select_observation
+        )
+        self.image_controller.on_select_taxon_id.connect(self.taxon_controller.select_taxon)
+        self.taxon_controller.search.iconic_taxon_filters.on_select.connect(
+            self.taxon_controller.select_taxon
+        )
+
+        # Update photo tab when a taxon is selected
+        self.taxon_controller.on_select.connect(self.image_controller.select_taxon)
+
+        # Update photo and taxon tabs when an observation is selected
+        self.observation_controller.on_select.connect(self.image_controller.select_observation)
+        self.observation_controller.on_select.connect(
+            lambda obs: self.taxon_controller.display_taxon(obs.taxon, notify=False)
+        )
+
+        # Settings that take effect immediately
+        self.settings_menu.all_ranks.on_click.connect(self.taxon_controller.search.reset_ranks)
+        self.settings_menu.dark_mode.on_click.connect(set_theme)
+        self.settings_menu.show_logs.on_click.connect(self.toggle_log_tab)
+
+        # Tabs
+        self.tabs = QTabWidget()
+        self.tabs.setIconSize(QSize(32, 32))
+        self.tabs.addTab(self.image_controller, fa_icon('fa.camera'), 'Photos')
+        self.tabs.addTab(self.taxon_controller, fa_icon('fa5s.spider'), 'Species')
+        self.tabs.addTab(self.observation_controller, fa_icon('fa5s.binoculars'), 'Observations')
+
+        # Root layout: tabs + progress bar
+        self.root_widget = QWidget()
+        self.root = VerticalLayout(self.root_widget)
+        self.root.addWidget(self.tabs)
+        self.root.addWidget(self.app.threadpool.progress)
+        self.setCentralWidget(self.root_widget)
+
+        # Optionally show Logs tab
+        self.log_tab_idx = self.tabs.addTab(
+            self.app.log_handler.widget, fa_icon('fa.file-text-o'), 'Logs'
+        )
+        self.tabs.setTabVisible(self.log_tab_idx, self.app.settings.show_logs)
+
+        # Switch to different tab if requested from Photos tab
+        self.image_controller.on_select_taxon_tab.connect(
+            lambda: self.tabs.setCurrentWidget(self.taxon_controller)
+        )
+        self.image_controller.on_select_observation_tab.connect(
+            lambda: self.tabs.setCurrentWidget(self.observation_controller)
+        )
+
+        # Connect file picker <--> recent/favorite dirs
+        self.image_controller.gallery.on_load_images.connect(self.app.user_dirs.add_recent_dirs)
+        self.app.user_dirs.on_dir_open.connect(self.image_controller.gallery.load_file_dialog)
+
+        # Toolbar actions
+        self.toolbar = Toolbar(self, self.app.user_dirs)
+        self.toolbar.run_button.triggered.connect(self.image_controller.run)
+        self.toolbar.refresh_tags_button.triggered.connect(self.image_controller.refresh)
+        self.toolbar.open_button.triggered.connect(self.image_controller.gallery.load_file_dialog)
+        self.toolbar.paste_button.triggered.connect(self.image_controller.paste)
+        self.toolbar.clear_button.triggered.connect(self.image_controller.clear)
+        self.toolbar.refresh_obs_button.triggered.connect(self.observation_controller.refresh)
+        self.toolbar.fullscreen_button.triggered.connect(self.toggle_fullscreen)
+        self.toolbar.reset_db_button.triggered.connect(self.reset_db)
+        self.toolbar.settings_button.triggered.connect(self.show_settings)
+        self.toolbar.exit_button.triggered.connect(QApplication.instance().quit)
+        self.toolbar.docs_button.triggered.connect(self.open_docs)
+        self.toolbar.about_button.triggered.connect(self.open_about)
+
+        # Menu bar and status bar
+        self.toolbar.populate_menu(self.menuBar())
+        self.addToolBar(self.toolbar)
+        self.statusbar = QStatusBar(self)
+        self.setStatusBar(self.statusbar)
+
+        # Load any valid image paths provided on command line (or from drag & drop)
+        self.image_controller.gallery.load_images(
+            [a for a in sys.argv if not (a == __file__ or a.endswith('.exe'))]
+        )
+
+        # Debug
+        if self.app.settings.debug:
+            QShortcut(QKeySequence('F9'), self).activated.connect(self.reload_qss)
+            demo_images = list((ASSETS_DIR / 'demo_images').glob('*.jpg'))
+            self.image_controller.gallery.load_images(demo_images)  # type: ignore
+            self.observation_controller.select_observation(56830941)
+
+    def check_username(self):
+        """If username isn't saved, show popup dialog to prompt user to enter it"""
+        if self.app.settings.username:
+            return
+
+        username, ok = QInputDialog.getText(
+            self,
+            'iNaturalist username',
+            'Enter your iNaturalist username to fetch your observations',
+        )
+        if ok:
+            self.app.settings.username = username
+            self.app.settings.write()
+            self.observation_controller.load_user_observations()
+
+    def closeEvent(self, _):
+        """Save settings before closing the app"""
+        self.app.settings.write()
+        self.taxon_controller.user_taxa.write()
+
+    def info(self, message: str):
+        """Show a message both in the status bar and in the logs"""
+        self.statusbar.showMessage(message)
+        logger.info(message)
+
+    def mousePressEvent(self, event):
+        """Deselect focus from text edit fields when clicking anywhere else on the window"""
+        focused_widget = QApplication.focusWidget()
+        if isinstance(focused_widget, QLineEdit):
+            focused_widget.clearFocus()
+        super().mousePressEvent(event)
+
+    def open_docs(self):
+        """Open the documentation in a web browser"""
+        webbrowser.open(DOCS_URL)
+
+    def open_about(self):
+        """Show an About dialog with basic app information"""
+        about = QMessageBox()
+        about.setIconPixmap(QPixmap(str(APP_ICON)))
+        about.setTextFormat(Qt.RichText)
+
+        version = pkg_version('naturtag')
+        repo_link = f"<a href='{REPO_URL}'>{REPO_URL}</a>"
+        license_link = f"<a href='{REPO_URL}/LICENSE'>MIT License</a>"
+        attribution = f'Ⓒ {datetime.now().year} Jordan Cook, {license_link}'
+        app_dir_link = f"<a href='{self.app.settings.data_dir}'>{self.app.settings.data_dir}</a>"
+
+        about.setText(
+            f'<b>Naturtag v{version}</b><br/>'
+            f'{attribution}'
+            f'<br/>Source: {repo_link}'
+            f'<br/>User data directory: {app_dir_link}'
+        )
+        about.exec()
+
+    def reload_qss(self):
+        """Reload Qt stylesheet"""
+        set_theme(dark_mode=self.app.settings.dark_mode)
+
+    # TODO: progress spinner
+    def reset_db(self):
+        """Reset the database"""
+        response = QMessageBox.question(
+            self,
+            'Reset database?',
+            'This will delete all observation and taxonomy data saved in the local database. Continue?',
+        )
+        if response == QMessageBox.Yes:
+            self.info('Resetting database...')
+            setup(self.app.settings, overwrite=True)
+            self.info('Database reset complete')
+
+    def show_settings(self):
+        """Show the settings menu"""
+        self.settings_menu.show()
+
+    def toggle_fullscreen(self) -> bool:
+        """Toggle fullscreen, and change icon for toolbar fullscreen button"""
+        if not self.isFullScreen():
+            self._flags = self.windowFlags()
+            self.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowType_Mask)
+            self.showFullScreen()
+            self.toolbar.fullscreen_button.setIcon(fa_icon('mdi.fullscreen-exit'))
+        else:
+            self.setWindowFlags(self._flags)
+            self.showNormal()
+            self.toolbar.fullscreen_button.setIcon(fa_icon('mdi.fullscreen'))
+        return self.isFullScreen()
+
+    def toggle_log_tab(self, checked: bool = True):
+        self.tabs.setTabVisible(self.log_tab_idx, checked)
+
+
+def main():
+    app = NaturtagApp(sys.argv)
+    splash = QSplashScreen(QPixmap(str(APP_LOGO)).scaledToHeight(512))
+    splash.show()
+    app.post_init()
+
+    set_theme(dark_mode=app.settings.dark_mode)
+    window = MainWindow(app)
+    window.show()
+    splash.finish(window)
+    window.check_username()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/naturtag/naturtag-0.8.0b0-py3-none-any.whl/naturtag/controllers/taxon_view.py
@@ -0,0 +1,198 @@
+"""Components for displaying taxon info"""
+import webbrowser
+from collections import deque
+from logging import getLogger
+from typing import Iterator
+
+from pyinaturalist import Taxon
+from PySide6.QtCore import Qt, QThread, Signal
+from PySide6.QtWidgets import QGroupBox, QPushButton
+
+from naturtag.app.style import fa_icon
+from naturtag.constants import SIZE_SM
+from naturtag.settings import UserTaxa
+from naturtag.widgets import (
+    GridLayout,
+    HorizontalLayout,
+    TaxonImageWindow,
+    TaxonInfoCard,
+    TaxonList,
+    set_pixmap_async,
+)
+from naturtag.widgets.layouts import VerticalLayout
+from naturtag.widgets.taxon_images import TaxonPhoto
+
+logger = getLogger(__name__)
+
+
+class TaxonInfoSection(HorizontalLayout):
+    """Section to display selected taxon photo and basic info"""
+
+    on_select = Signal(Taxon)  #: A taxon object was selected (from nav or another screen)
+    on_select_id = Signal(int)  #: A taxon ID was selected (from 'parent' button)
+
+    def __init__(self):
+        super().__init__()
+        self.hist_prev: deque[Taxon] = deque()  # Viewing history for current session only
+        self.hist_next: deque[Taxon] = deque()  # Set when loading from history
+        self.history_taxon: Taxon = None  # Set when loading from history, to avoid loop
+        self.selected_taxon: Taxon = None
+
+        self.group_box = QGroupBox('No taxon selected')
+        root = VerticalLayout(self.group_box)
+        images = HorizontalLayout()
+        root.addLayout(images)
+        self.addWidget(self.group_box)
+        self.setAlignment(Qt.AlignTop)
+
+        # Medium taxon default photo
+        self.image = TaxonPhoto(hover_icon=True, hover_event=False)  # Disabled until 1st load
+        self.image.setFixedHeight(395)  # Height of 5 thumbnails + spacing
+        self.image.setAlignment(Qt.AlignTop)
+        images.addWidget(self.image)
+
+        # Additional taxon thumbnails
+        self.thumbnails = GridLayout(n_columns=2)
+        self.thumbnails.setSpacing(5)
+        self.thumbnails.setAlignment(Qt.AlignTop)
+        images.addLayout(self.thumbnails)
+
+        # Back and Forward buttons: We already have the full Taxon object
+        button_layout = HorizontalLayout()
+        root.addLayout(button_layout)
+        self.prev_button = QPushButton('Back')
+        self.prev_button.setIcon(fa_icon('ei.chevron-left'))
+        self.prev_button.clicked.connect(self.prev)
+        self.prev_button.setEnabled(False)
+        button_layout.addWidget(self.prev_button)
+
+        self.next_button = QPushButton('Forward')
+        self.next_button.setIcon(fa_icon('ei.chevron-right'))
+        self.next_button.clicked.connect(self.next)
+        self.next_button.setEnabled(False)
+        button_layout.addWidget(self.next_button)
+
+        # Parent button: We need to fetch the full Taxon object, so just pass the ID
+        self.parent_button = QPushButton('Parent')
+        self.parent_button.setIcon(fa_icon('ei.chevron-up'))
+        self.parent_button.clicked.connect(self.select_parent)
+        self.parent_button.setEnabled(False)
+        button_layout.addWidget(self.parent_button)
+
+        # Link button: Open web browser to taxon info page
+        self.link_button = QPushButton('View on iNaturalist')
+        self.link_button.setIcon(fa_icon('mdi.web', primary=True))
+        self.link_button.clicked.connect(lambda: webbrowser.open(self.selected_taxon.url))
+        self.link_button.setEnabled(False)
+        button_layout.addWidget(self.link_button)
+
+        # Fullscreen image viewer
+        self.image_window = TaxonImageWindow()
+        self.image.on_click.connect(self.image_window.display_taxon_fullscreen)
+
+    def load(self, taxon: Taxon):
+        """Load default photo + additional thumbnails"""
+        if self.selected_taxon and taxon.id == self.selected_taxon.id:
+            return
+
+        # Append to nav history, unless we just loaded a taxon from history
+        if self.selected_taxon and taxon.id != getattr(self.history_taxon, 'id', None):
+            self.hist_prev.append(self.selected_taxon)
+            self.hist_next.clear()
+        logger.debug(
+            f'Navigation: {" | ".join([t.name for t in self.hist_prev])} | [{taxon.name}] | '
+            f'{" | ".join([t.name for t in self.hist_next])}'
+        )
+
+        # Set title and main photo
+        self.history_taxon = None
+        self.selected_taxon = taxon
+        self.group_box.setTitle(taxon.full_name)
+        self.image.hover_event = True
+        self.image.taxon = taxon
+        set_pixmap_async(
+            self.image,
+            photo=taxon.default_photo,
+            size='medium',
+            priority=QThread.HighPriority,
+        )
+        self._update_nav_buttons()
+
+        # Load additional thumbnails
+        self.thumbnails.clear()
+        for i, photo in enumerate(taxon.taxon_photos[1:11] if taxon.taxon_photos else []):
+            thumb = TaxonPhoto(taxon=taxon, idx=i + 1, rounded=True)
+            thumb.setFixedSize(*SIZE_SM)
+            thumb.on_click.connect(self.image_window.display_taxon_fullscreen)
+            set_pixmap_async(thumb, photo=photo, size='thumbnail')
+            self.thumbnails.addWidget(thumb)
+
+    def prev(self):
+        if not self.hist_prev:
+            return
+        self.history_taxon = self.hist_prev.pop()
+        self.hist_next.appendleft(self.selected_taxon)
+        self.on_select.emit(self.history_taxon)
+
+    def next(self):
+        if not self.hist_next:
+            return
+        self.history_taxon = self.hist_next.popleft()
+        self.hist_prev.append(self.selected_taxon)
+        self.on_select.emit(self.history_taxon)
+
+    def select_taxon(self, taxon: Taxon):
+        self.load(taxon)
+        self.on_select.emit(taxon)
+
+    def select_parent(self):
+        self.on_select_id.emit(self.selected_taxon.parent_id)
+
+    def _update_nav_buttons(self):
+        """Update status and tooltip for 'back', 'forward', 'parent', and 'view on iNat' buttons"""
+        self.prev_button.setEnabled(bool(self.hist_prev))
+        self.prev_button.setToolTip(self.hist_prev[-1].full_name if self.hist_prev else None)
+        self.next_button.setEnabled(bool(self.hist_next))
+        self.next_button.setToolTip(self.hist_next[0].full_name if self.hist_next else None)
+        self.parent_button.setEnabled(bool(self.selected_taxon.parent))
+        self.parent_button.setToolTip(
+            self.selected_taxon.parent.full_name if self.selected_taxon.parent else None
+        )
+        self.link_button.setEnabled(True)
+        self.link_button.setToolTip(self.selected_taxon.url)
+
+
+class TaxonomySection(HorizontalLayout):
+    """Section to display ancestors and children of selected taxon"""
+
+    def __init__(self, user_taxa: UserTaxa):
+        super().__init__()
+
+        self.ancestors_group = self.add_group(
+            'Ancestors', min_width=400, max_width=500, policy_min_height=False
+        )
+        self.ancestors_list = TaxonList(user_taxa)
+        self.ancestors_group.addWidget(self.ancestors_list.scroller)
+
+        self.children_group = self.add_group(
+            'Children', min_width=400, max_width=500, policy_min_height=False
+        )
+        self.children_list = TaxonList(user_taxa)
+        self.children_group.addWidget(self.children_list.scroller)
+
+    def load(self, taxon: Taxon):
+        """Populate taxon ancestors and children"""
+        logger.debug(f'Loading {len(taxon.ancestors)} ancestors and {len(taxon.children)} children')
+
+        def get_label(text: str, items: list) -> str:
+            return text + (f' ({len(items)})' if items else '')
+
+        self.ancestors_group.set_title(get_label('Ancestors', taxon.ancestors))
+        self.ancestors_list.set_taxa(taxon.ancestors)
+        self.children_group.set_title(get_label('Children', taxon.children))
+        self.children_list.set_taxa(taxon.children)
+
+    @property
+    def taxa(self) -> Iterator['TaxonInfoCard']:
+        yield from self.ancestors_list.cards
+        yield from self.children_list.cards
