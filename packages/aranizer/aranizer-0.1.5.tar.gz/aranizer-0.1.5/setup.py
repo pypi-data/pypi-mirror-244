586edd5215a5713c52d348c63b0156fdbe944a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:05:55 +0000

    Add package aranizer/0.1.5/aranizer-0.1.5.tar.gz

---
+++ packages/aranizer/aranizer-0.1.5.tar.gz/aranizer-0.1.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='aranizer',
+    version='0.1.5',
+    packages=find_packages(),
+    url='https://github.com/omarnj-lab/aranizer',
+    license='MIT',
+    author='omar najar',
+    author_email='onajar@psu.edu.sa',
+    description='Aranizer:A Custom Tokenizer for Enhanced Arabic Language Processing',
+    install_requires=[
+        'transformers',
+        'sentence_transformers'
+    ],
+    include_package_data=True
+)
