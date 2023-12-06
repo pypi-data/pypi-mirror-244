0949bb279c46dbd273c5927a34ac0ff55adea22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 05:05:10 +0000

    Add package monitopy/0.0.1/monitopy-0.0.1.tar.gz

---
+++ packages/monitopy/monitopy-0.0.1.tar.gz/monitopy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='monitopy',
+    version='0.0.1',
+    packages=['monitopy'],
+    install_requires=[
+        'watchdog'
+    ],
+    author="Rachid Jeffali",
+    description="Une bibliothèque de surveillance (watchdog) simple mais puissante pour les changements dans les fichiers et répertoires, avec une prise en charge avancée de la gestion d'événements."
+)
