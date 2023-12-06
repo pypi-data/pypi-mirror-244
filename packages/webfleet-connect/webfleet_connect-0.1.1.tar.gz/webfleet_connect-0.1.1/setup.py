d66f96d6ab805a34904d2ab02aca57a6997d0ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 14:05:31 +0000

    Add package webfleet-connect/0.1.1/webfleet_connect-0.1.1.tar.gz

---
+++ packages/webfleet-connect/webfleet_connect-0.1.1.tar.gz/webfleet_connect-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+  name='webfleet_connect',
+  version='0.1.1',
+  description='The WEBFLEET.connect API connects software applications with the Webfleet fleet management solution. Via WEBFLEET.connect you can enhance the value of all types of business solutions, including routing and scheduling optimization, ERP, Transport Management System (TMS), supply chain planning, asset management, and much more.',
+  url='https://github.com/movomx/webfleet_connect_python',
+  author='movomx',
+  author_email='alex.guajardo@movomx.com',
+  license='MIT License',
+  packages=['webfleet_connect'],
+  install_requires=['requests', 'python-dotenv'],
+
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Topic :: Software Development :: Libraries'
+  ]
+)
