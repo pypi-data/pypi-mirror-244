3ea4955f505a08b9bd9532da15da27eccff73e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 06:04:48 +0000

    Add package MOVE3/0.7/MOVE3-0.7.tar.gz

---
+++ packages/MOVE3/MOVE3-0.7.tar.gz/MOVE3-0.7/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from version import __version__ as myVersion
+import os, io
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf8")
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+def read(path, encoding="utf-8"):
+    path = os.path.join(os.path.dirname(__file__), path)
+    with io.open(path, encoding=encoding) as fp:
+        return fp.read()
+
+with open('README.md', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+def get_install_requirements(path):
+    content = read(path)
+    return [req for req in content.split("\n") if req != "" and not req.startswith("#")]
+
+setup(name='MOVE3',
+      description='Maintence of Variance Toolbox',
+      url='https://github.com/danhamill/MOVE3',
+      download_url='https://github.com/danhamill/MOVE3',
+      author='Daniel Hamill',
+      author_email='daniel.d.hamill@usace.army.mil',
+      packages=['move3.core'],
+      python_requires='>3.9',
+      install_requires=['altair<5', 'numpy', 'pandas', 'scikit_learn', 'setuptools', 'pytest'],
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        "Topic :: Utilities",
+      ],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      tests_require=['pytest'],
+      version = myVersion
+      )
