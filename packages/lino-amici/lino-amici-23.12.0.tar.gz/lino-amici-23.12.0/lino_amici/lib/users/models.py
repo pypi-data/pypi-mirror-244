1e51c18b29bb49f9ac3cf89f230d36aae76784c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 16:05:15 +0000

    Add package lino-amici/23.12.0/lino-amici-23.12.0.tar.gz

---
+++ packages/lino-amici/lino-amici-23.12.0.tar.gz/lino-amici-23.12.0/lino_amici/lib/users/models.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2013-2016 Luc Saffre
+# License: GNU Affero General Public License v3 (see file COPYING for details)
+
+"""Database models for :mod:`lino_welfare.modlib.users`.
+
+"""
+
+from lino.modlib.users.models import *
+
+
+
+from .ui import *
