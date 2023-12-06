9ab5b84efb1b05f9ff0519969c34f2f5d1c6e567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 06:04:16 +0000

    Add package rlvortex/0.0.30/rlvortex-0.0.30-py3-none-any.whl

---
+++ packages/rlvortex/rlvortex-0.0.30-py3-none-any.whl/rlvortex/policy/quick_build.py
@@ -0,0 +1,18 @@
+from typing import Callable
+import torch.nn as nn
+import numpy as np
+
+
+def mlp(sizes, activation, output_activation: Callable = nn.Identity):
+    layers = []
+    for j in range(len(sizes) - 1):
+        act = activation if j < len(sizes) - 2 else output_activation
+        layers += [nn.Linear(sizes[j], sizes[j + 1]), act()]
+    return nn.Sequential(*layers)
+
+
+def init_weights(m):
+    if type(m) in (nn.Linear, nn.Conv2d):
+        nn.init.orthogonal_(m.weight.data, np.sqrt(float(2)))
+        if m.bias is not None:
+            m.bias.data.fill_(0)
