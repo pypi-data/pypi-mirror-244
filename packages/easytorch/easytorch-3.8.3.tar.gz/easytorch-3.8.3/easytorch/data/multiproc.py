64b0fa5f58c266a469915a6516bc940885dac67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-06 14:05:32 +0000

    Add package easytorch/3.8.3/easytorch-3.8.3.tar.gz

---
+++ packages/easytorch/easytorch-3.8.3.tar.gz/easytorch-3.8.3/easytorch/data/multiproc.py
@@ -0,0 +1,98 @@
+import multiprocessing as _mp
+import traceback as _tb
+from functools import partial as _partial
+from typing import Callable
+
+import numpy as _np
+import random as _random
+import torch as _torch
+from torch.utils.data._utils.collate import default_collate as _default_collate
+
+LOG_FREQ = 100
+
+
+def _job(total, func, i, f):
+    print(f"Working on: [ {i}/{total} ]", end='\n' if i % LOG_FREQ == 0 else '\r')
+    try:
+        return func(f)
+    except Exception as e:
+        _tb.print_exc()
+        print(f"{f} ### {e}")
+
+
+def multiRun(nproc: int, data_list: list, func: Callable) -> list:
+    _files = []
+    for ix, file in enumerate(data_list, 1):
+        _files.append([ix, file])
+
+    with _mp.Pool(processes=nproc) as pool:
+        return list(
+            pool.starmap(_partial(_job, len(_files), func), _files)
+        )
+
+
+def safe_collate(batch):
+    r"""Safely select batches/skip dataset_cls(errors in file loading."""
+    return _default_collate([b for b in batch if b])
+
+
+def num_workers(conf, loader_args, distributed=False):
+    if distributed:
+        return (loader_args['num_workers'] + conf['num_gpus'] - 1) // conf['num_gpus']
+    return loader_args.get('num_workers', 0)
+
+
+def batch_size(conf, loader_args, distributed=False):
+    if distributed:
+        loader_args['batch_size'] = loader_args['batch_size'] // conf['num_gpus']
+    return loader_args['batch_size']
+
+
+def seed_worker(worker_id):
+    seed = (int(_torch.initial_seed()) + worker_id) % (2 ** 32 - 1)
+    _np.random.seed(seed)
+    _random.seed(seed)
+
+
+def _et_data_job(file, mode, conf, dataset_cls, diskcache):
+    dataset = dataset_cls(mode=mode, **conf)
+    try:
+        dataset.add(files=[file], diskcache=diskcache, verbose=False)
+    except Exception as e:
+        _tb.print_exc()
+        print(f"{file} ### {e}")
+    return dataset
+
+
+def et_data_job(mode, conf, dataset_cls, total, verbose, diskcache, i, file):
+    if verbose:
+        print(f"Working on: [ {i} / {total} ]", end='\n' if i % LOG_FREQ == 0 else '\r')
+    return _et_data_job(file, mode, conf, dataset_cls, diskcache)
+
+
+def multi_load(mode, files, conf, dataset_cls, diskcache=None) -> list:
+    r"""Note: Only works with easytorch's default args from easytorch import args"""
+    _files = []
+    for ix, f in enumerate(files, 1):
+        _files.append([ix, f])
+
+    if conf.get("multi_load") and len(files) > 1:
+        nw = min(num_workers(conf, conf, conf.get('use_ddp')), len(_files))
+        with _mp.Pool(processes=max(1, nw)) as pool:
+            dataset_list = list(
+                pool.starmap(
+                    _partial(
+                        et_data_job, mode, conf, dataset_cls, len(_files), conf.get('verbose'), diskcache
+                    ),
+                    _files
+                )
+            )
+            return [d for d in dataset_list if len(d) >= 1]
+
+    dataset_list, total = [], len(_files)
+    for ix, file in _files:
+        if conf.get('verbose'):
+            print(f"Loading... {ix}/{total}", end='\n' if ix % LOG_FREQ == 0 else '\r')
+        dataset_list.append(_et_data_job(file, mode, conf, dataset_cls, diskcache))
+
+    return [d for d in dataset_list if len(d) >= 1]
---
+++ packages/easytorch/easytorch-3.8.3.tar.gz/easytorch-3.8.3/easytorch/utils/__init__.py
@@ -0,0 +1,76 @@
+import os as _os
+import copy as _copy
+import json as _json
+
+
+class FrozenDict(dict):
+    def __init__(self, _dict):
+        super().__init__(_dict if _dict is not None else {})
+
+    def prompt(self, key, value):
+        raise ValueError(f'*** '
+                         f'Attempt to modify frozen dict '
+                         f'[{key} : {self[key]}] with [{key} : {value}]'
+                         f' ***')
+
+    def __setitem__(self, key, value):
+        if key not in self:
+            super(FrozenDict, self).__setitem__(key, value)
+        else:
+            self.prompt(key, value)
+
+    def update(self, **kw):
+        for k, v in kw.items():
+            self[k] = v
+
+
+def save_scores(save_dir, cache, name='', file_keys=[]):
+    for fk in file_keys:
+        with open(save_dir + _os.sep + f'{name}_{fk}.csv', 'w') as file:
+            header = cache.get('log_header', '')
+            header = header.replace('|', ',')
+            if isinstance(header, list):
+                header = ','.join(header)
+            file.write(header + '\n')
+            for line in cache[fk]:
+                if isinstance(line, list):
+                    file.write(','.join([str(s) for s in line]) + '\n')
+                else:
+                    file.write(f'{line}\n')
+
+
+def jsonable(obj):
+    try:
+        _json.dumps(obj)
+        return True
+    except:
+        return False
+
+
+def clean_recursive(obj):
+    r"""
+    Make everything in cache safe to save in json files.
+    """
+    if not isinstance(obj, dict):
+        return
+    for k, v in obj.items():
+        if isinstance(v, dict):
+            clean_recursive(v)
+        elif isinstance(v, list):
+            for i in v:
+                clean_recursive(i)
+        elif not jsonable(v):
+            obj[k] = f'{v}'
+
+
+def save_cache(conf, cache, name=''):
+    _cache = {**cache, 'conf': conf}
+    with open(conf['save_dir'] + _os.sep + f"{name}.json", 'w') as fp:
+        try:
+            log = _copy.deepcopy(_cache)
+            clean_recursive(log)
+            _json.dump(log, fp)
+        except Exception as e:
+            with open(conf['save_dir'] + _os.sep + f"{name}.txt", 'w') as raw:
+                raw.write(f"{e}\n")
+                raw.write(f"{_cache}")
